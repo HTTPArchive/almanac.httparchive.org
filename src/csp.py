b249513c547a9c8cb304022ded0702c82753c3f7
Mike Geyser <mikegeyser@gmail.com>
2019-06-03 11:42:44 +0200

    Coming soon
    Addresses some of #25 and closes #31.
    
    Created an interim landing page for the almanac. I have emulated
    some of the structure of the main HA site, for sake of consistency.
    I have included the talisman/CSP stuff, and have manually copied
    across some images and styles for the placeholder, but haven't
    included the full bootstrap.min.css.

---
+++ src/csp.py
@@ -0,0 +1,24 @@
+csp = {
+    'default-src': '\'self\'',
+    'style-src': [
+        '\'self\'',
+        '\'unsafe-inline\'',
+        'fonts.googleapis.com'
+    ],
+    'script-src': [
+        '\'self\'',
+        'www.google-analytics.com'
+    ],
+    'font-src': [
+        '\'self\'',
+        'fonts.gstatic.com'
+    ],
+    'connect-src': [
+        '\'self\'',
+        'www.google-analytics.com'
+    ],
+    'img-src': [
+        '\'self\'',
+        'www.google-analytics.com'
+    ]
+}
