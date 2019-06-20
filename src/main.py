ef7a6a438e69260537400b97b7ef91278a0b3a3f
Mike Geyser <mikegeyser@gmail.com>
2019-06-20 10:03:58 +0200

    Refactored the validating of year and language into a decorator (#58)
    * Refactored the validating of year and language into a decorator
    
    Refactored the i18n validation into a dedicated decorator that will
    respect the selection of language and year. It will also try to
    read the user language out of the 'Accept-Language' header as far
    as possible. I've left a *lot* of debug logging in place, so that
    we can verify that it's doing the right thing. The only real compromise
    has been to accept the year as a parameter for the splash page, which
    I feel is reasonable.
    
    * Renamed validate decorator
    
    Renamed the `validate_i18n` decorator to `validate`, and moved
    the `abort` import back to flask.
    
    * Added tests for parsing 'Accept-Language'
    
    Wrote some tests to ensure that the parse_accept_language logic
    support all of the required scenarios. A notable omission is that
    I haven't tested the decorator itself, yet, as it requires mocking
    the http request.
    
    * Added todos for the custom error page and renamed the test to follow a better convention.
    
    * Be more permissive with defaulting args
    
    Changed the decorator to be more permissive of the wrapped function's
    args. It will inspect the args that a function accepts before trying to
    set them on kwargs, so that we don't bloat the route functions
    unnecessarily.
    
    * Added detail on how to run the tests to the README.md

--- src/main.py
+++ src/main.py
@@ -1,79 +1,53 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template
 from flask_talisman import Talisman
 from csp import csp
+from validate import validate
 import logging
 
 app = Flask(__name__)
 Talisman(app,
          content_security_policy=csp,
          content_security_policy_nonce_in=['script-src'])
-
-DEFAULT_YEAR = '2019'
-DEFAULT_LANG = 'en'
-SUPPORTED_YEARS = {
-    '2019': ('en', 'ja')
-}
+logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route('/')
 @app.route('/<lang>/')
-def index(lang=DEFAULT_LANG):
-    validate(lang)
-
+@validate
+def index(lang):
     return render_template('%s/splash.html' % lang)
 
 
 @app.route('/<year>/outline')
 @app.route('/<lang>/<year>/outline')
-def outline(year, lang=DEFAULT_LANG):
-    validate(lang, year)
-
+@validate
+def outline(year, lang):
     return render_template('%s/%s/outline.html' % (lang, year))
 
 
 @app.route('/<year>/contributors')
 @app.route('/<lang>/<year>/contributors')
-def contributors(year, lang=DEFAULT_LANG):
-    validate(lang, year)
-
+@validate
+def contributors(year, lang):
     # TODO: Get contributor data and pass into the template.
     return render_template('%s/%s/contributors.html' % (lang, year), contributors={})
 
 
 @app.route('/<year>/methodology')
 @app.route('/<lang>/<year>/methodology')
-def methodology(year, lang=DEFAULT_LANG):
-    validate(lang, year)
-
+@validate
+def methodology(year, lang):
     return render_template('%s/%s/methodology.html' % (lang, year))
 
 
 @app.route('/<year>/<chapter>/')
 @app.route('/<lang>/<year>/<chapter>')
-def chapter(year, chapter, lang=DEFAULT_LANG):
-    validate(lang, year)
-
+@validate
+def chapter(year, chapter, lang):
     # TODO: Validate the chapter.
     # TODO: Get chapter data and pass into the template.
+
     return render_template('%s/%s/chapter.html' % (lang, year), chapter=chapter)
-
-
-## This could become a decorator, and be encapsulated. Would that make this a
-## cleaner solution? I looked at implementing one, but it seemed complicated.
-def validate(lang, year=DEFAULT_YEAR):
-    validate_year(year)
-    validate_lang(lang, year)
-
-
-def validate_year(year):
-    if year not in SUPPORTED_YEARS:
-        abort(404)
-
-
-def validate_lang(lang, year):
-    supported_langs = SUPPORTED_YEARS[year]
-    if lang not in supported_langs:
-        abort(404)
 
 
 @app.errorhandler(500)
