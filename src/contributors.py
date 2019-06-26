1fa1f55297cd185c1de7d17895a8aa5ddca19892
Kari <42503401+KJLarson@users.noreply.github.com>
2019-06-25 20:30:09 -0500

    Declare global variable first
    Co-Authored-By: Rick Viscomi <rviscomi@users.noreply.github.com>

--- src/contributors.py
+++ src/contributors.py
@@ -7,6 +7,8 @@ def get_contributors():
   return contributors_json
 
 def update_contributors():
+  global contributors_json
+  
   with open('config/contributors.json', 'r') as contributors_file:
     contributors_json = json.load(contributors_file)
 # need to return data
