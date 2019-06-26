9a3ba5aec585175bb02a4210bcecd9af5279c98e
Kari L <kjlarson1@outlook.com>
2019-06-25 20:35:45 -0500

    Remove comments

--- src/contributors.py
+++ src/contributors.py
@@ -6,8 +6,6 @@ contributors_json = {}
 def get_contributors():
   with open('config/contributors.json', 'r') as contributors_file:
     contributors_json = json.load(contributors_file)
-# need to return data
 
-# Alphabatize data?
 
 get_contributors()
