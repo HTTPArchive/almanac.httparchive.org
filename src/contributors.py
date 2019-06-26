a410bc650322a3542e2461e3576c6d7fba352b2b
Kari <42503401+KJLarson@users.noreply.github.com>
2019-06-25 20:29:35 -0500

    Add method to update contributors. Progress on #57
    Co-Authored-By: Rick Viscomi <rviscomi@users.noreply.github.com>

--- src/contributors.py
+++ src/contributors.py
@@ -4,6 +4,9 @@ contributors_json = {}
 
 
 def get_contributors():
+  return contributors_json
+
+def update_contributors():
   with open('config/contributors.json', 'r') as contributors_file:
     contributors_json = json.load(contributors_file)
 # need to return data
