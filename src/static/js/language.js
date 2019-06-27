843230504d077b1bd9dfa2e46238125b819b2135
tanhengyeow <E0032242@u.nus.edu>
2019-06-28 02:26:19 +0800

    Address review comments

--- src/static/js/language.js
+++ src/static/js/language.js
@@ -1,41 +1,19 @@
-const supportedLang = ['en', 'ja'];
-
-const langSwitcher = document.querySelector('.language-switcher');
-
-langSwitcher.addEventListener('change', (e) => {
+const languageSelect = document.querySelector('.language-switcher');
+languageSelect.addEventListener('change', (e) => {
   if (e.target.value) {
-    window.location.href =
-      updatePath(window.location.pathname, e.target.value);
+    window.location = e.target.value;
   }
 });
 
-function updatePath(path, value) {
-  // Current path has no lang prefix
-  if (path.length === 1) {
-    return path + value;
-  }
-
-  const langPrefix = path.substring(1,3);
-  if (supportedLang.includes(langPrefix) && path[3] === '/') {
-    return path.replace(langPrefix, value);
-  }
-}
-
-const defaultOption = document.querySelector('.default-language');
-const alternateOptions = document.querySelectorAll('.alternate-language');
-
-// Generate default language option
-defaultOption.innerHTML = generateOptionText(defaultOption.value);
-
-// Generate alternate languages' options
-for (i = 0; i < alternateOptions.length; i++) {
-  alternateOptions[i].innerHTML = generateOptionText(alternateOptions[i].value);
+const languageOptions = document.querySelectorAll('.language-switcher option');
+for (i = 0; i < languageOptions.length; i++) {
+  languageOptions[i].innerHTML = generateOptionText(languageOptions[i].lang);
 }
 
 function generateOptionText(language) {
   switch(language) {
     case 'en':
-      return '🇬🇧 English';
+      return '🇺🇸 English';
     case 'ja':
       return '🇯🇵 Japanese';
   }
