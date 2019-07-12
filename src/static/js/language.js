cf681c303ff75f6fbfba7d0d6294503621d25380
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-07-12 17:19:39 -0400

    Language class (#69)
    * Language class
    
    * fix tests
    
    * Added hreflang alternate tags (#74)
    
    * add link alternate hreflang tags
    
    * correct indentation
    
    * second try to correct indentation ..
    
    * update view args in template method
    
    * fix tests

--- src/static/js/language.js
+++ src/static/js/language.js
@@ -4,17 +4,3 @@ languageSelect.addEventListener('change', (e) => {
     window.location = e.target.value;
   }
 });
-
-const languageOptions = document.querySelectorAll('.language-switcher option');
-for (i = 0; i < languageOptions.length; i++) {
-  languageOptions[i].innerHTML = generateOptionText(languageOptions[i].lang);
-}
-
-function generateOptionText(language) {
-  switch(language) {
-    case 'en':
-      return '🇺🇸 English';
-    case 'ja':
-      return '🇯🇵 日本語';
-  }
-}
