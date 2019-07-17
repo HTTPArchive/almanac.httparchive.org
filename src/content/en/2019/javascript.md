067b8dc30b04224c41dc02d20739b9e9b86ccc77
Mike Geyser <mikegeyser@gmail.com>
2019-07-17 09:51:24 +0200

    Generating metadata
    Parsing the markdown metadata section, and generating metadata tags from it. This will
    mean that we can translate purely from the markdown without having to worry about the
    templates at all.

---
+++ src/content/en/2019/javascript.md
@@ -0,0 +1,18 @@
+---
+part_number: I
+chapter_number: 1
+title: JavaScript
+description: Something about JavaScript
+hero_image: ''
+authors: [addyosmani, housseindjirdeh, mathiasbynens]
+reviewers: [rwaldron, RReverser]
+translaters: []
+---
+
+# Chapter 1: JavaScript
+
+This is a test.
+
+And **only** a test
+
+> If it was anything more, it would certainly have had better placeholder text
--- src/generate_chapters.py
+++ src/generate_chapters.py
@@ -1,6 +1,7 @@
 import os
 import re
 import mistune
+import yaml
 
 renderer = mistune.Renderer()
 markdown = mistune.Markdown(renderer=renderer)
@@ -10,11 +11,15 @@ TEMPLATE = """
 {{% extends "en/base.html" %}}
 
 {{% block title %}}
-Chapter {{{{ chapter }}}} | The Web Almanac by HTTP Archive
+{title}
+{{% endblock %}}
+
+{{% block meta %}}
+  <meta name="description" content="{description}">
 {{% endblock %}}
 
 {{% block main %}}
-{content}
+{body}
 {{% endblock %}}
 """
 
@@ -29,21 +34,30 @@ def generate_chapters():
             for chapter_file in os.scandir(year_dir.path):
                 chapter = re.sub('.md$', '', chapter_file.name)
 
-                content = get_content(chapter_file)
-                write_template(language, year, chapter, content)
+                (metadata, body) = parse_file(chapter_file)
+
+                write_template(language, year, chapter, metadata, body)
 
 
-def get_content(chapter_file):
+def parse_file(chapter_file):
     with open(chapter_file, 'r') as f:
         content = f.read()
-    return markdown(content)
+
+    pattern = r'^\s*(?:-{3})(.*?)(?:-{3})\s*(.+)$'
+    (metadata_text, body_text) = re.findall(pattern, content, re.S | re.M)[0]
+
+    metadata = yaml.load(metadata_text)
+    body = markdown(body_text)
+
+    return (metadata, body)
 
 
-def write_template(language, year, chapter, content):
+def write_template(language, year, chapter, metadata, body):
     path = 'templates/%s/%s/chapters/%s.html' % (language, year, chapter)
 
     with open(path, 'w') as file_to_write:
-        file_to_write.write(TEMPLATE.format(content=content))
+        file_to_write.write(TEMPLATE.format(
+            body=body, title=metadata['title'], description=metadata['description']))
 
 
 generate_chapters()
