ffbc4c036098537b42a9d6900b73e29a0d9b8f74
Mike Geyser <mikegeyser@gmail.com>
2019-07-17 18:54:54 +0200

    Added some comments around the regex, and extracted the metadata into a JSON object.

--- src/generate_chapters.py
+++ src/generate_chapters.py
@@ -6,6 +6,7 @@ import yaml
 renderer = mistune.Renderer()
 markdown = mistune.Markdown(renderer=renderer)
 
+
 def generate_chapters():
     for language_dir in os.scandir('content'):
         language = language_dir.name
@@ -25,10 +26,19 @@ def parse_file(chapter_file):
     with open(chapter_file, 'r') as f:
         content = f.read()
 
+    # This regex will match the metadata of the markdown file and the body of the
+    # markdown file seperately. It disambiguiates the two by looking for '---' as a
+    # line separation between the two sections. The metadata section (called front
+    # matter) is essentially just YAML so can be parse directly into an object.
+    # https://yaml.org/spec/1.2/spec.html#id2760395
     pattern = r'^\s*(?:-{3})(.*?)(?:-{3})\s*(.+)$'
-    (metadata_text, body_text) = re.findall(pattern, content, re.S | re.M)[0]
+    (metadata_text, body_text) = re.findall(
+        pattern, content, re. DOTALL | re.MULTILINE)[0]
 
-    metadata = yaml.load(metadata_text)
+    metadata = yaml.load(metadata_text, Loader=yaml.SafeLoader)
+
+    # TODO: Parse the body_text and find placeholders for generating embedded SVG.
+
     body = markdown(body_text)
 
     return (metadata, body)
@@ -43,8 +53,7 @@ def write_template(language, year, chapter, metadata, body):
     path = 'templates/%s/%s/chapters/%s.html' % (language, year, chapter)
 
     with open(path, 'w') as file_to_write:
-        file_to_write.write(template.format(
-            body=body, title=metadata['title'], description=metadata['description']))
+        file_to_write.write(template.format(body=body, metadata=metadata))
 
 
 generate_chapters()
