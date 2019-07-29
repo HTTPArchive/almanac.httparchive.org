b5104dcbe492ef9242b02c620fcce14209e4f0bf
Patrick Hulce <patrick.hulce@gmail.com>
2019-07-29 17:32:55 -0500

    trim the fat

--- sql/2019/05_Third_Parties/05_06.sql
+++ sql/2019/05_Third_Parties/05_06.sql
@@ -6,19 +6,11 @@ SELECT
   SUM(requestBytes) AS totalBytes
 FROM (
   SELECT
-      page AS pageUrl,
-      NET.HOST(page) AS pageDomain,
-      url AS requestUrl,
-      REGEXP_EXTRACT(payload, r'(?i)content-type:\s*([a-z0-9_\/-]+)') AS contentType,
       SAFE_CAST(REGEXP_EXTRACT(payload, r'_bytesIn":(\d+)') AS INT64) AS requestBytes,
       NET.HOST(url) AS requestDomain,
-      DomainsOver50Table.requestDomain as thirdPartyDomain,
-      ThirdPartyTable.category as thirdPartyCategory
+      DomainsOver50Table.requestDomain as thirdPartyDomain
     FROM
       `httparchive.requests.2019_07_01_mobile`
-    LEFT JOIN
-      `lighthouse-infrastructure.third_party_web.2019_07_01` AS ThirdPartyTable
-    ON NET.HOST(url) = ThirdPartyTable.domain
     LEFT JOIN
       `lighthouse-infrastructure.third_party_web.2019_07_01_all_observed_domains` AS DomainsOver50Table
     ON NET.HOST(url) = DomainsOver50Table.requestDomain
