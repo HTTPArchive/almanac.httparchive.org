2f00714be77bc58252b0d3fb89e3b4c66e798115
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2019-07-31 11:25:59 +0900

    refactor: move to a simpler code style

--- sql/2019/06_Fonts/06_01.sql
+++ sql/2019/06_Fonts/06_01.sql
@@ -2,26 +2,37 @@
 
 -- counts the local and hosted fonts
 
-WITH
-  summary_pages AS (
-    SELECT * FROM `httparchive.summary_pages.2019_07_01_desktop`
-    UNION ALL
-    SELECT * FROM `httparchive.summary_pages.2019_07_01_mobile`
-  ),
-  summary_requests AS (
-    SELECT * FROM `httparchive.summary_requests.2019_07_01_desktop`
-    UNION ALL
-    SELECT * FROM `httparchive.summary_requests.2019_07_01_mobile`
-  ),
-  merge_table AS (
-    SELECT * FROM summary_requests JOIN (
-      SELECT pageid, url AS page_url FROM summary_pages
-    ) USING (pageid)
-  ),
-  processedTable AS (
-    SELECT (NET.HOST(url) != NET.HOST(page_url)) as hosted, url, page_url
-    FROM merge_table WHERE type = 'font'
-  )
 SELECT
-  COUNTIF(hosted = true) / COUNT(0) AS hosted, COUNTIF(hosted = false) / COUNT(0) AS local
-FROM processedTable
+  COUNTIF(NET.HOST(page) != NET.HOST(url)) / COUNT(0) AS hosted,
+  COUNTIF(NET.HOST(page) = NET.HOST(url)) / COUNT(0) AS local
+FROM
+  (SELECT _TABLE_SUFFIX, pageid, url FROM `httparchive.summary_requests.2019_07_01_*` WHERE type = 'font')
+JOIN
+  (SELECT _TABLE_SUFFIX, pageid, url AS page FROM `httparchive.summary_pages.2019_07_01_*`)
+USING (_TABLE_SUFFIX, pageid)
+
+-- old code but does the same thing as above
+
+-- WITH
+--   summary_pages AS (
+--     SELECT * FROM `httparchive.summary_pages.2019_07_01_desktop`
+--     UNION ALL
+--     SELECT * FROM `httparchive.summary_pages.2019_07_01_mobile`
+--   ),
+--   summary_requests AS (
+--     SELECT * FROM `httparchive.summary_requests.2019_07_01_desktop`
+--     UNION ALL
+--     SELECT * FROM `httparchive.summary_requests.2019_07_01_mobile`
+--   ),
+--   merge_table AS (
+--     SELECT * FROM summary_requests JOIN (
+--       SELECT pageid, url AS page_url FROM summary_pages
+--     ) USING (pageid)
+--   ),
+--   processedTable AS (
+--     SELECT (NET.HOST(url) != NET.HOST(page_url)) as hosted, url, page_url
+--     FROM merge_table WHERE type = 'font'
+--   )
+-- SELECT
+--   COUNTIF(hosted = true) / COUNT(0) AS hosted, COUNTIF(hosted = false) / COUNT(0) AS local
+-- FROM processedTable
--- sql/2019/06_Fonts/06_02.sql
+++ sql/2019/06_Fonts/06_02.sql
@@ -2,29 +2,43 @@
 
 -- counts the host_url of a given font
 
-WITH
-  summary_pages AS (
-    SELECT * FROM `httparchive.summary_pages.2019_07_01_desktop`
-    UNION ALL
-    SELECT * FROM `httparchive.summary_pages.2019_07_01_mobile`
-  ),
-  summary_requests AS (
-    SELECT * FROM `httparchive.summary_requests.2019_07_01_desktop`
-    UNION ALL
-    SELECT * FROM `httparchive.summary_requests.2019_07_01_mobile`
-  ),
-  merge_table AS (
-    SELECT * FROM summary_requests JOIN (
-      SELECT pageid, url AS page_url FROM summary_pages
-    ) USING (pageid)
-  ),
-  processedTable AS (
-    SELECT (NET.HOST(url) != NET.HOST(page_url)) as hosted, url, page_url
-    FROM merge_table WHERE type = 'font'
+SELECT
+  APPROX_TOP_COUNT(NET.HOST(url), 500) AS font_host
+FROM
+  (SELECT
+    (NET.HOST(url) != NET.HOST(page)) as hosted, url, page
+  FROM
+    (SELECT _TABLE_SUFFIX, pageid, url FROM `httparchive.summary_requests.2019_07_01_*` WHERE type = 'font')
+  JOIN
+    (SELECT _TABLE_SUFFIX, pageid, url AS page FROM `httparchive.summary_pages.2019_07_01_*`)
+  USING (_TABLE_SUFFIX, pageid)
   )
-SELECT value AS host, count
-FROM (
-  SELECT APPROX_TOP_COUNT(NET.HOST(url), 500) AS font_host
-  FROM processedTable
-  WHERE hosted = true
-), UNNEST(font_host)
+WHERE hosted = true
+
+
+-- WITH
+--   summary_pages AS (
+--     SELECT * FROM `httparchive.summary_pages.2019_07_01_desktop`
+--     UNION ALL
+--     SELECT * FROM `httparchive.summary_pages.2019_07_01_mobile`
+--   ),
+--   summary_requests AS (
+--     SELECT * FROM `httparchive.summary_requests.2019_07_01_desktop`
+--     UNION ALL
+--     SELECT * FROM `httparchive.summary_requests.2019_07_01_mobile`
+--   ),
+--   merge_table AS (
+--     SELECT * FROM summary_requests JOIN (
+--       SELECT pageid, url AS page_url FROM summary_pages
+--     ) USING (pageid)
+--   ),
+--   processedTable AS (
+--     SELECT (NET.HOST(url) != NET.HOST(page_url)) as hosted, url, page_url
+--     FROM merge_table WHERE type = 'font'
+--   )
+-- SELECT value AS host, count
+-- FROM (
+--   SELECT APPROX_TOP_COUNT(NET.HOST(url), 500) AS font_host
+--   FROM processedTable
+--   WHERE hosted = true
+-- ), UNNEST(font_host)
