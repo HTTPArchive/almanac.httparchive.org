6e8f26b118131d740e0874d3e0839d2b43cb1b46
Jason Haralson <jason.haralson@gmail.com>
2019-08-01 16:30:06 -0400

    Update 08_01.sql
    Updated to account for the "summary_response_bodies"

--- sql/2019/08_Security/08_01.sql
+++ sql/2019/08_Security/08_01.sql
@@ -7,10 +7,8 @@ BigQuery usage notes:
   === archive table (limited data available == 20.2 M
      no cross-origin-response-policy, ross-origin-opener-policy, sec-fetch-*
   === live tables (2019_07_01_*) = 124.9 GB 
-*/
 
-
-
+--- not using "almanac.summmary response bodies" 
 SELECT 
       count(distinct p.pageid) pages,
        ROUND(SUM(IF(LOWER(r.respOtherHeaders) LIKE "%nel = %",1,0))/count(distinct p.pageid),3) NEL,
@@ -29,3 +27,21 @@ JOIN (
    ) as p 
 ON p.pageid = r.pageid
  WHERE firstHtml = true
+ 
+ */
+
+SELECT
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)nel =')) / COUNT(0) AS pct_NEL,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)report-to = ')) / COUNT(0) AS pct_ReportTo,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = ')) / COUNT(0) AS pct_ReferrerPolicy,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)feature-policy = ')) / COUNT(0) AS pct_FeaturePolicy,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)x-content-type-options = ')) / COUNT(0) AS pct_XContentTypeOptions,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)x-xss-protection = ')) / COUNT(0) AS pct_XXssProtection,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)x-frame-options = ')) / COUNT(0) AS pct_XFrameOptions,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)cross-origin-response-policy = ')) / COUNT(0) AS pct_XOriginResponsePolicy,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)cross-origin-opener-policy = ')) / COUNT(0) AS pct_XOriginOpenerPolicy,
+  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)sec-fetch-%:')) / COUNT(0) AS pct_SecFetch
+FROM
+  `httparchive.almanac.summary_response_bodies`
+WHERE
+  firstHtml
