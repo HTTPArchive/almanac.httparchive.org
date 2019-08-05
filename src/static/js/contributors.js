63131c42f9b95a0ed42a82dcfb17030169a4c5f8
tanhengyeow <E0032242@u.nus.edu>
2019-08-05 11:01:33 +0800

    Fix review comments

--- src/static/js/contributors.js
+++ src/static/js/contributors.js
@@ -3,20 +3,6 @@ const buttons = document.querySelectorAll('.filter-container button');
 const contributors = document.querySelector('.contributors');
 
 for (let button of buttons) {
-  const buttonClassName = button.className;
-
-  // Dynamically generate CSS that reacts to contributors' view state and buttons' toggle state
-  style.textContent += `
-  .contributors.${buttonClassName} .contributor.${buttonClassName} {
-    display: block;
-  }
-
-  .contributors.${buttonClassName} button.${buttonClassName} {
-    background: #0053ba;
-    color: white;
-  }
-  `;
-
   button.addEventListener('click', e => {
     const teamId = e.target.className;
     contributors.classList.toggle(teamId);
