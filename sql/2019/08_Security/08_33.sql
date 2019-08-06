24636c925a3536102495c257b1b7ec8d5694093f
Jason Haralson <jason.haralson@gmail.com>
2019-08-06 00:12:19 -0400

    Update to dynamic parsing

--- sql/2019/08_Security/08_33.sql
+++ sql/2019/08_Security/08_33.sql
@@ -1,32 +1,29 @@
 
 #standardSQL
-# 08_33: Groupings of "x-xss-protection" parsed values by percentage
-#   
+# 08_33: Groupings of "cross-origin-opener-policy" parsed values by percentage
+#   Move to dynamic parsing for now
+#    
+# 
 #   `httparchive.almanac.summary_response_bodies` archive = 71.5GB 
+#       0 rows available some far
 #   `httparchive.summary_requests.2019_07_01_*` = 118.3 GB
 # 
 #   break at "report=" to generalize
 # 
 #
 #   Within "archive" I only see 3 buckets, "null", "allow-postmessage" and "same-origin"
-# I see references to "Deny", "Allow" in Safari. 
+#   I see references to "Deny", "Allow" in Safari. 
 
-
-select ROUND(100*(cnt/total),5) as pct, cnt, flat_xopenpolicyvals
-from 
-(
-select
-count(*) as cnt,
-sum(count(*)) OVER() AS total,
-flat_xopenpolicyvals
-from
-(
-SELECT   
-   REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'cross-origin-opener-policy = ([^,\r\n]*)') AS xopenpolicyvals
-  FROM
-    `httparchive.almanac.summary_response_bodies`
-)
-left join unnest(xopenpolicyvals) flat_xopenpolicyvals   
-group by flat_xopenpolicyvals
-)
-order by pct desc
+SELECT
+  val,
+  COUNT(0) AS freq,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (), 2) AS pct
+FROM
+  `httparchive.almanac.summary_requests`,
+  UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'cross-origin-opener-policy = ([\\w-]+)')) AS val
+WHERE
+  firstHtml
+GROUP BY
+  val
+ORDER BY
+  freq DESC
