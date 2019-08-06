c142063952237b0aebbcf6e94ed9e370c6ff98a4
Jason Haralson <jason.haralson@gmail.com>
2019-08-05 22:48:28 -0400

    Updates per Review

--- sql/2019/08_Security/08_25-34Sum.sql
+++ sql/2019/08_Security/08_25-34Sum.sql
@@ -1,25 +1,27 @@
-/*
-standard SQL
-08_01
-
-Calculate usage percentage for each of the 10 metrics 08.25-34 metrics in one run
-
-BigQuery usage notes: 
- == archive.summary_response_bodies =  71.5 GB usage
- */
+# standard SQL
+# 08_24-34Sum.sql
+#
+# Calculate usage percentage for each of the 10 metrics 08.25-34 metrics in one run
+#
+# BigQuery usage notes: 
+# == archive.summary_response_bodies =  71.5 GB usage (firstHtml partitioning)
+# == archive.summary_requests = 117.5 GB 
+#  
+# cross-origin-opener-policy - only 6 instances of this data across the archive
+# sec-fetch-* - zero instances of this data across the archive
 
 SELECT
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)nel =')) / COUNT(0) AS pct_NEL,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)report-to = ')) / COUNT(0) AS pct_ReportTo,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = ')) / COUNT(0) AS pct_ReferrerPolicy,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)feature-policy = ')) / COUNT(0) AS pct_FeaturePolicy,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)x-content-type-options = ')) / COUNT(0) AS pct_XContentTypeOptions,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)x-xss-protection = ')) / COUNT(0) AS pct_XXssProtection,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)x-frame-options = ')) / COUNT(0) AS pct_XFrameOptions,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)cross-origin-response-policy = ')) / COUNT(0) AS pct_XOriginResponsePolicy,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)cross-origin-opener-policy = ')) / COUNT(0) AS pct_XOriginOpenerPolicy,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)sec-fetch-%:')) / COUNT(0) AS pct_SecFetch
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)nel =')) * 100 / COUNT(0),2) AS pct_nel,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)report-to = ')) * 100 / COUNT(0),2) AS pct_report_to,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)referrer-policy = ')) * 100 / COUNT(0),2) AS pct_referrer_policy,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)feature-policy = ')) * 100 / COUNT(0),2) AS pct_feature_policy,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)x-content-type-options = ')) * 100 / COUNT(0),2) AS pct_xcontent_type_options,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)x-xss-protection = ')) * 100 / COUNT(0),2) AS pct_x_xss_protection,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)x-frame-options = ')) * 100 / COUNT(0),2) AS pct_x_frame_options,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)cross-origin-resource-policy = ')) * 100 / COUNT(0),2) AS pct_x_origin_resource_policy,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)cross-origin-opener-policy = ')) * 100 / COUNT(0),2) AS pct_x_origin_opener_policy,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)sec-fetch-[dest|mode|site|user] = ')) * 100 / COUNT(0),2) AS pct_sec_fetches
 FROM
   `httparchive.almanac.summary_response_bodies`
-WHERE
-  firstHtml
+ WHERE
+   firstHtml
