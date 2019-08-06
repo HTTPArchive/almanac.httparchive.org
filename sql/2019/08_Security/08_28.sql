23c5ab90585307f860c95ebf5046eefaa7122c40
Jason Haralson <jason.haralson@gmail.com>
2019-08-05 23:25:41 -0400

    Update to improved Query

--- sql/2019/08_Security/08_28.sql
+++ sql/2019/08_Security/08_28.sql
@@ -1,25 +1,20 @@
 #standardSQL
 # 08_28: Groupings of "feature-policy" parsed values buckets
-#   good to review the result data to sort/flatten again? 
-#
+
 #   `httparchive.almanac.summary_response_bodies` archive = 71.5GB 
 #   `httparchive.summary_requests.2019_07_01_*` = 118.3 GB
  
-select ROUND(100*(cnt/total),5) as pct, cnt, flat_xssvals
-from 
-(
-select
-count(*) as cnt,
-sum(count(*)) OVER() AS total,
-flat_xssvals
-from
-(
-SELECT   
-   REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'feature-policy = ([^,\r\n]*)') AS xssvals
-  FROM
-    `httparchive.almanac.summary_response_bodies`
-)
-left join unnest(xssvals) flat_xssvals   
-group by flat_xssvals
-)
-order by pct desc
+SELECT
+  policy,
+  COUNT(0) AS freq,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (), 2) AS pct
+FROM
+  `httparchive.almanac.summary_response_bodies`,
+  UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'feature-policy = ([^,\r\n]+)')) AS value,
+  UNNEST(SPLIT(value, ';')) AS policy
+WHERE
+  firstHtml
+GROUP BY
+  policy
+ORDER BY
+  freq DESC
