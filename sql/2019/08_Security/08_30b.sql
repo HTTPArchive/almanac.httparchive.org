0b7bcf37adda0ae2347c659366151a0abbe57779
Jason Haralson <jason.haralson@gmail.com>
2019-08-05 23:46:40 -0400

    Update for code standards

--- sql/2019/08_Security/08_30b.sql
+++ sql/2019/08_Security/08_30b.sql
@@ -1,26 +1,26 @@
 #standardSQL
-# 08_29: Groupings of "x-xss-protection" parsed values by percentage
-#   
+# 08_30b: Groupings of "x-xss-protection" parsed values buckets
+#    Inactive = 0
+#    Active = 1 (.*)
+#    Mode Block = 1 + mode=block
+#    Reporting = 1 + report=
+#    Active Block Reporting (combo of all 3)     
+# 
+#   Pct are against all FirstHTML responses
 #   `httparchive.almanac.summary_response_bodies` archive = 71.5GB 
 #   `httparchive.summary_requests.2019_07_01_*` = 118.3 GB
-#  
-#   break at "report=" to generalize
 
-select ROUND(100*(cnt/total),5) as pct, cnt, flat_xssvals
-from 
-(
-select
-count(*) as cnt,
-sum(count(*)) OVER() AS total,
-flat_xssvals
-from
-(
-SELECT   
-   REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'x-xss-protection = (.*report=|[^,\r\n]*)') AS xssvals
-  FROM
-    `httparchive.almanac.summary_response_bodies`
-)
-left join unnest(xssvals) flat_xssvals   
-group by flat_xssvals
-)
-order by pct desc
+SELECT
+  COUNT(0) AS TotCount,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)x-xss-protection = 0')) * 100 / COUNT(0),2) AS pct_inactive,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)x-xss-protection = 1')) * 100 / COUNT(0),2) AS pct_active,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)x-xss-protection = 1.*mode=block.*')) * 100 / COUNT(0),2) AS pct_block,
+  ROUND(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)x-xss-protection = 1.*report=.*')) * 100 / COUNT(0),2) AS pct_reporting,
+  ROUND(COUNTIF(
+    REGEXP_CONTAINS(respOtherHeaders, '(?i)x-xss-protection = 1.*report=.*')
+    and 
+    REGEXP_CONTAINS(respOtherHeaders, '(?i)x-xss-protection = 1.*mode=block.*')
+  ) * 100 / COUNT(0),2) AS pct_active_block_reporting
+FROM
+  `httparchive.almanac.summary_response_bodies`
+  where firstHtml
