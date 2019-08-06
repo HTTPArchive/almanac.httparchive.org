81494a07f963cf4fedbbfedd475a0988f279390f
Jason Haralson <jason.haralson@gmail.com>
2019-08-05 23:39:50 -0400

    Update to Grouping Query

--- sql/2019/08_Security/08_29.sql
+++ sql/2019/08_Security/08_29.sql
@@ -4,21 +4,17 @@
 #   `httparchive.almanac.summary_response_bodies` archive = 71.5GB 
 #   `httparchive.summary_requests.2019_07_01_*` = 118.3 GB
 
-select ROUND(100*(cnt/total),5) as pct, cnt, flat_contenttypes
-from 
-(
-select
-count(*) as cnt,
-sum(count(*)) OVER() AS total,
-flat_contenttypes
-from
-(
-SELECT   
-   REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'x-content-type-options = ([^,\r\n]*)') AS contenttypes
-  FROM
-    `httparchive.almanac.summary_response_bodies`
-)
-left join unnest(contenttypes) flat_contenttypes    
-group by flat_contenttypes
-)
-order by pct desc
+SELECT
+  policy,
+  COUNT(0) AS freq,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (), 2) AS pct
+FROM
+  `httparchive.almanac.summary_response_bodies`,
+  UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'x-content-type-options = ([^,\r\n]+)')) AS value,
+  UNNEST(SPLIT(value, ';')) AS policy
+WHERE
+  firstHtml
+GROUP BY
+  policy
+ORDER BY
+  freq DESC
