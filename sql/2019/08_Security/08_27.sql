6f6f27056caeb7215e353013a075667e90a25efc
Jason Haralson <jason.haralson@gmail.com>
2019-08-05 23:08:49 -0400

    Move to the raw parsed values

--- sql/2019/08_Security/08_27.sql
+++ sql/2019/08_Security/08_27.sql
@@ -1,29 +1,20 @@
 #standardSQL
 # 08_27: Groupings of "referrer-policy" parsed values buckets
-#   8 groups
-#    no-referrer = does not contain "-when-downgrade"
-#    no-referrer-when-downgrade
-#    origin  = does not contain "-when-cross-origin"
-#    origin-when-cross-origin
-#    same-origin
-#    strict-strict = does not contain "-origin-when-cross-origin"
-#    strict-strict-origin-when-cross-origin
-#    unsafe-url
+#   raw values pull from the header 
 #
 #   `httparchive.almanac.summary_response_bodies` archive = 71.5GB 
 #   `httparchive.summary_requests.2019_07_01_*` = 118.3 GB
 
 SELECT
-  COUNT(0) AS TotCount,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy =')) / COUNT(0) AS pct_TotXFrames,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = no-referrer[^-]')) / COUNT(0) AS pct_NoRef,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = no-referrer-when-downgrade')) / COUNT(0) AS pct_NoRefDowngrade,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = origin[^-]')) / COUNT(0) AS pct_Origin,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = origin-when-cross-origin')) / COUNT(0) AS pct_OriginXOrigin,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = same-origin')) / COUNT(0) AS pct_SameOrigin,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = strict-strict[^-]')) / COUNT(0) AS pct_StrictOrigin,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = strict-strict-origin-when-cross-origin')) / COUNT(0) AS pct_StrictOriginXOrigin,
-  COUNTIF(REGEXP_CONTAINS(LOWER(respOtherHeaders), '(?i)referrer-policy = unsafe-url')) / COUNT(0) AS pct_UnsafeUrl
+  policy,
+  COUNT(0) AS freq,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (), 2) AS pct
 FROM
-  `httparchive.almanac.summary_response_bodies`
-  where firstHtml
+  `httparchive.almanac.summary_requests`,
+  UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders), 'referrer-policy = ([\\w-]+)')) AS policy
+WHERE
+  firstHtml
+GROUP BY
+  policy
+ORDER BY
+  freq DESC
