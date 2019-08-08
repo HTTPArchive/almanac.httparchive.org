112cb72ada8640e3eebf71f046d69519aed7f1c3
Rick Viscomi <rviscomi@gmail.com>
2019-08-08 13:14:35 -0400

    markup metrics

--- sql/2019/03_Markup/03_01a.sql
+++ sql/2019/03_Markup/03_01a.sql
@@ -1,26 +1,24 @@
 #standardSQL
 # 03_01a: % of pages with deprecated elements
-CREATE TEMPORARY FUNCTION getElements(payload STRING)
-RETURNS ARRAY<STRING> LANGUAGE js AS '''
+CREATE TEMPORARY FUNCTION containsDeprecatedElement(payload STRING)
+RETURNS BOOLEAN LANGUAGE js AS '''
 try {
   var $ = JSON.parse(payload);
   var elements = JSON.parse($._element_count)
-  return Object.keys(elements);
+  var deprecatedElements = new Set(["applet","acronym","bgsound","dir","frame","frameset","noframes","isindex","keygen","listing","menuitem","nextid","noembed","plaintext","rb","rtc","strike","xmp","basefont","big","blink","center","font","marquee","multicol","nobr","spacer","tt"]);
+  return !!Object.keys(elements).find(e => {
+    return deprecatedElements.has(e);
+  });
 } catch (e) {
-  return [];
+  return false;
 }
 ''';
 
-CREATE TEMPORARY FUNCTION isDeprecated(element STRING) AS (
-  element IN ("applet","acronym","bgsound","dir","frame","frameset","noframes","isindex","keygen","listing","menuitem","nextid","noembed","plaintext","rb","rtc","strike","xmp","basefont","big","blink","center","font","marquee","multicol","nobr","spacer","tt")
-);
-
 SELECT
   _TABLE_SUFFIX AS client,
-  COUNTIF(isDeprecated(element)) AS pages,
-  ROUND(COUNTIF(isDeprecated(element)) * 100 / COUNT(0), 2) AS pct_pages
+  COUNTIF(containsDeprecatedElement(payload)) AS pages,
+  ROUND(COUNTIF(containsDeprecatedElement(payload)) * 100 / COUNT(0), 2) AS pct_pages
 FROM
-  `httparchive.pages.2019_07_01_*`,
-  UNNEST(getElements(payload)) AS element
+  `httparchive.pages.2019_07_01_*`
 GROUP BY
   client
