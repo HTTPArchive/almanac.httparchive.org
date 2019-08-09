42013141c450372fedc1e1c5e5be912c99ff0d69
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-08-09 07:19:02 +0530

    changed comment

--- sql/2019/07_Performance/07_20.sql
+++ sql/2019/07_Performance/07_20.sql
@@ -1,6 +1,6 @@
 #standardSQL
 # 07_20: Percentiles of scripting CPU time
-#where the main thread of the browser was busy
+#corresponding to the time main thread of the browser was busy
 SELECT
   client,
   ROUND(APPROX_QUANTILES(scriptingCpuTime, 1000)[OFFSET(100)] / 1000, 2) AS p10,
