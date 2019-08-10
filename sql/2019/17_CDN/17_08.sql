5dc9641e4bb712a9d809725aee2d843118c5da92
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-08-10 23:09:47 +0530

    Modified query to return percentage

--- sql/2019/17_CDN/17_08.sql
+++ sql/2019/17_CDN/17_08.sql
@@ -1,11 +1,9 @@
 #standardSQL
-# 17_08: Count of sites with keep-alive header
+# 17_08: Percentage of responses with keep-alive header
 SELECT
   _TABLE_SUFFIX AS client,
-  COUNT(respOtherHeaders) AS via
+  ROUND(SUM(IF(LOWER(respOtherHeaders) LIKE "%keep-alive%", 1, 0)) / COUNT(0), 4) AS pctKeepAlive
 FROM 
   `httparchive.summary_requests.2019_07_01_*`
-WHERE
-   STRPOS(LOWER(respOtherHeaders), "keep-alive") > 0
 GROUP BY
   client
