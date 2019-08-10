3f3c173ac412f88ea6d2764bd6dca79d3f5eb845
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-08-10 23:17:49 +0530

    Modified query to return percentage

--- sql/2019/17_CDN/17_11.sql
+++ sql/2019/17_CDN/17_11.sql
@@ -1,11 +1,9 @@
 #standardSQL
-# 17_11: Count of sites with content-disposition header
+# 17_11: Percentage of responses with content-disposition header
 SELECT
   _TABLE_SUFFIX AS client,
-  COUNT(respOtherHeaders) AS vary
+  ROUND(SUM(IF(LOWER(respOtherHeaders) LIKE "%content-disposition%", 1, 0)) / COUNT(0), 4) AS pctContentDisposition
 FROM 
   `httparchive.summary_requests.2019_07_01_*`
-WHERE
-   STRPOS(LOWER(respOtherHeaders), "content-disposition") > 0
 GROUP BY
   client
