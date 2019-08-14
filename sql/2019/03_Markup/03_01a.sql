ba09877fa4a5ca77321d310ce2168b84af5b84f9
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-08-13 23:25:27 -0400

    Markup chapter queries (#115)
    * markup metrics
    
    * markup metrics
    
    * markup queries
    
    * markup queries
    
    * markup query

---
+++ sql/2019/03_Markup/03_01a.sql
@@ -0,0 +1,24 @@
+#standardSQL
+# 03_01a: % of pages with deprecated elements
+CREATE TEMPORARY FUNCTION containsDeprecatedElement(payload STRING)
+RETURNS BOOLEAN LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(payload);
+  var elements = JSON.parse($._element_count)
+  var deprecatedElements = new Set(["applet","acronym","bgsound","dir","frame","frameset","noframes","isindex","keygen","listing","menuitem","nextid","noembed","plaintext","rb","rtc","strike","xmp","basefont","big","blink","center","font","marquee","multicol","nobr","spacer","tt"]);
+  return !!Object.keys(elements).find(e => {
+    return deprecatedElements.has(e);
+  });
+} catch (e) {
+  return false;
+}
+''';
+
+SELECT
+  _TABLE_SUFFIX AS client,
+  COUNTIF(containsDeprecatedElement(payload)) AS pages,
+  ROUND(COUNTIF(containsDeprecatedElement(payload)) * 100 / COUNT(0), 2) AS pct_pages
+FROM
+  `httparchive.pages.2019_07_01_*`
+GROUP BY
+  client
---
+++ sql/2019/03_Markup/03_01b.sql
@@ -0,0 +1,34 @@
+#standardSQL
+# 03_01b: Top deprecated elements
+CREATE TEMPORARY FUNCTION getElements(payload STRING)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(payload);
+  var elements = JSON.parse($._element_count)
+  return Object.keys(elements);
+} catch (e) {
+  return [];
+}
+''';
+
+CREATE TEMPORARY FUNCTION isDeprecated(element STRING) AS (
+  element IN ("applet","acronym","bgsound","dir","frame","frameset","noframes","isindex","keygen","listing","menuitem","nextid","noembed","plaintext","rb","rtc","strike","xmp","basefont","big","blink","center","font","marquee","multicol","nobr","spacer","tt")
+);
+
+SELECT
+  _TABLE_SUFFIX AS client,
+  element AS deprecated,
+  COUNT(0) AS freq,
+  SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX), 2) AS pct
+FROM
+  `httparchive.pages.2019_07_01_*`,
+  UNNEST(getElements(payload)) AS element
+WHERE
+  isDeprecated(element)
+GROUP BY
+  client,
+  deprecated
+ORDER BY
+  freq / total DESC,
+  client
