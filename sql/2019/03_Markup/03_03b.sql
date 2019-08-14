6ce16e569fa4d1a5ce045ee33a35b4e9d6380f73
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-08-13 23:25:27 -0400

    Markup chapter queries (#115)
    * markup metrics
    
    * markup metrics
    
    * markup queries
    
    * markup queries
    
    * markup query

---
+++ sql/2019/03_Markup/03_03b.sql
@@ -0,0 +1,28 @@
+#standardSQL
+# 03_03b: Top custom elements ("slang")
+CREATE TEMPORARY FUNCTION getCustomElements(payload STRING)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(payload);
+  var elements = JSON.parse($._element_count)
+  return Object.keys(elements).filter(e => e.includes('-'));
+} catch (e) {
+  return [];
+}
+''';
+
+SELECT
+  _TABLE_SUFFIX AS client,
+  custom_element,
+  COUNT(0) AS freq,
+  SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX), 2) AS pct
+FROM
+  `httparchive.pages.2019_07_01_*`,
+  UNNEST(getCustomElements(payload)) AS custom_element
+GROUP BY
+  client,
+  custom_element
+ORDER BY
+  freq / total DESC,
+  client
