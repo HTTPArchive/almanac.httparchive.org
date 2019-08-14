1090d71a6b4b76607c7613577e5dc880ddfc4897
Andrew Galloni <andrew@cloudflare.com>
2019-08-14 16:49:26 +0100

    Fix tables used in percentage calcs to not point to 1k samples

--- sql/2019/08_Security/08_01.sql
+++ sql/2019/08_Security/08_01.sql
@@ -3,7 +3,7 @@
 SELECT 
   _TABLE_SUFFIX AS client,
   DISTINCT(JSON_EXTRACT_SCALAR(payload, '$._tls_version')) as tls_version,
-  ROUND(COUNT(0) / (SELECT COUNT(0) FROM `httparchive.almanac.requests_desktop_1k` WHERE JSON_EXTRACT_SCALAR(payload, '$._tls_version') IS NOT NULL), 2) pct_tls_version
+  ROUND(COUNT(0) / (SELECT COUNT(0) FROM `httparchive.requests.2019_07_01_*` WHERE JSON_EXTRACT_SCALAR(payload, '$._tls_version') IS NOT NULL), 2) pct_tls_version
 FROM 
   `httparchive.requests.2019_07_01_*`
 WHERE
--- sql/2019/08_Security/08_02.sql
+++ sql/2019/08_Security/08_02.sql
@@ -2,7 +2,7 @@
 # 08_02: Unique issuer
 SELECT 
   DISTINCT(JSON_EXTRACT_SCALAR(payload, '$._securityDetails.issuer')) AS issuer,
-  ROUND(count(0) / (SELECT COUNT(0) FROM `httparchive.almanac.requests_desktop_1k` WHERE JSON_EXTRACT(payload, '$._securityDetails') IS NOT NULL), 2) AS pct_issuer
+  ROUND(count(0) / (SELECT COUNT(0) FROM `httparchive.requests.2019_07_01_*` WHERE JSON_EXTRACT(payload, '$._securityDetails') IS NOT NULL), 2) AS pct_issuer
 FROM 
   `httparchive.requests.2019_07_01_*`
 WHERE 
