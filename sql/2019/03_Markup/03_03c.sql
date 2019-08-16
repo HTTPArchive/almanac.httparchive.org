d3dc8bb467f5037cd788a399b5e82fb2e773096e
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-08-16 12:58:14 -0400

    Update 03_03c.sql

--- sql/2019/03_Markup/03_03c.sql
+++ sql/2019/03_Markup/03_03c.sql
@@ -4,7 +4,8 @@ CREATE TEMPORARY FUNCTION getCustomElements(payload STRING)
 RETURNS ARRAY<STRING> LANGUAGE js AS '''
 try {
   var $ = JSON.parse(payload);
-  var elements = JSON.parse($._element_count)
+  var elements = JSON.parse($._element_count);
+  if (Array.isArray(elements) || typeof elements != 'object') return [];
   return Object.keys(elements).filter(e => e.includes('-'));
 } catch (e) {
   return [];
