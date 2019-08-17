fa560c871b93bc9fa44701fa8bfa52f6ad64f14a
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-08-17 13:28:58 +0530

    Used array methods to simplify the UDF

--- sql/2019/07_Performance/07_17.sql
+++ sql/2019/07_Performance/07_17.sql
@@ -1,21 +1,16 @@
 #standardSQL
 # 07_17: Percentiles of blocking CSS requests
 #This metric comes from Lighthouse only
-CREATE TEMPORARY FUNCTION renderBlockingCSS(payload STRING)
+CREATE TEMPORARY FUNCTION renderBlockingCSS(report STRING)
 RETURNS NUMERIC LANGUAGE js AS '''
-  var obj = JSON.parse(payload); 
-  if (obj == null)
-    return 0;
-  else {
-    var css = 0;
-    var blkelements = JSON.parse(payload);
-    for (x of blkelements) {
-      if ((x.url).toLowerCase().includes(".css") == true) {
-        css = css + 1;
-      }
-    }
-    return css;
-  }
+try {
+  var $ = JSON.parse(report);
+  return $.audits['render-blocking-resources'].details.items.filter(item => {
+    return item.url.toLowerCase().includes('.css');
+  }).length;
+} catch (e) {
+  return null;
+}
 ''';
 
 SELECT
@@ -24,10 +19,7 @@ SELECT
   APPROX_QUANTILES(renderBlockingCSS, 1000)[OFFSET(500)] AS p50,
   APPROX_QUANTILES(renderBlockingCSS, 1000)[OFFSET(750)] AS p75,
   APPROX_QUANTILES(renderBlockingCSS, 1000)[OFFSET(900)] AS p90
-FROM 
-( 
-  SELECT
-    renderBlockingCSS(JSON_EXTRACT(report, '$.audits.render-blocking-resources.details.items')) AS renderBlockingCSS
-  FROM
-    `httparchive.lighthouse.2019_07_01_mobile`    
+FROM (
+  SELECT renderBlockingCSS(report) AS renderBlockingCSS
+  FROM `httparchive.lighthouse.2019_07_01_mobile`
 )
