3d4b7cd300002b546dc307c2c513adca8fe43d5c
Jason Haralson <jason.haralson@gmail.com>
2019-08-17 00:51:21 -0400

    Fix column name

--- sql/2019/08_Security/08_31.sql
+++ sql/2019/08_Security/08_31.sql
@@ -9,7 +9,7 @@ SELECT
   SUM(COUNT(0)) OVER (PARTITION BY client) AS client_tot,
   xframe_val,
   count(0) as xframe_val_freq,
-  ROUND(COUNT(0)*100/SUM(COUNT(0)) OVER (PARTITION BY client),2) as xss_val_pct 
+  ROUND(COUNT(0)*100/SUM(COUNT(0)) OVER (PARTITION BY client),2) as xframe_val_pct 
 FROM
   `httparchive.almanac.summary_response_bodies`,
   UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'x-frame-options = ([^,\r\n]+)')) AS xframe_val
