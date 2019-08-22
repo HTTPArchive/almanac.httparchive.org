141c0dbcf9c0f19168e4e296775652f8e5d02034
Rick Viscomi <rviscomi@gmail.com>
2019-08-22 13:16:33 -0400

    css queries

--- sql/2019/02_CSS/02_45.sql
+++ sql/2019/02_CSS/02_45.sql
@@ -1,49 +1,20 @@
 #standardSQL
-# 02_45: Distribution of selector class length per page
-CREATE TEMPORARY FUNCTION getAllValues(css STRING)
-RETURNS ARRAY<INT64> LANGUAGE js AS '''
-try {
-  var reduceValues = (values, rule) => {
-    if ('rules' in rule) {
-      return rule.rules.reduce(reduceValues, values);
-    }
-    var selectors = rule.selectors || rule.selector && [rule.selector];
-    if (!selectors) {
-      return values;
-    }
-
-    rule.selectors.forEach(selector => {
-      if (selector.includes('.')) {
-        var classLength = selector.split('.').length - 1;
-        values.push(classLength);
-      }
-    });
-    return values;
-  };
-  var $ = JSON.parse(css);
-  return $.stylesheet.rules.reduce(reduceValues, []);
-} catch (e) {
-  return [];
-}
-''';
-
+# 02_45: Distribution of classes per element per page
 SELECT
   client,
-  APPROX_QUANTILES(keyframes, 1000)[OFFSET(100)] AS p10,
-  APPROX_QUANTILES(keyframes, 1000)[OFFSET(250)] AS p25,
-  APPROX_QUANTILES(keyframes, 1000)[OFFSET(500)] AS p50,
-  APPROX_QUANTILES(keyframes, 1000)[OFFSET(750)] AS p75,
-  APPROX_QUANTILES(keyframes, 1000)[OFFSET(900)] AS p90
+  APPROX_QUANTILES(classes, 1000)[OFFSET(100)] AS p10,
+  APPROX_QUANTILES(classes, 1000)[OFFSET(250)] AS p25,
+  APPROX_QUANTILES(classes, 1000)[OFFSET(500)] AS p50,
+  APPROX_QUANTILES(classes, 1000)[OFFSET(750)] AS p75,
+  APPROX_QUANTILES(classes, 1000)[OFFSET(900)] AS p90
 FROM (
   SELECT
     client,
-    COUNT(DISTINCT value) AS keyframes
+    ARRAY_LENGTH(SPLIT(value, ' ')) AS classes
   FROM
-    `httparchive.almanac.parsed_css`
-  LEFT JOIN
-    UNNEST(getAllValues(css)) AS value
-  GROUP BY
-    client,
-    page)
+    `httparchive.almanac.summary_response_bodies`,
+    UNNEST(REGEXP_EXTRACT_ALL(body, '(?i)class=[\'"]([^\'"]+)')) AS value
+  WHERE
+    firstHtml)
 GROUP BY
   client
