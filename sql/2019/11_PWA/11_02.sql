316d4b31975ec17c42788bfa66f10e86c9045bf1
Jason Haralson <jason.haralson@gmail.com>
2019-08-24 17:55:17 -0400

    Move to real July target

--- sql/2019/11_PWA/11_02.sql
+++ sql/2019/11_PWA/11_02.sql
@@ -16,4 +16,4 @@ SELECT
     COUNTIF(CAST(JSON_EXTRACT_SCALAR(report, '$.audits.installable-manifest.score') as NUMERIC) = 1) AS score_sum,
     ROUND(COUNTIF(CAST(JSON_EXTRACT_SCALAR(report, '$.audits.installable-manifest.score') as NUMERIC) = 1) * 100 / COUNT(url), 2) as score_percentage
 FROM
-    `httparchive.almanac.lighthouse_mobile_1k`
+    `httparchive.lighthouse.2019_07_01_mobile`
