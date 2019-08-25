1b92879cba932ffe0574b4ea0c64c72cf2bda496
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-08-25 11:59:22 +0530

    Return freq, total, pct; SUM(IF()) to COUNTIF

--- sql/2019/17_CDN/17_06.sql
+++ sql/2019/17_CDN/17_06.sql
@@ -2,7 +2,9 @@
 # 17_06: Percentage of responses with timing-allow-origin header
 SELECT
   _TABLE_SUFFIX AS client,
-  ROUND(SUM(IF(LOWER(respOtherHeaders) LIKE "%timing-allow-origin%", 1, 0)) / COUNT(0), 4) AS pctTimingAllowOrigin
+  COUNTIF(LOWER(respOtherHeaders) LIKE "%timing-allow-origin%") AS freq,
+  COUNT(0) AS total,
+  ROUND(COUNTIF(LOWER(respOtherHeaders) LIKE "%timing-allow-origin%") * 100 / COUNT(0), 2) AS pct
 FROM 
   `httparchive.summary_requests.2019_07_01_*`
 GROUP BY
