cd839062cf65195c51fc0c4c961634b29619d3a0
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-08-25 11:45:22 +0530

    Return freq, total, pct; SUM(IF()) to COUNTIF

--- sql/2019/17_CDN/17_05.sql
+++ sql/2019/17_CDN/17_05.sql
@@ -2,7 +2,9 @@
 # 17_05: Percentage of responses with strict-transport-security header
 SELECT
   _TABLE_SUFFIX AS client,
-  ROUND(SUM(IF(LOWER(respOtherHeaders) LIKE "%strict-transport-security%", 1, 0)) / COUNT(0), 4) AS pctStrictTransportSecurity
+  COUNTIF(LOWER(respOtherHeaders) LIKE "%strict-transport-security%") AS freq,
+  COUNT(0) AS total,
+  ROUND(COUNTIF(LOWER(respOtherHeaders) LIKE "%strict-transport-security%") * 100 / COUNT(0), 2) AS pct
 FROM
   `httparchive.summary_requests.2019_07_01_*`
 GROUP BY
