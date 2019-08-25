29d62c3320a31e690e284e85f1547360370f58c6
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-08-25 12:20:20 +0530

    Change to resp_via != ' ' and COUNTIF

--- sql/2019/17_CDN/17_07.sql
+++ sql/2019/17_CDN/17_07.sql
@@ -1,11 +1,11 @@
 #standardSQL
-# 17_07: Count of sites with via header
+# 17_07: Percentage of responses with via header
 SELECT
   _TABLE_SUFFIX AS client,
-  COUNT(respOtherHeaders) AS via
+  COUNTIF(resp_via != '') AS freq,
+  COUNT(0) AS total,
+  ROUND(COUNTIF(resp_via != '') * 100 / COUNT(0), 2) AS pct
 FROM 
   `httparchive.summary_requests.2019_07_01_*`
-WHERE
-   STRPOS(LOWER(respOtherHeaders), "via") > 0
 GROUP BY
   client
