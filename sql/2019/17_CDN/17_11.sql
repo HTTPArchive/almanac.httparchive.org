21735e7a6ea269b684e39e2ed07bbb9be28442db
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2019-08-25 13:26:45 +0530

    Return freq, total, pct; SUM(IF()) to COUNTIF

--- sql/2019/17_CDN/17_11.sql
+++ sql/2019/17_CDN/17_11.sql
@@ -2,7 +2,9 @@
 # 17_11: Percentage of responses with content-disposition header
 SELECT
   _TABLE_SUFFIX AS client,
-  ROUND(SUM(IF(LOWER(respOtherHeaders) LIKE "%content-disposition%", 1, 0)) / COUNT(0), 4) AS pctContentDisposition
+  COUNTIF(LOWER(respOtherHeaders) LIKE "%content-disposition%") AS freq,
+  COUNT(0) AS total,
+  ROUND(COUNTIF(LOWER(respOtherHeaders) LIKE "%content-disposition%") * 100 / COUNT(0), 2) AS pct
 FROM 
   `httparchive.summary_requests.2019_07_01_*`
 GROUP BY
