c4e0f07320b546a128a2f005b73e498c8da32d85
Rick Viscomi <rviscomi@gmail.com>
2019-08-29 17:37:15 -0400

    ecom queries

---
+++ sql/2019/13_Ecommerce/13_06.sql
@@ -0,0 +1,21 @@
+#standardSQL
+# 13_06: Distribution of image stats
+SELECT
+  percentile,
+  _TABLE_SUFFIX AS client,
+  APPROX_QUANTILES(reqImg, 1000)[OFFSET(percentile * 10)] AS image_count,
+  ROUND(APPROX_QUANTILES(bytesImg, 1000)[OFFSET(percentile * 10)] / 1024, 2) AS image_kbytes
+FROM
+  `httparchive.summary_pages.2019_07_01_*`
+JOIN
+  `httparchive.technologies.2019_07_01_*`
+USING (_TABLE_SUFFIX, url),
+UNNEST([10, 25, 50, 75, 90]) AS percentile
+WHERE
+  category = 'Ecommerce'
+GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
+  client
