bf77b1f8ed5c50b41a87cd7441ab56965118c289
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2019-09-04 23:08:00 +0800

    chore(06_02.sql): remove previous sql comment

--- sql/2019/06_Fonts/06_02.sql
+++ sql/2019/06_Fonts/06_02.sql
@@ -14,31 +14,3 @@ FROM
   USING (_TABLE_SUFFIX, pageid)
   )
 WHERE hosted = true
-
-
--- WITH
---   summary_pages AS (
---     SELECT * FROM `httparchive.summary_pages.2019_07_01_desktop`
---     UNION ALL
---     SELECT * FROM `httparchive.summary_pages.2019_07_01_mobile`
---   ),
---   summary_requests AS (
---     SELECT * FROM `httparchive.summary_requests.2019_07_01_desktop`
---     UNION ALL
---     SELECT * FROM `httparchive.summary_requests.2019_07_01_mobile`
---   ),
---   merge_table AS (
---     SELECT * FROM summary_requests JOIN (
---       SELECT pageid, url AS page_url FROM summary_pages
---     ) USING (pageid)
---   ),
---   processedTable AS (
---     SELECT (NET.HOST(url) != NET.HOST(page_url)) as hosted, url, page_url
---     FROM merge_table WHERE type = 'font'
---   )
--- SELECT value AS host, count
--- FROM (
---   SELECT APPROX_TOP_COUNT(NET.HOST(url), 500) AS font_host
---   FROM processedTable
---   WHERE hosted = true
--- ), UNNEST(font_host)
