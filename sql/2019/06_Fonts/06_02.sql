7c76ccbeefba281c330a2660ce18727d62ff643f
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2019-09-04 23:26:00 +0800

    refactor(06_02.sql): change to requested code

--- sql/2019/06_Fonts/06_02.sql
+++ sql/2019/06_Fonts/06_02.sql
@@ -3,14 +3,16 @@
 -- counts the host_url of a given font
 
 SELECT
-  APPROX_TOP_COUNT(NET.HOST(url), 500) AS font_host
-FROM
-  (SELECT
-    (NET.HOST(url) != NET.HOST(page)) as hosted, url, page
+  font_host.value AS host,
+  font_host.count / total_requests AS pct_requests
+FROM (
+  SELECT
+    APPROX_TOP_COUNT(NET.HOST(url), 500) AS font_host,
+    COUNT(DISTINCT url) AS total_requests
   FROM
     (SELECT _TABLE_SUFFIX, pageid, url FROM `httparchive.summary_requests.2019_07_01_*` WHERE type = 'font')
   JOIN
     (SELECT _TABLE_SUFFIX, pageid, url AS page FROM `httparchive.summary_pages.2019_07_01_*`)
   USING (_TABLE_SUFFIX, pageid)
-  )
-WHERE hosted = true
+  WHERE NET.HOST(url) != NET.HOST(page)),
+UNNEST(font_host) AS font_host
