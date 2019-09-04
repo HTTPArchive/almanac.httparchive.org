9d3f55c5c5ad592f0afb5908ae0051fbb14e15c6
Paul Calvano <paulcalvano@yahoo.com>
2019-09-04 16:35:57 -0400

    Update 15_04.sql

--- sql/2019/15_Compression/15_04.sql
+++ sql/2019/15_Compression/15_04.sql
@@ -1,17 +1,21 @@
-SELECT mimeType, count(*) total,
-       SUM(IF(resp_content_encoding = "gzip",1,0)) gzip,
-       SUM(IF(resp_content_encoding = "br",1,0)) brotli,
-       SUM(IF(resp_content_encoding = "deflate",1,0)) deflate,
-       SUM(IF(resp_content_encoding IN("gzip","deflate","br"),0,1)) NoTextCompression,
-       ROUND(
-           SUM(
-               IF(resp_content_encoding IN("gzip", "deflate", "br"),1,0)
-           ) / COUNT(*),2) CompressedPercentage,
-       ROUND(
-           SUM(
-               IF(resp_content_encoding = "br",1,0)
-            ) / COUNT(*),2) BrotliCompressedPercentage
-FROM `httparchive.summary_requests.2019_07_01_mobile`   
-GROUP BY mimeType
-HAVING total > 1000
-ORDER BY total DESC
+#standardSQL
+  # 15_04: Compression by Content type
+SELECT
+  _TABLE_SUFFIX AS client,
+  mimeType,
+  COUNT(*) AS num_requests,
+  SUM(IF(resp_content_encoding = "gzip",1,0)) AS gzip,
+  SUM(IF(resp_content_encoding = "br",1,0)) AS brotli,
+  SUM(IF(resp_content_encoding = "deflate",1,0)) AS deflate,
+  SUM(IF(resp_content_encoding IN("gzip","deflate","br"),0,1)) AS no_text_compression,
+  ROUND(SUM(IF(resp_content_encoding IN("gzip","deflate","br"),1,0)) / COUNT(0),2) AS pct_compressed,
+  ROUND(SUM(IF(resp_content_encoding = "br",1,0)) / COUNT(0),2) AS pct_compressed_brotli
+FROM
+  `httparchive.summary_requests.2019_07_01_*`
+GROUP BY
+  client,
+  mimeType
+HAVING
+  num_requests > 1000
+ORDER BY
+  num_requests DESC
