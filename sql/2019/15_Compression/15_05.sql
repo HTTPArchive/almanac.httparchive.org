fc5d836bd2cfe763dfb78e52cc3140cc3e210d5d
Paul Calvano <paulcalvano@yahoo.com>
2019-09-04 16:44:31 -0400

    Update 15_05.sql

--- sql/2019/15_Compression/15_05.sql
+++ sql/2019/15_Compression/15_05.sql
@@ -1,7 +1,15 @@
-#15.05 - Distribution of Text Based Compression Lighthouse Scores
-SELECT JSON_EXTRACT_SCALAR(report, "$.audits.uses-text-compression.score") TextCompressionScore,
-       count(*)
-FROM `httparchive.lighthouse.2019_07_01_mobile` 
-where JSON_EXTRACT_SCALAR(report, "$.audits.uses-text-compression.score") != "1"
-GROUP BY TextCompressionScore
-ORDER BY TextCompressionScore ASC
+#standardSQL
+# 15.05 - Distribution of Text Based Compression Lighthouse Scores
+SELECT 
+  _TABLE_SUFFIX AS client,
+  JSON_EXTRACT_SCALAR(report, "$.audits.uses-text-compression.score") AS text_compression_score,
+  COUNT(*) AS num_pages,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX), 2) AS pct_pages
+FROM 
+  `httparchive.lighthouse.2019_07_01_*` 
+GROUP BY 
+  client,
+  text_compression_score
+ORDER BY 
+  client, 
+  text_compression_score ASC
