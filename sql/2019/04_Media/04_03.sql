6770629b51da3ba793a293a8595e3d664033fadb
dougsillars <doug.sillars@gmail.com>
2019-09-06 07:25:08 +0100

    updated 403 removedredundant 0401 subqueries

--- sql/2019/04_Media/04_03.sql
+++ sql/2019/04_Media/04_03.sql
@@ -1,27 +1,19 @@
 #standardSQL
-/*
-standard SQL
-04_03b
-
-5.25 gb query
-
-how many SVG files are requested AS discrete files (5.1M)
-and breakdown of SVG filesize in 2% buckets
-
-*/
-SELECT 
-    APPROX_QUANTILES(COUNT, 1000)[offset(250)] AS p25,
-    APPROX_QUANTILES(COUNT, 1000)[offset(500)] AS p50,
-    APPROX_QUANTILES(COUNT, 1000)[offset(750)] AS p75,
-    APPROX_QUANTILES(COUNT, 1000)[offset(900)] AS p90,
-    COUNT(pageid) AS pageCOUNT
- FROM(
-SELECT  pageid,
-        COUNT(*) COUNT
-FROM(
-
-SELECT url, pageid, respsize, ext, format
-
-FROM `httparchive.summary_requests.2019_07_01_*`
-WHERE ext ="svg")
-GROUP BY pageid)
+# 04_03: SVG usage
+SELECT
+  client,
+  percentile,
+  APPROX_QUANTILES(requests, 1000)[OFFSET(percentile * 10)] AS requests,
+  ROUND(APPROX_QUANTILES(bytes, 1000)[OFFSET(percentile * 10)] / 1024, 2) AS kbytes
+FROM (
+  SELECT _TABLE_SUFFIX AS client, COUNT(0) AS requests, SUM(respSize) AS bytes
+  FROM `httparchive.summary_requests.2019_07_01_*`
+  WHERE format = 'svg'
+  GROUP BY client, pageid),
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
+GROUP BY
+  client,
+  percentile
+ORDER BY
+  client,
+  percentile
