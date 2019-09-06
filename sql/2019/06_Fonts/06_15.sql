5892120de2ed21f654eca0499eac4a8db609faec
Toni-Jan Keith Monserrat <tjmonsi@users.noreply.github.com>
2019-09-07 06:02:58 +0800

    Fonts queries (#105)
    * feat(font): add 06_01-04,09,13,16 with placeholders for others
    
    * feat(fonts): add a few more placeholders
    
    * Update sql/2019/06_Fonts/06_01.sql
    
    fix: use percentage
    
    Co-Authored-By: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * refactor: move to a simpler code style
    
    * refactor: refactor use js in parseCSS, use lighthouse
    
    * refactor: change code to use function and blink_features
    
    * chore(06_01.sql): remove previous SQL
    
    * chore(06_02.sql): remove previous sql comment
    
    * chore(06_03.sql): remove previous sql comment
    
    * chore(06_04.sql): remove previous sql
    
    * chore(06_09a.sql): remove previous sql
    
    * chore(06_13.sql): remove previous sql
    
    * chore(06_16.sql): remove previous sql
    
    * refactor(06_02.sql): change to requested code
    
    * fix(06_16.sql): use almanac.parsed_css so as not to overload mem
    
    * chore(06_16.sql): add name to column
    
    * fix(06_09b.sql): fix overload mem
    
    * first pass at font queries
    
    * font queries part 1
    
    * rm files with no queries

---
+++ sql/2019/06_Fonts/06_15.sql
@@ -0,0 +1,37 @@
+#standardSQL
+# 06_15: % of pages preconnecting a web font host
+CREATE TEMPORARY FUNCTION getPreconnectUrls(payload STRING)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(payload);
+  var almanac = JSON.parse($._almanac);
+  return almanac['link-nodes'].filter(link => link.rel == 'preconnect').map(link => link.href);
+} catch (e) {
+  return [];
+}
+''';
+
+SELECT
+  client,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  ROUND(COUNT(DISTINCT page) * 100 / total, 2) AS pct
+FROM
+  (SELECT _TABLE_SUFFIX AS client, url AS page, payload FROM `httparchive.pages.2019_07_01_*`)
+JOIN
+  (SELECT client, page, url FROM `httparchive.almanac.requests` WHERE type = 'font')
+USING
+  (client, page)
+JOIN
+  (SELECT _TABLE_SUFFIX AS client, COUNT(0) AS total FROM `httparchive.summary_pages.2019_07_01_*` GROUP BY _TABLE_SUFFIX)
+USING
+  (client),
+  UNNEST(getPreconnectUrls(payload)) AS preconnect_url
+WHERE
+  # hosts match
+  NET.HOST(preconnect_url) = NET.HOST(url) AND
+  # protocols match
+  SUBSTR(preconnect_url, 0, 5) = SUBSTR(url, 0, 5)
+GROUP BY
+  client,
+  total
