bd420622c92ceabdb0a9eed450cd5df668cafffc
GiacomoPignoni <giacomo.pignoni@gmail.com>
2019-09-06 19:28:22 +0200

    Fix avatars name and quotes

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import urllib.parse
 
 config_json = {}
 
-DEFAULT_AVATAR_FOLDER_PATH = "/static/images/avatars/"
+DEFAULT_AVATAR_FOLDER_PATH = '/static/images/avatars/'
 AVATAR_SIZE = 200
 AVATARS_NUMBER = 15
 
@@ -21,10 +21,10 @@ def update_config():
 
     for contributor_id, contributor in config_json['2019']['contributors'].items():
       if 'gravatar' in contributor:
-        gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(contributor['gravatar'].lower().encode()).hexdigest() + ".jpg?"
+        gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(contributor['gravatar'].lower().encode()).hexdigest() + '.jpg?'
         gravatar_url += urllib.parse.urlencode({'d': 'mp','s':str(AVATAR_SIZE)})
         contributor['avatar_url'] = gravatar_url
       else:
-        contributor['avatar_url'] = DEFAULT_AVATAR_FOLDER_PATH + str(hash(contributor_id) % AVATARS_NUMBER) + ".jpg"
+        contributor['avatar_url'] = DEFAULT_AVATAR_FOLDER_PATH + str(hash(contributor_id) % AVATARS_NUMBER) + '.jpg'
         
 update_config()
