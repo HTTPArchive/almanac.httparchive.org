641b9b70e88259b04447f0e064d84f7d4344e495
Pigna <34923063+GiacomoPignoni@users.noreply.github.com>
2019-09-06 20:13:19 +0200

    Added gravatar url generation for contributors (#138)
    * Added gravatar url generation for contributors
    
    * Added requested changes on gravatar url generation
    
    * Fix avatars name and quotes

--- src/config.py
+++ src/config.py
@@ -1,7 +1,12 @@
+import hashlib
 import json
+import urllib.parse
 
 config_json = {}
 
+DEFAULT_AVATAR_FOLDER_PATH = '/static/images/avatars/'
+AVATAR_SIZE = 200
+AVATARS_NUMBER = 15
 
 def get_config(year):
   global config_json
@@ -14,5 +19,12 @@ def update_config():
   with open('config/2019.json', 'r') as config_file:
     config_json['2019'] = json.load(config_file)
 
-
+    for contributor_id, contributor in config_json['2019']['contributors'].items():
+      if 'gravatar' in contributor:
+        gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(contributor['gravatar'].lower().encode()).hexdigest() + '.jpg?'
+        gravatar_url += urllib.parse.urlencode({'d': 'mp','s':str(AVATAR_SIZE)})
+        contributor['avatar_url'] = gravatar_url
+      else:
+        contributor['avatar_url'] = DEFAULT_AVATAR_FOLDER_PATH + str(hash(contributor_id) % AVATARS_NUMBER) + '.jpg'
+        
 update_config()
