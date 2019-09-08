1da8862bcd2c269082c456fede07377fb01cf5f4
Paul Calvano <paulcalvano@yahoo.com>
2019-09-08 00:24:05 -0400

    Chapter 15 - Compression Queries (#127)
    * Create 15_04.sql
    
    * Update 15_01.sql
    
    * Create 15_05.sql
    
    * Create 15_06.sql
    
    * Update 15_01.sql
    
    * Update 15_04.sql
    
    * Update 15_05.sql
    
    * Update 15_06.sql
    
    * Update 15_06.sql
    
    * Update 20_01.sql
    
    * Create 20_03.sql
    
    * Update 20_03.sql
    
    * Create 20_08.sql
    
    * Create 20_09.sql
    
    * Create 20_10.sql
    
    * Create 20_11.sql
    
    * Create 20_12.sql

---
+++ sql/2019/20_HTTP_2/20_09.sql
@@ -0,0 +1,33 @@
+#standardSQL
+# 20.09 - Count of non-HTTP/2 Sites Grouped By Server
+CREATE TEMPORARY FUNCTION getServerHeader(payload STRING)
+RETURNS STRING
+LANGUAGE js AS """
+  try {
+    var $ = JSON.parse(payload);
+    var headers = $.response.headers;
+    // Find server header
+    var st = headers.find(function(e) { 
+      return e['name'].toLowerCase() === 'server'
+    });
+    // Remove everything after / in the server header value and return
+    return st['value'].split("/")[0]; 
+  } catch (e) {
+    return '';
+  }
+""";
+
+SELECT 
+ _TABLE_SUFFIX AS client,
+  getServerHeader(payload) AS server_header,
+  COUNT(*) AS num_pages,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX), 2) AS pct
+FROM 
+  `httparchive.requests.2019_07_01_*` 
+WHERE
+  JSON_EXTRACT_SCALAR(payload, "$._is_base_page")  = "true"
+  AND JSON_EXTRACT_SCALAR(payload, "$._protocol") != "HTTP/2"
+GROUP BY
+  client,
+  server_header
+ORDER BY num_pages DESC
