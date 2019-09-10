26fa17660f5bcaabfdbcf3c7d34c49a27db9cdaa
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-09-10 16:20:31 -0400

    fixups (#152)
    - always group by client
    - prefer explicit percentiles over array of quantiles

--- sql/2019/01_JavaScript/01_14.sql
+++ sql/2019/01_JavaScript/01_14.sql
@@ -13,7 +13,11 @@ RETURNS BOOLEAN LANGUAGE js AS '''
 ''';
 
 SELECT
+  _TABLE_SUFFIX AS client,
   COUNTIF(hasScriptPreload(payload)) AS num_pages,
+  COUNT(0) AS total,
   ROUND(COUNTIF(hasScriptPreload(payload)) * 100 / COUNT(0), 2) AS pct_script_preload
 FROM
   `httparchive.pages.2019_07_01_*`
+GROUP BY
+  client
