87c4679fb7d6e06965d8d1a13d115384969cf86b
Jason Haralson <jason.haralson@gmail.com>
2019-09-10 12:26:13 -0400

    Updated to leverage blink_features table

--- sql/2019/11_PWA/11_01.sql
+++ sql/2019/11_PWA/11_01.sql
@@ -8,12 +8,12 @@
 # == archive.summary_requests = 117.5 GB 
 
 SELECT
-  _TABLE_SUFFIX AS client,
-  count(0) as tot,
-  ROUND(SUM(IF(JSON_EXTRACT(payload, '$._blinkFeatureFirstUsed.Features.ServiceWorkerControlledPage') IS NOT NULL, 1, 0)) * 100 / COUNT(0), 2) AS percent
+  client,
+  num_urls AS freq,
+  total_urls AS total,
+  ROUND(pct_urls * 100, 2) AS pct
 FROM
-  `httparchive.almanac.pages_*`
-GROUP BY
-  client
-ORDER BY
-  client
+  `httparchive.blink_features.usage`
+WHERE
+  yyyymmdd = '20190701' AND
+  feature = 'ServiceWorkerControlledPage'
