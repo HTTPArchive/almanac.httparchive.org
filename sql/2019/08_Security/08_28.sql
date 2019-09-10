cdda4d05af69c612cdf80933fb869e0e557a991e
Jason Haralson <jason.haralson@gmail.com>
2019-09-10 13:01:43 -0400

    Update formatting with TABLE_SUFFIX wildcard table

--- sql/2019/08_Security/08_28.sql
+++ sql/2019/08_Security/08_28.sql
@@ -1,22 +1,23 @@
 #standardSQL
 # 08_28: Groupings of "feature-policy" parsed values buckets
-
-#   `httparchive.almanac.summary_response_bodies` archive = 71.5GB 
+#
+#
 #   `httparchive.summary_requests.2019_07_01_*` = 118.3 GB
  
 SELECT
-  client,
-  SUM(COUNT(0)) OVER (PARTITION BY client) AS client_tot,
+  _TABLE_SUFFIX AS client,
   policy,
-  count(0) as policy_freq,
-  ROUND(COUNT(0)*100/SUM(COUNT(0)) OVER (PARTITION BY client),2) as policy_pct
+  COUNT(0) as freq,
+  SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX), 2) as pct 
 FROM
-  `httparchive.almanac.summary_response_bodies`,
-  UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'feature-policy = ([^,\r\n]+)')) AS value,
+  `httparchive.summary_requests.2019_07_01_*`,
+  UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders), 'feature-policy = ([\\w-]+)')) AS value,
   UNNEST(SPLIT(value, ';')) AS policy
 WHERE
   firstHtml
 GROUP BY
-  client, policy
+  client,
+  policy
 ORDER BY
-  client, policy_freq DESC
+  freq / total DESC
