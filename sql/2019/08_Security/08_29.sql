e1af86f472c43ca6cb8291afb3e271a8af64365a
Jason Haralson <jason.haralson@gmail.com>
2019-09-10 13:02:56 -0400

    Update formatting with TABLE_SUFFIX wildcard table

--- sql/2019/08_Security/08_29.sql
+++ sql/2019/08_Security/08_29.sql
@@ -1,22 +1,22 @@
 #standardSQL
 # 08_29: Groupings of "x-content-type-options" parsed values by percentage
 #    
-#   `httparchive.almanac.summary_response_bodies` archive = 71.5GB 
+#
 #   `httparchive.summary_requests.2019_07_01_*` = 118.3 GB
 
 SELECT
-  client,
-  SUM(COUNT(0)) OVER (PARTITION BY client) AS client_tot,
+  _TABLE_SUFFIX AS client,
   policy,
-  count(0) as policy_freq,
-  ROUND(COUNT(0)*100/SUM(COUNT(0)) OVER (PARTITION BY client),2) as policy_pct
+  COUNT(0) as freq,
+  SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX), 2) as pct 
 FROM
-  `httparchive.almanac.summary_response_bodies`,
-  UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders),r'x-content-type-options = ([^,\r\n]+)')) AS value,
-  UNNEST(SPLIT(value, ';')) AS policy
+  `httparchive.summary_requests.2019_07_01_*`,
+  UNNEST(REGEXP_EXTRACT_ALL(LOWER(respOtherHeaders), 'x-content-type-options = ([\\w-]+)')) AS policy
 WHERE
   firstHtml
 GROUP BY
-  client, policy
+  client,
+  policy
 ORDER BY
-  client, policy_freq DESC
+  freq / total DESC
