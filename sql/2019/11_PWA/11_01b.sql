f998535df0494060df93c4149d4fc17ed934b5a1
Jason Haralson <jason.haralson@gmail.com>
2019-09-10 12:28:02 -0400

    Moved to blink_features table query

--- sql/2019/11_PWA/11_01b.sql
+++ sql/2019/11_PWA/11_01b.sql
@@ -1,21 +1,18 @@
 #standardSQL
 #
-# timeline view from the top-level HTTPArchive page 
-#    https://httparchive.org/reports/progressive-web-apps?start=2017_08_01&end=latest&view=list#swControlledPages
-# 13.6TB 
-
+# Moved to leverage smaller "blink_features" table
+# 7.92 MB
 
 SELECT
-  SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
-  UNIX_DATE(CAST(REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '-') AS DATE)) * 1000 * 60 * 60 * 24 AS timestamp,
-  IF(ENDS_WITH(_TABLE_SUFFIX, 'desktop'), 'desktop', 'mobile') AS client,
-  ROUND(SUM(IF(JSON_EXTRACT(payload, '$._blinkFeatureFirstUsed.Features.ServiceWorkerControlledPage') IS NOT NULL, 1, 0)) * 100 / COUNT(0), 2) AS percent
+  yyyymmdd AS date,
+  client,
+  num_urls AS freq,
+  total_urls AS total,
+  ROUND(pct_urls * 100, 2) AS pct
 FROM
-  `httparchive.pages.*`
-GROUP BY
-  date,
-  timestamp,
-  client
+  `httparchive.blink_features.usage`
+WHERE
+  feature = 'ServiceWorkerControlledPage'
 ORDER BY
   date DESC,
   client
