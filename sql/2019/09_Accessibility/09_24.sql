82df6f2334325411c0ff663cfb10f0dc79f16892
Rick Viscomi <rviscomi@gmail.com>
2019-09-11 18:23:18 -0400

    a11y queries

---
+++ sql/2019/09_Accessibility/09_24.sql
@@ -0,0 +1,20 @@
+#standardSQL
+# 09_24: % pages with aria-posinset and aria-setsize
+SELECT
+  client,
+  COUNTIF(posinset AND setsize) AS freq,
+  COUNT(0) AS total,
+  ROUND(COUNTIF(posinset AND setsize) * 100 / COUNT(0), 2) AS pct
+FROM (
+  SELECT
+    client,
+    REGEXP_CONTAINS(body, '\\saria-posinset=') AS posinset,
+    REGEXP_CONTAINS(body, '\\saria-setsize=') AS setsize
+  FROM
+    `httparchive.almanac.summary_response_bodies`
+  WHERE
+    firstHtml)
+GROUP BY
+  client
+ORDER BY
+  freq / total DESC
