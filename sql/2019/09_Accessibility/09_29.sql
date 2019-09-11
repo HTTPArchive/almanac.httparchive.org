5c891bfa645c6b3dc0a365646775c16dea26d729
David Fox <david@davidjfox.com>
2019-09-11 16:05:00 -0500

    Update to add percentage out of sites which are applicable

--- sql/2019/09_Accessibility/09_29.sql
+++ sql/2019/09_Accessibility/09_29.sql
@@ -3,6 +3,8 @@
 SELECT
   COUNTIF(CAST(JSON_EXTRACT_SCALAR(report, '$.audits.color-contrast.score') as NUMERIC) = 1) AS total_sufficient,
   COUNT(0) AS total_sites,
-  ROUND(COUNTIF(CAST(JSON_EXTRACT_SCALAR(report, '$.audits.color-contrast.score') as NUMERIC) = 1) * 100 / COUNT(0), 2) AS sufficient_percent
+  COUNTIF(JSON_EXTRACT_SCALAR(report, '$.audits.color-contrast.score') IS NOT NULL) AS total_applicable,
+  ROUND(COUNTIF(CAST(JSON_EXTRACT_SCALAR(report, '$.audits.color-contrast.score') as NUMERIC) = 1) * 100 / COUNTIF(JSON_EXTRACT_SCALAR(report, '$.audits.color-contrast.score') IS NOT NULL), 2) AS perc_in_applicable,
+  ROUND(COUNTIF(CAST(JSON_EXTRACT_SCALAR(report, '$.audits.color-contrast.score') as NUMERIC) = 1) * 100 / COUNT(0), 2) AS perc_in_all_sites
 FROM
   `httparchive.lighthouse.2019_07_01_mobile`
