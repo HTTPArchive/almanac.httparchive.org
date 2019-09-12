d45f390efa131bb071dffd090005dfa4bf113cbc
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-09-11 23:57:01 -0400

    Accessibility queries 1 of 2 (#151)
    * a11y queries
    
    * heading query
    
    * a11y queries
    
    * a11y queries
    
    * fix 35

--- sql/2019/09_Accessibility/09_17.sql
+++ sql/2019/09_Accessibility/09_17.sql
@@ -1,17 +1,17 @@
 #standardSQL
-# 09_16: % pages using invalid/required form field attributes
+# 09_17: % pages using tables with headings
 SELECT
   client,
-  attr,
+  heading,
   COUNT(DISTINCT page) AS pages,
   total,
   ROUND(COUNT(DISTINCT page) * 100 / total, 2) AS pct
 FROM
   `httparchive.almanac.summary_response_bodies`,
   UNNEST(ARRAY_CONCAT(
-    REGEXP_EXTRACT_ALL(body, '<input[^>]+(aria-invalid|aria-required)\\b'),
-    REGEXP_EXTRACT_ALL(body, '<input[^>]+[^-](required)\\b')
-  )) AS attr
+    REGEXP_EXTRACT_ALL(LOWER(body), '<(th)\\b'),
+    REGEXP_EXTRACT_ALL(LOWER(body), '\\brole=[\'"]?(rowheader|columnheader)\\b')
+  )) AS heading
 JOIN
   (SELECT _TABLE_SUFFIX AS client, COUNT(0) AS total FROM `httparchive.pages.2019_07_01_*` GROUP BY _TABLE_SUFFIX)
 USING (client)
@@ -20,6 +20,6 @@ WHERE
 GROUP BY
   client,
   total,
-  attr
+  heading
 ORDER BY
   pages / total DESC
