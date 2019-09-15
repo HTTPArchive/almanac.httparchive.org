6bfc2656c55437714be95c583388701b93cff086
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-09-15 04:26:11 -0400

    CMS queries (#123)

---
+++ sql/2019/14_CMS/14_03.sql
@@ -0,0 +1,26 @@
+#standardSQL
+# 14_02: AMP plugin mode
+SELECT
+  client,
+  amp_plugin_mode,
+  COUNT(DISTINCT url) AS freq,
+  SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS total,
+  ROUND(COUNT(DISTINCT url) * 100 / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client), 2) AS pct
+FROM (
+  SELECT
+    client,
+    page AS url,
+    SPLIT(REGEXP_EXTRACT(body, '(?i)<meta[^>]+name=[\'"]?generator[^>]+content=[\'"]?AMP Plugin v(\\d+\\.\\d+[^\'">]*)'), ';')[SAFE_OFFSET(1)] AS amp_plugin_mode
+  FROM
+    `httparchive.almanac.summary_response_bodies`
+  WHERE
+    firstHtml)
+INNER JOIN
+  (SELECT _TABLE_SUFFIX AS client, url FROM `httparchive.technologies.2019_07_01_*` WHERE app = 'WordPress')
+USING
+  (client, url)
+GROUP BY
+  client,
+  amp_plugin_mode
+ORDER BY
+  freq / total DESC
