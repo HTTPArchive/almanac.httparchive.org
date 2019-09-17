cc0813410f339f6b6ff75f9becf841a2b5c487a1
Rick Viscomi <rviscomi@gmail.com>
2019-09-17 02:34:08 -0400

    fix query

--- sql/2019/11_PWA/11_04b.sql
+++ sql/2019/11_PWA/11_04b.sql
@@ -1,25 +1,25 @@
 #standardSQL
 # 11_04b: Manifests that are not JSON parsable
-CREATE TEMPORARY FUNCTION getManifestProps(manifest STRING)
-RETURNS ARRAY<STRING> LANGUAGE js AS '''
+CREATE TEMPORARY FUNCTION canParseManifest(manifest STRING)
+RETURNS BOOLEAN LANGUAGE js AS '''
 try {
-  return Object.keys(JSON.parse(manifest));
+  JSON.parse(manifest);
+  return true;
 } catch (e) {
-  return null;
+  return false;
 }
 ''';
 
 SELECT
   client,
-  freq,
-  total,
-  ROUND(freq * 100 / total, 2) AS pct
-FROM (
-  SELECT
-    client,
-    COUNTIF(getManifestProps(body) IS NULL) AS freq,
-    COUNT(DISTINCT page) AS total
-  FROM
-    `httparchive.almanac.manifests`
-  GROUP BY
-    client)
+  canParseManifest(body) AS can_parse,
+  COUNT(DISTINCT page) AS freq,
+  SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client) AS total,
+  ROUND(COUNT(DISTINCT page) * 100 / SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client), 2) AS pct
+FROM
+  `httparchive.almanac.manifests`
+GROUP BY
+  client,
+  can_parse
+ORDER BY
+  freq DESC
