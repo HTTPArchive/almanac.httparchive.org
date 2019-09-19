71106ce13d73272853d126c7a00cc329c1c65419
David Fox <david@davidjfox.com>
2019-09-19 11:57:22 -0500

    16.10 and 16.09 edit

--- sql/2019/16_Caching/16_10.sql
+++ sql/2019/16_Caching/16_10.sql
@@ -1,21 +1,21 @@
 #standardSQL
-# 16_09: Use of Vary (how many dimensions, what headers, etc.)
+# 16_10: Use of other Cache-Control directives (e.g., public, private, immutable)
 SELECT
   client,
   all_requests,
-  total_with_vary,
+  total_using_control,
   header_name,
   COUNT(0) AS occurrences,
-  ROUND(COUNT(0) * 100 / total_with_vary, 2) AS pct_of_vary,
+  ROUND(COUNT(0) * 100 / total_using_control, 2) AS pct_of_control,
   ROUND(COUNT(0) * 100 / all_requests, 2) AS pct_all_requests
 FROM
   `httparchive.almanac.requests`,
-  UNNEST(REGEXP_EXTRACT_ALL(LOWER(resp_vary), r'([a-z][^,\s="\']*)')) AS header_name
+  UNNEST(REGEXP_EXTRACT_ALL(LOWER(resp_cache_control), r'([a-z][^,\s="\']*)')) AS header_name
 JOIN (
   SELECT
     client,
     COUNT(0) AS all_requests,
-    COUNTIF(TRIM(resp_vary) != "") AS total_with_vary
+    COUNTIF(TRIM(resp_cache_control) != "") AS total_using_control
   FROM
     `httparchive.almanac.requests`
   GROUP BY
@@ -25,7 +25,7 @@ USING (client)
 GROUP BY
   client,
   all_requests,
-  total_with_vary,
+  total_using_control,
   header_name
 ORDER BY
   occurrences DESC
