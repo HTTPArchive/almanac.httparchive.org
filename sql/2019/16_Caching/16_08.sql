2ef9ec380d1f09c7e0bd6b06cf957b12ebc1a178
David Fox <david@davidjfox.com>
2019-09-19 12:30:54 -0500

    Add 16.06 and update column names for 16.08

--- sql/2019/16_Caching/16_08.sql
+++ sql/2019/16_Caching/16_08.sql
@@ -4,10 +4,10 @@ SELECT
   client,
   COUNT(0) AS total_requests,
 
-  COUNTIF(uses_max_age) AS max_age_count,
-  COUNTIF(uses_expires) AS expires_count,
-  COUNTIF(uses_max_age AND uses_expires) AS uses_both_count,
-  COUNTIF(NOT uses_max_age AND NOT uses_expires) AS uses_neither,
+  COUNTIF(uses_max_age) AS total_max_age,
+  COUNTIF(uses_expires) AS total_expires,
+  COUNTIF(uses_max_age AND uses_expires) AS total_using_both,
+  COUNTIF(NOT uses_max_age AND NOT uses_expires) AS total_using_neither,
 
   ROUND(COUNTIF(uses_max_age) * 100 / COUNT(0), 2) AS pct_max_age,
   ROUND(COUNTIF(uses_expires) * 100 / COUNT(0), 2) AS pct_expires,
