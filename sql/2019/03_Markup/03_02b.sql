7d6f6c316945b324df8db278a7fd3df3675a08b2
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-09-23 13:01:44 -0400

    Update 03_02b.sql

--- sql/2019/03_Markup/03_02b.sql
+++ sql/2019/03_Markup/03_02b.sql
@@ -1,12 +1,12 @@
 #standardSQL
 # 03_02b: Top elements
 CREATE TEMPORARY FUNCTION getElements(payload STRING)
-RETURNS ARRAY<STRING> LANGUAGE js AS '''
+RETURNS ARRAY<STRUCT<name STRING, freq INT64>> LANGUAGE js AS '''
 try {
   var $ = JSON.parse(payload);
   var elements = JSON.parse($._element_count);
   if (Array.isArray(elements) || typeof elements != 'object') return [];
-  return Object.keys(elements);
+  return Object.entries(elements).map(([name, freq]) => ({name, freq}));
 } catch (e) {
   return [];
 }
@@ -14,16 +14,18 @@ try {
 
 SELECT
   _TABLE_SUFFIX AS client,
-  element,
-  COUNT(0) AS freq,
-  SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
-  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX), 2) AS pct
+  element.name,
+  SUM(element.freq) AS freq,
+  SUM(SUM(element.freq)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
+  ROUND(SUM(element.freq) * 100 / SUM(SUM(element.freq)) OVER (PARTITION BY _TABLE_SUFFIX), 2) AS pct
 FROM
   `httparchive.pages.2019_07_01_*`,
   UNNEST(getElements(payload)) AS element
 GROUP BY
   client,
-  element
+  element.name
 ORDER BY
   freq / total DESC,
   client
+LIMIT
+  10000
