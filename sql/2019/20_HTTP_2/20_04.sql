bf9432d99af1651108f30d6bfb9fde52552075e4
Paul Calvano <paulcalvano@yahoo.com>
2019-09-24 23:40:42 -0400

    Update 20_04.sql

--- sql/2019/20_HTTP_2/20_04.sql
+++ sql/2019/20_HTTP_2/20_04.sql
@@ -1,5 +1,5 @@
 #standardSQL
-# 20.04 - Number of HTTP sites which return upgrade HTTP header containing H2
+# 20.04 - Number of HTTP (not HTTPS) sites which return upgrade HTTP header containing h2.
 CREATE TEMPORARY FUNCTION getUpgradeHeader(payload STRING)
 RETURNS STRING
 LANGUAGE js AS """
@@ -18,13 +18,12 @@ LANGUAGE js AS """
 SELECT 
   _TABLE_SUFFIX AS client,
   JSON_EXTRACT_SCALAR(payload, "$._is_base_page") AS is_base_page,  
-  getUpgradeHeader(payload) as upgrade,
   COUNT(*) AS num_requests
 FROM 
   `httparchive.requests.2019_07_01_*`
 WHERE
   url LIKE "http://%"
+  AND getUpgradeHeader(payload) LIKE "%h2%"
 GROUP BY
   client,
-  is_base_page,
-  upgrade
+  is_base_page
