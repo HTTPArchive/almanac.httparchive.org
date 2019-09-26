e8abe30a39646ce45de9f6d0d5f9d302d477327f
Paul Calvano <paulcalvano@yahoo.com>
2019-09-26 01:24:49 -0400

    Create 20_15.sql

---
+++ sql/2019/20_HTTP_2/20_15.sql
@@ -0,0 +1,41 @@
+#standardSQL
+# 20.15 - Measure number of TCP Connections per site.
+SELECT 
+  "mobile" AS client,
+  JSON_EXTRACT_SCALAR(payload, "$._protocol") AS protocol,
+  COUNT(*) AS num_pages,
+  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(50)] AS median,
+  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(75)] AS p75,
+  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(95)] AS p95
+FROM 
+  `httparchive.requests.2019_07_01_mobile` AS requests
+INNER JOIN
+  `httparchive.summary_pages.2019_07_01_mobile` AS summary
+ON
+  requests.url = summary.url
+WHERE
+  JSON_EXTRACT_SCALAR(payload, "$._is_base_page") = "true"
+GROUP BY 
+  client, 
+  protocol
+
+UNION ALL
+
+SELECT 
+  "desktop" AS client,
+  JSON_EXTRACT_SCALAR(payload, "$._protocol") AS protocol,
+  COUNT(*) AS num_pages,
+  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(50)] AS median,
+  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(75)] AS p75,
+  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(95)] AS p95
+FROM 
+  `httparchive.requests.2019_07_01_desktop` AS requests
+INNER JOIN
+  `httparchive.summary_pages.2019_07_01_desktop` AS summary
+ON
+  requests.url = summary.url
+WHERE
+  JSON_EXTRACT_SCALAR(payload, "$._is_base_page") = "true"
+GROUP BY 
+  client, 
+  protocol
