ff706715884fb84ad74065ce1e5c6e8fe6f0598c
Paul Calvano <paulcalvano@yahoo.com>
2019-09-25 23:42:40 -0400

    Create 20_07.sql

---
+++ sql/2019/20_HTTP_2/20_07.sql
@@ -0,0 +1,28 @@
+#standardSQL
+# 20.07 - % of sites affected by CDN prioritization issues (H2 and served by CDN)
+SELECT 
+  client,
+  IF(pages.cdn = "", "Not using CDN", pages.cdn) AS CDN,
+  IF(prioritization_status IS NOT null, prioritization_status, "Unknown") prioritizes_correctly,
+  COUNT(*) AS num_pages,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY client), 2) AS pct
+FROM 
+  (
+    SELECT 
+	    _TABLE_SUFFIX AS client,
+	    url,
+			JSON_EXTRACT_SCALAR(payload, "$._base_page_cdn") as cdn
+	  FROM 
+	    `httparchive.requests.2019_07_01_*` 
+	  WHERE 
+	  	JSON_EXTRACT_SCALAR(payload, "$._protocol") ="HTTP/2"
+      AND JSON_EXTRACT_SCALAR(payload, "$._is_base_page") = "TRUE"
+  ) AS pages
+LEFT JOIN 
+  `httparchive.almanac.h2_prioritization_cdns_201909` AS h2_pri
+ON pages.cdn = h2_pri.cdn
+GROUP BY
+  client,
+  CDN,
+  prioritizes_correctly
+ORDER BY num_pages DESC
