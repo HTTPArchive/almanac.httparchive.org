66ce09e8b17df10fae7437a135b2cb1fb3f21991
David Fox <david@davidjfox.com>
2019-10-02 21:59:00 -0500

    Changes to queries to add more context

--- sql/2019/12_Mobile_Web/12_15.sql
+++ sql/2019/12_Mobile_Web/12_15.sql
@@ -2,12 +2,12 @@
 
 # input attributes occurence defined set % (minus placeholder and required)
 
-CREATE TEMPORARY FUNCTION hasInputAttributes(payload STRING)
-RETURNS BOOL LANGUAGE js AS '''
+CREATE TEMPORARY FUNCTION getInputStats(payload STRING)
+RETURNS STRUCT<has_advanced_attributes BOOLEAN, total_inputs INT64> LANGUAGE js AS '''
   try {
     var $ = JSON.parse(payload);
     var almanac = JSON.parse($._almanac);
-    var found = almanac['input-elements'].findIndex(node => {
+    var found_index = almanac['input-elements'].findIndex(node => {
         var search = Object.keys(node).findIndex(attr => {
             if(attr.match(/(autocomplete|min|max|pattern|step)/i)) {
                 return true;
@@ -16,15 +16,28 @@ RETURNS BOOL LANGUAGE js AS '''
 
         return search >= 0 ? true : false;
     });
-    return found >= 0 ? true : false;
+
+    return {
+      has_advanced_attributes: found_index >= 0 ? true : false,
+      total_inputs: almanac['input-elements'].length
+    };
   } catch (e) {
-    return false;
+    return {
+      has_advanced_attributes: false,
+      total_inputs: 0
+    };
   }
 ''';
 
 SELECT
     COUNT(0) as count,
-    COUNTIF(hasInputAttributes(payload)) AS occurence,
-    ROUND(COUNTIF(hasInputAttributes(payload)) * 100 / SUM(COUNT(0)) OVER (), 2) AS occurence_perc
-FROM
+    COUNTIF(input_stats.total_inputs > 0) AS total_applicable,
+
+    COUNTIF(input_stats.has_advanced_attributes) AS total_pages_using,
+    ROUND(COUNTIF(input_stats.has_advanced_attributes) * 100 / COUNTIF(input_stats.total_inputs > 0), 2) AS occurence_perc
+FROM (
+  SELECT
+    getInputStats(payload) as input_stats
+  FROM
     `httparchive.pages.2019_07_01_mobile`
+)
