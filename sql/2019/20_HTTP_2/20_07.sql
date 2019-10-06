d2ec7a6a937915f82ffe6f43f74d40c61f1df37e
Paul Calvano <paulcalvano@yahoo.com>
2019-10-06 08:00:40 -0400

    Updated 20.07to use almanac tables

--- sql/2019/20_HTTP_2/20_07.sql
+++ sql/2019/20_HTTP_2/20_07.sql
@@ -9,14 +9,14 @@ SELECT
 FROM 
   (
     SELECT 
-      _TABLE_SUFFIX AS client,
+      client,
       url,
       JSON_EXTRACT_SCALAR(payload, "$._cdn_provider") as cdn
     FROM 
-      `httparchive.requests.2019_07_01_*` 
+      `httparchive.almanac.requests` 
     WHERE 
       JSON_EXTRACT_SCALAR(payload, "$._protocol") ="HTTP/2"
-      AND JSON_EXTRACT_SCALAR(payload, "$._is_base_page") = "true"
+      AND firstHtml
   ) AS pages
 LEFT JOIN 
   `httparchive.almanac.h2_prioritization_cdns_201909` AS h2_pri
