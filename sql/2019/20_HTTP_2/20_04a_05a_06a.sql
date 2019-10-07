1dcfb87d737dd61645cc5944e2220cbe1756e7cb
Paul Calvano <paulcalvano@yahoo.com>
2019-10-06 23:00:47 -0400

    Updated 20_04a_05a_06a to include https indicator

--- sql/2019/20_HTTP_2/20_04a_05a_06a.sql
+++ sql/2019/20_HTTP_2/20_04a_05a_06a.sql
@@ -19,6 +19,7 @@ SELECT
   client,
   firstHtml,  
   JSON_EXTRACT_SCALAR(payload, "$._protocol") AS protocol,
+  IF(url LIKE "https://%","https","http") AS http_or_https,
   getUpgradeHeader(payload) AS upgrade,
   COUNT(*) AS num_requests
 FROM 
@@ -27,4 +28,5 @@ GROUP BY
   client,
   firstHtml,
   protocol,
+  http_or_https,
   upgrade
