4b90c9dab71eef65204400ee756b8f64609b959a
Paul Calvano <paulcalvano@yahoo.com>
2019-10-06 22:27:55 -0400

    Updated 20.04a_05a_06a to use the almanac tables

--- sql/2019/20_HTTP_2/20_04a_05a_06a.sql
+++ sql/2019/20_HTTP_2/20_04a_05a_06a.sql
@@ -1,13 +1,13 @@
 #standardSQL
-# 20.04a_5a_6a - Detailed upgrade headers for 20.04, 20.05 and 20.06
-CREATE TEMPORARY FUNCTION getUpgradeHeader(payload STRING)
+# 20.04a_5a_6a - Detailed alt-svc headers
+CREATE TEMPORARY FUNCTION getAltSvcHeader(payload STRING)
 RETURNS STRING
 LANGUAGE js AS """
   try {
     var $ = JSON.parse(payload);
     var headers = $.response.headers;
     var st = headers.find(function(e) { 
-      return e['name'].toLowerCase() === 'upgrade'
+      return e['name'].toLowerCase() === 'alt-svc'
     });
     return st['value'];
   } catch (e) {
@@ -16,15 +16,15 @@ LANGUAGE js AS """
 """;
 
 SELECT 
-  _TABLE_SUFFIX AS client,
-  JSON_EXTRACT_SCALAR(payload, "$._is_base_page") AS is_base_page,  
+  client,
+  firstHtml,  
   JSON_EXTRACT_SCALAR(payload, "$._protocol") AS protocol,
-  getUpgradeHeader(payload) AS upgrade,
+  getAltSvcHeader(payload) AS altsvc,
   COUNT(*) AS num_requests
 FROM 
-  `httparchive.requests.2019_07_01_*`
+  `httparchive.almanac.requests` 
 GROUP BY
   client,
-  is_base_page,
+  firstHtml,
   protocol,
-  upgrade
+  altsvc
