5d4b09f5d7f7158c633d8685b8786830351c88d6
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-10-14 17:08:31 -0400

    show all scores

--- sql/2019/10_SEO/10_12.sql
+++ sql/2019/10_SEO/10_12.sql
@@ -1,11 +1,11 @@
 #standardSQL
 # 10_12: robots.txt
 SELECT
-  COUNTIF(robots_txt) AS freq,
-  COUNT(0) AS total,
-  ROUND(COUNTIF(robots_txt) * 100 / COUNT(0), 2) AS pct
-FROM (
-  SELECT
-    JSON_EXTRACT_SCALAR(report, '$.audits.robots-txt.score') = '1' AS robots_txt
-  FROM
-    `httparchive.lighthouse.2019_07_01_mobile`)
+  JSON_EXTRACT_SCALAR(report, '$.audits.robots-txt.score') AS score,
+  COUNT(0) AS freq,
+  SUM(COUNT(0)) OVER () AS total,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (), 2) AS pct
+FROM
+  `httparchive.lighthouse.2019_07_01_mobile`
+GROUP BY
+  score
