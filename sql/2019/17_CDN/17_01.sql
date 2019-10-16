90fc4e21a81fda603dc243916a54ea7695941d8f
Colin Bendell <colin@bendell.ca>
2019-10-16 16:41:20 -0400

    update 17_01, _02, _12 and _13

--- sql/2019/17_CDN/17_01.sql
+++ sql/2019/17_CDN/17_01.sql
@@ -2,16 +2,31 @@
 # 17_01: Top CDNs used on the root HTML pages
 SELECT
   client,
-  _cdn_provider AS cdn,
-  COUNT(0) AS freq,
-  SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  ROUND((COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY client)), 2) AS pct
+  cdn,
+  COUNTif(firstHtml) AS firstHtmlHits,
+  SUM(COUNTif(firstHtml)) OVER (PARTITION BY client) AS firstHtmlTotalHits,
+  ROUND((COUNTIF(firstHtml) * 100 / (0.001 + SUM(COUNTIF(firstHtml)) OVER (PARTITION BY client))), 2) AS firstHtmlHitsPct,
+  COUNTif(not firstHtml and not sameHost and sameDomain) AS subDomainHits,
+  SUM(COUNTif(not firstHtml and not sameHost and sameDomain)) OVER (PARTITION BY client) AS subDomainTotalHits,
+  ROUND((COUNTIF(not firstHtml and not sameHost and sameDomain) * 100 / (0.001 + SUM(COUNTIF(not firstHtml and not sameHost and sameDomain)) OVER (PARTITION BY client))), 2) AS subDomainHitsPct,
+  COUNTif(not firstHtml and not sameHost and not sameDomain) AS thirdPartyHits,
+  SUM(COUNTif(not firstHtml and not sameHost and not sameDomain)) OVER (PARTITION BY client) AS thirdPartyTotalHits,
+  ROUND((COUNTIF(not firstHtml and not sameHost and not sameDomain) * 100 / (0.001 + SUM(COUNTIF(not firstHtml and not sameHost and not sameDomain)) OVER (PARTITION BY client))), 2) AS thirdPartyHitsPct,
+  COUNT(0) AS hits,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS totalHits,
+  ROUND((COUNT(0) * 100 / (0.001 + SUM(COUNT(0)) OVER (PARTITION BY client))), 2) AS hitsPct
 FROM
-  `httparchive.almanac.requests`
-WHERE
-  firstHtml
+(
+  SELECT 
+      client, pageid, requestid, page, url, firstHtml, respBodySize,
+      REGEXP_EXTRACT(_cdn_provider, r'^([^,]*).*') cdn, # sometimes _cdn provider detection includes multiple entries. we bias for the DNS detected entry which is the first entry
+      if(NET.HOST(url) = NET.HOST(page), true, false) sameHost,
+      if(NET.HOST(url) = NET.HOST(page) OR NET.REG_DOMAIN(url) = NET.REG_DOMAIN(page), true, false) AS sameDomain # if toplevel reg_domain will return null so we group this as sameDomain
+    FROM `httparchive.almanac.requests`
+    GROUP BY client, pageid, requestid, page, url, firstHtml, cdn, respBodySize
+)
 GROUP BY
   client,
   cdn
 ORDER BY
-  freq / total DESC
+  firstHtmlHitsPct DESC
--- sql/2019/17_CDN/17_02.sql
+++ sql/2019/17_CDN/17_02.sql
@@ -1,18 +1,28 @@
 #standardSQL
 # 17_02: Percentage of the sites which use a CDN for any resource
 SELECT
-  client,
-  COUNTIF(cdncount > 0) AS freq,
-  COUNT(*) AS total,
-  ROUND(100 * (COUNTIF(cdncount > 0)/COUNT(*)), 2) AS pct
-FROM
-(
-  SELECT
-    _TABLE_SUFFIX AS client,
-    pageid AS pageid,
-    COUNTIF(_cdn_provider != '') AS cdncount
+    client,
+    firstHtml,
+    sameHost,
+    sameDomain,
+    not sameHost AND not sameDomain AS thirdParty,
+    COUNTIF(_cdn_provider != '') AS cdnHits,
+    COUNT(0) AS hits,
+    ROUND((COUNTIF(_cdn_provider != '') * 100) / count(0), 2) AS hitsPct,
+    sum(case when _cdn_provider != '' then respBodySize else 0 end) as cdnBytes,
+    sum(respBodySize) as bytes,
+    ROUND((sum(case when _cdn_provider != '' then respBodySize else 0 end) * 100) / sum(respBodySize), 2) AS bytesPct
   FROM
-    `httparchive.summary_requests.2019_07_01_*`
-  GROUP BY client, pageid
-)
-GROUP BY client
+    (
+      SELECT
+        client, pageid, requestid, page, url, firstHtml, _cdn_provider, respBodySize,
+        case when NET.HOST(url) = NET.HOST(page) then true else false end sameHost,
+        case when NET.HOST(url) = NET.HOST(page) OR NET.REG_DOMAIN(url) = NET.REG_DOMAIN(page) then true else false end sameDomain # if toplevel reg_domain will return null so we group this as sameDomain
+      FROM `httparchive.almanac.requests`
+      GROUP BY client, pageid, requestid, page, url, firstHtml, _cdn_provider, respBodySize
+    )
+  GROUP BY
+    client,
+    firstHtml,
+    sameHost,
+    sameDomain
