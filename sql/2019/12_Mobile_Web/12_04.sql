45a32afb156e9da4442625523a4d73717334a4ab
David Fox <david@davidjfox.com>
2019-10-28 20:51:43 -0500

    Small fix to zooming and scaling metric (#244)
    * Only look at values of 1 or less
    
    * Apply fix to 09_26 as well

--- sql/2019/12_Mobile_Web/12_04.sql
+++ sql/2019/12_Mobile_Web/12_04.sql
@@ -5,18 +5,18 @@ SELECT
   COUNT(0) AS total_sites,
   COUNTIF(has_meta_viewport) AS total_viewports,
   COUNTIF(not_scalable) AS total_no_scale,
-  COUNTIF(locked_max_scale) AS total_locked_max_scale,
-  COUNTIF(not_scalable OR locked_max_scale) AS total_either,
+  COUNTIF(max_scale_1_or_less IS TRUE) AS total_locked_max_scale,
+  COUNTIF(not_scalable OR (max_scale_1_or_less IS TRUE)) AS total_either,
 
   ROUND(COUNTIF(not_scalable) * 100 / COUNT(0), 2) AS perc_sites_no_scale,
-  ROUND(COUNTIF(locked_max_scale) * 100 / COUNT(0), 2) AS perc_sites_locked_max_scale,
-  ROUND(COUNTIF(not_scalable OR locked_max_scale) * 100 / COUNT(0), 2) AS perc_sites_either
+  ROUND(COUNTIF(max_scale_1_or_less IS TRUE) * 100 / COUNT(0), 2) AS perc_sites_locked_max_scale,
+  ROUND(COUNTIF(not_scalable OR (max_scale_1_or_less IS TRUE)) * 100 / COUNT(0), 2) AS perc_sites_either
 FROM (
   SELECT
     client,
     meta_viewport IS NOT NULL AS has_meta_viewport,
-    REGEXP_EXTRACT(meta_viewport, '(?i)user-scalable\\s*=\\s*(no|0)') IS NOT NULL AS not_scalable,
-    REGEXP_EXTRACT(meta_viewport, '(?i)maximum-scale\\s*=\\s*1') IS NOT NULL AS locked_max_scale
+    REGEXP_EXTRACT(meta_viewport, r'(?i)user-scalable\s*=\s*(no|0)') IS NOT NULL AS not_scalable,
+    SAFE_CAST(REGEXP_EXTRACT(meta_viewport, r'(?i)maximum-scale\s*=\s*([0-9]*\.[0-9]+|[0-9]+)') AS FLOAT64) <= 1 AS max_scale_1_or_less
   FROM (
     SELECT
       _TABLE_SUFFIX AS client,
