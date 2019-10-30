0539065c238d4f196d1b7e24e75c42d96e355703
Yvo Schaap <yvo@yvoschaap.com>
2019-10-30 10:05:43 +0100

    CSS magic

--- src/config.py
+++ src/config.py
@@ -20,11 +20,12 @@ def update_config():
     config_json['2019'] = json.load(config_file)
 
     for contributor_id, contributor in config_json['2019']['contributors'].items():
-      if 'gravatar' in contributor:
-        gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(contributor['gravatar'].lower().encode()).hexdigest() + '.jpg?'
-        gravatar_url += urllib.parse.urlencode({'d': 'mp','s':str(AVATAR_SIZE)})
-        contributor['avatar_url'] = gravatar_url
-      else:
-        contributor['avatar_url'] = DEFAULT_AVATAR_FOLDER_PATH + str(hash(contributor_id) % AVATARS_NUMBER) + '.jpg'
+      if 'avatar_url' not in contributor:
+        if 'gravatar' in contributor:
+          gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(contributor['gravatar'].lower().encode()).hexdigest() + '.jpg?'
+          gravatar_url += urllib.parse.urlencode({'d': 'mp','s':str(AVATAR_SIZE)})
+          contributor['avatar_url'] = gravatar_url
+        else:
+          contributor['avatar_url'] = DEFAULT_AVATAR_FOLDER_PATH + str(hash(contributor_id) % AVATARS_NUMBER) + '.jpg'
 
 update_config()
