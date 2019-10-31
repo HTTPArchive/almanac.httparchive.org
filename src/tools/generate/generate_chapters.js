6cc58a2dff6be15f5a94630da5ed309872b62d0b
Mike Geyser <mikegeyser@gmail.com>
2019-10-31 08:02:28 +0200

    Prevented the formatted from wrapping after 80 characters, and fixed some rendering issues in a11y.md

--- src/tools/generate/generate_chapters.js
+++ src/tools/generate/generate_chapters.js
@@ -54,7 +54,7 @@ const write_template = async (language, year, chapter, metadata, body, toc) => {
   const path = `templates/${language}/${year}/chapters/${chapter}.html`;
 
   let html = await ejs.renderFile(template, { metadata, body, toc });
-  let fomatted_html = prettier.format(html, { parser: 'html' });
+  let fomatted_html = prettier.format(html, { parser: 'html', printWidth: Number.MAX_SAFE_INTEGER });
 
   await fs.outputFile(path, fomatted_html, 'utf8');
 
