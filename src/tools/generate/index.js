213f2c3c6a8768ff960a39355768df6bc25a4f95
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-11-07 15:47:08 -0800

    Disable auto-generated last-modified (#366)
    #317

--- src/tools/generate/index.js
+++ src/tools/generate/index.js
@@ -2,7 +2,8 @@ let { generate_last_updated } = require('./generate_last_updated');
 let { generate_chapters } = require('./generate_chapters');
 
 (async () => {
-  await generate_last_updated();
+  // TODO: Only regenerate for changed contents.
+  //await generate_last_updated();
 
   // TODO: Generate visualisations
   await generate_chapters();
