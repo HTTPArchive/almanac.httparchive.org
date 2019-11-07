d3fe4ad195a1f9aafae5c94fcc84b3a30cc93544
Mike Geyser <mikegeyser@gmail.com>
2019-11-07 14:42:27 +0200

    Added a more specific regex to only add the loading attribute to images inside figures.

--- src/tools/generate/lazy_load_images.js
+++ src/tools/generate/lazy_load_images.js
@@ -1,7 +1,11 @@
-const lazy_load_images = (html) => {
-  const re = /<img\s/gi;
+// This regex is very aggressive, and may include images that are inside `pre`  tags.
+const all_images_regex = /<img\s/gi;
 
-  return html.replace(re, () => `<img loading="lazy"`);
+// This regex only matches images inside `figure` tags.
+const images_inside_figures = /<figure(?: [^<>]+)?>(?:(?:(?!<figure).)*)(<img)(?:(?:(?!<figure).)*)<\/figure>/gis;
+
+const lazy_load_images = (html) => {
+  return html.replace(images_inside_figures, `<img loading="lazy"`);
 };
 
 module.exports = {
