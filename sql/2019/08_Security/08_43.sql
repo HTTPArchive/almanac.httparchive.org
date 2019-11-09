44caed564d8a15a6fc61705191fb3f165104eccf
Paul Calvano <paulcalvano@yahoo.com>
2019-11-09 17:20:12 -0500

    Updated HTTP vs HTTPS sql

--- sql/2019/08_Security/08_43.sql
+++ sql/2019/08_Security/08_43.sql
@@ -1,11 +1,11 @@
 # 08_43: HTTP vs HTTPS
 SELECT
   client,
-  SUM(IF(url LIKE 'https://%', 1, 0))  AS https,
-  SUM(IF(url LIKE 'http://%', 1, 0))  AS http,
-  ROUND((SUM(IF(url LIKE 'https://%', 1, 0)) / COUNT(*))*100,2) AS pct_https,
-  ROUND((SUM(IF(url LIKE 'http://%', 1, 0)) / COUNT(*))*100,2) AS pct_http
+  COUNTIF(STARTS_WITH(url, 'https://'))  AS https,
+  COUNTIF(STARTS_WITH(url, 'http://'))  AS http,
+  ROUND((COUNTIF(STARTS_WITH(url, 'https://')) / COUNT(*))*100,2) AS pct_https,
+  ROUND((COUNTIF(STARTS_WITH(url, 'http://')) / COUNT(*))*100,2) AS pct_http
 FROM
-  `httparchive.almanac.requests` 
+  `httparchive.almanac.summary_requests`  
 GROUP BY
   client
