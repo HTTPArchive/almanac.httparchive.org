3d141e44e3aced41a950cb43961cf3b05b531ff2
Rick Viscomi <rviscomi@users.noreply.github.com>
2019-11-10 11:29:12 -0800

    revert

--- sql/2019/05_Third_Parties/05_11.sql
+++ sql/2019/05_Third_Parties/05_11.sql
@@ -1,48 +1,18 @@
 #standardSQL
-# Percentile breakdown page-relative percentage of requests that are third party requests broken down by third party category.
+# Percentage of requests that are third party requests broken down by third party category by resource type.
 SELECT
-  percentile,
   client,
-  COUNT(0) AS numberOfPages,
-  COUNTIF(numberOfThirdPartyRequests > 0) AS numberOfPagesWithThirdParty,
-  APPROX_QUANTILES(numberOfThirdPartyRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentThirdPartyRequestsQuantiles,
-  APPROX_QUANTILES(numberOfAdRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentAdRequestsQuantiles,
-  APPROX_QUANTILES(numberOfAnalyticsRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentAnalyticsRequestsQuantiles,
-  APPROX_QUANTILES(numberOfSocialRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentSocialRequestsQuantiles,
-  APPROX_QUANTILES(numberOfVideoRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentVideoRequestsQuantiles,
-  APPROX_QUANTILES(numberOfUtilityRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentUtilityRequestsQuantiles,
-  APPROX_QUANTILES(numberOfHostingRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentHostingRequestsQuantiles,
-  APPROX_QUANTILES(numberOfMarketingRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentMarketingRequestsQuantiles,
-  APPROX_QUANTILES(numberOfCustomerSuccessRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentCustomerSuccessRequestsQuantiles,
-  APPROX_QUANTILES(numberOfContentRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentContentRequestsQuantiles,
-  APPROX_QUANTILES(numberOfCdnRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentCdnRequestsQuantiles,
-  APPROX_QUANTILES(numberOfTagManagerRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentTagManagerRequestsQuantiles,
-  APPROX_QUANTILES(numberOfOtherRequests / numberOfRequests, 1000)[OFFSET(percentile * 10)] AS percentOtherRequestsQuantiles
+  thirdPartyCategory,
+  contentType,
+  COUNT(0) AS totalRequests,
+  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (), 4) AS percentRequests
 FROM (
   SELECT
-    client,
-    pageUrl,
-    COUNT(0) AS numberOfRequests,
-    COUNTIF(thirdPartyDomain IS NULL) AS numberOfFirstPartyRequests,
-    COUNTIF(thirdPartyDomain IS NOT NULL) AS numberOfThirdPartyRequests,
-    COUNTIF(thirdPartyCategory = 'ad') AS numberOfAdRequests,
-    COUNTIF(thirdPartyCategory = 'analytics') AS numberOfAnalyticsRequests,
-    COUNTIF(thirdPartyCategory = 'social') AS numberOfSocialRequests,
-    COUNTIF(thirdPartyCategory = 'video') AS numberOfVideoRequests,
-    COUNTIF(thirdPartyCategory = 'utility') AS numberOfUtilityRequests,
-    COUNTIF(thirdPartyCategory = 'hosting') AS numberOfHostingRequests,
-    COUNTIF(thirdPartyCategory = 'marketing') AS numberOfMarketingRequests,
-    COUNTIF(thirdPartyCategory = 'customer-success') AS numberOfCustomerSuccessRequests,
-    COUNTIF(thirdPartyCategory = 'content') AS numberOfContentRequests,
-    COUNTIF(thirdPartyCategory = 'cdn') AS numberOfCdnRequests,
-    COUNTIF(thirdPartyCategory = 'tag-manager') AS numberOfTagManagerRequests,
-    COUNTIF(thirdPartyCategory = 'other' OR thirdPartyCategory IS NULL) AS numberOfOtherRequests
-  FROM (
-    SELECT
       client,
-      page AS pageUrl,
-      DomainsOver50Table.requestDomain AS thirdPartyDomain,
-      ThirdPartyTable.category AS thirdPartyCategory
+      type AS contentType,
+      IFNULL(ThirdPartyTable.category,
+        IF(DomainsOver50Table.requestDomain IS NULL, 'first-party', 'other')
+      ) AS thirdPartyCategory
     FROM
       `httparchive.almanac.summary_requests`
     LEFT JOIN
@@ -51,15 +21,8 @@ FROM (
     LEFT JOIN
       `lighthouse-infrastructure.third_party_web.2019_07_01_all_observed_domains` AS DomainsOver50Table
     ON NET.HOST(url) = DomainsOver50Table.requestDomain
-  )
-  GROUP BY
-    client,
-    pageUrl
-),
-  UNNEST([10, 25, 50, 75, 90]) AS percentile
+)
 GROUP BY
-  percentile,
-  client
-ORDER BY
-  percentile,
-  client
+  client,
+  thirdPartyCategory,
+  contentType
