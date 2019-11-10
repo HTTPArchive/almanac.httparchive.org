0dae963cc01018c7d77e9fea829bce9d12f9ec15
Colin Bendell <colin@bendell.ca>
2019-11-10 08:15:59 -0500

    Updated s-max-age, vary and TLS+Protocol versions

--- sql/2019/17_CDN/17_10b.sql
+++ sql/2019/17_CDN/17_10b.sql
@@ -1,8 +1,18 @@
 #standardSQL
 # 17_10b: Common vary headers as seen from CDNs
 
-SELECT client, resp_vary, count(*) as total
-FROM `httparchive.almanac.requests`
-where _cdn_provider != ''
-group by client, resp_vary
-order by total desc
+SELECT
+  client, firstHtml, vary,
+  if(_cdn_provider != '', 'CDN', 'Origin') as source,
+  count(0) as total
+FROM
+  `httparchive.almanac.requests`,
+  UNNEST(split(REGEXP_REPLACE(REGEXP_REPLACE(lower(resp_vary), '\"', ''), '[, ]+|\\\\0', ','), ',')) as vary
+group by
+  client, firstHtml, vary, source
+having
+  vary <> '' and vary is not null
+order by
+  client desc,
+  firstHtml desc,
+  total desc
