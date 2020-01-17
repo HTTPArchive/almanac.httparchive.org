12314118d059964df0a508e3e4107a9edf023ad4
Barry <barry@tunetheweb.com>
2020-01-17 15:36:41 +0000

    Correct error where wrong date is used in sitemap for non-chapters

--- src/tools/generate/generate_sitemap.js
+++ src/tools/generate/generate_sitemap.js
@@ -52,7 +52,7 @@ const get_static_pages = async (sitemap_chapters) => {
   for (const loc of await files) {
     if (fs.existsSync(`templates/${loc}`)) {
       const file = await fs.readFile(`templates/${loc}`, 'utf-8');
-      const match = file.match(/"datePublished": "([0-9\-\+\:T]*)"/);
+      const match = file.match(/"dateModified": "([0-9\-\+\:T]*)"/);
       const lastmod = set_min_date(match[1]);
       const url = strip_html_extension(loc);
 
