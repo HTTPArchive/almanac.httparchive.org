5ac7c7a0a9da48bcca761b712d7a0b8922357264
Barry <barry@tunetheweb.com>
2020-04-06 01:26:57 +0100

    Better way to set non-live years

--- src/config.py
+++ src/config.py
@@ -7,8 +7,6 @@ from language import Language, DEFAULT_LANGUAGE
 
 SUPPORTED_YEARS = []
 DEFAULT_YEAR = '2019'
-#Use the following variable to temporarily exclude years until they are ready to be published
-EXCLUDED_YEARS = ['2020']
 
 config_json = {}
 
@@ -25,29 +23,38 @@ def get_config(year):
 
 
 def get_entries_from_json(json_config, p_key, s_key):
-    entries = []
-    if p_key in json_config:
-        for values in json_config.get(p_key):
-             entries.append(values.get(s_key))
-    return entries
+  entries = []
+  if p_key in json_config:
+    for values in json_config.get(p_key):
+      entries.append(values.get(s_key))
+  return entries
 
 
 def get_chapters(json_config):
-    chapters = []
-    data = get_entries_from_json(json_config,'outline','chapters')
-    for list in data:
-        for entry in list:
-            chapters.append(entry.get('slug'))
-    return chapters
+  chapters = []
+  data = get_entries_from_json(json_config,'outline','chapters')
+  for list in data:
+    for entry in list:
+      chapters.append(entry.get('slug'))
+  return chapters
 
 
 def get_languages(json_config):
-    languages = []
-    data = get_entries_from_json(json_config,'settings','supported_languages')
-    for list in data:
-        for entry in list:
-            languages.append(getattr(Language,entry))
-    return languages
+  languages = []
+  data = get_entries_from_json(json_config,'settings','supported_languages')
+  for list in data:
+    for entry in list:
+      languages.append(getattr(Language,entry))
+  return languages
+
+
+def get_live(json_config):
+  isLive = False
+  data = get_entries_from_json(json_config,'settings','isLive')
+  for list in data:
+    if list == True:
+      isLive = True      
+  return isLive
 
 
 def update_config():
@@ -56,29 +63,31 @@ def update_config():
   global SUPPORTED_LANGUAGES
   global config_json
 
+  config_files = []
+
   for root, directories, files in os.walk('config'):
     for file in files:
       if '.json' in file:
-        year = file[0:4]
-        if year not in EXCLUDED_YEARS:
-          SUPPORTED_YEARS.append(year)
+        config_files.append(file[0:4])
 
-  for year in SUPPORTED_YEARS:
+  for year in config_files:
     config_filename = 'config/%s.json' % year
     with open(config_filename, 'r') as config_file:
       json_config = json.load(config_file)
       config_json[year] = json_config
 
-      SUPPORTED_LANGUAGES.update({year : get_languages(json_config)})
-      SUPPORTED_CHAPTERS.update({year : set(get_chapters(json_config))})
+      if get_live(json_config):
+        SUPPORTED_YEARS.append(year)
+        SUPPORTED_LANGUAGES.update({year : get_languages(json_config)})
+        SUPPORTED_CHAPTERS.update({year : set(get_chapters(json_config))})
 
-      for contributor_id, contributor in json_config['contributors'].items():
-        if 'avatar_url' not in contributor:
-          if 'gravatar' in contributor:
-            gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(contributor['gravatar'].lower().encode()).hexdigest() + '.jpg?'
-            gravatar_url += urllib.parse.urlencode({'d': 'mp','s':str(AVATAR_SIZE)})
-            contributor['avatar_url'] = gravatar_url
-          else:
-            contributor['avatar_url'] = DEFAULT_AVATAR_FOLDER_PATH + str(hash(contributor_id) % AVATARS_NUMBER) + '.jpg'
-        
+        for contributor_id, contributor in json_config['contributors'].items():
+          if 'avatar_url' not in contributor:
+            if 'gravatar' in contributor:
+              gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(contributor['gravatar'].lower().encode()).hexdigest() + '.jpg?'
+              gravatar_url += urllib.parse.urlencode({'d': 'mp','s':str(AVATAR_SIZE)})
+              contributor['avatar_url'] = gravatar_url
+            else:
+              contributor['avatar_url'] = DEFAULT_AVATAR_FOLDER_PATH + str(hash(contributor_id) % AVATARS_NUMBER) + '.jpg'
+              
 update_config()
