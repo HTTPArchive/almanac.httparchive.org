1588038c270219a79fad1807e8dc0cca53e1931b
Barry <barry@tunetheweb.com>
2020-04-18 20:16:14 +0100

    Spacing fixes

--- src/tools/generate/generate_figure_ids.js
+++ src/tools/generate/generate_figure_ids.js
@@ -7,8 +7,8 @@ const generate_figure_ids = (html) => {
   //Some of our 2019 chapters (markdown and performance) had different fig ids
   const figcaption_regex = /<figcaption(.*?)>(.*?)([0-9]+)\./gi;
   if (html.includes('<figure id="fig1"')) {
-   return html.replace(figcaption_regex, "<figcaption$1><a href=\"#fig$3\" class=\"anchor-link\">$2 $3.</a>");
-  }else {
+    return html.replace(figcaption_regex, "<figcaption$1><a href=\"#fig$3\" class=\"anchor-link\">$2 $3.</a>");
+  } else {
     return html.replace(figcaption_regex, "<figcaption$1><a href=\"#fig-$3\" class=\"anchor-link\">$2 $3.</a>");
   }
 };
