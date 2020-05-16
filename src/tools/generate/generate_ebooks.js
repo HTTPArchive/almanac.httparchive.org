89814de0f68ea39a13eca0a02bb70415e72bc595
Barry <barry@tunetheweb.com>
2020-05-16 08:34:31 +0100

    Add Prince Support (inc fixing figure aria labels)

--- src/tools/generate/generate_ebooks.js
+++ src/tools/generate/generate_ebooks.js
@@ -1,4 +1,4 @@
-const fs = require('fs-extra');
+  const fs = require('fs-extra');
 const ejs = require('ejs');
 const prettier = require('prettier');
 
@@ -13,6 +13,10 @@ const update_links = (chapter) => {
   let body = chapter.body;
   // Replace current chapter links to full anchor link (e.g. #introduction -> #javascript-introduction)
   body = body.replace(/href="#/g,'href="#' + chapter.metadata.chapter + '-');
+  // Replace aria-labelledby ids to full id (e.g. aria-labelledby="fig-1..." -> aria-labelledby="javascript-fig-1...")
+  body = body.replace(/aria-labelledby="fig([0-9_-])/g,'aria-labelledby="' + chapter.metadata.chapter + '-fig$1');
+  // Replace aria-describedby ids to full id (e.g. aria-describedby="fig-1" -> aria-describedby="javascript-fig-1")
+  body = body.replace(/aria-describedby="fig([0-9_-])/g,'aria-describedby="' + chapter.metadata.chapter + '-fig$1');
   // Replace current chapter fig ids to full id (e.g. id="fig-1" -> id="javascript-fig-1")
   body = body.replace(/id="fig([0-9_-])/g,'id="' + chapter.metadata.chapter + '-fig$1');
   // Replace current chapter  header ids to full id (e.g. <h2 id="introduction"> -> <h2 id="javascript-introduction">)
@@ -23,6 +27,8 @@ const update_links = (chapter) => {
   body = body.replace(/<a href=".\//g,'<a href="#');
   // Replace figure image links to full site, to avoid 127.0.0.1:8080 links
   body = body.replace(/<a href="\/static\/images/g,'<a href="https://almanac.httparchive.org/static/images');
+  // Remove lazy-loading attributes
+  body = body.replace(/ loading="lazy"/g,'');
   return body;
 }
 
