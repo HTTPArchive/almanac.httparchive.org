9fe572932f38971ac5bd3f831a42bd4a1b0e9940
Barry Pollard <barry_pollard@hotmail.com>
2020-05-20 17:29:19 +0100

    Switch eBook PDF generation from WeasyPrint to Prince (#833)
    * Move to EJS templates for consistency and to fix missing author pictures
    
    * Add Prince Support (inc fixing figure aria labels)
    
    * Add YouTube fallback
    
    * Fix table in Japanese chapter
    
    * move from WeasyPrint to Prince
    
    * Update README.md
    
    * Remove unnecessary change
    
    * Update statement for PDF accessibility
    
    * Regen PDFs
    
    * Fix typos
    
    * Fix spacing
    
    * Review feedback
    
    * Review feedback
    
    * Review feedback
    
    * Misc fixes
    
    * Make contributors line up
    
    * Fix text wrapping issue in Methodology section
    
    * Prince supports SVG social media icons
    
    * Add print support in case we ever want to print a physical copy
    
    * Remove link underlines in print mode
    
    * Add footnotes and left/right pages
    
    * Fix typos
    
    * templates/base/2019/base_ebook.html
    
    * Avoid hanging headers
    
    * Review feedback

--- src/tools/generate/generate_ebooks.js
+++ src/tools/generate/generate_ebooks.js
@@ -13,16 +13,24 @@ const update_links = (chapter) => {
   let body = chapter.body;
   // Replace current chapter links to full anchor link (e.g. #introduction -> #javascript-introduction)
   body = body.replace(/href="#/g,'href="#' + chapter.metadata.chapter + '-');
+  // Replace aria-labelledby ids to full id (e.g. aria-labelledby="fig-1..." -> aria-labelledby="javascript-fig-1...")
+  body = body.replace(/aria-labelledby="fig([0-9_-])/g,'aria-labelledby="' + chapter.metadata.chapter + '-fig$1');
+  // Replace aria-describedby ids to full id (e.g. aria-describedby="fig-1" -> aria-describedby="javascript-fig-1")
+  body = body.replace(/aria-describedby="fig([0-9_-])/g,'aria-describedby="' + chapter.metadata.chapter + '-fig$1');
   // Replace current chapter fig ids to full id (e.g. id="fig-1" -> id="javascript-fig-1")
   body = body.replace(/id="fig([0-9_-])/g,'id="' + chapter.metadata.chapter + '-fig$1');
-  // Replace current chapter  header ids to full id (e.g. <h2 id="introduction"> -> <h2 id="javascript-introduction">)
-  body = body.replace(/<h([0-9]) id="/g,'<h$1 id="' + chapter.metadata.chapter + '-');
+  // Replace current chapter header ids to full id (e.g. <h2 id="introduction"> -> <h2 id="javascript-introduction">)
+  body = body.replace(/<h([0-6]) id="/g,'<h$1 id="' + chapter.metadata.chapter + '-');
   // Replace other chapter references with hash to anchor link (e.g. ./javascript#fig-1 -> #javascript-fig-1)
   body = body.replace(/<a href=".\/([a-z0-9-]+)#/g,'<a href="#$1-');
   // Replace other chapter references to anchor link (e.g. ./javascript -> #javascript)
   body = body.replace(/<a href=".\//g,'<a href="#');
+  // For external links add footnote span
+  body = body.replace(/href="(http.*?)"(.*?)>(.*?)<\/a>/g,'href="$1"$2>$3<span class="fn">$1</span></a>');
   // Replace figure image links to full site, to avoid 127.0.0.1:8080 links
   body = body.replace(/<a href="\/static\/images/g,'<a href="https://almanac.httparchive.org/static/images');
+  // Remove lazy-loading attributes
+  body = body.replace(/ loading="lazy"/g,'');
   return body;
 }
 
