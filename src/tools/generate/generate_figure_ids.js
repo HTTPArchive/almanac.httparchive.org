4cecff683d234bbfa0ae239f54eda8c29462fe8e
Barry <barry@tunetheweb.com>
2020-05-21 15:20:38 +0100

    Add "Show Description" button to figures

--- src/tools/generate/generate_figure_ids.js
+++ src/tools/generate/generate_figure_ids.js
@@ -4,6 +4,9 @@ const generate_figure_ids = (html) => {
   let i = 1;
   html = html.replace(re, () => `<figure id='fig-${i++}'>`);
 
+  //Add the show description button
+  html = html.replace(/<div id="(fig[0-9]+-description)"/gi, '<button id="$1-button" hidden class="fig-description-button" aria-expanded="false" aria-controls="$1" data-link-id="$1" data-show-text="{{ self.show_description() }}" data-hide-text="{{ self.hide_description() }}">{{ self.show_description() }}</button><div id="$1"');
+
   //Some of our 2019 chapters (markdown and performance) had different fig ids
   const figcaption_regex = /<figcaption(.*?)>(.*?)([0-9]+)\./gi;
   if (html.includes('<figure id="fig1"')) {
