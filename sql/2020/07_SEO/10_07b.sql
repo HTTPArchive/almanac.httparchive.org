cfb83d746629551b91f817ee39cfb509d4ee891c
Tony McCreath <tony@websiteadvantage.com.au>
2020-07-19 17:56:36 +0930

    Copying over scripts from 2019

---
+++ sql/2020/07_SEO/10_07b.sql
@@ -0,0 +1,21 @@
+#standardSQL
+# 10_07b: <title> length
+SELECT
+  percentile,
+  client,
+  APPROX_QUANTILES(LENGTH(title), 1000)[OFFSET(percentile * 10)] AS title_length
+FROM (
+  SELECT
+    client,
+    REGEXP_EXTRACT(body, '(?i)<title>([^(</title>)]*)</title>') AS title
+  FROM
+    `httparchive.almanac.summary_response_bodies`
+  WHERE
+    firstHtml),
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
+GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
+  client
---
+++ sql/2020/07_SEO/10_09b.sql
@@ -0,0 +1,11 @@
+#standardSQL
+# 10_09b: image alt attribute usage
+SELECT
+  COUNTIF(img_alt) AS freq,
+  COUNT(0) AS total,
+  ROUND(COUNTIF(img_alt) * 100 / COUNT(0), 2) AS pct
+FROM (
+  SELECT
+    JSON_EXTRACT_SCALAR(report, '$.audits.image-alt.score') = '1' AS img_alt
+  FROM
+    `httparchive.lighthouse.2019_07_01_mobile`)
