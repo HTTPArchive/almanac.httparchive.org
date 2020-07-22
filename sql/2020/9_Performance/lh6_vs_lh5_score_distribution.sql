8e949842cd56f924f8d18360ff6a1e1a23033136
dooman87@gmail.com <Dmitry Pokidov>
2020-07-23 07:15:54 +1000

    Delta for LH5 and LH6 performance scores

--- sql/2020/9_Performance/lh6_vs_lh5_score_distribution.sql
+++ sql/2020/9_Performance/lh6_vs_lh5_score_distribution.sql
@@ -1,9 +1,21 @@
 #standardSQL
 # LH5 vs LH6 score. WIP
 
-# Performance Score from LH5 and LH6
-SELECT lh6.url AS url,
-  JSON_EXTRACT(lh6.report, '$.categories.performance.score') AS perf_score_lh6,
-  JSON_EXTRACT(lh5.report, '$.categories.performance.score') AS perf_score_lh5,
-  FROM `httparchive.sample_data.lighthouse_mobile_10k` lh6
-  JOIN `httparchive.scratchspace.2020_03_01_lighthouse_mobile_10k` lh5 ON lh5.url=lh6.url
+# Calculates minimum, maximum and average delta between LH5 and LH6 performance score
+SELECT MIN(perf_score_delta) AS min_delta, MAX(perf_score_delta) AS max_delta, AVG(perf_score_delta) AS avg_delta
+FROM
+(
+  SELECT
+    url,
+    perf_score_lh6,
+    perf_score_lh5,
+    (perf_score_lh6 - perf_score_lh5) as perf_score_delta
+  FROM
+  (
+    SELECT lh6.url AS url,
+      CAST(JSON_EXTRACT(lh6.report, '$.categories.performance.score') AS NUMERIC) AS perf_score_lh6,
+      CAST(JSON_EXTRACT(lh5.report, '$.categories.performance.score') AS NUMERIC) AS perf_score_lh5,
+      FROM `httparchive.sample_data.lighthouse_mobile_10k` lh6
+      JOIN `httparchive.scratchspace.2020_03_01_lighthouse_mobile_10k` lh5 ON lh5.url=lh6.url
+  )
+)
