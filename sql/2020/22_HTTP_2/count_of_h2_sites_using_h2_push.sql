6a583a46f82c00a513ae915098af9f0e2e84d495
Greg Wolf <greg@hilltoptech.net>
2020-07-23 10:33:05 -0400

    2020 SQL
    Updated 2019 SQL using sample_data

---
+++ sql/2020/22_HTTP_2/count_of_h2_sites_using_h2_push.sql
@@ -0,0 +1,19 @@
+#standardSQL
+# 22_HTTP_2 - Count of HTTP/2 Sites using HTTP/2 Push
+SELECT 
+  client,
+  COUNT(DISTINCT page) AS num_pages
+FROM (
+
+  SELECT 
+    client,
+    page
+  FROM 
+    `httparchive.sample_data.requests` 
+  WHERE 
+    JSON_EXTRACT_SCALAR(payload, "$._protocol") = "HTTP/2"
+    AND 
+    JSON_EXTRACT_SCALAR(payload, "$._was_pushed") = "1"
+)
+GROUP BY
+  client
