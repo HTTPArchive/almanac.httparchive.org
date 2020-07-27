eba93aa042144bce3ca04e8d3b92a3694d7b1d43
Max Ostapenko <bvz2001@gmail.com>
2020-07-27 04:46:47 +0100

    percent of pages with third party

---
+++ sql/2020/06_Third_Parties/percent_of_pages_with_third_party.sql
@@ -0,0 +1,56 @@
+#standardSQL
+# Percent of pages that include third party resources.
+
+WITH
+  requests AS (
+  SELECT
+    'desktop' AS client,
+    page,
+    url
+  FROM
+    `httparchive.requests.2020_06_01_desktop`
+  UNION ALL
+  SELECT
+    'mobile' AS client,
+    page,
+    url
+  FROM
+    `httparchive.requests.2020_06_01_mobile` ),
+  third_party AS (
+  SELECT
+    domain
+  FROM
+    `lighthouse-infrastructure.third_party_web.2020_05_01`),
+  base AS (
+  SELECT
+    requests.client,
+    requests.page,
+    third_party.domain
+  FROM
+    requests
+  LEFT JOIN
+    third_party
+  ON
+    NET.HOST(requests.url) = NET.HOST(third_party.domain)
+  GROUP BY
+    client,
+    page,
+    domain)
+
+SELECT
+  client,
+  COUNT(0) AS total_pages,
+  COUNTIF(third_party > 0) AS pages_third_party,
+  COUNTIF(third_party > 0) / COUNT(0) AS pct_pages_third_party
+FROM (
+  SELECT
+    client,
+    page,
+    COUNTIF(domain IS NOT NULL) AS third_party
+  FROM
+    base
+  GROUP BY
+    client,
+    page )
+GROUP BY
+  client
