a5cf826d538c8545119b5718d39f3680372e58e2
Max Ostapenko <bvz2001@gmail.com>
2020-07-27 01:19:20 +0100

    performance observer

---
+++ sql/2020/09_Performance/performance_observer.sql
@@ -0,0 +1,22 @@
+#standardSQL
+# Percent of pages using Performance observer
+  
+SELECT
+  client,
+  COUNT(0) AS total_pages,
+  COUNTIF(performance_pages > 0) AS performance_pages,
+  COUNTIF(performance_pages > 0) / COUNT(0) AS pct_performance_pages,
+FROM (
+  SELECT
+    client,
+    COUNTIF(body LIKE '%new PerformanceObserver%') AS performance_pages,
+  FROM
+    `httparchive.almanac.summary_response_bodies`
+  WHERE
+    date = "2019-07-01"
+    AND type = "script"
+  GROUP BY
+    client,
+    page)
+GROUP BY
+  client
