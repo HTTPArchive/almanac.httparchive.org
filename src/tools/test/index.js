a2d99fe10587ffcd49e6478ef7e21bc2398b2967
Barry <barry@tunetheweb.com>
2020-07-30 03:15:19 +0100

    Remove old comment

--- src/tools/test/index.js
+++ src/tools/test/index.js
@@ -1,9 +1,5 @@
 let { test_status_codes } = require('./test_status_codes');
 
 (async () => {
-  // Can uncomment this to get latest timestamps from origin:main
-  // let { generate_last_updated } = require('./generate_last_updated');
-  // await generate_last_updated();
-
   await test_status_codes();
 })();
