6d61c8618076031c86308d44ddc1b9d1ae20496f
Barry <barry@tunetheweb.com>
2020-08-06 18:16:18 +0100

    Stop repeating commands for Mac

--- src/tools/scripts/update_timestamps_versions_on_commit.sh
+++ src/tools/scripts/update_timestamps_versions_on_commit.sh
@@ -29,7 +29,9 @@ fi
 
 if [ $(uname) = 'Darwin' ]; then
   echo "Running MacOS"
-  MACOS=true
+  SED_ARGS = "-i '' -E"
+else
+  SED_ARGS = "-E"
 fi
 
 NEW_LONG_DATE=`date +%Y%m%d%H%M%S`
@@ -46,11 +48,7 @@ function update_versions {
   do
     CHANGED_FILE=`basename ${CHANGED_FILE}`
     echo "Updating ${CHANGED_FILE} version number to ${NEW_LONG_DATE}"
-    if [ "${MACOS}" = true ]; then
-      sed -i '' -E s/${CHANGED_FILE}\?v=[0-9]+/${CHANGED_FILE}\?v=${NEW_LONG_DATE}/ templates/base/*/*.html templates/base.html
-    else
-      sed -i -r s/${CHANGED_FILE}\?v=[0-9]+/${CHANGED_FILE}\?v=${NEW_LONG_DATE}/ templates/base/*/*.html templates/base.html
-    fi
+    sed ${SED_ARGS} s/${CHANGED_FILE}\?v=[0-9]+/${CHANGED_FILE}\?v=${NEW_LONG_DATE}/ templates/base/*/*.html templates/base.html
   done
 }
 
@@ -64,17 +62,9 @@ function update_timestamp {
     if [[ "${CHANGED_FILE}" =~ ${FILE_PATTERN} ]]; then
       echo "Updating ${CHANGED_FILE} timestamp to ${NEW_SHORT_DATE}:"
       if [ "${DIRECTORY}" = "content" ]; then
-        if [ "${MACOS}" = true ]; then
-          sed -i '' -E "s/^last_updated: [0-9-]+T/last_updated: ${NEW_SHORT_DATE}T/" ../${CHANGED_FILE}
-        else
-          sed -i -r "s/^last_updated: [0-9-]+T/last_updated: ${NEW_SHORT_DATE}T/" ../${CHANGED_FILE}
-        fi
+        sed ${SED_ARGS} "s/^last_updated: [0-9-]+T/last_updated: ${NEW_SHORT_DATE}T/" ../${CHANGED_FILE}
       else
-        if [ "${MACOS}" = true ]; then
-          sed -i '' -E "s/block date_modified %}[0-9-]+T/block date_modified %}${NEW_SHORT_DATE}T/" ../${CHANGED_FILE}
-        else
-          sed -i "s/block date_modified %}[0-9-]+T/block date_modified %}${NEW_SHORT_DATE}T/" ../${CHANGED_FILE}
-        fi
+        sed ${SED_ARGS} -E "s/block date_modified %}[0-9-]+T/block date_modified %}${NEW_SHORT_DATE}T/" ../${CHANGED_FILE}
       fi
     fi
   done
