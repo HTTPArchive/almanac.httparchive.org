4e056e0a9c11f9526f0e0931401521892ecd0700
Barry Pollard <barry_pollard@hotmail.com>
2020-08-07 15:58:25 +0100

    Add Bash linter (#1177)
    * Add Bash linter
    
    * Turn off echoing of commands

--- src/tools/scripts/run_and_test_website.sh
+++ src/tools/scripts/run_and_test_website.sh
@@ -14,8 +14,6 @@
 
 # exit when any command fails instead of trying to continue on
 set -e
-# echo commands to screen for debugging
-set -x
 
 # This script must be run from src directory
 if [ -d "src" ]; then
@@ -36,8 +34,7 @@ npm run generate
 echo "Starting website"
 python main.py background &
 # Check website is running as won't have got feedback as backgrounded
-# use [p]ython so we don't match the grep itself
-ps -ef | grep "[p]ython main.py"
+pgrep -f "python main.py"
 
 echo "Testing website"
 npm run test
