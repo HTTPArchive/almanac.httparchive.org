078c50322575ac1cf7f6fe44d8534c8a007e097a
Dmitry Pokidov <dooman87@gmail.com>
2020-08-09 22:05:54 +1000

    Distribution of LH5 and LH6 permormance scores on mobile

---
+++ sql/2020/09_Performance/lh6_performance_score_distribution.sql
@@ -0,0 +1,11 @@
+SELECT
+  SAFE_DIVIDE(slow,    slow + avg + fast + hundred) slow,
+  SAFE_DIVIDE(avg,     slow + avg + fast + hundred) avg,
+  SAFE_DIVIDE(fast,    slow + avg + fast + hundred) fast,
+  SAFE_DIVIDE(hundred, slow + avg + fast + hundred) hundred,
+FROM (
+  SELECT COUNTIF(score < 0.3) slow, COUNTIF(score > 0.3 and score < 0.8) avg, COUNTIF(score > 0.8 and score < 1.0) fast, COUNTIF(score = 1.0) hundred
+  FROM (
+    SELECT CAST(JSON_EXTRACT(report, '$.categories.performance.score') AS NUMERIC) score FROM `httparchive.sample_data.lighthouse_mobile_10k`
+  )
+)
