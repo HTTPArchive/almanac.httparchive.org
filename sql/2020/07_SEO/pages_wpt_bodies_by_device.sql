4ab06fb64e7ae745b6460aef2406fee6db0f825e
antoineeripret <antoine.eripret@gmail.com>
2020-08-17 19:08:26 +0200

    Update pages_wpt_bodies_by_device.sql

--- sql/2020/07_SEO/pages_wpt_bodies_by_device.sql
+++ sql/2020/07_SEO/pages_wpt_bodies_by_device.sql
@@ -9,10 +9,28 @@ CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS
 # returns all the data we need from _wpt_bodies
 CREATE TEMPORARY FUNCTION get_wpt_bodies_info(wpt_bodies_string STRING)
 RETURNS STRUCT<
+
+  ##Tony  
   robots_has_robots_meta_tag BOOL,
   robots_has_x_robots_tag BOOL,
-  title_words INT64
+  title_words INT64, 
+
+  ##Antoine
+  n_titles INT64,
+  n_meta_descriptions INT64, 
+  n_h1 INT64, 
+  n_h2 INT64, 
+  n_h3 INT64, 
+  n_h4 INT64, 
+  n_non_empty_h1 INT64, 
+  n_non_empty_h2 INT64, 
+  n_non_empty_h3 INT64, 
+  n_non_empty_h4 INT64, 
+  has_same_h1_title BOOL
+
 # add more properties here...
+
+
 > LANGUAGE js AS '''
 var result = {};
 try {
@@ -107,6 +125,10 @@ try {
       }
     }; 
 
+
+
+
+
     if (Array.isArray(wpt_bodies) || typeof wpt_bodies != 'object') return result;
 
     if (wpt_bodies.robots) {
@@ -116,8 +138,69 @@ try {
 
     if (wpt_bodies.title) {
       if (wpt_bodies.title.rendered) {
+
+        //Number of words in the title tag
         result.title_words = wpt_bodies.title.rendered.primary.words;
+
+        //If the webpage has a title
+        result.n_titles = wpt_bodies.title.rendered.total
+
       }
+    }
+
+
+    if (wpt_bodies.meta_description) {
+      if (wpt_bodies.meta_descripton.rendered) {
+
+
+        //If the webpage has a meta description
+        result.n_meta_descriptions = wpt_bodies.meta_description.rendered.total;
+
+
+      }
+    }
+
+
+    if (wpt_bodies.headings) {
+      if (wpt_bodies.headings.rendered) {
+
+
+        //If the webpage has h1
+        result.n_h1 = wpt_bodies.headings.rendered.h1.total;
+
+
+        //If the webpage has h2
+        result.n_h2 = wpt_bodies.headings.rendered.h2.total;
+
+
+        //If the webpage has h3
+        result.n_h3 = wpt_bodies.headings.rendered.h3.total;
+
+
+        //If the webpage has h4
+        result.n_h4 = wpt_bodies.headings.rendered.h4.total;
+ 
+
+        //If the webpage has a non empty h1
+        result.n_non_empty_h1 = wpt_bodies.headings.rendered.h1.non_empty_total;
+ 
+        //If the webpage has a non empty h2
+        result.n_non_empty_h2 = wpt_bodies.headings.rendered.h2.non_empty_total;
+
+        //If the webpage has a non empty h3
+        result.n_non_empty_h3 = wpt_bodies.headings.rendered.h3.non_empty_total;
+
+        //If the webpage has a non empty h4
+        result.n_non_empty_h4 = wpt_bodies.headings.rendered.h4.non_empty_total;
+
+
+        //If h1 and title tag are the same
+        result.has_same_h1_title = wpt_bodies.headings.rendered.primary.matches_title;
+
+      }
+    }
+
+
     }
 
     // add more code to set result properties here...
@@ -129,7 +212,8 @@ return result;
 SELECT
   client,
   COUNT(0) AS total,
-
+  
+  ## Tony
   # Meta Robots inclusion
   # COUNTIF(wpt_bodies_info.robots_has_robots_meta_tag) AS freq_has_meta_robots,
   AS_PERCENT(COUNTIF(wpt_bodies_info.robots_has_robots_meta_tag), COUNT(0)) AS pct_has_meta_robots,
@@ -141,6 +225,52 @@ SELECT
   # titles with less than 5 words
   # COUNTIF(wpt_bodies_info.title_words < 5) AS freq_robots_has_x_robots_tag,
   AS_PERCENT(COUNTIF(wpt_bodies_info.title_words < 5), COUNT(0)) AS pct_short_title_tag,
+
+  ##Antoine
+  # meta title inclusion
+  # COUNTIF(wpt_bodies_info.n_titles < 0) AS freq_has_meta_title,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_titles < 0), COUNT(0)) AS pct_has_meta_title,
+
+  # meta description inclusion
+  # COUNTIF(wpt_bodies_info.n_meta_descriptions < 0) AS freq_has_meta_description,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_meta_descriptions < 0), COUNT(0)) AS pct_has_meta_description,
+
+  # H1 inclusion
+  # COUNTIF(wpt_bodies_info.n_h1 < 0) AS freq_has_h1,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_h1 < 0), COUNT(0)) AS pct_has_h1,
+
+  # H2 inclusion
+  # COUNTIF(wpt_bodies_info.n_h2 < 0) AS freq_has_h2,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_h2 < 0), COUNT(0)) AS pct_has_h2,
+
+  # H3 inclusion
+  # COUNTIF(wpt_bodies_info.n_h3 < 0) AS freq_has_h3,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_h3 < 0), COUNT(0)) AS pct_has_h3,
+
+  # H4 inclusion
+  # COUNTIF(wpt_bodies_info.n_h4 < 0) AS freq_has_h4,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_h4 < 0), COUNT(0)) AS pct_has_h4,
+
+  # Non-empty H1 inclusion
+  # COUNTIF(wpt_bodies_info.n_non_empty_h1 < 0) AS freq_has_non_empty_h1,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_non_empty_h1 < 0), COUNT(0)) AS pct_has_non_empty_h1,
+
+  # Non-empty H2 inclusion
+  # COUNTIF(wpt_bodies_info.n_non_empty_h2 < 0) AS freq_has_non_empty_h2,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_non_empty_h2 < 0), COUNT(0)) AS pct_has_non_empty_h2,
+
+  # Non-empty H3 inclusion
+  # COUNTIF(wpt_bodies_info.n_non_empty_h3 < 0) AS freq_has_non_empty_h3,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_non_empty_h3 < 0), COUNT(0)) AS pct_has_non_empty_h3,
+
+  # Non-empty H4 inclusion
+  # COUNTIF(wpt_bodies_info.n_non_empty_h4 < 0) AS freq_has_non_empty_h4,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.n_non_empty_h4 < 0), COUNT(0)) AS pct_has_non_empty_h4,
+
+  # Same title and H1
+  # COUNTIF(wpt_bodies_info.has_same_h1_title ) AS freq_has_same_h1_title,
+  AS_PERCENT(COUNTIF(wpt_bodies_info.has_same_h1_title ), COUNT(0)) AS pct_has_same_h1_title
+
 
   # add more fields here...
   # split the query up into logical groups if it gets too large.
