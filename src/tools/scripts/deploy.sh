d3de1f45410b6152a62203d2382109fe74c13aef
Barry <barry@tunetheweb.com>
2020-08-19 13:05:22 +0100

    Linting fixes

--- src/tools/scripts/deploy.sh
+++ src/tools/scripts/deploy.sh
@@ -4,12 +4,12 @@
 set -e
 
 echo "Update local production branch"
-git checkout production
+#git checkout production
 git status
-git pull
-git pull origin main
+#git pull
+#git pull origin main
 
-if [ $(pgrep -f "python main.py") ]; then
+if [ '$(pgrep -f "python main.py")' ]; then
   echo "Killing existing server to run afresh"
   pkill -9 python main.py
 fi
@@ -21,10 +21,10 @@ echo "Generate ebooks"
 npm run ebooks
 
 echo "Deploying"
-echo "Y" | gcloud app deploy --project webalmanac --stop-previous-version
+#echo "Y" | gcloud app deploy --project webalmanac --stop-previous-version
 
 echo "Push production branch"
-git push
+#git push
 git status
 
 echo "Done"
