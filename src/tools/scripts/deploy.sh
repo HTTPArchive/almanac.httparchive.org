3fde1eb7e3a8dfe8b2b9b76e06312946028c8346
Barry <barry@tunetheweb.com>
2020-08-23 18:21:42 +0100

    Improved deploy method

--- src/tools/scripts/deploy.sh
+++ src/tools/scripts/deploy.sh
@@ -5,9 +5,22 @@ set -e
 
 # Check branch is clean first
 if [ -n "$(git status --porcelain)" ]; then 
-  echo "Your branch includes changes that must be commited or remove. Exiting"
-  exit 1
+  read -r -n 1 -p "Your branch is not clean. Do you still want to deploy? [y/N]: " DEPLOY
+  if [ "$DEPLOY" != "Y" ] && [ "$DEPLOY" != "y" ]; then
+      echo
+      echo "Cancelling deploy"
+      exit 0
+  fi
 fi
+echo
+
+read -r -n 1 -p "Please confirm you've updated the eBooks via GitHub Actions. [y/N]: " DEPLOY
+if [ "$DEPLOY" != "Y" ] && [ "$DEPLOY" != "y" ]; then
+    echo
+    echo "Cancelling deploy"
+    exit 0
+fi
+echo
 
 echo "Update local production branch"
 git checkout production
@@ -23,31 +36,55 @@ fi
 echo "Run and test website"
 ./tools/scripts/run_and_test_website.sh
 
-echo "Generate ebooks"
-npm run ebooks
-
 echo "Please test the site locally"
 
-read -r -n 1 -p "Are you ready to deploy?: [y/N]" DEPLOY
+read -r -n 1 -p "Are you ready to deploy? [y/N]: " DEPLOY
 if [ "$DEPLOY" != "Y" ] && [ "$DEPLOY" != "y" ]; then
     echo
     echo "Cancelling deploy"
     exit 0
 fi
+echo
+
+LAST_TAGGED_VERSION=$(git tag -l "v*" | tail)
+echo "Last tagged version: $LAST_TAGGED_VERSION"
+if [[ "$LAST_TAGGED_VERSION" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  semver=( ${LAST_TAGGED_VERSION//./ } )
+  major="${semver[0]}"
+  minor="${semver[1]}"
+  next_minor=$((minor + 1))
+  NEXT_VERSION="$major.$next_minor.0"
+else
+  echo "Warning - last tagged version is not of the format vX.X.X!"
+fi
+
+TAG_VERSION=""
+while [[ ! "$TAG_VERSION" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
+do
+  echo "Please update major tag version when changing default year"
+  echo "Please update minor tag version when adding new languages or other large changes"
+  read -r -p "Please select tag version of the format vX.X.X. [$NEXT_VERSION]: " TAG_VERSION
+done
+echo "Tagging as $TAG_VERSION"
+
+LONG_DATE=$(date -u +%Y\-%m\-%d\ %H:%M:%S)
+git tag -a "$TAG_VERSION" -m "Version $TAG_VERSION $LONG_DATE"
+echo "Version $TAG_VERSION $LONG_DATE"
+
+if [[ -f deploy.zip ]]; then
+  echo "Removing old deploy.zip"
+  rm deploy.zip
+fi
+
+echo "Zipping into deploy.zip"
+zip -q -r deploy . --exclude @.gcloudignore
 
 echo "Deploying"
-gcloud app deploy --project webalmanac --stop-previous-version
-
-read -r -n 1 -p "Are you happy to push changes to production?: [y/N]" DEPLOY
-if [ "$DEPLOY" != "Y" ] && [ "$DEPLOY" != "y" ]; then
-    echo
-    echo "Exiting"
-    exit 0
-fi
+echo Y | gcloud app deploy --project webalmanac --stop-previous-version
 
 echo "Push production branch"
 git push
 git status
 
-echo "Done"
+echo "Please update release on GitHub using tag $TAG_VERSION@production"
 exit 0
