d447ed50bfee6bcdc0f9e7af3a1a0ff3d6608a60
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-09-03 01:51:01 +0800

    update

--- sql/2020/04_Fonts/04_15.VF_ @supports.sql
+++ sql/2020/04_Fonts/04_15.VF_ @supports.sql
@@ -3,31 +3,43 @@
 CREATE TEMPORARY FUNCTION checksSupports(css STRING)
 RETURNS ARRAY<STRING> LANGUAGE js AS '''
 try {
-var reduceValues = (values, rule) => {
-if (rule.type == 'supports' && rule.supports.toLowerCase().includes('font-variation-settings')) {
-values.push(rule.supports.toLowerCase());
-}
-return values;
-};
-var $ = JSON.parse(css);
-return $.stylesheet.rules.reduce(reduceValues, []);
+    var reduceValues = (values, rule) => {
+        if (rule.type == 'supports' && rule.supports.toLowerCase().includes('font-variation-settings')) {
+            values.push(rule.supports.toLowerCase());
+        }
+        return values;
+    };
+    var $ = JSON.parse(css);
+    return $.stylesheet.rules.reduce(reduceValues, []);
 } catch (e) {
-return [];
+    return [];
 }
 ''';
 
 SELECT
- client,
- COUNT(DISTINCT page) AS freq_support_vf,
- total_page,
- ROUND(COUNT(DISTINCT page) * 100 / total_page, 2) AS pct_support_vf
-FROM
- `httparchive.almanac.parsed_css`
-JOIN
- (SELECT _TABLE_SUFFIX AS client, COUNT(0) AS total_page FROM `httparchive.summary_pages.2020_08_01_*` GROUP BY _TABLE_SUFFIX)
+  client,
+  COUNT(DISTINCT page) AS freq_support_vf,
+  total_page,
+  ROUND(COUNT(DISTINCT page) * 100 / total_page, 2) AS pct_support_vf
+FROM (
+  SELECT
+    *
+  FROM
+    `httparchive.almanac.parsed_css`
+  WHERE
+    date='2020-08-01')
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    COUNT(0) AS total_page
+  FROM
+    `httparchive.summary_pages.2020_08_01_*`
+  GROUP BY
+    _TABLE_SUFFIX)
 USING
- (client)
+  (client)
 WHERE
- ARRAY_LENGTH(checksSupports(css)) > 0
+  ARRAY_LENGTH(checksSupports(css)) > 0
 GROUP BY
- client, total_page
+  client,
+  total_page
