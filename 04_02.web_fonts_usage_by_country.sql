d4c8485553ab92ab037c9665050467c2f62f4571
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-09-02 10:20:43 +0800

    update

---
+++ 04_02.web_fonts_usage_by_country.sql
@@ -0,0 +1,28 @@
+#standardSQL
+#country web fonts usage 
+SELECT
+ _TABLE_SUFFIX AS client,
+ country,
+ COUNT(0) AS freq_url,
+ ROUND(APPROX_QUANTILES(bytesFont, 1000)[OFFSET(500)] / 1024, 2) AS median_font_bytes
+FROM (
+SELECT
+ DISTINCT 
+  origin,
+  `chrome-ux-report`.experimental.GET_COUNTRY(country_code) AS country
+FROM 
+  `chrome-ux-report.materialized.country_summary`
+WHERE 
+  yyyymm = 202007)
+JOIN 
+  `httparchive.summary_pages.2020_08_01_*`
+ON 
+  CONCAT(origin, '/') = url
+WHERE 
+  bytesFont IS NOT NULL
+GROUP BY
+ client,
+ country
+ORDER BY
+ client,
+ country
