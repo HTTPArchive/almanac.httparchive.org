2dc2b0e79b44deb246d99c3543917219bd6298de
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-09-03 01:05:49 +0800

    update

--- sql/2020/04_Fonts/04_13.font_stretch.sql
+++ sql/2020/04_Fonts/04_13.font_stretch.sql
@@ -3,35 +3,41 @@
 CREATE TEMPORARY FUNCTION usesFontStretch(css STRING)
 RETURNS ARRAY<STRING> LANGUAGE js AS '''
 try {
-var reduceValues = (values, rule) => {
-if ('rules' in rule) {
-return rule.rules.reduce(reduceValues, values);
-}
-if (!('declarations' in rule)) {
-return values;
-}
-return values.concat(rule.declarations.filter(d => d.property.toLowerCase() == 'font-stretch').map(d => d.value));
-};
-var $ = JSON.parse(css);
-return $.stylesheet.rules.reduce(reduceValues, []);
+    var reduceValues = (values, rule) => {
+        if ('rules' in rule) {
+            return rule.rules.reduce(reduceValues, values);
+        }
+        if (!('declarations' in rule)) {
+            return values;
+        }
+        return values.concat(rule.declarations.filter(d => d.property.toLowerCase() == 'font-stretch').map(d => d.value));
+    };
+    var $ = JSON.parse(css);
+    return $.stylesheet.rules.reduce(reduceValues, []);
 } catch (e) {
-return [];
+    return [];
 }
 ''';
 
 SELECT
- client,
- COUNT(DISTINCT page) AS freq_stretch,
- total_page,
- ROUND(COUNT(DISTINCT page) * 100 / total_page, 2) AS pct_strech
+  client,
+  COUNT(DISTINCT page) AS freq_stretch,
+  total_page,
+  ROUND(COUNT(DISTINCT page) * 100 / total_page, 2) AS pct_strech
 FROM
- `httparchive.almanac.parsed_css`
-JOIN
- (SELECT _TABLE_SUFFIX AS client, COUNT(0) AS total_page FROM `httparchive.summary_pages.2020_08_01_*` GROUP BY client)
+  `httparchive.almanac.parsed_css`
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    COUNT(0) AS total_page
+  FROM
+    `httparchive.summary_pages.2020_08_01_*`
+  GROUP BY
+    client)
 USING
- (client)
+  (client)
 WHERE
- ARRAY_LENGTH(usesFontStretch(css)) > 0
+  ARRAY_LENGTH(usesFontStretch(css)) > 0
 GROUP BY
- client,
- total_page
+  client,
+  total_page
