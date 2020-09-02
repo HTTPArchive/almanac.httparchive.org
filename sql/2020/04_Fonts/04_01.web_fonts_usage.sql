64c6fa07d45135e01b8496339746a605ccbe2679
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-09-02 10:38:51 +0800

    update

--- sql/2020/04_Fonts/04_01.web_fonts_usage.sql
+++ sql/2020/04_Fonts/04_01.web_fonts_usage.sql
@@ -1,5 +1,5 @@
 #standardSQL
-#web fonts usage
+#web_fonts_usage
 SELECT
  _TABLE_SUFFIX AS client,
  SUBSTR(_TABLE_SUFFIX, 0, 10) AS date,
@@ -7,7 +7,11 @@ SELECT
  COUNT(0) AS total,
  ROUND(COUNTIF(reqFont>0) * 100 / count(0),2) AS pct_fonts,
  ROUND(APPROX_QUANTILES(bytesFont , 1000)[OFFSET (500)]/1024,2) AS median_byteFont,
-FROM `httparchive.summary_pages.*`
-WHERE reqFont IS NOT NULL and bytesFont IS NOT NULL
-GROUP BY client, date
-ORDER BY client, date DESC
+FROM 
+ `httparchive.summary_pages.*`
+WHERE 
+ reqFont IS NOT NULL and bytesFont IS NOT NULL
+GROUP BY 
+ client, date
+ORDER BY 
+ client, date DESC
