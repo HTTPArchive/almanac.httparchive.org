79332523e6d01a0960edb32bceadb3addee5e894
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-09-02 10:27:58 +0800

    updat

--- 04_06.font_display_with_fcp.sql
+++ 04_06.font_display_with_fcp.sql
@@ -1,5 +1,5 @@
 #standardSQL
-#font-display with fcp 
+#font_display_with_fcp
 CREATE TEMPORARY FUNCTION getFontDisplay(css STRING)
 RETURNS ARRAY<STRING> LANGUAGE js AS '''
 try {
@@ -34,12 +34,12 @@ SELECT
  COUNT(DISTINCT page) AS freq_display,
  COUNT(0) AS total_display,
  ROUND(COUNT(DISTINCT page) * 100 / COUNT(0), 2) AS pct_display,
- countif(fast_fcp>=0.75) as fast_fcp_display,
- countif(NOT(slow_fcp >=0.25) AND NOT(fast_fcp>=0.75)) avg_fcp_display,
- countif(slow_fcp>=0.25)*100 aslow_fcp_display,
- round(countif(fast_fcp>=0.75)*100/count(0),0) as pct_fast_fcp_display,
- round(countif(NOT(slow_fcp >=0.25) AND NOT(fast_fcp>=0.75))*100/count(0),0) as pct_avg_fcp_display,
- round(countif(slow_fcp>=0.25)*100/count(0),0) as pct_slow_fcp_display,
+ COUNTIF(fast_fcp>=0.75) as fast_fcp_display,
+ COUNTIF(NOT(slow_fcp >=0.25) AND NOT(fast_fcp>=0.75)) avg_fcp_display,
+ COUNTIF(slow_fcp>=0.25)*100 aslow_fcp_display,
+ ROUND(countif(fast_fcp>=0.75)*100/count(0),0) as pct_fast_fcp_display,
+ ROUND(countif(NOT(slow_fcp >=0.25) AND NOT(fast_fcp>=0.75))*100/count(0),0) as pct_avg_fcp_display,
+ ROUND(countif(slow_fcp>=0.25)*100/count(0),0) as pct_slow_fcp_display,
 FROM 
  `httparchive.almanac.parsed_css`,
  UNNEST(getFontDisplay(css)) AS font_display
@@ -47,11 +47,11 @@ JOIN
  (SELECT _TABLE_SUFFIX AS client, COUNT(0) AS total_page FROM `httparchive.sample_data.summary_pages_*` GROUP BY _TABLE_SUFFIX, client)
 USING  
  (client)
-join  
+JOIN  
  (select origin, fast_fcp, slow_fcp,
-from  
+FROM  
  `chrome-ux-report.materialized.device_summary` where yyyymm=202007)
-on 
+ON 
  concat(origin, '/')=page
 group by  
  client, page, font_display
