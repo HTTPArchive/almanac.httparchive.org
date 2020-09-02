2a4ddf572aeac86696d0b93ab0e00f3a0aee7c55
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-09-03 01:02:05 +0800

    update

--- sql/2020/04_Fonts/04_10.font_formats.sql
+++ sql/2020/04_Fonts/04_10.font_formats.sql
@@ -8,12 +8,6 @@ SELECT
  ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY client), 2) AS pct_fmt,
 FROM
  `httparchive.almanac.requests`
-join
- (select origin, fast_fcp, slow_fcp,
-from
- `chrome-ux-report.materialized.device_summary` where yyyymm=202007)
-ON
- concat(origin, '/')= url
 WHERE
  type = 'font' AND mimeType!= ''
 GROUP BY
