dd2757d75db3f218da83c91befc512d382be10c5
Barry <barry@tunetheweb.com>
2020-09-04 12:01:27 +0100

    fix linting errors

--- src/tools/scripts/test_template_changes.sh
+++ src/tools/scripts/test_template_changes.sh
@@ -56,7 +56,7 @@ set -e
 echo "Differences:"
 cat "${TEMP_DIFF_FILENAME}"
 
-NUM_DIFFS=$(cat "${TEMP_DIFF_FILENAME}" | wc -l)
+NUM_DIFFS=$(wc -l < "${TEMP_DIFF_FILENAME}")
 if [ "${NUM_DIFFS}" -ne "0" ]; then
   ESCAPED_OUTPUT=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/%0A/g' -e 's/\r/%0D/g' -e 's/\%/%25/g' -i "${TEMP_DIFF_FILENAME}")
   PR_COMMENT="The following diffs happen in the templates due to differences in this branch and main:%0A\`\`\`${ESCAPED_OUTPUT}\`\`\`"
