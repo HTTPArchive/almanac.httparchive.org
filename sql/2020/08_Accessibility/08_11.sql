69cb4eed866280568b0b2b753d8da9ee42ba65ab
David Fox <david@davidjfox.com>
2020-09-07 14:30:14 -0500

    Query optimizations

--- sql/2020/08_Accessibility/08_11.sql
+++ sql/2020/08_Accessibility/08_11.sql
@@ -1,35 +1,26 @@
 #standardSQL
 # 08_11: % sites with links to to the same page, # or javascript:void
-# TODO: Does same_page.total include hash_link occurances?
-CREATE TEMPORARY FUNCTION getLinkStats(payload STRING)
-RETURNS STRUCT<same_page INT64, hash_only_link INT64, javascript_void_links INT64> LANGUAGE js AS '''
-try {
-  var $ = JSON.parse(payload);
-  var anchors = JSON.parse($._wpt_bodies).anchors.rendered;
-  return {
-    same_page: anchors.same_page.total,
-    hash_only_link: anchors.hash_only_link,
-    javascript_void_links: anchors.javascript_void_links,
-  };
-} catch (e) {
-  return [];
-}
-''';
+# NOTE: same_page.total includes empty hash links (#) but excludes all others (#foobar)
 SELECT
   client,
   COUNT(0) AS total_sites,
+  COUNTIF(total_anchors > 0) AS sites_with_anchors,
 
-  COUNTIF(link_stats.same_page > 0) AS has_same_page,
-  COUNTIF(link_stats.hash_only_link > 0) AS has_hash_only_link,
-  COUNTIF(link_stats.javascript_void_links > 0) AS has_javascript_void_links,
+  COUNTIF(same_page > 0) AS has_same_page,
+  COUNTIF(hash_only > 0) AS has_hash_only_link,
+  COUNTIF(javascript_void > 0) AS has_javascript_void_links,
 
-  ROUND((COUNTIF(link_stats.same_page > 0) / COUNT(0)) * 100, 2) AS pct_has_same_page,
-  ROUND((COUNTIF(link_stats.hash_only_link > 0) / COUNT(0)) * 100, 2) AS pct_has_hash_only_link,
-  ROUND((COUNTIF(link_stats.javascript_void_links > 0) / COUNT(0)) * 100, 2) AS pct_has_javascript_void_links
+  ROUND((COUNTIF(same_page > 0) / COUNTIF(total_anchors > 0)) * 100, 2) AS pct_has_same_page,
+  ROUND((COUNTIF(hash_only > 0) / COUNTIF(total_anchors > 0)) * 100, 2) AS pct_has_hash_only_link,
+  ROUND((COUNTIF(javascript_void > 0) / COUNTIF(total_anchors > 0)) * 100, 2) AS pct_has_javascript_void_links
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
-    getLinkStats(payload) AS link_stats
+
+    CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, "$._wpt_bodies"), "$.anchors.rendered.same_page.total") AS INT64) AS same_page,
+    CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, "$._wpt_bodies"), "$.anchors.rendered.hash_only_link") AS INT64) AS hash_only,
+    CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, "$._wpt_bodies"), "$.anchors.rendered.javascript_void_links") AS INT64) AS javascript_void,
+    IFNULL(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, "$._element_count"), "$.a") AS INT64), 0) AS total_anchors
   FROM
     `httparchive.almanac.pages_desktop_*`
 )
