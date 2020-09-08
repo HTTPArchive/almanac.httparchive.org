4ce61a1075a99c0750223d4b4c635cbfe7c19a13
unknown <apppletsai@gmail.com>
2020-09-09 01:20:33 +0800

    'update commit'

--- sql/2020/04_Fonts/04_11.typeface.sql
+++ sql/2020/04_Fonts/04_11.typeface.sql
@@ -1,29 +1,64 @@
 #standardSQL
-#typeface
-CREATE TEMPORARY FUNCTION getFontFamilies(css STRING)
-RETURNS ARRAY < STRING > LANGUAGE js AS '''
+#typeface_by_country
+CREATE TEMPORARY FUNCTION
+  getFontFamilies(css STRING)
+  RETURNS ARRAY<STRING>
+  LANGUAGE js AS '''
 try {
-    var $ = JSON.parse(css);
-    return $.stylesheet.rules.filter(rule => rule.type == 'font-face').map(rule => {
-        var family = rule.declarations && rule.declarations.find(d => d.property == 'font-family');
-        return family && family.value.replace(/[\'"]/g, '');
-    }).filter(family => family);
+var $ = JSON.parse(css);
+return $.stylesheet.rules.filter(rule => rule.type == 'font-face').map(rule => {
+var family = rule.declarations && rule.declarations.find(d => d.property == 'font-family');
+return family && family.value.replace(/['"]/g, '');
+}).filter(family => family);
 } catch (e) {
-    return [];
+return [];
 }
 ''';
 
+WITH
+  countries AS (
+  SELECT
+    *,
+    'ad' AS country_code,
+    'Andorra' AS country
+  FROM
+    `chrome-ux-report.country_ad.201808`
+  UNION ALL
+  SELECT
+    *,
+    'ae' AS country_code,
+    'United Arab Emirates' AS country
+  FROM
+    `chrome-ux-report.country_ae.201808`
+  UNION ALL
+  SELECT
+    *,
+    'af' AS country_code,
+    'Afghanistan' AS country
+  FROM
+    `chrome-ux-report.country_af.201808`
+  UNION ALL
+  SELECT
+    *,
+    'ag' AS country_code,
+    'Antigua and Barbuda' AS country
+  FROM
+    `chrome-ux-report.country_ag.201808` )
 SELECT
- client,
- font_family,
- COUNT(0) AS freq_typeface,
- SUM(COUNT(0)) OVER (PARTITION BY client) AS total_typeface,
- ROUND(COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY client), 2) AS pct
-FROM `httparchive.almanac.parsed_css`, 
-UNNEST(getFontFamilies(css)) AS font_family 
-WHERE date='2020-08-01'
+  client,
+  country,
+  font_family,
+  COUNT(0) AS freq,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
+FROM
+  countries,
+  `httparchive.almanac.parsed_css`,
+  UNNEST(getFontFamilies(css)) AS font_family
+WHERE
+  CONCAT(origin, '/') = page
 GROUP BY
- client,
- font_family
+  client,
+  country,
+  font_family
 ORDER BY
- freq_typeface DESC
+  freq / total DESC
