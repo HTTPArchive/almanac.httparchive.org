acb47decf8b719f77cee23e3e6d1b9297823dc46
Greg Wolf <greg@hilltoptech.net>
2020-09-18 15:19:36 -0400

    Update sql/2020/22_HTTP_2/measure_number_of_tcp_connections_per_site.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/22_HTTP_2/measure_number_of_tcp_connections_per_site.sql
+++ sql/2020/22_HTTP_2/measure_number_of_tcp_connections_per_site.sql
@@ -1,41 +1,36 @@
 # standardSQL
 # Measure number of TCP Connections per site.
-SELECT 
-  "mobile" AS client,
-  JSON_EXTRACT_SCALAR(payload, "$._protocol") AS protocol,
+SELECT
+  percentile,
+  client,
+  protocol,
   COUNT(0) AS num_pages,
-  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(50)] AS median,
-  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(75)] AS p75,
-  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(95)] AS p95
-FROM 
-  `httparchive.requests.2020_08_01_mobile` AS requests
-INNER JOIN
-  `httparchive.summary_pages.2020_08_01_mobile` AS summary
-ON
-  requests.url = summary.url
-WHERE
-  JSON_EXTRACT_SCALAR(payload, "$._is_base_page") = "true"
-GROUP BY 
-  client, 
+  APPROX_QUANTILES(_connections, 1000)[OFFSET(percentile * 10)] AS connections
+FROM (
+  SELECT
+    client,
+    page,
+    JSON_EXTRACT_SCALAR(payload, '$._protocol') AS protocol,
+  FROM
+    `httparchive.almanac.requests`
+  WHERE
+    date = '2020-08-01' AND
+    firstHtml)
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url AS page,
+    _connections,
+  FROM
+    `httparchive.summary_pages.2020_08_01_*`)
+USING
+  (client, page),
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
+GROUP BY
+  percentile,
+  client,
   protocol
-
-UNION ALL
-
-SELECT 
-  "desktop" AS client,
-  JSON_EXTRACT_SCALAR(payload, "$._protocol") AS protocol,
-  COUNT(0) AS num_pages,
-  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(50)] AS median,
-  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(75)] AS p75,
-  APPROX_QUANTILES(_connections, 100)[SAFE_ORDINAL(95)] AS p95
-FROM 
-  `httparchive.requests.2020_08_01_desktop` AS requests
-INNER JOIN
-  `httparchive.summary_pages.2020_08_01_desktop` AS summary
-ON
-  requests.url = summary.url
-WHERE
-  JSON_EXTRACT_SCALAR(payload, "$._is_base_page") = "true"
-GROUP BY 
-  client, 
+ORDER BY
+  percentile,
+  client,
   protocol
