a4eed40a3d78a928c97aadd593e86b32c0c120ff
Greg Wolf <greg@hilltoptech.net>
2020-09-18 16:15:13 -0400

    Update detailed_upgrade_headers.sql

--- sql/2020/22_HTTP_2/detailed_upgrade_headers.sql
+++ sql/2020/22_HTTP_2/detailed_upgrade_headers.sql
@@ -1,27 +1,16 @@
 # standardSQL
 # Detailed upgrade headers for 20.04, 20.05 and 20.06
-CREATE TEMPORARY FUNCTION getUpgradeHeader(payload STRING)
-RETURNS STRING LANGUAGE js AS """
-  try {
-    var $ = JSON.parse(payload);
-    var headers = $.response.headers;
-    return headers.find(h => h.name.toLowerCase() === 'upgrade').value.trim();
-  } catch (e) {
-    return '';
-  }
-""";
-
 SELECT
   client,
-  firstHtml,
+  firstHtml,  
   JSON_EXTRACT_SCALAR(payload, '$._protocol') AS protocol,
   IF(url LIKE 'https://%', 'https', 'http') AS http_or_https,
-  NORMALIZE_AND_CASEFOLD(getUpgradeHeader(payload)) AS upgrade,
+  regexp_extract(regexp_extract(respOtherHeaders, r'(?is)Upgrade = (.*)'), r'(?im)^([^=]*?)(?:, [a-z-]+ = .*)') is not null AS upgrade,
   COUNT(0) AS num_requests,
   SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
   COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
 FROM
-  `httparchive.almanac.requests`
+  `httparchive.sample_data.requests`
 WHERE
   date = '2020-08-01'
 GROUP BY
