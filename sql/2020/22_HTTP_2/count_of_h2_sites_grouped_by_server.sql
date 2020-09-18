62bd9dedd773b6d3ac68e463bdba4dc23c179efe
Greg Wolf <greg@hilltoptech.net>
2020-09-18 15:24:24 -0400

    Update sql/2020/22_HTTP_2/count_of_h2_sites_grouped_by_server.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/22_HTTP_2/count_of_h2_sites_grouped_by_server.sql
+++ sql/2020/22_HTTP_2/count_of_h2_sites_grouped_by_server.sql
@@ -1,35 +1,22 @@
 # standardSQL
 # Count of HTTP/2 Sites Grouped By Server
-CREATE TEMPORARY FUNCTION getServerHeader(payload STRING)
-RETURNS STRING
-LANGUAGE js AS """
-  try {
-    var $ = JSON.parse(payload);
-    var headers = $.response.headers;
-    // Find server header
-    var st = headers.find(function(e) { 
-      return e['name'].toLowerCase() === 'server'
-    });
-    // Remove everything after / in the server header value and return
-    return st['value'].split("/")[0]; 
-  } catch (e) {
-    return '';
-  }
-""";
-
-SELECT 
+SELECT
   client,
-  getServerHeader(payload) AS server_header,
+  # Omit server version
+  NORMALIZE_AND_CASEFOLD(REGEXP_EXTRACT(resp_server, r'\s*([^/]*)\s*')) AS server_header,
   COUNT(0) AS num_pages,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
   ROUND(COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client), 4) AS pct
-FROM 
-  `httparchive.almanac.requests` 
+FROM
+  `httparchive.almanac.requests`
 WHERE
-  date='2020-08-01' AND
+  date = '2020-08-01' AND
   firstHtml AND
-  JSON_EXTRACT_SCALAR(payload, "$._protocol") = "HTTP/2"
+  JSON_EXTRACT_SCALAR(payload, '$._protocol') = 'HTTP/2'
 GROUP BY
   client,
   server_header
-ORDER BY 
+HAVING
+  num_pages >= 100
+ORDER BY
   num_pages DESC
