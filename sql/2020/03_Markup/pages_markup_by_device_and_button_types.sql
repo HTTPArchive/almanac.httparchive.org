3300d7318c5c5c2c13b00de2d20851664c12a5be
Tony McCreath <tony@websiteadvantage.com.au>
2020-09-20 07:52:24 +0930

    Markup 2020 queries (#1137)
    * Initial mapping of queries from 2019
    
    * Initial mapping of queries from 2019
    
    * comments
    
    * fixed merge conflict
    
    * New AS_PERCENT function
    
    * reviewing all scripts
    experiment on speed improvements
    
    * refining experiment
    
    * lots of changes
    
    * added buttons field
    
    * moved instructions into the README
    more metrics
    
    * almost complete
    
    * testing and creating sample sheets
    
    * switched to live tables
    
    * removed unused queries
    
    * newline at end of files
    
    * removed all test code
    removed commented out code
    
    * Reduced some limits
    Added an order by
    comments
    total -> freq
    
    * adding/shrinking limits
    moved some metrics to a percentile query
    removed error reporting
    
    * Update sql/2020/03_Markup/summary_pages_by_device.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * now based on pages
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

---
+++ sql/2020/03_Markup/pages_markup_by_device_and_button_types.sql
@@ -0,0 +1,47 @@
+#standardSQL
+# pages markup metrics grouped by device and button type
+
+# helper to create percent fields
+CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS (
+  ROUND(SAFE_DIVIDE(freq, total), 4)
+);
+
+# returns all the data we need from _markup
+CREATE TEMPORARY FUNCTION get_markup_buttons_info(markup_string STRING)
+RETURNS ARRAY<STRUCT<
+name STRING,
+freq INT64
+>> LANGUAGE js AS '''
+var result = [];
+try {
+    var markup = JSON.parse(markup_string); 
+
+    if (Array.isArray(markup) || typeof markup != 'object') return result;
+
+    if (markup.buttons && markup.buttons.types) {
+      return Object.entries(markup.buttons.types).map(([name, freq]) => ({name, freq})); 
+    }
+
+} catch (e) {}
+return result;
+''';
+
+SELECT
+  _TABLE_SUFFIX AS client,
+  button_type_info.name AS button_type,
+  COUNTIF(button_type_info.freq > 0) AS freq_page_with_button, 
+  AS_PERCENT(COUNTIF(button_type_info.freq > 0), total) AS pct_page_with_button,
+  SUM(button_type_info.freq) AS freq_button, 
+  AS_PERCENT(SUM(button_type_info.freq), SUM(SUM(button_type_info.freq)) OVER (PARTITION BY _TABLE_SUFFIX)) AS pct_button
+FROM
+    `httparchive.pages.2020_08_01_*`
+    JOIN
+      (SELECT _TABLE_SUFFIX, COUNT(0) AS total FROM 
+      `httparchive.pages.2020_08_01_*`
+      GROUP BY _TABLE_SUFFIX) # to get an accurate total of pages per device. also seems fast
+    USING (_TABLE_SUFFIX),
+    UNNEST(get_markup_buttons_info(JSON_EXTRACT_SCALAR(payload, '$._markup'))) AS button_type_info
+GROUP BY
+  client,
+  button_type
+LIMIT 1000
