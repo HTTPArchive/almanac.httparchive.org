85baf32063d649520b1b26db2eddf1958ad64d7d
Greg Wolf <greg@hilltoptech.net>
2020-09-20 16:08:28 -0400

    Update number_of_h2_pushed_resources_and_avg_bytes_by_content_type.sql
    Added percentile

--- sql/2020/22_HTTP_2/number_of_h2_pushed_resources_and_avg_bytes_by_content_type.sql
+++ sql/2020/22_HTTP_2/number_of_h2_pushed_resources_and_avg_bytes_by_content_type.sql
@@ -1,32 +1,34 @@
 # standardSQL
 # Number of HTTP/2 Pushed Resources and Average Bytes by Content type
 SELECT 
+  percentile,
   client,
   content_type,
   COUNT(DISTINCT page) AS num_pages,
-  APPROX_QUANTILES(num_requests, 1000)[OFFSET(500)] AS median_pushed_requests,
-  APPROX_QUANTILES(kb_transfered, 1000)[OFFSET(500)] AS median_kb_transfered,
+  APPROX_QUANTILES(num_requests, 1000)[OFFSET(500)] AS pushed_requests,
+  APPROX_QUANTILES(kb_transfered, 1000)[OFFSET(500)] AS kb_transfered,
 FROM (
   SELECT 
-    percentile,
     client,
     page,
     JSON_EXTRACT_SCALAR(payload, "$._contentType") as content_type,
     SUM(CAST(JSON_EXTRACT_SCALAR(payload, "$._bytesIn") AS INT64)/1024) AS kb_transfered,
     COUNT(0) AS num_requests
   FROM 
-    `httparchive.almanac.requests`,
-    UNNEST([10, 25, 50, 75, 90]) AS percentile
+    `httparchive.almanac.requests`  
   WHERE 
     date='2020-08-01' AND
     JSON_EXTRACT_SCALAR(payload, "$._protocol") = "HTTP/2" AND
     JSON_EXTRACT_SCALAR(payload, "$._was_pushed") = "1"
   GROUP BY 
-    percentile,
     client,
     page,
-    content_type
-)
+    content_type),
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
 GROUP BY
+  percentile,
   client,
   content_type
+ORDER BY
+  percentile,
+  client
