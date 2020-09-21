12087f28efc329793402e62f58bf019f2f264f6e
Greg Wolf <greg@hilltoptech.net>
2020-09-21 08:09:52 -0400

    Update and rename tls_1_3_adoption_for_h2.sql to tls_adoption_by_http_version.sql

--- sql/2020/22_HTTP_2/tls_adoption_by_http_version.sql
+++ sql/2020/22_HTTP_2/tls_adoption_by_http_version.sql
@@ -2,6 +2,7 @@
 # Distribution of TLS versions
 SELECT
   client,
+  http_version,
   tls_version,
   COUNT(0) AS freq,
   SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
@@ -9,13 +10,16 @@ SELECT
 FROM (
   SELECT
     client,
+    JSON_EXTRACT_SCALAR(payload, '$._protocol') as http_version,
     JSON_EXTRACT_SCALAR(payload, '$._tls_version') AS tls_version
   FROM `httparchive.almanac.requests` WHERE date='2020-08-01'
   )
 WHERE
+  http_version IS NOT NULL AND  
   tls_version IS NOT NULL
 GROUP BY
   client,
+  http_version,
   tls_version
 ORDER BY
   freq / total DESC
