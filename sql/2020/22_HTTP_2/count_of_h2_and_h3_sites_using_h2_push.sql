81e1cd83dea3fab8159f887277255fec7fea11e3
Greg Wolf <greg@hilltoptech.net>
2020-09-21 13:13:25 -0400

    Update and rename count_of_h2_sites_using_h2_push.sql to count_of_h2_and_h3_sites_using_h2_push.sql

--- sql/2020/22_HTTP_2/count_of_h2_and_h3_sites_using_h2_push.sql
+++ sql/2020/22_HTTP_2/count_of_h2_and_h3_sites_using_h2_push.sql
@@ -1,5 +1,5 @@
 # standardSQL
-# Count of HTTP/2 Sites using HTTP/2 Push
+# Count of H2 and H3 Sites using HTTP/2 Push
 SELECT
   client,
   COUNT(DISTINCT IF(was_pushed, page, NULL)) AS num_pages_with_push,
@@ -14,6 +14,10 @@ FROM (
     `httparchive.almanac.requests`
   WHERE
     date = '2020-08-01' AND
-    JSON_EXTRACT_SCALAR(payload, '$._protocol') = 'HTTP/2')
+    (LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "http/2" OR
+     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "%quic%" OR
+     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "h3%" OR
+     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "http/3%")   
+    )
 GROUP BY
   client
