41c760cd4709e9811226e298f727647ba11a9326
Pokidov N. Dmitry <dooman87@gmail.com>
2020-09-21 20:43:16 +1000

    Update sql/2020/09_Performance/lh5_performance_score_distribution.sql
    Formatting
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/09_Performance/lh5_performance_score_distribution.sql
+++ sql/2020/09_Performance/lh5_performance_score_distribution.sql
@@ -7,7 +7,11 @@ SELECT
   SAFE_DIVIDE(fast,    slow + avg + fast + hundred) fast,
   SAFE_DIVIDE(hundred, slow + avg + fast + hundred) hundred,
 FROM (
-  SELECT COUNTIF(score < 0.3) slow, COUNTIF(score > 0.3 and score < 0.8) avg, COUNTIF(score > 0.8 and score < 1.0) fast, COUNTIF(score = 1.0) hundred
+  SELECT
+    COUNTIF(score < 0.3) AS slow,
+    COUNTIF(score > 0.3 AND score < 0.8) AS avg,
+    COUNTIF(score > 0.8 AND score < 1.0) AS fast,
+    COUNTIF(score = 1.0) AS hundred
   FROM (
     SELECT CAST(JSON_EXTRACT(report, '$.categories.performance.score') AS NUMERIC) score FROM `httparchive.lighthouse.2020_05_01_mobile`
   )
