a3334cb1f0c0dbc6d72e322ab602ea958f88a950
Greg Wolf <greg@hilltoptech.net>
2020-09-21 18:45:24 -0400

    Update and rename number_of_h2_pushed_resources_and_avg_bytes_by_content_type.sql to number_of_h2_and_h3_pushed_resources_and_bytes_by_content_type.sql

--- sql/2020/22_HTTP_2/number_of_h2_and_h3_pushed_resources_and_bytes_by_content_type.sql
+++ sql/2020/22_HTTP_2/number_of_h2_and_h3_pushed_resources_and_bytes_by_content_type.sql
@@ -1,8 +1,9 @@
 # standardSQL
-# Number of HTTP/2 Pushed Resources and Average Bytes by Content type
+# Number of H2 and H3 Pushed Resources and Bytes by Content type
 SELECT 
   percentile,
   client,
+  http_version,
   content_type,
   COUNT(DISTINCT page) AS num_pages,
   APPROX_QUANTILES(num_requests, 1000)[OFFSET(500)] AS pushed_requests,
@@ -11,6 +12,7 @@ FROM (
   SELECT 
     client,
     page,
+    JSON_EXTRACT_SCALAR(payload, '$._protocol') as http_version,
     JSON_EXTRACT_SCALAR(payload, "$._contentType") as content_type,
     SUM(CAST(JSON_EXTRACT_SCALAR(payload, "$._bytesIn") AS INT64)/1024) AS kb_transfered,
     COUNT(0) AS num_requests
@@ -18,16 +20,21 @@ FROM (
     `httparchive.almanac.requests`  
   WHERE 
     date='2020-08-01' AND
-    JSON_EXTRACT_SCALAR(payload, "$._protocol") = "HTTP/2" AND
-    JSON_EXTRACT_SCALAR(payload, "$._was_pushed") = "1"
+    JSON_EXTRACT_SCALAR(payload, "$._was_pushed") = "1" AND
+    (LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "http/2" OR
+     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "%quic%" OR
+     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "h3%" OR
+     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "http/3%")  
   GROUP BY 
     client,
+    http_version,
     page,
     content_type),
   UNNEST([10, 25, 50, 75, 90]) AS percentile
 GROUP BY
   percentile,
   client,
+  http_version,
   content_type
 ORDER BY
   percentile,
