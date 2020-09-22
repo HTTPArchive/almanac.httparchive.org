03e9bd19aebfc377aeebfceb1dcd65dd006a8bcd
Greg Wolf <greg@hilltoptech.net>
2020-09-22 17:29:31 -0400

    Update sql/2020/22_HTTP_2/percentage_of_resources_loaded_over_HTTP_by_version_per_site.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/22_HTTP_2/percentage_of_resources_loaded_over_HTTP_by_version_per_site.sql
+++ sql/2020/22_HTTP_2/percentage_of_resources_loaded_over_HTTP_by_version_per_site.sql
@@ -1,36 +1,32 @@
 # standardSQL
 # Percentage of resources loaded over HTTP by version per site
 SELECT
+  percentile,
   client,
-  APPROX_QUANTILES((http_0_9/num_requests) * 100, 1000)[OFFSET(percentile * 10)] AS pct_http_0_9,
-  APPROX_QUANTILES((http_1_0/num_requests) * 100, 1000)[OFFSET(percentile * 10)] AS pct_http_1_0,
-  APPROX_QUANTILES((http_1_1/num_requests) * 100, 1000)[OFFSET(percentile * 10)] AS pct_http_1_1,
-  APPROX_QUANTILES((http_2/num_requests) * 100, 1000)[OFFSET(percentile * 10)] AS pct_http_2,
-  APPROX_QUANTILES((quic/num_requests) * 100, 1000)[OFFSET(percentile * 10)] AS pct_quic
+  APPROX_QUANTILES(http_0_9, 1000)[OFFSET(percentile * 10)] AS pct_http_0_9,
+  APPROX_QUANTILES(http_1_0, 1000)[OFFSET(percentile * 10)] AS pct_http_1_0,
+  APPROX_QUANTILES(http_1_1, 1000)[OFFSET(percentile * 10)] AS pct_http_1_1,
+  APPROX_QUANTILES(http_2, 1000)[OFFSET(percentile * 10)] AS pct_http_2,
+  APPROX_QUANTILES(quic, 1000)[OFFSET(percentile * 10)] AS pct_quic
 FROM (
-  SELECT 
-    percentile,
+  SELECT
     client,
-    page,
-    COUNT(0) AS num_requests, 
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/0.9",1,0)) AS http_0_9, 
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/1.0",1,0)) AS http_1_0, 
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/1.1",1,0)) AS http_1_1,
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="HTTP/2",1,0)) AS http_2,
-    SUM(IF((
-     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "%quic%" OR
-     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "h3%" OR
-     LOWER(JSON_EXTRACT_SCALAR(payload, "$._protocol")) LIKE "http/3%"
-      ),1,0)) AS quic
+    COUNTIF(JSON_EXTRACT_SCALAR(payload, "$._protocol") = "http/0.9") / COUNT(0) AS http_0_9, 
+    COUNTIF(JSON_EXTRACT_SCALAR(payload, "$._protocol") = "http/1.0") / COUNT(0) AS http_1_0, 
+    COUNTIF(JSON_EXTRACT_SCALAR(payload, "$._protocol") = "http/1.1") / COUNT(0) AS http_1_1,
+    COUNTIF(JSON_EXTRACT_SCALAR(payload, "$._protocol") = "HTTP/2") / COUNT(0) AS http_2,
+    COUNTIF(REGEXP_CONTAINS(JSON_EXTRACT_SCALAR(payload, "$._protocol"), r"(?i)(quic|^h3|^http/3)")) AS quic
   FROM 
-    `httparchive.almanac.requests`,           
-    UNNEST([10, 25, 50, 75, 90]) AS percentile
+    `httparchive.almanac.requests`
   WHERE
-    date='2020-08-01'
+    date = '2020-08-01'
   GROUP BY
-    percentile,
     client,
-    page
-)
-GROUP BY 
+    page),           
+    UNNEST([10, 25, 50, 75, 90]) AS percentile
+GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
   client
