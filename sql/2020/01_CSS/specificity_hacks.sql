86e06119e0a143e68183f16285a85c2593d0a7cb
Rick Viscomi <rviscomi@gmail.com>
2020-09-25 16:00:45 -0400

    use partition

--- sql/2020/01_CSS/specificity_hacks.sql
+++ sql/2020/01_CSS/specificity_hacks.sql
@@ -114,7 +114,9 @@ FROM (
     page,
     getSpecificityHacks(css) AS hack
   FROM
-    `httparchive.almanac.parsed_css`),
+    `httparchive.almanac.parsed_css`
+  WHERE
+    date = '2020-08-01'),
   UNNEST([10, 25, 50, 75, 90, 95, 99, 100]) AS percentile
 GROUP BY
   percentile,
