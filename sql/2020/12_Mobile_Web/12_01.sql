eb79d1c995a139ea034f7ecbe75a0d8ea424dec7
David Fox <david@davidjfox.com>
2020-09-27 15:55:54 -0500

    Begin edits to 12_01

--- sql/2020/12_Mobile_Web/12_01.sql
+++ sql/2020/12_Mobile_Web/12_01.sql
@@ -1,22 +1,16 @@
 #standardSQL
 # 12_01: 4G, 3G and 2G distribution
 SELECT
-  _4G,
-  _3G,
-  _2G
-FROM (
-  SELECT
-    COUNT(0) OVER () AS n,
-    ROUND(_4GDensity * 100, 2) AS _4G,
-    ROUND(_3GDensity * 100, 2) AS _3G,
-    ROUND(_2GDensity * 100, 2) AS _2G,
-    ROW_NUMBER() OVER (ORDER BY _4GDensity DESC) AS row
-  FROM
-    `chrome-ux-report.materialized.metrics_summary`
-  WHERE
-    yyyymm = 202007 AND
-    _4GDensity + _3GDensity + _2GDensity > 0
-  ORDER BY
-    _4G DESC)
+  percentile,
+  APPROX_QUANTILES(_4GDensity, 1000)[SAFE_ORDINAL(percentile * 10)] AS _4GDensity,
+  APPROX_QUANTILES(_3GDensity, 1000)[SAFE_ORDINAL(percentile * 10)] AS _3GDensity,
+  APPROX_QUANTILES(_2GDensity, 1000)[SAFE_ORDINAL(percentile * 10)] AS _2GDensity
+FROM
+  `chrome-ux-report.materialized.metrics_summary`,
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
 WHERE
-  MOD(row, CAST(FLOOR(n / 1000) AS INT64)) = 0
+  yyyymm = 202007
+GROUP BY
+  percentile
+ORDER BY
+  percentile
