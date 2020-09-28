32e7e66bda6df4ef59c589ce0a43ae1e03bf4bd4
Barry Pollard <barry_pollard@hotmail.com>
2020-09-28 17:11:44 +0100

    PWA 2020 queries (#1283)
    * Initial commit with queries converted from 2019
    
    * Remove duplicates, add lots of new queries
    
    * Misc fixes for problems encountered when running them
    
    * Changes to non-libraries script
    
    * Better Lightouse Query
    
    * Review feedback
    
    * Strip out really large lighthouse queries

---
+++ sql/2020/14_PWA/lighthouse_pwa_score.sql
@@ -0,0 +1,34 @@
+#standardSQL
+# Percentiles of lighthouse pwa score
+# Based on 07_24: Percentiles of lighthouse performance score
+# This metric comes from Lighthouse only and is only available in mobile in HTTP Archive dataset
+SELECT
+  '2019_07_01' AS date,
+  percentile,
+  APPROX_QUANTILES(score, 1000)[OFFSET(percentile * 10)] AS score
+FROM (
+  SELECT
+     CAST(JSON_EXTRACT(report, '$.categories.pwa.score') AS NUMERIC) AS score
+  FROM
+     `httparchive.lighthouse.2019_07_01_mobile`),
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
+GROUP BY
+  date,
+  percentile
+UNION ALL
+SELECT
+  '2020_08_01' AS date,
+  percentile,
+  APPROX_QUANTILES(score, 1000)[OFFSET(percentile * 10)] AS score
+FROM (
+  SELECT
+     CAST(JSON_EXTRACT(report, '$.categories.pwa.score') AS NUMERIC) AS score
+  FROM
+     `httparchive.lighthouse.2020_08_01_mobile`),
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
+GROUP BY
+  date,
+  percentile
+ORDER BY
+  date,
+  percentile
