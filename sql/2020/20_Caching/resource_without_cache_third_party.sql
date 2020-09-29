6064aea3a5e1190bf922a321d22997ee125aa083
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-09-30 00:01:53 +0530

    Third party resources served without using cache.

---
+++ sql/2020/20_Caching/resource_without_cache_third_party.sql
@@ -0,0 +1,34 @@
+#standardSQL
+# Third party resources served without using cache
+SELECT
+  client,
+  COUNT(0) AS total_requests,
+  party,
+  type,
+  COUNTIF(not_cacheable) AS total_not_cacheable,
+  COUNTIF(NOT not_cacheable AND uses_cache) AS total_using_cache,
+  COUNTIF(NOT not_cacheable AND NOT uses_cache) AS total_not_using_cache,
+
+  ROUND(COUNTIF(not_cacheable) * 100 / COUNT(0), 2) AS perc_not_cacheable,
+  ROUND(COUNTIF(NOT not_cacheable AND uses_cache) * 100 / COUNT(0), 2) AS perc_using_cache,
+  ROUND(COUNTIF(NOT not_cacheable AND NOT uses_cache) * 100 / COUNT(0), 2) AS perc_not_using_cache
+FROM (
+  SELECT
+    client,
+    type,
+    IF (STRPOS(NET.HOST(url), REGEXP_EXTRACT(NET.REG_DOMAIN(page), r'([\w-]+)')) > 0, 1, 3) AS party,
+    REGEXP_CONTAINS(resp_cache_control, r'(?i)(no-cache|no-store)') AS not_cacheable,
+    expAge > 0 AS uses_cache
+  FROM
+    `httparchive.almanac.requests`
+  WHERE
+    date = '2019-07-01'
+)
+GROUP BY
+  client,
+  type,
+  party
+ORDER BY
+  type,
+  client,
+  party
