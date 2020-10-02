0ecea027945d69de0f7995b9eee4150fef4a8b70
Max Ostapenko <bvz2001@gmail.com>
2020-10-03 00:20:45 +0100

    median function update

--- sql/2020/09_Performance/median_lcp_score_by_month.sql
+++ sql/2020/09_Performance/median_lcp_score_by_month.sql
@@ -1,23 +1,17 @@
 #standardSQL
 # Median of Largest contentful paint 75% score by month
 
+
 SELECT
   date,
   device,
-  median_p75_lcp
-FROM (
-  SELECT
-    date,
-    device,
-    PERCENTILE_CONT(p75_lcp, 0.5) OVER(PARTITION BY date, device) AS median_p75_lcp
-  FROM
-    `chrome-ux-report.materialized.device_summary`
-  WHERE
-    date >= "2020-09-01"
-    AND date <= "2020-09-30"
-    AND device IN ('desktop','phone')
-)
+  APPROX_QUANTILES(p75_lcp, 2 RESPECT NULLS)[OFFSET(1)] AS approx_median_p75_lcp,
+FROM
+  `chrome-ux-report.materialized.device_summary`
+WHERE
+  date >= "2019-09-01"
+  AND date <= "2020-09-30"
+  AND device IN ('desktop','phone')
 GROUP BY
   date,
-  device,
-  median_p75_lcp
+  device
