f52241f2926ea98b28b31eef58d99aa86ddd9786
Dmitry Pokidov <dooman87@gmail.com>
2020-10-04 21:36:40 +1100

    * Using percentage to compare performance scores between LH5 and LH6. * Using last year dataset to show distribution of LH performance score

--- sql/2020/09_Performance/lh5_performance_score_distribution.sql
+++ sql/2020/09_Performance/lh5_performance_score_distribution.sql
@@ -13,6 +13,6 @@ FROM (
     COUNTIF(score > 0.8 AND score < 1.0) AS fast,
     COUNTIF(score = 1.0) AS hundred
   FROM (
-    SELECT CAST(JSON_EXTRACT(report, '$.categories.performance.score') AS NUMERIC) score FROM `httparchive.lighthouse.2020_05_01_mobile`
+    SELECT CAST(JSON_EXTRACT(report, '$.categories.performance.score') AS NUMERIC) score FROM `httparchive.lighthouse.2019_07_01_mobile`
   )
 )
--- sql/2020/09_Performance/lh6_vs_lh5_performance_score_02.sql
+++ sql/2020/09_Performance/lh6_vs_lh5_performance_score_02.sql
@@ -1,23 +1,30 @@
 #standardSQL
 
-# Calculates number of sites where the performance score changed low ( < 10), medium (10-30) or big (> 30) between
+# Calculates percentage of sites where the performance score changed low ( < 10), medium (10-30) or big (> 30) between
 # LH 5 and 6 versions.
+
 SELECT
-  COUNTIF(perf_score_delta <= 0.1) AS small_change,
-  COUNTIF(perf_score_delta > 0.1 AND perf_score_delta <= 0.3) AS mid_change,
-  COUNTIF(perf_score_delta > 0.3) AS big_change
-FROM
-(
+  SAFE_DIVIDE(small_change, small_change + mid_change + big_change) AS small,
+  SAFE_DIVIDE(mid_change,   small_change + mid_change + big_change) AS avg,
+  SAFE_DIVIDE(big_change,   small_change + mid_change + big_change) AS big
+FROM (
   SELECT
-    perf_score_lh6,
-    perf_score_lh5,
-    (perf_score_lh6 - perf_score_lh5) AS perf_score_delta
+    COUNTIF(perf_score_delta <= 0.1) AS small_change,
+    COUNTIF(perf_score_delta > 0.1 AND perf_score_delta <= 0.3) AS mid_change,
+    COUNTIF(perf_score_delta > 0.3) AS big_change
   FROM
   (
     SELECT
-      CAST(JSON_EXTRACT(lh6.report, '$.categories.performance.score') AS NUMERIC) AS perf_score_lh6,
-      CAST(JSON_EXTRACT(lh5.report, '$.categories.performance.score') AS NUMERIC) AS perf_score_lh5
-      FROM `httparchive.lighthouse.2020_09_01_mobile` lh6
-      JOIN `httparchive.lighthouse.2020_05_01_mobile` lh5 ON lh5.url=lh6.url
+      perf_score_lh6,
+      perf_score_lh5,
+      (perf_score_lh6 - perf_score_lh5) AS perf_score_delta
+    FROM
+    (
+      SELECT
+        CAST(JSON_EXTRACT(lh6.report, '$.categories.performance.score') AS NUMERIC) AS perf_score_lh6,
+        CAST(JSON_EXTRACT(lh5.report, '$.categories.performance.score') AS NUMERIC) AS perf_score_lh5
+        FROM `httparchive.lighthouse.2020_09_01_mobile` lh6
+        JOIN `httparchive.lighthouse.2020_05_01_mobile` lh5 ON lh5.url=lh6.url
+    )
   )
 )
