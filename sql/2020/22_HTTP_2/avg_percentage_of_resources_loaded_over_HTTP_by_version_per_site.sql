a87eef5cd479a54f467d3bc92e084ef7a66ceb4b
Greg Wolf <greg@hilltoptech.net>
2020-10-05 14:25:23 -0400

    fix percentage and tls queries

--- sql/2020/22_HTTP_2/avg_percentage_of_resources_loaded_over_HTTP_by_version_per_site.sql
+++ sql/2020/22_HTTP_2/avg_percentage_of_resources_loaded_over_HTTP_by_version_per_site.sql
@@ -1,32 +1,29 @@
 # standardSQL
-# Percentage of resources loaded over HTTP by version per site
+# Average percentage of resources loaded over HTTP .9, 1, 1.1, 2, and QUIC per site
 SELECT
-  percentile,
   client,
-  APPROX_QUANTILES(http_0_9, 1000)[OFFSET(percentile * 10)] AS pct_http_0_9,
-  APPROX_QUANTILES(http_1_0, 1000)[OFFSET(percentile * 10)] AS pct_http_1_0,
-  APPROX_QUANTILES(http_1_1, 1000)[OFFSET(percentile * 10)] AS pct_http_1_1,
-  APPROX_QUANTILES(http_2, 1000)[OFFSET(percentile * 10)] AS pct_http_2,
-  APPROX_QUANTILES(quic, 1000)[OFFSET(percentile * 10)] AS pct_quic
+  ROUND(AVG(http_0_9/num_requests) * 100,2) avg_pct_http_0_9,
+  ROUND(AVG(http_1_0/num_requests) * 100,2) avg_pct_http_1_0,  
+  ROUND(AVG(http_1_1/num_requests) * 100,2) avg_pct_http_1_1,
+  ROUND(AVG(http_2/num_requests) * 100,2) avg_pct_http_2,
+  ROUND(AVG(quic/num_requests) * 100,2) avg_pct_quic
 FROM (
-  SELECT
+  SELECT 
     client,
-    COUNTIF(JSON_EXTRACT_SCALAR(payload, "$._protocol") = "http/0.9") / COUNT(0) AS http_0_9, 
-    COUNTIF(JSON_EXTRACT_SCALAR(payload, "$._protocol") = "http/1.0") / COUNT(0) AS http_1_0, 
-    COUNTIF(JSON_EXTRACT_SCALAR(payload, "$._protocol") = "http/1.1") / COUNT(0) AS http_1_1,
-    COUNTIF(JSON_EXTRACT_SCALAR(payload, "$._protocol") = "HTTP/2") / COUNT(0) AS http_2,
-    COUNTIF(REGEXP_CONTAINS(JSON_EXTRACT_SCALAR(payload, "$._protocol"), r"(?i)(quic|^h3|^http/3)")) AS quic
+    page,
+    COUNT(0) AS num_requests, 
+    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/0.9",1,0)) AS http_0_9, 
+    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/1.0",1,0)) AS http_1_0, 
+    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/1.1",1,0)) AS http_1_1,
+    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="HTTP/2",1,0)) AS http_2,
+    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="QUIC",1,0)) AS quic    
   FROM 
     `httparchive.almanac.requests`
   WHERE
-    date = '2020-08-01'
+    date = '2020-09-01'
   GROUP BY
     client,
-    page),           
-    UNNEST([10, 25, 50, 75, 90]) AS percentile
-GROUP BY
-  percentile,
-  client
-ORDER BY
-  percentile,
+    page
+)
+GROUP BY 
   client
