ae695a6b88d71d8d12d02f6ae050721de57b04e5
Greg Wolf <greg@hilltoptech.net>
2020-10-05 17:48:55 -0400

    Update sql/2020/22_HTTP_2/avg_percentage_of_resources_loaded_over_HTTP_by_version_per_site.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/22_HTTP_2/avg_percentage_of_resources_loaded_over_HTTP_by_version_per_site.sql
+++ sql/2020/22_HTTP_2/avg_percentage_of_resources_loaded_over_HTTP_by_version_per_site.sql
@@ -2,28 +2,25 @@
 # Average percentage of resources loaded over HTTP .9, 1, 1.1, 2, and QUIC per site
 SELECT
   client,
-  ROUND(AVG(http_0_9/num_requests) * 100,2) avg_pct_http_0_9,
-  ROUND(AVG(http_1_0/num_requests) * 100,2) avg_pct_http_1_0,  
-  ROUND(AVG(http_1_1/num_requests) * 100,2) avg_pct_http_1_1,
-  ROUND(AVG(http_2/num_requests) * 100,2) avg_pct_http_2,
-  ROUND(AVG(quic/num_requests) * 100,2) avg_pct_quic
+  protocol,
+  AVG(num_requests / total_requests) AS avg_pct_requests_per_page
 FROM (
   SELECT 
     client,
     page,
-    COUNT(0) AS num_requests, 
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/0.9",1,0)) AS http_0_9, 
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/1.0",1,0)) AS http_1_0, 
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="http/1.1",1,0)) AS http_1_1,
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="HTTP/2",1,0)) AS http_2,
-    SUM(IF(JSON_EXTRACT_SCALAR(payload, "$._protocol") ="QUIC",1,0)) AS quic    
+    IF(JSON_EXTRACT_SCALAR(payload, '$._protocol') IN ('http/0.9', 'http/1.0', 'http/1.1', 'HTTP/2', 'QUIC'), JSON_EXTRACT_SCALAR(payload, '$._protocol'), 'other') AS protocol,
+    COUNT(0) AS num_requests,
+    SUM(COUNT(0)) OVER (PARTITION BY client, page) AS total_requests
   FROM 
     `httparchive.almanac.requests`
   WHERE
-    date = '2020-09-01'
+    date = '2020-08-01'
   GROUP BY
     client,
-    page
-)
+    page,
+    protocol)
 GROUP BY 
-  client
+  client,
+  protocol
+ORDER BY
+  avg_pct_requests_per_page DESC
