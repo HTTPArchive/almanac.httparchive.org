c2539576ff28889059cfe1a3bf2924a3772037a8
Greg Wolf <greg@hilltoptech.net>
2020-10-07 12:56:37 -0400

    Update tls_adoption_by_http_version.sql

--- sql/2020/22_HTTP_2/tls_adoption_by_http_version.sql
+++ sql/2020/22_HTTP_2/tls_adoption_by_http_version.sql
@@ -1,36 +1,40 @@
 # standardSQL
-# Distribution of TLS versions
+# Distribution of TLS versions by HHTP Version
 SELECT
-  req.client,
-  JSON_EXTRACT_SCALAR(payload, '$._protocol') AS protocol,
-  domain.tls_version,
-  COUNT(0) AS freq,
-  COUNTIF(firstHtml) AS freq_firstHtml,
-  COUNTIF(firstReq) AS freq_firstReq
-FROM
-  `httparchive.almanac.requests` req,
-    (SELECT
+  client,
+  protocol,
+  tls_version,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM (
+  SELECT
     client,
-    NET.HOST(url) AS domain,
-    MAX(IFNULL(JSON_EXTRACT_SCALAR(payload, '$._tls_version'), JSON_EXTRACT_SCALAR(payload, '$._securityDetails.protocol'))) AS tls_version
+    page,
+    IF(JSON_EXTRACT_SCALAR(payload, '$._protocol') IN ('http/0.9', 'http/1.0', 'http/1.1', 'HTTP/2', 'QUIC', 'http/2+quic/46', 'HTTP/3'), JSON_EXTRACT_SCALAR(payload, '$._protocol'), 'other') AS protocol,
+    IFNULL(JSON_EXTRACT_SCALAR(payload, '$._tls_version'), JSON_EXTRACT_SCALAR(payload, '$._securityDetails.protocol')) AS tls_version
   FROM
     `httparchive.almanac.requests`
   WHERE
     date = '2020-08-01' AND
-    url like 'https://%'
+    STARTS_WITH(url, 'https') AND
+    firstHtml)
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.summary_pages.2020_08_01_*`
+  WHERE
+    STARTS_WITH(url, 'https')
   GROUP BY
-    client,
-    domain) domain
-WHERE
-  req.client = domain.client AND
-  NET.HOST(req.url) = domain.domain AND
-  req.date = '2020-08-01' AND
-  req.url like 'https://%'
+    client)
+USING
+  (client)
 GROUP BY
   client,
   protocol,
-  tls_version
+  tls_version,
+  total
 ORDER BY
-  freq desc,
-  client,
-  protocol
+  pct DESC
