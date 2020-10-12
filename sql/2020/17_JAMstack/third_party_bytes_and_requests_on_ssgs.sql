281528190e1ab4786909c32fd437eccd85d2661e
Artem Denysov <denysov.artem@gmail.com>
2020-10-12 19:08:34 +0300

    Jamstack 2020 queries (#1228)
    * add cdn_ttfb query
    
    * Update cdn_ttfb.sql
    
    * Add CWV for SSG
    
    * Add CWV distribution, thirdparty requests
    
    * Rename third party bytes
    
    * Improve CWV passing, add ssg adoption comparing to 2019
    
    * Add more queries
    
    * Update sql/2020/17_JAMstack/cdn_core_web_vitals_distribution.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/17_JAMstack/cdn_core_web_vitals_distribution.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Unitize ssg adoption query yoy
    
    * Use firstHtml for summary_requests, adjust readme
    
    * Add CO2, image adoption, LH median results
    
    * Fix namings
    
    * add image adoption queries
    
    * Align WV query
    
    * alignment
    
    * Update sql/2020/17_JAMstack/core_web_vitals_distribution.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/17_JAMstack/median_lighthouse_score.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/17_JAMstack/core_web_vitals_passing.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/17_JAMstack/third_party_bytes_and_requests_on_ssgs.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

---
+++ sql/2020/17_JAMstack/third_party_bytes_and_requests_on_ssgs.sql
@@ -0,0 +1,52 @@
+#standardSQL
+# Third party bytes and requests on SSGs
+SELECT
+  percentile,
+  client,
+  APPROX_QUANTILES(requests, 1000)[OFFSET(percentile * 10)] AS requests,
+  APPROX_QUANTILES(bytes, 1000)[OFFSET(percentile * 10)] / 1024 AS kbytes
+FROM (
+  SELECT
+    client,
+    COUNT(0) AS requests,
+    SUM(respSize) AS bytes
+  FROM (
+    SELECT
+      client,
+      page,
+      url,
+      respSize
+    FROM
+      `httparchive.almanac.requests`
+    WHERE
+      date = '2020-08-01')
+  JOIN (
+    SELECT
+      _TABLE_SUFFIX AS client,
+      url AS page
+    FROM
+      `httparchive.technologies.2020_08_01_*`
+    WHERE
+      LOWER(category) = "static site generator" OR
+	    app = "Next.js")
+  USING
+    (client, page)
+  WHERE
+    NET.HOST(url) IN (
+      SELECT
+        domain
+      FROM
+        `httparchive.almanac.third_parties`
+      WHERE
+        date = '2020-08-01' AND
+        category != 'hosting')
+  GROUP BY
+    client,
+    page),
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
+  client
