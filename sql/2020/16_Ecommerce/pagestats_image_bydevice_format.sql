83958200fb391b19969ad44f01500a29928b1cdc
Rick Viscomi <rviscomi@users.noreply.github.com>
2020-10-15 18:53:55 -0400

    Update sql/2020/16_Ecommerce/pagestats_image_bydevice_format.sql

--- sql/2020/16_Ecommerce/pagestats_image_bydevice_format.sql
+++ sql/2020/16_Ecommerce/pagestats_image_bydevice_format.sql
@@ -9,7 +9,7 @@ SELECT
 FROM
   `httparchive.almanac.summary_requests`
 JOIN
-  (SELECT _TABLE_SUFFIX AS client, url AS page FROM `httparchive.technologies.2020_07_01_*` WHERE category = 'Ecommerce')
+  (SELECT _TABLE_SUFFIX AS client, url AS page FROM `httparchive.technologies.2020_08_01_*` WHERE category = 'Ecommerce')
 USING (client, page)
 WHERE
   type = 'image'
