0323b71ee4fc28ef1b124f37587635d221f79375
Barry Pollard <barry_pollard@hotmail.com>
2020-10-19 15:53:46 +0100

    Improved HTTP/2 by site usage queries (#1368)

---
+++ sql/2020/22_HTTP_2/percentiles_of_resources_loaded_over_HTTP2_or_better_per_site.sql
@@ -0,0 +1,25 @@
+# standardSQL
+# Percentiles of sites that load resources of HTTP/2 or above
+SELECT
+  client,
+  percentile,
+  APPROX_QUANTILES(ROUND(http2_pct, 2), 1000)[OFFSET(percentile * 10)] AS http2_or_above
+FROM (
+  SELECT
+    client,
+    page,
+    COUNTIF(JSON_EXTRACT_SCALAR(payload, '$._protocol') IN ('HTTP/2', 'QUIC', 'http/2+quic/46')) / COUNT(0) AS http2_pct
+  FROM 
+    `httparchive.almanac.requests`
+  WHERE
+    date = '2020-08-01'
+  GROUP BY
+    client,
+    page),
+  UNNEST([5, 6, 7, 8, 9, 10, 25, 50, 75, 90, 95, 100]) AS percentile
+GROUP BY 
+  client,
+  percentile
+ORDER BY
+  client,
+  percentile
