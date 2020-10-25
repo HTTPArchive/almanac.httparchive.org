ecd26a46ae70d46375f4e3db3bb4536281928e38
Tony McCreath <tony@websiteadvantage.com.au>
2020-10-26 10:25:29 +1030

    added element frequency queries

---
+++ sql/2020/03_Markup/pages_element_count_by_device_and_element_count.sql
@@ -0,0 +1,31 @@
+#standardSQL
+# frequency of the number of element used on a page
+
+# returns all the data we need from _element_count
+CREATE TEMPORARY FUNCTION get_element_count(element_count_string STRING)
+RETURNS INT64 LANGUAGE js AS '''
+try {
+    if (!element_count_string) return result;
+
+    var element_count = JSON.parse(element_count_string);
+
+    if (Array.isArray(element_count) || typeof element_count != 'object') return result;
+
+    return Object.values(element_count).reduce((total, freq) => total + (parseInt(freq, 10) || 0), 0);  
+
+} catch (e) {}
+return null;
+''';
+
+SELECT
+  _TABLE_SUFFIX AS client,
+  get_element_count(JSON_EXTRACT_SCALAR(payload, '$._element_count')) AS elements,
+  COUNT(0) AS freq
+FROM 
+  `httparchive.pages.2020_08_01_*`
+GROUP BY
+  elements,
+  client
+ORDER BY
+  elements,
+  client
---
+++ sql/2020/03_Markup/pages_element_count_by_device_and_element_type_count.sql
@@ -0,0 +1,31 @@
+#standardSQL
+# frequency of the number of element types used on a page
+
+# returns all the data we need from _element_count
+CREATE TEMPORARY FUNCTION get_element_type_count(element_count_string STRING)
+RETURNS INT64 LANGUAGE js AS '''
+try {
+    if (!element_count_string) return result;
+
+    var element_count = JSON.parse(element_count_string);
+
+    if (Array.isArray(element_count) || typeof element_count != 'object') return result;
+
+    return Object.keys(element_count).length;  
+
+} catch (e) {}
+return null;
+''';
+
+SELECT
+  _TABLE_SUFFIX AS client,
+  get_element_type_count(JSON_EXTRACT_SCALAR(payload, '$._element_count')) AS element_types,
+  COUNT(0) AS freq
+FROM 
+  `httparchive.pages.2020_08_01_*`
+GROUP BY
+  element_types,
+  client
+ORDER BY
+  element_types,
+  client
