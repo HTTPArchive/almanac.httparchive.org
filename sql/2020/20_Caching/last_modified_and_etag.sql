112b11ade76ba7c1294d31cd0966fa7e4c42de15
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-10-27 16:07:23 +0530

    Mapped to writeup, fixed review comments.

--- sql/2020/20_Caching/last_modified_and_etag.sql
+++ sql/2020/20_Caching/last_modified_and_etag.sql
@@ -1,25 +1,34 @@
 #standardSQL
-# Presence of Last-Modified and ETag header
+# Presence of Last-Modified and ETag header, statistics on weak, strong, and invalid ETag.
 SELECT
   client,
   COUNT(0) AS total_requests,
+  COUNTIF(uses_no_etag) AS no_etag,
   COUNTIF(uses_etag) AS total_etag,
+  COUNTIF(weak_etag) AS total_weak_etag,
+  COUNTIF(strong_etag) AS total_strong_etag,
+  COUNTIF(NOT weak_etag AND NOT strong_etag AND uses_etag) AS total_invalid_etag,
   COUNTIF(uses_last_modified) AS total_last_modified,
   COUNTIF(uses_etag AND uses_last_modified) AS total_using_both,
   COUNTIF(NOT uses_etag AND NOT uses_last_modified) AS total_using_neither,
-  ROUND(COUNTIF(uses_etag) * 100 / COUNT(0), 2) AS pct_etag,
-  ROUND(COUNTIF(uses_last_modified) * 100 / COUNT(0), 2) AS pct_last_modified,
-  ROUND(COUNTIF(uses_etag AND uses_last_modified) * 100 / COUNT(0), 2) AS pct_uses_both,
-  ROUND(COUNTIF(NOT uses_etag AND NOT uses_last_modified) * 100 / COUNT(0), 2) AS pct_uses_neither
+  COUNTIF(uses_no_etag) * 100 / COUNT(0) AS pct_noetag,
+  COUNTIF(uses_etag) * 100 / COUNT(0) AS pct_etag,
+  COUNTIF(weak_etag) * 100 / COUNT(0) AS pct_weak_etag,
+  COUNTIF(strong_etag) * 100 / COUNT(0) AS pct_strong_etag,
+  COUNTIF(NOT weak_etag AND NOT strong_etag AND uses_etag) * 100 / COUNT(0) AS pct_invalid_etag,
+  COUNTIF(uses_last_modified) * 100 / COUNT(0) AS pct_last_modified,
+  COUNTIF(uses_etag AND uses_last_modified) * 100 / COUNT(0) AS pct_uses_both,
+  COUNTIF(NOT uses_etag AND NOT uses_last_modified) * 100 / COUNT(0) AS pct_uses_neither
 FROM (
   SELECT
-    client,
+    _TABLE_SUFFIX AS client,
+    TRIM(resp_etag) = "" AS uses_no_etag,
     TRIM(resp_etag) != "" AS uses_etag,
-    TRIM(resp_last_modified) != "" AS uses_last_modified
+    TRIM(resp_last_modified) != "" AS uses_last_modified,
+    REGEXP_CONTAINS(TRIM(resp_etag), '^W/\".*\"') AS weak_etag,
+    REGEXP_CONTAINS(TRIM(resp_etag), '^\".*\"') AS strong_etag
   FROM
-    `httparchive.almanac.requests`
-  WHERE
-    date = '2020-08-01'
+    `httparchive.summary_requests.2020_08_01_*`
 )
 GROUP BY
   client
