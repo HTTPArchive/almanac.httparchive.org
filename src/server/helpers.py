27cd062e27415723b1f8f40d3fca064c67026e76
Barry <barry@tunetheweb.com>
2020-10-31 19:40:53 +0000

    Handle more than base

--- src/server/helpers.py
+++ src/server/helpers.py
@@ -52,8 +52,10 @@ def render_error_template(error, status_code):
     lang = request.view_args.get('lang')
     year = request.view_args.get('year')
 
-    # Special error handling for base templates
-    if lang == 'base':
+    # Special error handling for base templates and other templates that might
+    # exist but shouldn't be called
+    supported_langs = [lan.lang_code for lan in (SUPPORTED_LANGUAGES.get(year) or [DEFAULT_LANGUAGE])]
+    if lang not in supported_langs:
         lang = DEFAULT_LANGUAGE.lang_code
 
     if not (os.path.isfile(TEMPLATES_DIR + '/%s/%s/error.html' % (lang, year))):
