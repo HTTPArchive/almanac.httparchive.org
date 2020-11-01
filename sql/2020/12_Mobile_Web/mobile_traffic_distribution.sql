2074f03d9679f6e0691b58dd01c860987ced3534
David Fox <david@davidjfox.com>
2020-11-01 09:49:01 -0600

    Only use sites that have desktop and phone entires

--- sql/2020/12_Mobile_Web/mobile_traffic_distribution.sql
+++ sql/2020/12_Mobile_Web/mobile_traffic_distribution.sql
@@ -4,11 +4,27 @@ SELECT
   percentile,
   APPROX_QUANTILES(phoneDensity, 1000)[OFFSET(percentile * 10)] AS pct_traffic_from_mobile,
 FROM
-  `chrome-ux-report.materialized.device_summary`,
+  (
+    # Since there are more phone sites indexed than desktop, only use sites which have both desktop and phone entries to get more accurate data
+    SELECT
+      phoneDensity
+    FROM
+      `chrome-ux-report.materialized.device_summary`
+    JOIN (
+      SELECT
+        origin,
+      FROM
+        `chrome-ux-report.materialized.device_summary`
+      WHERE
+        date = '2020-08-01' AND
+        device = 'desktop'
+    )
+    USING (origin)
+    WHERE
+      date = '2020-08-01' AND
+      device = 'phone'
+  ),
   UNNEST(GENERATE_ARRAY(1, 100)) AS percentile
-WHERE
-  date = '2020-08-01' AND
-  device = 'phone'
 GROUP BY
   percentile
 ORDER BY
