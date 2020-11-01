380e5c2bc04b528206bb2e2f127a72ad9aad6536
Barry <barry@tunetheweb.com>
2020-11-01 23:11:29 +0000

    Improved git diff

--- src/tools/scripts/set_lighthouse_urls.sh
+++ src/tools/scripts/set_lighthouse_urls.sh
@@ -43,8 +43,6 @@ LIGHTHOUSE_URLS=""
 BASE_URLS=$(cat <<-END
 http://127.0.0.1:8080/en/2019/
 http://127.0.0.1:8080/en/2019/javascript
-http://127.0.0.1:8080/en/2019/mobile-web
-http://127.0.0.1:8080/en/2019/seo
 http://127.0.0.1:8080/en/2020/
 END
 )
@@ -55,7 +53,7 @@ if [ "${production}" == "1" ]; then
     LIGHTHOUSE_CONFIG_FILE="${LIGHTHOUSE_PROD_CONFIG_FILE}"
 elif [ "${RUN_TYPE}" != "workflow_dispatch" ] && [ "${COMMIT_SHA}" != "" ]; then
     # If this is part of pull request then get list of files as those changed
-    CHANGED_FILES=$(git diff-tree --diff-filter=AM --no-commit-id --name-only "main...${COMMIT_SHA}" content templates | grep -v base.html | grep -v ejs | grep -v base_ | grep -v toc.html | grep -v sitemap)
+    CHANGED_FILES=$(git diff --diff-filter=AM --no-commit-id --name-only main content templates | grep -v base.html | grep -v ejs | grep -v base_ | grep -v toc.html | grep -v sitemap)
 
     LIGHTHOUSE_URLS=$(echo "${CHANGED_FILES}" | sed 's/src\/content/http:\/\/127.0.0.1:8080/g' | sed 's/\.md//g' | sed 's/\/base\//\/en\//g' | sed 's/src\/templates/http:\/\/127.0.0.1:8080/g' | sed 's/\.html//g' | sed 's/_/-/g' | sed 's/\/2019\/accessibility-statement/\/accessibility-statement/g' )
     if [ "${LIGHTHOUSE_URLS}" = "" ]; then
