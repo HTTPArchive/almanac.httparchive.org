d64d9a90396ef541c208e539b58cf2c700b68d8c
Barry <barry@tunetheweb.com>
2020-11-02 00:27:37 +0000

    Cleanup

--- src/tools/scripts/set_lighthouse_urls.sh
+++ src/tools/scripts/set_lighthouse_urls.sh
@@ -52,10 +52,12 @@ if [ "${production}" == "1" ]; then
     LIGHTHOUSE_URLS=$(curl -s https://almanac.httparchive.org/sitemap.xml | grep "<loc" | grep -v static | sed 's/ *<loc>//g' | sed 's/<\/loc>//g')
     LIGHTHOUSE_CONFIG_FILE="${LIGHTHOUSE_PROD_CONFIG_FILE}"
 elif [ "${RUN_TYPE}" == "pull_request" ] && [ "${COMMIT_SHA}" != "" ]; then
+    # Checkout main to get list of differences
     git pull --quiet
     git checkout main
     # If this is part of pull request then get list of files as those changed
     CHANGED_FILES=$(git diff --name-only "main...${COMMIT_SHA}" --diff-filter=d content templates | grep -v base.html | grep -v ejs | grep -v base_ | grep -v toc.html | grep -v sitemap)
+    # Back to the pull request changes
     git checkout --progress --force "${COMMIT_SHA}"
 
     LIGHTHOUSE_URLS=$(echo "${CHANGED_FILES}" | sed 's/src\/content/http:\/\/127.0.0.1:8080/g' | sed 's/\.md//g' | sed 's/\/base\//\/en\//g' | sed 's/src\/templates/http:\/\/127.0.0.1:8080/g' | sed 's/\.html//g' | sed 's/_/-/g' | sed 's/\/2019\/accessibility-statement/\/accessibility-statement/g' )
