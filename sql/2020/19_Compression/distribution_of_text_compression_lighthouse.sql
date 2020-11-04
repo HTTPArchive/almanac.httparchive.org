9e1ce4cc26528c2d032ebb0305717175ef399155
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-11-04 12:05:57 +0800

    Update sql/2020/19_Compression/distribution_of_text_compression_lighthouse.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/19_Compression/distribution_of_text_compression_lighthouse.sql
+++ sql/2020/19_Compression/distribution_of_text_compression_lighthouse.sql
@@ -4,6 +4,7 @@ SELECT
   _TABLE_SUFFIX AS client,
   JSON_EXTRACT_SCALAR(report, "$.audits.uses-text-compression.score") AS text_compression_score,
   COUNT(0) AS num_pages,
+  SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
   COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS pct_pages
 FROM 
   `httparchive.lighthouse.2020_08_01_*` 
