de74fcd3b8d4bb3b931b6d503a2904e5cefe6e39
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-04 17:18:09 +0530

    Avoid blank resp_cache_control.
    A blank resp_cache_control may also result in exp_age = 0

--- sql/2020/20_Caching/ttl.sql
+++ sql/2020/20_Caching/ttl.sql
@@ -4,19 +4,18 @@ SELECT
   client,
   COUNT(0) AS total_requests,
   COUNTIF(NOT uses_cache_control AND NOT uses_expires) AS total_using_neither,
-  COUNTIF(uses_no_store) AS total_using_no_store,
-  COUNTIF(NOT uses_no_store AND exp_age = 0) AS total_exp_age_zero,
-  COUNTIF(NOT uses_no_store AND exp_age > 0) AS total_exp_age_gt_zero,
+  COUNTIF(NOT uses_no_store AND uses_max_age AND exp_age = 0) AS total_exp_age_zero,
+  COUNTIF(NOT uses_no_store AND uses_max_age AND exp_age > 0) AS total_exp_age_gt_zero,
   COUNTIF(NOT uses_cache_control AND NOT uses_expires) / COUNT(0) AS pct_using_neither,
-  COUNTIF(uses_no_store) / COUNT(0) AS pct_using_no_store,  
-  COUNTIF(NOT uses_no_store AND exp_age = 0) / COUNT(0) AS pct_using_exp_age_zero,
-  COUNTIF(NOT uses_no_store AND exp_age > 0) / COUNT(0) AS pct_using_exp_age_gt_zero
+  COUNTIF(NOT uses_no_store AND uses_max_age AND exp_age = 0) / COUNT(0) AS pct_using_exp_age_zero,
+  COUNTIF(NOT uses_no_store AND uses_max_age AND exp_age > 0) / COUNT(0) AS pct_using_exp_age_gt_zero
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
     TRIM(resp_cache_control) != "" AS uses_cache_control,
     TRIM(resp_expires) != "" AS uses_expires,
     REGEXP_CONTAINS(resp_cache_control, r'(?i)no-store') AS uses_no_store,
+    REGEXP_CONTAINS(resp_cache_control, r'(?i)max-age\s*=\s*[0-9]+') AS uses_max_age,
     expAge AS exp_age
   FROM
     `httparchive.summary_requests.2020_08_01_*`
