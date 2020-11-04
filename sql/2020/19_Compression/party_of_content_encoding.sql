c13907d1d0e0dcd10114c9affa26971c8e0d1909
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-11-04 12:09:44 +0800

    Update sql/2020/19_Compression/party_of_content_encoding.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/19_Compression/party_of_content_encoding.sql
+++ sql/2020/19_Compression/party_of_content_encoding.sql
@@ -2,10 +2,15 @@
 #party of content encoding
 SELECT
   client,
-  CASE
-   WHEN req_host!= ' ' THEN 'first-party'
-   ELSE 'thrid-party' END
-  AS party,
+  IF(NET.HOST(url) IN (
+    SELECT
+      domain
+    FROM
+      `httparchive.almanac.third_parties`
+    WHERE
+      date = '2020-08-01' AND
+      category != 'hosting'
+  ), 'third party', 'first party') AS party
   resp_content_encoding,
   COUNT(0) AS num_requests,
   COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
