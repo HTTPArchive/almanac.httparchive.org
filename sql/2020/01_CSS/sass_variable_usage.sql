c99f7d88de2036d8cfa9621d6a721e9237387a21
Rick Viscomi <rviscomi@gmail.com>
2020-11-05 12:27:55 -0500

    feedback part 2

--- sql/2020/01_CSS/sass_variable_usage.sql
+++ sql/2020/01_CSS/sass_variable_usage.sql
@@ -20,8 +20,6 @@ SELECT
   client,
   variable,
   COUNT(DISTINCT page) AS pages,
-  total_sass,
-  COUNT(DISTINCT page) / total_sass AS pct_pages,
   SUM(freq) AS freq,
   SUM(SUM(freq)) OVER (PARTITION BY client) AS total,
   SUM(freq) / SUM(SUM(freq)) OVER (PARTITION BY client) AS pct
@@ -34,18 +32,9 @@ FROM (
   FROM
     `httparchive.pages.2020_08_01_*`,
     UNNEST(getVariableUsage(payload)) AS variable)
-JOIN (
-  SELECT
-    _TABLE_SUFFIX AS client,
-    COUNT(DISTINCT IF(SAFE_CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._sass'), '$.scss.size') AS INT64) > 0, url, NULL)) OVER (PARTITION BY _TABLE_SUFFIX) AS total_sass
-  FROM
-    `httparchive.pages.2020_08_01_*`)
-USING
-  (client)
 GROUP BY
   client,
-  variable,
-  total_sass
+  variable
 ORDER BY
   pct DESC
 LIMIT
