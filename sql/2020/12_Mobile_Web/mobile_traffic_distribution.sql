c5117e698402a488f0d269e966696c5fcd9c90aa
David Fox <david@davidjfox.com>
2020-11-06 11:10:52 -0600

    Revert changes

--- sql/2020/12_Mobile_Web/mobile_traffic_distribution.sql
+++ sql/2020/12_Mobile_Web/mobile_traffic_distribution.sql
@@ -4,27 +4,11 @@ SELECT
   percentile,
   APPROX_QUANTILES(phoneDensity, 1000)[OFFSET(percentile * 10)] AS pct_traffic_from_mobile,
 FROM
-  (
-    # Since there are more phone sites indexed than desktop, only use sites which have both desktop and phone entries to get more accurate data
-    SELECT
-      phoneDensity
-    FROM
-      `chrome-ux-report.materialized.device_summary`
-    JOIN (
-      SELECT
-        origin,
-      FROM
-        `chrome-ux-report.materialized.device_summary`
-      WHERE
-        date = '2020-08-01' AND
-        device = 'desktop'
-    )
-    USING (origin)
-    WHERE
-      date = '2020-08-01' AND
-      device = 'phone'
-  ),
+  `chrome-ux-report.materialized.device_summary`,
   UNNEST(GENERATE_ARRAY(1, 100)) AS percentile
+WHERE
+  date = '2020-08-01' AND
+  device = 'phone'
 GROUP BY
   percentile
 ORDER BY
