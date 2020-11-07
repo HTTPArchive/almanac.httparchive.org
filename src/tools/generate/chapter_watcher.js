6c3edbf27c42c0ef02d33dce6f601b68686456f2
Barry <barry@tunetheweb.com>
2020-11-07 15:28:29 +0000

    Remove unused code

--- src/tools/generate/chapter_watcher.js
+++ src/tools/generate/chapter_watcher.js
@@ -6,18 +6,5 @@ watch('content', { filter: /\.md$/, recursive: true }, async function(evt, name)
   console.log('File modified: %s', name);
   if (evt != 'remove') {
     await generate_chapters(name)
-      /*
-      const command = 'npm run generate ' + name;
-      exec (command, (err, stdout, stderr) => {
-        if (err) {
-          // some err occurred
-          console.error(err)
-        } else {
-          // the *entire* stdout and stderr (buffered)
-          console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
-        }
-      });
-      */
   }
 });
