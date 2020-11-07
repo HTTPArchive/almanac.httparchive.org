ae835effe42b3acf24aebf3c1054c58afc36f031
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-07 23:13:29 +0530

    Added more metrics.

--- sql/2020/20_Caching/non_cacheable_by_resource_type.sql
+++ sql/2020/20_Caching/non_cacheable_by_resource_type.sql
@@ -4,14 +4,25 @@ SELECT
   client,
   resource_type,
   COUNT(0) AS total_requests,
-  COUNTIF(uses_cache_control AND uses_no_store) AS total_non_cacheable,
-  COUNTIF(uses_cache_control AND uses_no_store) / COUNT(0) AS pct_non_cacheable
+  COUNTIF(NOT uses_no_store) AS total_cacheable,
+  COUNTIF(uses_no_store) AS total_non_cacheable,
+  COUNTIF(NOT uses_cache_control AND NOT uses_expires) AS total_using_neither,
+  COUNTIF(NOT uses_no_store AND uses_max_age AND exp_age = 0) AS total_exp_age_zero,
+  COUNTIF(NOT uses_no_store AND uses_max_age AND exp_age > 0) AS total_exp_age_gt_zero,
+  COUNTIF(NOT uses_no_store) / COUNT(0) AS pct_cacheable, 
+  COUNTIF(uses_no_store) / COUNT(0) AS pct_non_cacheable,
+  COUNTIF(NOT uses_cache_control AND NOT uses_expires) / COUNTIF(NOT uses_no_store) AS pct_using_neither,
+  COUNTIF(NOT uses_no_store AND uses_max_age AND exp_age = 0) / COUNTIF(NOT uses_no_store) AS pct_using_exp_age_zero,
+  COUNTIF(NOT uses_no_store AND uses_max_age AND exp_age > 0) / COUNTIF(NOT uses_no_store) AS pct_using_exp_age_gt_zero
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
     type AS resource_type,
     TRIM(resp_cache_control) != "" AS uses_cache_control,
-    REGEXP_CONTAINS(resp_cache_control, r'(?i)no-store') AS uses_no_store
+    TRIM(resp_expires) != "" AS uses_expires,
+    REGEXP_CONTAINS(resp_cache_control, r'(?i)no-store') AS uses_no_store,
+    REGEXP_CONTAINS(resp_cache_control, r'(?i)max-age\s*=\s*[0-9]+') AS uses_max_age,
+    expAge AS exp_age
   FROM
     `httparchive.summary_requests.2020_08_01_*`
 )
@@ -20,5 +31,4 @@ GROUP BY
   resource_type
 ORDER BY
   client,
-  resource_type
-  
+  resource_type  
