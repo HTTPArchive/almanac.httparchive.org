45f82315c07afd038f0045303cc5722a397fc310
unknown <apppletsai@gmail.com>
2020-11-08 18:09:46 +0800

    change query of byte_saving as review suggestion

--- sql/2020/19_Compression/19_02.byte_saving_of_text_compression.sql
+++ sql/2020/19_Compression/19_02.byte_saving_of_text_compression.sql
@@ -4,9 +4,7 @@ SELECT
   _TABLE_SUFFIX AS client,
   IF(JSON_EXTRACT_SCALAR(report, "$.audits.uses-text-compression.score") != "1", 'compression', 'non_compression') AS compression,
   percents,
-  APPROX_QUANTILES(( CAST(JSON_EXTRACT_SCALAR(report, "$.audits.uses-text-compression.details.overallSavingsBytes") AS INT64) / 1024), 1000)[OFFSET(percents * 10)] AS kbyte_savings,
-  COUNT(0) AS num_pages,
-  SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,      
+  APPROX_QUANTILES(( CAST(JSON_EXTRACT_SCALAR(report, "$.audits.uses-text-compression.details.overallSavingsBytes") AS INT64) / 1024), 1000)[OFFSET(percents * 10)] AS kbyte_savings,     
 FROM
   `httparchive.lighthouse.2020_08_01_*`,
   unnest([10,25,50,75,90]) AS percents
@@ -14,7 +12,7 @@ WHERE
   report IS NOT NULL 
 GROUP BY 
   _TABLE_SUFFIX, 
-  report,
+  compression,
   percents
 ORDER BY 
   _TABLE_SUFFIX, percents ASC
