a5cbe77943ab7202c9ed55fdef71b20560a62033
AbbyTsai <44442451+AbbyTsai@users.noreply.github.com>
2020-11-08 15:02:45 +0800

    Fonts 2020 queries  (#1133)
    * add checklist of metrics
    
    * revert readme.md
    
    * Create pct_web fonts_usage.sql
    
    * fix linter checker
    
    * rename and update.
    
    * create
    
    * Create
    
    * create
    
    * create
    
    * create
    
    * rename
    
    * rename
    
    * create
    
    * Create
    
    * create
    
    * create
    
    * create
    
    * Create 04.12.font weight, font style
    
    * create
    
    * create
    
    * create
    
    * create
    
    * create
    
    * create
    
    * create
    
    * create
    
    * create
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * updat
    
    * update
    
    * Update 04_05.web_font_usage_breakdown_with_fcp.sql
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * move file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * Rename 04_13.font_stretch.sql to sql/2020/04_Fonts/04_13.font_stretch.sql
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * change file path
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Test commit
    
    * update files
    
    * "commit update"
    
    * "update commit"
    
    * "update commit"
    
    * 'update commit'
    
    * 'update commit'
    
    * update
    
    * 'update commit'
    
    * 'update commit'
    
    * 'update filename & files'
    
    * 'rename update'
    
    * Update sql/2020/04_Fonts/04_06.font_display_with_fcp.sql
    
    Co-authored-by: Paul Calvano <paulcalvano@yahoo.com>
    
    * update files
    
    * Update sql/2020/04_Fonts/04_01.web_fonts_usage.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/04_Fonts/04_01.web_fonts_usage.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/04_Fonts/04_01.web_fonts_usage.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/04_Fonts/04_02.web_fonts_usage_by_country.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/04_Fonts/04_04.local_vs_host_with_fcp.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/04_Fonts/04_06.font_display_with_fcp.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update sql/2020/04_Fonts/04_06.font_display_with_fcp.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * update commit
    
    * update review commit
    
    * update review commit
    
    * update review commit
    
    * "update commit"
    
    * update commit
    
    * update review commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * Update sql/2020/04_Fonts/04_04.local_vs_host_with_fcp.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * update commit
    
    * filename and opentype change
    
    Co-authored-by: Paul Calvano <paulcalvano@yahoo.com>
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

---
+++ sql/2020/04_Fonts/04_08.font_unicode_range_with_fcp.sql
@@ -0,0 +1,73 @@
+#standardSQL
+#font_unicode_range_with_fcp
+CREATE TEMPORARY FUNCTION getFonts(css STRING)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+    var reduceValues = (values, rule) => {
+        if ('rules' in rule) {
+            return rule.rules.reduce(reduceValues, values);
+        }
+        if (!('declarations' in rule)) {
+            return values;
+        }
+        if (rule.type != 'font-face') {
+            return values;
+        }
+        rule.declarations.forEach(d => {
+            if (d.property.toLowerCase() == 'unicode-range') {
+                values.push(d.value);
+            }
+        });
+        return values;
+    };
+    var $ = JSON.parse(css);
+    return $.stylesheet.rules.reduce(reduceValues, []);
+} catch (e) {
+    return [null];
+}
+''';
+SELECT
+  client,
+  CASE
+    WHEN unicode != " " THEN "unicode_ranges"
+  ELSE "none"
+  END AS use_unicode,
+  COUNT(DISTINCT page) AS pages,
+  SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client) AS total,
+  COUNT(DISTINCT page) / SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client) AS pct_range,
+  APPROX_QUANTILES(fcp, 1000)[OFFSET(500)] AS median_fcp,
+  APPROX_QUANTILES(lcp, 1000)[OFFSET(500)] AS median_lcp
+FROM (
+  SELECT
+    client,
+    page,
+    unicode
+  FROM
+    `httparchive.almanac.parsed_css`
+  LEFT JOIN
+    UNNEST(getFonts(css)) AS unicode
+  WHERE
+    date = '2020-08-01'
+  GROUP BY
+    client,
+    page,
+    unicode)
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url AS page,
+    CAST(JSON_EXTRACT_SCALAR(payload,
+        "$['_chromeUserTiming.firstContentfulPaint']") AS INT64) AS fcp,
+    CAST(JSON_EXTRACT_SCALAR(payload,
+        "$['_chromeUserTiming.LargestContentfulPaint']") AS INT64) AS lcp,
+  FROM
+    `httparchive.pages.2020_08_01_*`
+  GROUP BY
+    _TABLE_SUFFIX, page, payload)
+USING
+  (client, page)
+GROUP BY
+  client,
+  use_unicode
+ORDER BY
+  pages, client DESC
