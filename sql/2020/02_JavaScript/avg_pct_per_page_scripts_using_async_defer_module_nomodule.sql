755560e2d3b25c623702c7fac99266252644df3f
paulcalvano <paulcalvano@yahoo.com>
2020-11-09 00:16:48 -0500

    avg percent per page of scripts using async defer module nomodule

---
+++ sql/2020/02_JavaScript/avg_pct_per_page_scripts_using_async_defer_module_nomodule.sql
@@ -0,0 +1,44 @@
+#standardSQL
+# Average Percent per page of external scripts using Async, Defer, Module or NoModule attributes.
+SELECT 
+  client,
+  AVG(pct_external_async) AS avg_pct_external_async,
+  AVG(pct_external_defer) AS avg_pct_external_defer,
+  AVG(pct_external_module) AS avg_pct_external_module,
+  AVG(pct_external_nomodule) AS avg_pct_external_nomodule
+FROM
+(
+  SELECT 
+    client,
+    page,
+    COUNT(*) as external_scripts,
+    SUM(IF(script LIKE "%async%",1,0)) AS async,
+    SUM(IF(script LIKE "%defer%",1,0)) AS defer,
+    SUM(IF(script LIKE "%module%",1,0)) AS module,
+    SUM(IF(script LIKE "%nomodule%",1,0)) AS nomodule,
+    SUM(IF(script LIKE "%async%",1,0))    / COUNT(*) AS pct_external_async,
+    SUM(IF(script LIKE "%defer%",1,0))    / COUNT(*) AS pct_external_defer,
+    SUM(IF(script LIKE "%module%",1,0))   / COUNT(*) AS pct_external_module,
+    SUM(IF(script LIKE "%nomodule%",1,0)) / COUNT(*) AS pct_external_nomodule
+  FROM 
+  (
+    SELECT  
+      _TABLE_SUFFIX AS client,
+      page, 
+      url,
+      REGEXP_EXTRACT_ALL(LOWER(body), "(<script [^>]*)") AS scripts
+    FROM 
+      `httparchive.response_bodies.2020_08_01_*` 
+    WHERE  
+      LOWER(body) LIKE "%<html%"
+  )
+  CROSS JOIN
+    UNNEST(scripts) as script
+  WHERE 
+    script LIKE "%src%"
+  GROUP BY 
+    client,
+    page
+)
+GROUP BY 
+  client
