f937fdd8fb757ba044efe60ee7727d8432b4d72f
Barry Pollard <barry_pollard@hotmail.com>
2020-11-09 07:28:08 +0000

    Auto start npm run watch (#1457)
    * Add file watcher to start and stop
    
    * Add README changes
    
    * Add stop scripts
    
    * Fix issue with generate script on Windows
    
    * Fix bug on Window
    
    * Linting fixes
    
    * Remove unneeded exec
    
    * Restart server in debug mode at the end
    
    * Bug fixes and comments
    
    * Move file watcher in debug mode
    
    * Change start to call for windows

--- src/tools/generate/chapter_watcher.js
+++ src/tools/generate/chapter_watcher.js
@@ -1,6 +1,5 @@
 var watch = require('node-watch');
 let { generate_chapters } = require('./generate_chapters');
-const { exec } = require("child_process");
  
 watch('content', { filter: /\.md$/, recursive: true }, async function(evt, name) {
   console.log('File modified: %s', name);
--- src/tools/generate/generate_chapters.js
+++ src/tools/generate/generate_chapters.js
@@ -52,8 +52,8 @@ const generate_chapters = async (chapter_match) => {
     chapter_match = chapter_match.replace(/\.md$/,'');
     chapter_match = chapter_match.replace(/^content[\/\\]*/,'');
     chapter_match = (process.platform != 'win32')
-                ? 'content\/' +  '(' + chapter_match.replace(/\//g,')/(') + ')\.md'
-                : 'content\\' +  '(' + chapter_match.replace(/\\/g,')\(') + ')\.md';
+                ? 'content\/' +  '(' + chapter_match.replace(/\//g,')/(') + ').md'
+                : 'content\\\\' +  '(' + chapter_match.replace(/\\/g,')\\\\(') + ').md';
     re = new RegExp(chapter_match);
   } else {
     re = (process.platform != 'win32')
