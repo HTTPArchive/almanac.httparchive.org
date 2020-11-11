b444d2f4b212c9b36191bb96901adbf855a5de56
nrllh <13475745+nrllh@users.noreply.github.com>
2020-11-11 12:24:13 +0100

    query for Web Cryptography API

---
+++ sql/2020/11_Security/web_cryptography_api.sql
@@ -0,0 +1,15 @@
+#standartSQL
+SELECT
+  client,
+  feature,
+  num_urls,
+  total_urls,
+  pct_urls
+FROM
+  `httparchive.blink_features.usage`
+WHERE
+  (feature LIKE 'Crypto%'
+    OR feature LIKE 'Subtle%')
+  AND yyyymmdd='20200801'
+ORDER BY
+  pct_urls DESC
