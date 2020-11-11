b0379acc134c41ae64f8d4cd6481984f00620ace
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-11 10:39:40 +0530

    Corrected.
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/20_Caching/appcache_and_serviceworkers.sql
+++ sql/2020/20_Caching/appcache_and_serviceworkers.sql
@@ -4,7 +4,9 @@ SELECT
   IF(STARTS_WITH(url, 'https'), 'https', 'http') AS http_type,
   JSON_EXTRACT_SCALAR(report, "$.audits.appcache-manifest.score") AS using_appcache,
   JSON_EXTRACT_SCALAR(report, "$.audits.service-worker.score") AS using_serviceworkers,
-  count(*) AS occurences     
+  COUNT(0) AS occurrences,
+  SUM(COUNT(0)) OVER () AS total,
+  COUNT(0) / SUM(COUNT(0)) OVER () AS pct
 FROM 
   `httparchive.lighthouse.2020_08_01_mobile` 
 GROUP BY 
