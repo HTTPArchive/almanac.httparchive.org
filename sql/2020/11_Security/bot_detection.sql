f9a6869ad52d4c1014b16fb2acbfbd5a4dd3b877
nrllh <13475745+nrllh@users.noreply.github.com>
2020-11-11 12:24:53 +0100

    query for Bot detection

---
+++ sql/2020/11_Security/bot_detection.sql
@@ -0,0 +1,27 @@
+#standartSQL
+SELECT
+  _TABLE_SUFFIX AS client,
+  app,
+  COUNT(0) AS freq,
+  total,
+  ROUND(COUNT(0) * 100 / total, 2) AS pct
+FROM
+  `httparchive.technologies.2020_08_01_*`
+JOIN (
+  SELECT
+    _TABLE_SUFFIX,
+    COUNT(0) AS total
+  FROM
+    `httparchive.summary_pages.2020_08_01_*`
+  GROUP BY
+    _TABLE_SUFFIX)
+USING
+  (_TABLE_SUFFIX)
+WHERE
+  category = 'Security'
+GROUP BY
+  client,
+  total,
+  app
+ORDER BY
+  freq / total DESC 
