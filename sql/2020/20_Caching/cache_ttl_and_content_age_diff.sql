6a38b1984bc72a447ed33ec2ab7a240c6b6acce6
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-12 10:19:51 +0530

    Caching 2020 queries (#1318)
    * Update README.md
    
    * Initial file
    
    * To pass linting checks start with heading.
    
    * Remove query numbers in filename.
    
    * Added file with query number.
    
    Added a new blank line at the end.
    
    * TTL by resource and third-party
    
    * Resources served without cache option.
    
    * Third party resources served without using cache.
    
    * Seq. of GROUP BY and ORDER BY made similar.
    
    * Seq. of GROUP BY and ORDER BY made similar, date changed.
    
    * Seq. of GROUP BY and ORDER BY made similar.
    
    * Seq. of GROUP BY and ORDER BY made similar.
    
    * Initial version of query.
    
    * Initial version of query.
    
    * Initial version of query.
    
    * Initial version of query.
    
    * Seq. of fields in SELECT, GROUP, and ORDER made same.
    
    * Seq. of fields in SELECT, GROUP, and ORDER made same.
    
    * Date corrected.
    
    * Removed blank line.
    
    * Seq. of SELECT, GROUP, and ORDER BY made similar.
    
    * Mapped to writeup, fixed review comments.
    
    * Mapped to writeup.
    
    * Fixed naming inconsistencies.
    
    * Fixed naming convention.
    
    * Removed * 100.
    
    * Removed * 100.
    
    * Initial version, mapped to chapter.
    
    * Initial version, mapped to chapter.
    
    * Initial version, mapped to chapter.
    
    * Fixed naming inconsistency.
    
    * No longer used, as per chapter.
    
    * Initial version, mapped to chapter.
    
    * Updated to report invalid count and pct.
    
    * Removed, replaced by invalid_last_modified_and_expires_and_date.sql
    
    * Removed, not used.
    
    * Mapped query to chapter.
    
    * Mapped query to chapter.
    
    * Initial query.
    
    * Duplicate query.
    
    * Mapped query to chapter.
    
    * Not needed in context of the chapter.
    
    * Avoid blank resp_cache_control.
    
    A blank resp_cache_control may also result in exp_age = 0
    
    * Added no-store statistics.
    
    * Added cacheable %.
    
    * Initial version.
    
    * Will add a new version based on Chapter.
    
    * Will add a new version based on Chapter.
    
    * Will add a revised version as per Chapter metrics.
    
    * third party query not used.
    
    * Initial version.
    
    * Initial version.
    
    * Denominator in percent corrected.
    
    * Added more metrics.
    
    * Initial version
    
    * Renamed.
    
    * Initial version.
    
    * Renamed file.
    
    * Renamed file.
    
    * Renamed file.
    
    * Updated fetching of http_type.
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Corrected.
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Modified ORDER BY.
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Initial version.
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

---
+++ sql/2020/20_Caching/cache_ttl_and_content_age_diff.sql
@@ -0,0 +1,35 @@
+#standardSQL
+# Difference between Cache TTL and the contents age
+CREATE TEMPORARY FUNCTION toTimestamp(date_string STRING)
+RETURNS INT64 LANGUAGE js AS '''
+  try {
+    var timestamp = Math.round(new Date(date_string).getTime() / 1000);
+    return isNaN(timestamp) ? -1 : timestamp;
+  } catch (e) {
+    return -1;
+  }
+''';
+
+SELECT
+  client,
+  percentile,
+  APPROX_QUANTILES(diff_in_days, 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS diff_in_days
+FROM
+  (
+    SELECT
+      _TABLE_SUFFIX AS client,
+      ROUND((expAge - (startedDateTime - toTimestamp(resp_last_modified))) / 86400, 2)  AS diff_in_days
+    FROM
+      `httparchive.summary_requests.2020_08_01_*`
+    WHERE
+      resp_last_modified <> "" AND
+      expAge > 0
+  ),
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
+GROUP BY
+  client,
+  percentile
+ORDER BY
+  client,
+  percentile
+
