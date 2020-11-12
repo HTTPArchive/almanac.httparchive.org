d2a3bd40c74b4ebf486cdadf4b896b88dcd55287
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-12 14:48:14 +0530

    Initial files.

---
+++ sql/2020/20_Caching/cache_ttl_lighthouse_score.sql
@@ -0,0 +1,15 @@
+#standardSQL
+# Distribution of cache TTL Lighthouse scores
+SELECT 
+  _TABLE_SUFFIX AS client,
+  JSON_EXTRACT_SCALAR(report, "$.audits.uses-long-cache-ttl.score") AS caching_score,
+  COUNT(*) AS num_pages,
+  COUNT(0) * 100 / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS pct_pages
+FROM 
+  `httparchive.lighthouse.2020_08_01_*` 
+GROUP BY 
+  client,
+  caching_score
+ORDER BY 
+  client, 
+  caching_score ASC
