410311d6d9ca60d34efaf2d40ae2255375843382
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-12 22:03:55 +0530

    Added column name.

--- sql/2020/20_Caching/cache_wastedbytes_lighthouse.sql
+++ sql/2020/20_Caching/cache_wastedbytes_lighthouse.sql
@@ -2,7 +2,7 @@
 # Distribution of bytes wasted (absence of adequate caching) from Lighthouse
 SELECT 
   _TABLE_SUFFIX AS client,
-  ROUND(CAST(JSON_EXTRACT_SCALAR(report, "$.audits.uses-long-cache-ttl.details.summary.wastedBytes")as numeric)/1024/1024) mbyte_savings,
+  ROUND(CAST(JSON_EXTRACT_SCALAR(report, "$.audits.uses-long-cache-ttl.details.summary.wastedBytes")as numeric)/1024/1024) AS mbyte_savings,
   COUNT(*) num_pages,
   COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS pct_pages       
 FROM 
