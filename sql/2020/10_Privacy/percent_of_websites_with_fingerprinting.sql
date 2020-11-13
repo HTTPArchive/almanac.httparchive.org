f4d201b8a6334d6e9c564997089b68f9ded05bb3
Maksym Ostapenko <maksym.ostapenko@w0dyq.speedport.ip>
2020-11-13 03:18:49 +0100

    added client to fingerprint

--- sql/2020/10_Privacy/percent_of_websites_with_fingerprinting.sql
+++ sql/2020/10_Privacy/percent_of_websites_with_fingerprinting.sql
@@ -1,5 +1,5 @@
 #standardSQL
-# Percent of pages with fingerprint library
+# Percent of pages with fingerprint.js library
 
 WITH requests AS (
   SELECT
@@ -12,24 +12,18 @@ WITH requests AS (
 base AS(
   SELECT
     client,
-    page,
-    url
-  FROM
-    requests
-  GROUP BY
-    client,
-    page,
-    url
-)
-
-SELECT
-  total_pages,
-  fingerprint_pages,
-  fingerprint_pages / total_pages AS pct_fingerprint_pages,
-FROM (
-  SELECT
     COUNT(DISTINCT page) AS total_pages,
     COUNT(DISTINCT IF(url LIKE "%fingerprint2.min.js%" OR url LIKE "%fingerprintjs2%", page, NULL)) AS fingerprint_pages,
   FROM
-    base
+    requests
+  GROUP BY
+    client
 )
+
+SELECT
+  client,
+  total_pages,
+  fingerprint_pages,
+  fingerprint_pages / total_pages AS pct_fingerprint_pages,
+FROM
+  base
