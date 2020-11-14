9ab90bb5b94ac4b5fbb4562dc1c3b928b5091ebc
nrllh <13475745+nrllh@users.noreply.github.com>
2020-11-14 01:20:31 +0100

    Security 2020 queries

---
+++ sql/2020/11_Security/cryptominer_usage.sql
@@ -0,0 +1,24 @@
+#standardSQL
+SELECT
+  month,
+  client,
+  freq
+FROM (
+  SELECT
+    LEFT(_TABLE_SUFFIX, 10) AS month,
+  IF
+    (ENDS_WITH(_TABLE_SUFFIX, '_desktop'),
+      'desktop',
+      'mobile') AS client,
+    COUNT(0) AS freq
+  FROM
+    `httparchive.technologies.*`
+  WHERE
+    category = 'Cryptominers'
+    OR category='Cryptominer'
+  GROUP BY
+    _TABLE_SUFFIX)
+ORDER BY
+  client,
+  month,
+  freq DESC
