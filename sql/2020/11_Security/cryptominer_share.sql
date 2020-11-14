1a1fce1b20d2e053a6e12dd8c65ca2e14f02c290
nrllh <13475745+nrllh@users.noreply.github.com>
2020-11-14 01:21:27 +0100

    Security 2020 queries

---
+++ sql/2020/11_Security/cryptominer_share.sql
@@ -0,0 +1,38 @@
+#standardSQL
+--share of cryptominers
+WITH
+  total AS (
+  SELECT
+    COUNT(0) AS ttl
+  FROM
+    `httparchive.technologies.2020_10_01_desktop`
+  WHERE
+    category = 'Cryptominers'
+    OR category='Cryptominer')
+SELECT
+  app,
+  freq,
+  ROUND((freq/(
+      SELECT
+        ttl
+      FROM
+        total)),2) AS pct
+FROM (
+  SELECT
+    SUM(freq) AS freq,
+    app
+  FROM (
+    SELECT
+      COUNT(0) AS freq,
+      app
+    FROM
+      `httparchive.technologies.2020_08_01_desktop`
+    WHERE
+      category = 'Cryptominers'
+      OR category='Cryptominer'
+    GROUP BY
+      app)
+  GROUP BY
+    app)
+ORDER BY
+  pct DESC
