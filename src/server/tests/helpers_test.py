90d4b40f491b35bc9050b2a9b9ecaf73236c35cd
Barry <barry@tunetheweb.com>
2020-11-15 21:47:25 +0000

    Linting fixes

--- src/server/tests/helpers_test.py
+++ src/server/tests/helpers_test.py
@@ -1,7 +1,7 @@
 from server.helpers import get_file_date_info, get_versioned_filename, get_ebook_size_in_mb
 import re
 
-MIN_DATE="2019-11-01T00:00:00.000Z"
+MIN_DATE = "2019-11-01T00:00:00.000Z"
 
 
 def test_date_published_is_returned_and_correct_format():
@@ -33,7 +33,7 @@ def test_hash_is_returned_and_correct_format():
 
 
 def test_random_value_is_returned_as_none():
-    assert get_file_date_info('en/2019/index.html', 'rubbish') == None
+    assert get_file_date_info('en/2019/index.html', 'rubbish') is None
 
 
 def test_en_ebook_size_at_least_16_mb_info():
@@ -43,7 +43,7 @@ def test_en_ebook_size_at_least_16_mb_info():
 def test_versioned_css_file_is_of_correct_format():
     pattern = re.compile(r"^/static/css/2019.css\?v=[0-9a-f]{32}$")
     versioned_filename = get_versioned_filename('/static/css/2019.css')
-    assert pattern.match(versioned_filename);
+    assert pattern.match(versioned_filename)
 
 
 def test_non_versioned_css_file_is_of_correct_format():
@@ -52,20 +52,20 @@ def test_non_versioned_css_file_is_of_correct_format():
 
 
 def test_en_ebook_size_at_least_16_mb_function():
-    assert get_ebook_size_in_mb('en','2019') > 16
+    assert get_ebook_size_in_mb('en', '2019') > 16
 
 
 def test_ja_ebook_size_at_least_16_mb_function():
-    assert get_ebook_size_in_mb('ja','2019') > 16
+    assert get_ebook_size_in_mb('ja', '2019') > 16
 
 
 def test_ebook_size_non_existant_language_is_zero():
-    assert get_ebook_size_in_mb('rubbish','2019') == 0
+    assert get_ebook_size_in_mb('rubbish', '2019') == 0
 
 
 def test_ebook_size_non_existant_year_is_zero():
-    assert get_ebook_size_in_mb('en','rubbish') == 0
+    assert get_ebook_size_in_mb('en', 'rubbish') == 0
 
 
 def test_ebook_size_non_existant_language_and_year_is_zero():
-    assert get_ebook_size_in_mb('rubbish','rubbish') == 0
+    assert get_ebook_size_in_mb('rubbish', 'rubbish') == 0
