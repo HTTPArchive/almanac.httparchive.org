ad04c37d2c1324ba37c46584fa3342cb03fe48f4
Stefan Matei <stefan.matei@caphyon.com>
2020-11-17 08:45:00 +0200

    count pages with rel=preconnect.

--- sql/2020/05_Media/big_non_custom_metrics.sql
+++ sql/2020/05_Media/big_non_custom_metrics.sql
@@ -2,10 +2,12 @@
 # non custom metrics sql that uses regexp on response bodies
 # TODO: replace sample database with the real one when done
 # img src vs data-uri
+# count rel=preconnect
 SELECT client,
   count(0) as total,
   countif(has_img_data_uri) as has_img_data_uri,
   countif(has_img_src) as has_img_src,
+  countif(rel_preconnect) as rel_preconnect
 FROM
 (
   SELECT
@@ -13,6 +15,7 @@ FROM
     page,
     regexp_contains(body, r'<img[^><]*src=(?:\"|\')*data[:]image/(?:\"|\')*[^><]*>') as has_img_data_uri,
     regexp_contains(body, r'<img[^><]*src=[^><]*>') as has_img_src,
+    regexp_contains(body, r'<link[^><]*rel=(?:\"|\')*preconnect/(?:\"|\')*[^><]*>') as rel_preconnect
   FROM
     `httparchive.sample_data.summary_response_bodies_firstHtml`
   WHERE
