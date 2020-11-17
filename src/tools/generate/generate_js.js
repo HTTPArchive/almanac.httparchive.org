beda30fac1e74a4df9de917831205bf110121d4d
Barry Pollard <barry_pollard@hotmail.com>
2020-11-17 22:05:22 +0000

    Update web-vitals script (#1541)
    * Update web-vitals script
    
    * Error on script copy fail

--- src/tools/generate/generate_js.js
+++ src/tools/generate/generate_js.js
@@ -1,10 +1,11 @@
 const fs = require('fs-extra');
 
 const generate_js = async () => {
+  console.log(`\n Generating web-vitals.js`);
   fs.copy(
-    './node_modules/web-vitals/dist/web-vitals.es5.umd.min.js',
+    './node_modules/web-vitals/dist/web-vitals.base.umd.js',
     './static/js/web-vitals.js',
-  ).catch(err => console.error(err));
+  );
 };
 
 module.exports = {
