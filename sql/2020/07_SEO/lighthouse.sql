2daca9c28403de6692531434f4d0b8e29fd5ebaf
Tony McCreath <tony@websiteadvantage.com.au>
2020-11-17 12:01:19 +1030

    tidied up

--- sql/2020/07_SEO/lighthouse.sql
+++ sql/2020/07_SEO/lighthouse.sql
@@ -8,64 +8,68 @@ CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS
   ROUND(SAFE_DIVIDE(freq, total), 4)
 );
 
-CREATE TEMPORARY FUNCTION isCrawlable(report STRING) 
+CREATE TEMPORARY FUNCTION isCrawlableDetails(report STRING) 
 RETURNS STRUCT<
 disallow BOOL, 
 noindex BOOL,
 both BOOL,
 neither BOOL
 > DETERMINISTIC LANGUAGE js AS '''
-var result = {disallow:false, noindex:false};
+var result = {disallow: false, noindex: false};
 try {
     var $ = JSON.parse(report);
     var items = $.audits['is-crawlable'].details.items;
     result.noindex = items.filter(item => item.source.type ==='node').length > 0;
     result.disallow = items.filter(item => item.source.type ==='source-location').length > 0;
-    result.both = result.noindex && result.disallow;
-    result.neither = !result.noindex && !result.disallow;
 } catch (e) {
 }
 return result;
 ''';
 
-
 SELECT
     COUNT(0) AS total,
 
-    COUNTIF(is_crawlable) AS crawlable,
     COUNTIF(is_canonical) AS canonical,
-
-    AS_PERCENT(COUNTIF(is_crawlable), COUNT(0)) AS pct_crawlable,
     AS_PERCENT(COUNTIF(is_canonical), COUNT(0)) AS pct_canonical,
 
     COUNTIF(has_title) AS title,
-    COUNTIF(has_meta_description) AS meta_description,
-    COUNTIF(has_title AND has_meta_description) AS title_and_meta_description,
-
     AS_PERCENT(COUNTIF(has_title), COUNT(0)) AS pct_title,
+
+    COUNTIF(has_meta_description) AS meta_description,
     AS_PERCENT(COUNTIF(has_meta_description), COUNT(0)) AS pct_meta_description,
+
+    COUNTIF(has_title AND has_meta_description) AS title_and_meta_description, 
     AS_PERCENT(COUNTIF(has_title AND has_meta_description), COUNT(0)) AS pct_title_and_meta_description,
 
     COUNTIF(img_alt_on_all) AS img_alt_on_all,
     AS_PERCENT(COUNTIF(img_alt_on_all), COUNT(0)) AS pct_img_alt_on_all,
 
-    COUNTIF(robots_txt_valid) AS robots_txt_valid,
-    AS_PERCENT(COUNTIF(robots_txt_valid), COUNT(0)) AS pct_robots_txt_valid,
-
     COUNTIF(link_text_descriptive) AS link_text_descriptive,
     AS_PERCENT(COUNTIF(link_text_descriptive), COUNT(0)) AS pct_link_text_descriptive,
 
-    COUNTIF(isCrawlable.noindex) AS noindex,
-    AS_PERCENT(COUNTIF(isCrawlable.noindex), COUNT(0)) AS pct_noindex,  
+    COUNTIF(robots_txt_valid) AS robots_txt_valid,
+    AS_PERCENT(COUNTIF(robots_txt_valid), COUNT(0)) AS pct_robots_txt_valid,
 
-    COUNTIF(isCrawlable.disallow) AS disallow,
-    AS_PERCENT(COUNTIF(isCrawlable.disallow), COUNT(0)) AS pct_disallow,  
+    COUNTIF(is_crawlable) AS crawlable_pass,
+    AS_PERCENT(COUNTIF(is_crawlable), COUNT(0)) AS pct_crawlable_pass,
 
-    COUNTIF(isCrawlable.both) AS both,
-    AS_PERCENT(COUNTIF(isCrawlable.both), COUNT(0)) AS pct_both,  
+    COUNTIF(is_crawlable_details.noindex) AS crawlable_noindex,
+    AS_PERCENT(COUNTIF(is_crawlable_details.noindex), COUNT(0)) AS pct_crawlable_noindex,  
 
-    COUNTIF(isCrawlable.neither) AS neither,
-    AS_PERCENT(COUNTIF(isCrawlable.neither), COUNT(0)) AS pct_neither,  
+    COUNTIF(is_crawlable_details.disallow) AS crawlable_disallow,
+    AS_PERCENT(COUNTIF(is_crawlable_details.disallow), COUNT(0)) AS pct_crawlable_disallow,  
+
+    COUNTIF(is_crawlable_details.disallow AND is_crawlable_details.noindex) AS crawlable_disallow_noindex,
+    AS_PERCENT(COUNTIF(is_crawlable_details.disallow AND is_crawlable_details.noindex), COUNT(0)) AS pct_crawlable_disallow_noindex,  
+
+    COUNTIF(NOT(is_crawlable_details.disallow) AND NOT(is_crawlable_details.noindex)) AS crawlable_allow_index,
+    AS_PERCENT(COUNTIF(NOT(is_crawlable_details.disallow) AND NOT(is_crawlable_details.noindex)), COUNT(0)) AS pct_crawlable_allow_index,
+
+    COUNTIF(is_crawlable_details.disallow AND NOT(is_crawlable_details.noindex)) AS crawlable_disallow_index,
+    AS_PERCENT(COUNTIF(is_crawlable_details.disallow AND NOT(is_crawlable_details.noindex)), COUNT(0)) AS pct_crawlable_disallow_index,
+
+    COUNTIF(NOT(is_crawlable_details.disallow) AND is_crawlable_details.noindex) AS crawlable_allow_noindex,
+    AS_PERCENT(COUNTIF(NOT(is_crawlable_details.disallow) AND is_crawlable_details.noindex), COUNT(0)) AS pct_crawlable_allow_noindex
 FROM (
   SELECT
     JSON_EXTRACT_SCALAR(report, '$.audits.is-crawlable.score') = '1' AS is_crawlable,
@@ -75,9 +79,7 @@ FROM (
     JSON_EXTRACT_SCALAR(report, '$.audits.image-alt.score') = '1' AS img_alt_on_all,
     JSON_EXTRACT_SCALAR(report, '$.audits.robots-txt.score') = '1' AS robots_txt_valid,
     JSON_EXTRACT_SCALAR(report, '$.audits.link-text.score') = '1' AS link_text_descriptive,
-    isCrawlable(report) AS is_crawlable_details
+    isCrawlableDetails(report) AS is_crawlable_details
   FROM
-    #`httparchive.lighthouse.2020_08_01_*`
-    `httparchive.sample_data.lighthouse_mobile_10k`
-    )
-    
+    `httparchive.lighthouse.2020_08_01_*`
+)
