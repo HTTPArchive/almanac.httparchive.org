9a4f3181c0051d88c10b9718f0d92c98040bb644
Barry Pollard <barry_pollard@hotmail.com>
2020-11-19 22:11:33 +0000

    Node fail on error (#1553)

--- src/tools/generate/index.js
+++ src/tools/generate/index.js
@@ -1,6 +1,11 @@
 let { generate_chapters } = require('./generate_chapters');
 
 (async () => {
-  const first_arg = process.argv.slice(2)[0]
-  await generate_chapters(first_arg);
+  try {
+    const first_arg = process.argv.slice(2)[0]
+    await generate_chapters(first_arg);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 })();
