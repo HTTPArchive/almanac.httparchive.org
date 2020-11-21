63ee0078e9bae3b78a64414c352abc09866a4944
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-21 12:51:24 +0530

    Added reference to 3P table, summary_requests.

--- sql/2020/20_Caching/resource_age_party_and_type_wise.sql
+++ sql/2020/20_Caching/resource_age_party_and_type_wise.sql
@@ -12,15 +12,16 @@ RETURNS INT64 LANGUAGE js AS '''
 
 SELECT
   percentile,
-  client,
-  IF(NET.HOST(url) = NET.HOST(page), 'first party', 'third party') AS party,
+  _TABLE_SUFFIX AS client,
+  IF(NET.HOST(url) IN (
+    SELECT domain FROM `httparchive.almanac.third_parties` WHERE date = '2020-08-01' AND category != 'hosting'
+  ), 'third party', 'first party') AS party,
   type AS resource_type,
   APPROX_QUANTILES(ROUND((startedDateTime - toTimestamp(resp_last_modified)) / (1000 * 60 * 60 * 24 * 7)), 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS age_weeks
 FROM 
-  `httparchive.almanac.requests`,
+  `httparchive.summary_requests.2020_08_01_*`,
   UNNEST([10, 25, 50, 75, 90]) AS percentile
 WHERE
-  date = '2020-08-01' AND
   TRIM(resp_last_modified) <> ""
 GROUP BY
   percentile,
