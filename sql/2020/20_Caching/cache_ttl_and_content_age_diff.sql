00e9334fbed3cecbccd031666bc1f8effb67b33f
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-21 14:47:13 +0530

    toTimestamp to consider neg values as errors, day calculation made understandable.

--- sql/2020/20_Caching/cache_ttl_and_content_age_diff.sql
+++ sql/2020/20_Caching/cache_ttl_and_content_age_diff.sql
@@ -4,7 +4,7 @@ CREATE TEMPORARY FUNCTION toTimestamp(date_string STRING)
 RETURNS INT64 LANGUAGE js AS '''
   try {
     var timestamp = Math.round(new Date(date_string).getTime() / 1000);
-    return isNaN(timestamp) ? -1 : timestamp;
+    return isNaN(timestamp) || timestamp < 0 ? -1 : timestamp;
   } catch (e) {
     return -1;
   }
@@ -18,7 +18,7 @@ FROM
   (
     SELECT
       _TABLE_SUFFIX AS client,
-      ROUND((expAge - (startedDateTime - toTimestamp(resp_last_modified))) / 86400, 2)  AS diff_in_days
+      ROUND((expAge - (startedDateTime - toTimestamp(resp_last_modified))) / (60 * 60 * 24), 2)  AS diff_in_days
     FROM
       `httparchive.summary_requests.2020_08_01_*`
     WHERE
