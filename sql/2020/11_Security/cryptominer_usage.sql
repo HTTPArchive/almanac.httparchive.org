e655e50612c4f0836c71d48e2490d155d3175f23
Tom Van Goethem <tomvangoethem@gmail.com>
2020-11-24 11:30:05 +0100

    Security: update cryptominer query

--- sql/2020/11_Security/cryptominer_usage.sql
+++ sql/2020/11_Security/cryptominer_usage.sql
@@ -1,20 +1,15 @@
 #standardSQL
 SELECT
-  month,
-  client,
-  freq
-FROM (
-  SELECT
-    LEFT(_TABLE_SUFFIX, 10) AS month,
-    IF(ENDS_WITH(_TABLE_SUFFIX, '_desktop'), 'desktop', 'mobile') AS client,
-    COUNT(0) AS freq
-  FROM
-    `httparchive.technologies.*`
-  WHERE
-    category = 'Cryptominers'
-    OR category='Cryptominer'
-  GROUP BY
-    _TABLE_SUFFIX)
+  LEFT(_TABLE_SUFFIX, 10) AS month,
+  IF(ENDS_WITH(_TABLE_SUFFIX, '_desktop'), 'desktop', 'mobile') AS client,
+  COUNT(0) AS freq
+FROM
+  `httparchive.technologies.*`
+WHERE
+  category = 'Cryptominers'
+  OR category='Cryptominer'
+GROUP BY
+  _TABLE_SUFFIX
 ORDER BY
   client,
   month,
