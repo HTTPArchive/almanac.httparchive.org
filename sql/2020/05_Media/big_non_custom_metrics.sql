41262fd3e339af59c995abd43d3201e7d0fc82e1
Stefan Matei <stefan.matei@caphyon.com>
2020-11-24 19:51:37 +0200

    added more metrics to the big regexp query.

--- sql/2020/05_Media/big_non_custom_metrics.sql
+++ sql/2020/05_Media/big_non_custom_metrics.sql
@@ -3,11 +3,15 @@
 # TODO: replace sample database with the real one when done
 # img src vs data-uri
 # count rel=preconnect
+# video with src
+# video with source
 SELECT client,
-  count(0) as total,
-  countif(has_img_data_uri) as has_img_data_uri,
-  countif(has_img_src) as has_img_src,
-  countif(rel_preconnect) as rel_preconnect
+  COUNT(0) as total,
+  COUNTIF(has_img_data_uri) AS has_img_data_uri,
+  COUNTIF(has_img_src) AS has_img_src,
+  COUNTIF(rel_preconnect) AS rel_preconnect,
+  COUNTIF(has_video_src) AS has_video_src,
+  COUNTIF(has_video_source) AS has_video_source
 FROM
   (
   SELECT
@@ -15,7 +19,9 @@ FROM
     page,
     regexp_contains(body, r'<img[^><]*src=(?:\"|\')*data[:]image/(?:\"|\')*[^><]*>') as has_img_data_uri,
     regexp_contains(body, r'<img[^><]*src=[^><]*>') as has_img_src,
-    regexp_contains(body, r'<link[^><]*rel=(?:\"|\')*preconnect/(?:\"|\')*[^><]*>') as rel_preconnect
+    regexp_contains(body, r'<link[^><]*rel=(?:\"|\')*preconnect/(?:\"|\')*[^><]*>') as rel_preconnect,
+    regexp_contains(body, r'<video[^><]*src=[^><]*>') as has_video_src,
+    regexp_contains(body, r'<video[^><]*>.*?<source[^><]*>.*?</video>') as has_video_source
   FROM
     `httparchive.sample_data.summary_response_bodies_firstHtml`
   WHERE
