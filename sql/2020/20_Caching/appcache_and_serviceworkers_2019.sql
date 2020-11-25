00406a018e12ae28f937d8c9a413b61c084d9690
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-25 23:16:33 +0530

    Modifications to Caching 2020 queries  (#1498)
    * Initial files.
    
    * Corrected the query.
    
    * Added column name.
    
    * Review comments closed.
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Review comments closed.
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Removed space.
    
    * Initial file.
    
    * Initial file.
    
    * Review comments closed.
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Query for 2019.
    
    * Added reference to 3P table, summary_requests.
    
    * resp_last_modified of summary_requests is in msec.
    
    * Query for 2019.
    
    * Changed toTimestamp to consider negative timestamps as errors.
    
    * Fixed 3P, toTimestamp to consider negative timestamps as errors.
    
    * toTimestamp to consider neg values as errors, day calculation made understandable.
    
    * Return null on error.
    
    * Return null on error.
    
    * Return null on error.
    
    * Added ORDER BY.
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

---
+++ sql/2020/20_Caching/appcache_and_serviceworkers_2019.sql
@@ -0,0 +1,17 @@
+#standardSQL
+# Use of AppCache and ServiceWorkers
+SELECT
+  IF(STARTS_WITH(url, 'https'), 'https', 'http') AS http_type,
+  JSON_EXTRACT_SCALAR(report, "$.audits.appcache-manifest.score") AS using_appcache,
+  JSON_EXTRACT_SCALAR(report, "$.audits.service-worker.score") AS using_serviceworkers,
+  COUNT(0) AS occurrences,
+  SUM(COUNT(0)) OVER () AS total,
+  COUNT(0) / SUM(COUNT(0)) OVER () AS pct
+FROM 
+  `httparchive.lighthouse.2019_07_01_mobile` 
+GROUP BY 
+  http_type, 
+  using_appcache, 
+  using_serviceworkers
+ORDER BY
+  pct DESC
