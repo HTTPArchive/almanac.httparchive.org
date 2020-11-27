680c944b8a6f9806f251ca82eeb53ba7c32d264d
Stefan Matei <stefan.matei@caphyon.com>
2020-11-27 09:00:08 +0200

    Fixes for the video source type detection. With one regexp only the first source is detected for a video tag. First parse for the video tag, and then parse all sources inside.

--- sql/2020/05_Media/video_source_types.sql
+++ sql/2020/05_Media/video_source_types.sql
@@ -1,21 +1,21 @@
 #standardSQL
 # get video source types
-# TODO: replace sample_data with the real data when done
 
-SELECT client,
-  video_source as video_type,
-  count(0) as video_type_count
+SELECT
+  client,
+  video_type,
+  COUNT(0) AS video_type_count,
+  SAFE_DIVIDE(COUNT(0), SUM(COUNT(0)) OVER (PARTITION BY client)) AS video_type_pct
 FROM
-  (
-  SELECT
-    client,
-    page,
-    video_source
-  FROM
-    `httparchive.sample_data.summary_response_bodies_firstHtml`,
-    UNNEST(regexp_extract_ALL(body, r'<video[^><]*>.*?<source[^><]*type=(?:"|\')*video/([^"|\'|\\s|>]*)(?:"|\'|\\s|>).*?</video>')) as video_source
-  )
+  `httparchive.almanac.summary_response_bodies`,
+  UNNEST(REGEXP_EXTRACT_ALL(body, '(?i)(<video.*?</video>)')) AS video,
+  UNNEST(REGEXP_EXTRACT_ALL(video, r'(?i)type\s*=\s*["\'](video/[^\'";]*)')) AS video_type
+WHERE
+  date = '2020-08-01' AND
+  firstHtml
 GROUP BY
-  client, video_source
+  client,
+  video_type
 ORDER BY
-  client, video_type_count desc;
+  client,
+  video_type_count desc;
