82b957c74b7c6927ef5cd9efdd66b70c27f6be9d
Stefan Matei <smatei@gmail.com>
2020-11-28 21:07:17 +0200

    Media 2020 queries (#1327)
    * added sql for images with srcset w/wo sizes.
    
    * added sql for images with srcset with descriptor_x/descriptor_w.
    
    * added sql for picture using min resolution.
    
    * added sql for picture using min resolution
    
    * added query for picture format distribution.
    
    * added counts for main formats (webp, gif, jpg, png).
    
    * added sql for scrset candidates average.
    
    * add picture with orientation sql query.
    
    * added avif count to picture format distribution sql.
    
    * added sql for video autoplay muted.
    
    * added missing client column.
    
    * added video preload sql.
    
    * non custom-metrics sql: img src vs data-uri.
    
    * added client column for srcset average size for candidates.
    
    * added sql for usage of alt text in images.
    
    * count pages with rel=preconnect.
    
    * added sql for mimetype vs extension.
    
    * _score_progressive_jpeg sql.
    
    * add blank new line at the end of files.
    
    * added order by client to all sqls.
    
    * Fixed capitalisation.
    
    * select mimetypes and extensions from the almanac.requests table.
    
    * user percent for images alt sql. display both pages and image instances.
    
    * use percent in mimetype vs extension. use limit.
    
    * added more metrics to the big regexp query.
    
    * added figure and figcaption detection in the regexp big query.
    
    * for srcset with/wo sizes, compute percent of pages and occurances.
    
    * scrset descriptor - use percent for pages and instances.
    
    * display percent from pages for scrset average candates.
    
    * added percent of total numberof pictures for picture formats.
    
    * user percent of pages and occurances for picture using orientation.
    
    * formatting percents.
    
    * use percent of pages and percent of occurences - where possible. removed formatting percent.
    
    * use percent metrics for the expnsive regexp query.
    
    * sql to get video source types.
    
    * keep percents in the 0, 1 range.
    
    * Fixes for the video source type detection. With one regexp only the first source is detected for a video tag. First parse for the video tag, and then parse all sources inside.
    
    * fixed metric name.
    
    * added sql for video nodes and video js pages detection.
    
    * Update video_tag_and_js_player.sql
    
    switch from the sample db to the real db.
    
    * lint queries
    
    * uppercase fn name
    
    Co-authored-by: Stefan Matei <stefan.matei@caphyon.com>
    Co-authored-by: Barry <barry@tunetheweb.com>
    Co-authored-by: Rick Viscomi <rviscomi@gmail.com>

---
+++ sql/2020/05_Media/picture_using_min_resolution.sql
@@ -0,0 +1,37 @@
+#standardSQL
+# picture using min resolution
+
+# returns all the data we need from _media
+CREATE TEMPORARY FUNCTION get_media_info(media_string STRING)
+RETURNS STRUCT<
+  num_picture_using_min_resolution  INT64, 
+  num_picture_img INT64
+> LANGUAGE js AS '''
+var result = {};
+try {
+    var media = JSON.parse(media_string);
+
+    if (Array.isArray(media) || typeof media != 'object') return result;
+	
+    result.num_picture_using_min_resolution = media.num_picture_using_min_resolution;
+    result.num_picture_img = media.num_picture_img;
+
+} catch (e) {}
+return result;
+''';
+
+SELECT
+  client,
+  SAFE_DIVIDE(COUNTIF(media_info.num_picture_img > 0), COUNT(0)) AS pages_with_picture_pct,
+  SAFE_DIVIDE(COUNTIF(media_info.num_picture_using_min_resolution > 0), COUNTIF(media_info.num_picture_img > 0)) AS pages_with_picture_min_resolution_pct,
+  SAFE_DIVIDE(SUM(media_info.num_picture_using_min_resolution), SUM(media_info.num_picture_img)) AS occurences_of_picture_min_resolution_pct
+FROM (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    get_media_info(JSON_EXTRACT_SCALAR(payload, '$._media')) AS media_info
+  FROM
+    `httparchive.pages.2020_08_01_*`)
+GROUP BY
+  client
+ORDER BY
+  client
