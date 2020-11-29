b9dc2e84962249b4b7fd23b7f69c7a4f46e05622
Raghu Ramakrishnan <raghuramakrishnan71@gmail.com>
2020-11-29 11:46:59 +0530

    Included review comments.

--- sql/2020/20_Caching/resource_age_party_and_type_wise_groups.sql
+++ sql/2020/20_Caching/resource_age_party_and_type_wise_groups.sql
@@ -14,13 +14,20 @@ SELECT
   client,
   party,
   resource_type,
-  COUNTIF(age_weeks IS NOT NULL) AS requests_total,
+  COUNT(0) as requests_total,
+  COUNTIF(age_weeks IS NOT NULL) AS requests_with_age,
   COUNTIF(age_weeks < 0) AS age_neg,
   COUNTIF(age_weeks = 0) AS age_0wk,
   COUNTIF(age_weeks >= 1 AND age_weeks <= 7) AS age_1_to_7wk,
   COUNTIF(age_weeks >= 8 AND age_weeks <= 52) AS age_8_to_52wk,
   COUNTIF(age_weeks >= 53 AND age_weeks <= 104) AS age_gt_1y,
-  COUNTIF(age_weeks >= 105) AS age_gt_2y
+  COUNTIF(age_weeks >= 105) AS age_gt_2y,
+  SAFE_DIVIDE(COUNTIF(age_weeks < 0), COUNTIF(age_weeks IS NOT NULL)) AS age_neg_pct,
+  SAFE_DIVIDE(COUNTIF(age_weeks = 0), COUNTIF(age_weeks IS NOT NULL)) AS age_0wk_pct,
+  SAFE_DIVIDE(COUNTIF(age_weeks >= 1 AND age_weeks <= 7), COUNTIF(age_weeks IS NOT NULL)) AS age_1_to_7wk_pct,
+  SAFE_DIVIDE(COUNTIF(age_weeks >= 8 AND age_weeks <= 52), COUNTIF(age_weeks IS NOT NULL)) AS age_8_to_52wk_pct,
+  SAFE_DIVIDE(COUNTIF(age_weeks >= 53 AND age_weeks <= 104), COUNTIF(age_weeks IS NOT NULL)) AS age_gt_1y_pct,
+  SAFE_DIVIDE(COUNTIF(age_weeks >= 105), COUNTIF(age_weeks IS NOT NULL)) AS age_gt_2y_pct
 FROM
 ( 
   SELECT
