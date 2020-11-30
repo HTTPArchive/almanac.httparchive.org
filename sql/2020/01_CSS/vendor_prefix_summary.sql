8f130b38032c812e6c3183260ccc6ef821524188
Rick Viscomi <rviscomi@gmail.com>
2020-11-29 22:29:03 -0500

    spaces for tabs

--- sql/2020/01_CSS/vendor_prefix_summary.sql
+++ sql/2020/01_CSS/vendor_prefix_summary.sql
@@ -1,90 +1,90 @@
 #standardSQL
 CREATE TEMPORARY FUNCTION getPrefixStats(css STRING) RETURNS ARRAY<STRING> LANGUAGE js AS '''
 try {
-	function compute() {
-		let ret = {
-			total: {},
-			pseudo_classes: {},
-			pseudo_elements: {},
-			properties: {},
-			functions: {},
-			keywords: {},
-			media: {}
-		};
+  function compute() {
+    let ret = {
+      total: {},
+      pseudo_classes: {},
+      pseudo_elements: {},
+      properties: {},
+      functions: {},
+      keywords: {},
+      media: {}
+    };
 
-		ret.total = Object.fromEntries(Object.keys(ret).map(k => [k, 0]));
+    ret.total = Object.fromEntries(Object.keys(ret).map(k => [k, 0]));
 
-		walkRules(ast, rule => {
-			// Prefixed pseudos
-			if (rule.selectors) {
-				let pseudos = rule.selectors.flatMap(r => r.match(/::?-[a-z]+-[\\w-]+/g) || []);
+    walkRules(ast, rule => {
+      // Prefixed pseudos
+      if (rule.selectors) {
+        let pseudos = rule.selectors.flatMap(r => r.match(/::?-[a-z]+-[\\w-]+/g) || []);
 
-				for (let pseudo of pseudos) {
-					let type = "pseudo_" + (pseudo.indexOf("::") === 0? "elements" : "classes");
-					incrementByKey(ret[type], pseudo);
-					ret.total[type]++;
-				}
-			}
+        for (let pseudo of pseudos) {
+          let type = "pseudo_" + (pseudo.indexOf("::") === 0? "elements" : "classes");
+          incrementByKey(ret[type], pseudo);
+          ret.total[type]++;
+        }
+      }
 
-			if (rule.declarations) {
-				walkDeclarations(rule, ({property, value}) => {
-					// Prefixed properties
-					if (/^-[a-z]+-.+/.test(property)) {
-						incrementByKey(ret.properties, property);
-						ret.total.properties++;
-					}
+      if (rule.declarations) {
+        walkDeclarations(rule, ({property, value}) => {
+          // Prefixed properties
+          if (/^-[a-z]+-.+/.test(property)) {
+            incrementByKey(ret.properties, property);
+            ret.total.properties++;
+          }
 
-					// -prefix-function()
-					for (let call of extractFunctionCalls(value, {names: /^-[a-z]+-.+/})) {
-						incrementByKey(ret.functions, call.name);
-						ret.total.functions++;
-					}
+          // -prefix-function()
+          for (let call of extractFunctionCalls(value, {names: /^-[a-z]+-.+/})) {
+            incrementByKey(ret.functions, call.name);
+            ret.total.functions++;
+          }
 
-					// Prefixed keywords
-					if (!matches(property, /(^|-)(transition(-property)?|animation(-name)?)$/)) {
-						for (let k of value.matchAll(/(?<![-a-z])-[a-z]+-[a-z-]+(?=$|\\s|,|\\/)/g)) {
-							incrementByKey(ret.keywords, k);
-							ret.total.keywords++;
-						}
-					}
-				});
-			}
+          // Prefixed keywords
+          if (!matches(property, /(^|-)(transition(-property)?|animation(-name)?)$/)) {
+            for (let k of value.matchAll(/(?<![-a-z])-[a-z]+-[a-z-]+(?=$|\\s|,|\\/)/g)) {
+              incrementByKey(ret.keywords, k);
+              ret.total.keywords++;
+            }
+          }
+        });
+      }
 
-			// Prefixed media features
-			if (rule.media) {
-				let features = rule.media
-									.replace(/\\s+/g, "")
-									.match(/\\(-[a-z]+-[\\w-]+(?=[:\\)])/g);
+      // Prefixed media features
+      if (rule.media) {
+        let features = rule.media
+                  .replace(/\\s+/g, "")
+                  .match(/\\(-[a-z]+-[\\w-]+(?=[:\\)])/g);
 
-				if (features) {
-					features = features.map(s => s.slice(1));
+        if (features) {
+          features = features.map(s => s.slice(1));
 
-					for (let feature of features) {
-						incrementByKey(ret.media, feature);
-						ret.total.media++;
-					}
-				}
-			}
-		});
+          for (let feature of features) {
+            incrementByKey(ret.media, feature);
+            ret.total.media++;
+          }
+        }
+      }
+    });
 
-		ret.total.total = sumObject(ret.total);
+    ret.total.total = sumObject(ret.total);
 
-		for (let type in ret) {
-			ret[type] = sortObject(ret[type]);
-		}
+    for (let type in ret) {
+      ret[type] = sortObject(ret[type]);
+    }
 
-		return ret;
-	}
+    return ret;
+  }
 
 
-	let ast = JSON.parse(css);
-	let stats = compute(ast).total;
-	return Object.entries(stats).flatMap(([prop, freq]) => {
-		return Array(freq).fill(prop);
-	});
+  let ast = JSON.parse(css);
+  let stats = compute(ast).total;
+  return Object.entries(stats).flatMap(([prop, freq]) => {
+    return Array(freq).fill(prop);
+  });
 }
 catch (e) {
-	return [];
+  return [];
 }
 '''
 OPTIONS (library="gs://httparchive/lib/css-utils.js");
