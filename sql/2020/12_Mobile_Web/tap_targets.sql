5c35936b22f4c5bc12d234cfb9c1a6229b73cd58
David Fox <david@davidjfox.com>
2020-12-01 11:59:25 -0600

    Fixed variable name

--- sql/2020/12_Mobile_Web/tap_targets.sql
+++ sql/2020/12_Mobile_Web/tap_targets.sql
@@ -1,12 +1,12 @@
 #standardSQL
 # % mobile sites with correctly sized tap targets (note: the score is not binary)
 SELECT
-  COUNTIF(color_contrast_score IS NOT NULL) AS total_applicable,
-  COUNTIF(CAST(color_contrast_score AS NUMERIC) = 1) AS total_sufficient,
-  COUNTIF(CAST(color_contrast_score AS NUMERIC) = 1) / COUNTIF(color_contrast_score IS NOT NULL) AS perc_in_applicable
+  COUNTIF(tap_targets_score IS NOT NULL) AS total_applicable,
+  COUNTIF(CAST(tap_targets_score AS NUMERIC) = 1) AS total_sufficient,
+  COUNTIF(CAST(tap_targets_score AS NUMERIC) = 1) / COUNTIF(tap_targets_score IS NOT NULL) AS perc_in_applicable
 FROM (
   SELECT
-    JSON_EXTRACT_SCALAR(report, '$.audits.tap-targets.score') AS color_contrast_score
+    JSON_EXTRACT_SCALAR(report, '$.audits.tap-targets.score') AS tap_targets_score
   FROM
     `httparchive.lighthouse.2020_08_01_mobile`
 )
