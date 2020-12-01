f567d296cfcc591f124ee751180cecae129f3a58
Lea Verou <lea@verou.me>
2020-12-01 15:05:30 +0200

    Update repetition.sql

--- sql/2020/01_CSS/repetition.sql
+++ sql/2020/01_CSS/repetition.sql
@@ -1,76 +1,32 @@
 #standardSQL
-CREATE TEMPORARY FUNCTION getSpecificityInfo(css STRING)
+CREATE TEMPORARY FUNCTION getDeclarationCounts(css STRING)
 RETURNS STRUCT<
-  ruleCount NUMERIC,
-  selectorCount NUMERIC,
-  distribution ARRAY<STRUCT<specificity STRING, specificity_cmp STRING, freq INT64>>
+  total NUMERIC,
+  unique NUMERIC
 > LANGUAGE js AS '''
 try {
-  function extractSpecificity(ast) {
-    let ret = {
-      selectorCount: 0,
-      ruleCount: 0,
-      specificityCount: {},
-      maxSpecifity: [0, 0, 0]
-    };
+    function compute() {
+        let ret = {total: 0};
+        let unique = new Set();
 
-    let ss = [0, 0, 0];
+        walkDeclarations(ast, ({property, value}) => {
+          if (!property.startsWith("--")) { // Custom props are case sensitive
+              property = property.toLowerCase();
+          }
 
-    walkRules(ast, rule => {
-      ret.ruleCount++;
+          ret.total++;
+          unique.add(`${property}: ${value}`);
+        });
 
-      for (let selector of rule.selectors) {
-        ret.selectorCount++;
-        let s = parsel.specificity(selector);
-        ss = ss.map((a, i) => a + s[i]);
-        let max = Math.max(...s);
+        ret.unique = unique.size;
 
-        incrementByKey(ret.specificityCount, max <= 5? s + "" : "higher");
-
-        let base = Math.max(...ret.maxSpecifity, ...s);
-        if (parsel.specificityToNumber(s, base) > parsel.specificityToNumber(ret.maxSpecifity, base)) {
-          ret.maxSpecifity = s;
-          ret.maxSpecifitySelector = selector;
-        }
-      }
-    }, {type: "rule"});
-
-    ret.selectorsPerRule = ret.selectorCount / ret.ruleCount;
-    ret.avgSpecificity = ss.map(s => s / ret.selectorCount);
-
-    return ret;
-  }
-
-  function toComparableString(specificity) {
-    if (!specificity) {
-      return null;
-    }
-    if (specificity.split(',').length !== 3) {
-      return null;
+        return ret;
     }
 
-    // The highest unit of specificity is 9398, so we need 5 digits of padding.
-    // Fun fact: the most specific selector in the dataset is 1065,9398,7851!
-    return specificity.split(',').map(i => i.padStart(5, '0')).join('');
-  }
+
 
   const ast = JSON.parse(css);
-  let specificity = extractSpecificity(ast);
-  let ruleCount = specificity.ruleCount;
-  let selectorCount = specificity.selectorCount;
-  let distribution = Object.entries(specificity.specificityCount).map(([specificity, freq]) => {
-    return {
-      specificity,
-      freq,
-      specificity_cmp: toComparableString(specificity)
-    }
-  });
-
-  return {
-    ruleCount,
-    selectorCount,
-    distribution
-  };
+  return compute(ast);
 } catch (e) {
   return null;
 }
@@ -80,19 +36,19 @@ OPTIONS (library="gs://httparchive/lib/css-utils.js");
 SELECT
   percentile,
   client,
-  APPROX_QUANTILES(rule_count, 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS rule_count,
-  APPROX_QUANTILES(selector_count, 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS selector_count,
-  APPROX_QUANTILES(SAFE_DIVIDE(selector_count, rule_count), 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS selectors_per_rule
+  APPROX_QUANTILES(total, 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS total,
+  APPROX_QUANTILES(unique, 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS unique,
+  APPROX_QUANTILES(SAFE_DIVIDE(unique, total), 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS unique_ratio
 FROM (
   SELECT
     client,
-    SUM(info.ruleCount) AS rule_count,
-    SUM(info.selectorCount) AS selector_count
+    SUM(info.total) AS total,
+    SUM(info.unique) AS unique
   FROM (
     SELECT
       client,
       page,
-      getSpecificityInfo(css) AS info
+      getDeclarationCounts(css) AS info
     FROM
       `httparchive.almanac.parsed_css`
     WHERE
