aaf9e81628352ea0dcdbf2567f0e86639d9fac44
unknown <apppletsai@gmail.com>
2020-12-01 19:29:03 +0800

    add test in year and static_avatar

--- src/server/tests/config_test.py
+++ src/server/tests/config_test.py
@@ -2,7 +2,23 @@ from server.config import get_config, get_chapters, get_languages, get_live, \
     SUPPORTED_YEARS, SUPPORTED_CHAPTERS, SUPPORTED_LANGUAGES
 
 
-def test_get_config_in_suppoted_year():
+test_year = ['2020', '2019', '2018']
+
+
+def test_get_config_year():
+    assert get_config(test_year[0]) is not None
+    assert get_config(test_year[1]) is not None
+    assert get_config(test_year[2]) is None
+
+
+def test_static_avatar():
+    for contributor in get_config('2020'):
+        if contributor == 'michelleoconnor':
+            static_url = contributor['avatar_url']
+            assert static_url is not None and static_url.startswith('/static')
+
+
+def test_get_config_in_supported_year():
     for year in SUPPORTED_YEARS:
         assert get_config(year) is not None
 
