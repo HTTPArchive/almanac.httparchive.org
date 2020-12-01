654ac797d575f76d4fe1763904fce30120570952
Barry Pollard <barry_pollard@hotmail.com>
2020-12-01 14:53:31 +0000

    2020 CMS Chapter (#1513)
    * 2020 CMS Chapter
    
    * Turn on ToDo again until ready to launch
    
    * Correct duplicate graph
    
    * Review feedback
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update cms.md
    
    * Add Alex's Bio and Featured Stats
    
    * Fix website link
    
    * Add other lighthouse scores
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update src/content/en/2020/cms.md
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Update cms.md
    
    * Added chart for Wordpress plugins
    
    * Change PWA for SEO
    
    * Apply suggestions from code review
    
    * todos
    
    Co-authored-by: Rick Viscomi <rviscomi@gmail.com>
    Co-authored-by: alexdenning <alexdenning@gmail.com>
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/15_CMS/median_lighthouse_score.sql
+++ sql/2020/15_CMS/median_lighthouse_score.sql
@@ -6,7 +6,9 @@ SELECT
   # See https://github.com/HTTPArchive/almanac.httparchive.org/pull/1087#issuecomment-684983795
   #APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.performance.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_performance,
   APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.accessibility.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_accessibility,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.pwa.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_pwa
+  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.pwa.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_pwa,
+  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.seo.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_seo,
+  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.best-practices.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_best_practices
 FROM
   `httparchive.lighthouse.2020_08_01_mobile`
 JOIN
