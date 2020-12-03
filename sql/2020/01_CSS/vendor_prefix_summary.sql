c2b8dfa40bc9c30978fb45563ae93f73156b270b
Lea Verou <lea@verou.me>
2020-12-03 15:13:47 +0200

    Update vendor_prefix_summary.sql

--- sql/2020/01_CSS/vendor_prefix_summary.sql
+++ sql/2020/01_CSS/vendor_prefix_summary.sql
@@ -1,92 +1,94 @@
 #standardSQL
 CREATE TEMPORARY FUNCTION getPrefixStats(css STRING) RETURNS ARRAY<STRING> LANGUAGE js AS '''
 try {
-	function compute(ast) {
-		let ret = {
-			total: {},
-			pseudo_classes: {},
-			pseudo_elements: {},
-			properties: {},
-			functions: {},
-			keywords: {},
-			media: {}
-		};
+  function compute() {
+    let ret = {
+      total: {},
+      pseudo_classes: {},
+      pseudo_elements: {},
+      properties: {},
+      functions: {},
+      keywords: {},
+      media: {}
+    };
 
-		ret.total = Object.fromEntries(Object.keys(ret).map(k => [k, 0]));
+    ret.total = Object.fromEntries(Object.keys(ret).map(k => [k, 0]));
 
-		walkRules(ast, rule => {
-			// Prefixed pseudos
-			if (rule.selectors) {
-				let pseudos = rule.selectors.flatMap(r => r.match(/::?-[a-z]+-[\\w-]+/g) || []);
+    walkRules(ast, rule => {
+      // Prefixed pseudos
+      if (rule.selectors) {
+        let pseudos = rule.selectors.flatMap(r => r.match(/::?-[a-z]+-[\\w-]+/g) || []);
 
-				for (let pseudo of pseudos) {
-					let type = "pseudo_" + (pseudo.indexOf("::") === 0? "elements" : "classes");
-					incrementByKey(ret[type], pseudo);
-					ret.total[type]++;
-				}
-			}
+        for (let pseudo of pseudos) {
+          let type = "pseudo_" + (pseudo.indexOf("::") === 0? "elements" : "classes");
+          incrementByKey(ret[type], pseudo);
+          ret.total[type]++;
+        }
+      }
 
-			if (rule.declarations) {
-				walkDeclarations(rule, ({property, value}) => {
-					// Prefixed properties
-					if (/^-[a-z]+-.+/.test(property)) {
-						incrementByKey(ret.properties, property);
-						ret.total.properties++;
-					}
+      if (rule.declarations) {
+        walkDeclarations(rule, ({property, value}) => {
+          if (value.length > 1000) {
+            return;
+          }
 
-					if (value.length <= 1000) {
-						// -prefix-function()
-						for (let call of extractFunctionCalls(value, {names: /^-[a-z]+-.+/})) {
-							incrementByKey(ret.functions, call.name);
-							ret.total.functions++;
-						}
+          // Prefixed properties
+          if (/^-[a-z]+-.+/.test(property)) {
+            incrementByKey(ret.properties, property);
+            ret.total.properties++;
+          }
 
-						// Prefixed keywords
-						if (!matches(property, /(^|-)(transition(-property)?|animation(-name)?)$/)) {
-							for (let k of value.matchAll(/(?<![-a-z])-[a-z]+-[a-z-]+(?=$|\\s|,|\\/)/g)) {
-								incrementByKey(ret.keywords, k);
-								ret.total.keywords++;
-							}
-						}
-					}					
-				});
-			}
+          // -prefix-function()
+          for (let call of extractFunctionCalls(value, {names: /^-[a-z]+-.+/})) {
+            incrementByKey(ret.functions, call.name);
+            ret.total.functions++;
+          }
 
-			// Prefixed media features
-			if (rule.media) {
-				let features = rule.media
-									.replace(/\\s+/g, "")
-									.match(/\\(-[a-z]+-[\\w-]+(?=[:\\)])/g);
+          // Prefixed keywords
+          if (!matches(property, /(^|-)(transition(-property)?|animation(-name)?)$/)) {
+            for (let k of value.matchAll(/(?<![-a-z])-[a-z]+-[a-z-]+(?=$|\\s|,|\\/)/g)) {
+              incrementByKey(ret.keywords, k);
+              ret.total.keywords++;
+            }
+          }
+        });
+      }
 
-				if (features) {
-					features = features.map(s => s.slice(1));
+      // Prefixed media features
+      if (rule.media) {
+        let features = rule.media
+                  .replace(/\\s+/g, "")
+                  .match(/\\(-[a-z]+-[\\w-]+(?=[:\\)])/g);
 
-					for (let feature of features) {
-						incrementByKey(ret.media, feature);
-						ret.total.media++;
-					}
-				}
-			}
-		});
+        if (features) {
+          features = features.map(s => s.slice(1));
 
-		ret.total.total = sumObject(ret.total);
+          for (let feature of features) {
+            incrementByKey(ret.media, feature);
+            ret.total.media++;
+          }
+        }
+      }
+    });
 
-		for (let type in ret) {
-			ret[type] = sortObject(ret[type]);
-		}
+    ret.total.total = sumObject(ret.total);
 
-		return ret;
-	}
+    for (let type in ret) {
+      ret[type] = sortObject(ret[type]);
+    }
+
+    return ret;
+  }
 
 
-	let ast = JSON.parse(css);
-	let stats = compute(ast).total;
-	return Object.entries(stats).flatMap(([prop, freq]) => {
-		return Array(freq).fill(prop);
-	});
+  let ast = JSON.parse(css);
+  let stats = compute(ast).total;
+  return Object.entries(stats).flatMap(([prop, freq]) => {
+    return Array(freq).fill(prop);
+  });
 }
 catch (e) {
-	return [];
+  return [];
 }
 '''
 OPTIONS (library="gs://httparchive/lib/css-utils.js");
@@ -99,8 +101,8 @@ FROM (
     prop,
     COUNT(DISTINCT page) AS pages,
     COUNT(0) AS freq,
-    SUM(COUNT(0)) / 2 OVER (PARTITION BY client) AS total,
-    COUNT(0) / (SUM(COUNT(0)) / 2) OVER (PARTITION BY client) AS pct
+    SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
+    COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
   FROM
     `httparchive.almanac.parsed_css`,
     UNNEST(getPrefixStats(css)) AS prop
@@ -109,5 +111,7 @@ FROM (
   GROUP BY
     client,
     prop)
+WHERE
+  pages >= 1000
 ORDER BY
   pct DESC
