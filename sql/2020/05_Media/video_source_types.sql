bf1097123894317c32683ba20e32b7ae42b91cdb
Stefan Matei <smatei@gmail.com>
2020-12-03 06:15:39 +0200

    Media 2020 queries (#1612)
    * stop extracting at ? character - fixed video/mp4?... video type to lower case.
    
    * Update sql/2020/05_Media/video_source_types.sql
    
    Co-authored-by: Stefan Matei <stefan.matei@caphyon.com>
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2020/05_Media/video_source_types.sql
+++ sql/2020/05_Media/video_source_types.sql
@@ -3,13 +3,13 @@
 
 SELECT
   client,
-  video_type,
+  LOWER(video_type) AS video_type,
   COUNT(0) AS video_type_count,
   SAFE_DIVIDE(COUNT(0), SUM(COUNT(0)) OVER (PARTITION BY client)) AS video_type_pct
 FROM
   `httparchive.almanac.summary_response_bodies`,
   UNNEST(REGEXP_EXTRACT_ALL(body, '(?i)(<video.*?</video>)')) AS video,
-  UNNEST(REGEXP_EXTRACT_ALL(video, r'(?i)type\s*=\s*["\'](video/[^\'";]*)')) AS video_type
+  UNNEST(REGEXP_EXTRACT_ALL(video, r'(?i)type\s*=\s*["\'](video/[^\'";?]*)')) AS video_type
 WHERE
   date = '2020-08-01' AND
   firstHtml
