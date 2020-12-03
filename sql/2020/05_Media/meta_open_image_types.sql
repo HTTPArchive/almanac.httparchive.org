9cda14686c3ffc469b4b6e7eddb85a2ef80d8474
Stefan Matei <smatei@gmail.com>
2020-12-03 13:28:44 +0200

    Media 2020 queries (#1620)
    * usage of og:image og:video in meta.
    
    * detect image and video types used in open grahp meta.
    
    * changed js method name. added comment for regex.
    
    * update method names.
    
    * I used a mix of var and let. Use var everywhere.
    
    * update regex. smaller limit for video count, as there are fewer videos.
    
    * comment.
    
    Co-authored-by: Stefan Matei <stefan.matei@caphyon.com>

---
+++ sql/2020/05_Media/meta_open_image_types.sql
@@ -0,0 +1,52 @@
+#standardSQL
+# meta open graph image types
+
+# returns all the data we need from _almanac
+CREATE TEMPORARY FUNCTION get_meta_og_image_types(almanac_string STRING)
+RETURNS STRUCT<
+  image_types ARRAY<STRING>
+> LANGUAGE js AS '''
+var result = {};
+try {
+    var almanac = JSON.parse(almanac_string);
+
+    if (Array.isArray(almanac) || typeof almanac != 'object') return result;
+
+    for (var node of almanac['meta-nodes']['nodes']) {
+      if (node['property'] === 'og:image') {
+        if (result.image_types == null)
+          result.image_types = [];
+        var url = node['content'];
+        // parse image extension from url
+        // https://www.example.com/filename.ext?param=1234#anchor
+        // first group is for file name, second for extension and 
+        // third one for the remaining of the url
+        var rex = new RegExp('([^/]+)[.]([^/#?&]+)([#?][^/]*)?$');
+        var ext = url.match(rex);
+        result.image_types.push(ext[2].toLowerCase().trim());  
+      }
+    }
+} catch (e) {}
+return result;
+''';
+
+SELECT
+  client,
+  image_type,
+  COUNT(0) AS image_type_count,
+  SAFE_DIVIDE(COUNT(0), SUM(COUNT(0)) OVER (PARTITION BY client)) AS image_type_pct
+FROM (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url,
+    get_meta_og_image_types(JSON_EXTRACT_SCALAR(payload, '$._almanac')) AS almanac_info
+  FROM
+    `httparchive.pages.2020_08_01_*`),
+  UNNEST(almanac_info.image_types) AS image_type
+GROUP BY
+  client, image_type
+HAVING
+  image_type_count > 100
+ORDER BY
+  client,
+  image_type_count DESC;
