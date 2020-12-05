d0a5e821e039dfdf0bfdf44e40cd8e943265d411
Rick Viscomi <rviscomi@users.noreply.github.com>
2020-12-05 14:54:26 -0500

    Update 04_11a.popular_typeface.sql

--- sql/2020/04_Fonts/04_11a.popular_typeface.sql
+++ sql/2020/04_Fonts/04_11a.popular_typeface.sql
@@ -1,32 +1,33 @@
 #standardSQL
 #popular_typeface
 CREATE TEMPORARY FUNCTION getFontFamilies(css STRING)
-RETURNS ARRAY < STRING > LANGUAGE js AS '''
+RETURNS ARRAY <STRING> LANGUAGE js AS '''
 try {
-    var $ = JSON.parse(css);
-    return $.stylesheet.rules.filter(rule => rule.type == 'font-face').map(rule => {
-        var family = rule.declarations && rule.declarations.find(d => d.property == 'font-family');
-        return family && family.value.replace(/[\'"]/g, '');
-    }).filter(family => family);
+  var $ = JSON.parse(css);
+  return $.stylesheet.rules.filter(rule => rule.type == 'font-face').map(rule => {
+    var family = rule.declarations && rule.declarations.find(d => d.property == 'font-family');
+    return family && family.value.replace(/[\'"]/g, '');
+  }).filter(family => family);
 } catch (e) {
-    return [];
+  return [];
 }
 ''';
+
 SELECT
-  client,
-  font_family,
-  COUNT(0) AS freq,
-  SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
-FROM
-  `httparchive.almanac.parsed_css`,
-  UNNEST(getFontFamilies(css)) AS font_family
+  *
+FROM (
+  SELECT DISTINCT
+    client,
+    font_family,
+    COUNT(DISTINCT page) OVER (PARTITION BY client, font_family) AS pages,
+    COUNT(DISTINCT page) OVER (PARTITION BY client) AS total,
+    COUNT(DISTINCT page) OVER (PARTITION BY client, font_family) / COUNT(DISTINCT page) OVER (PARTITION BY client) AS pct
+  FROM
+    `httparchive.almanac.parsed_css`,
+    UNNEST(getFontFamilies(css)) AS font_family
+  WHERE
+    date = '2020-08-01')
 WHERE
-  date="2020-08-01"
-GROUP BY
-  client,
-  font_family
-HAVING
-  freq >= 1000 
+  pages >= 1000
 ORDER BY
-  client, pct DESC
+  pct DESC
