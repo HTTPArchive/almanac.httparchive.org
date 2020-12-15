a66773672904b9b70e523573e47a3d9274e3ad57
Stefan Matei <stefan.matei@caphyon.com>
2020-12-15 09:36:56 +0200

    Got 04_11a.sql and 04_11b.sql from almanac.httparchive.org\sql\2019\04_Media and run them on the current data set.

---
+++ sql/2020/05_Media/bytes_per_pixel.sql
@@ -0,0 +1,90 @@
+#standardSQL
+# Bytes per pixel per image format
+CREATE TEMPORARY FUNCTION getImages(payload STRING)
+RETURNS ARRAY<STRUCT<url STRING, naturalWidth INT64, naturalHeight INT64, width INT64, height INT64>> LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(payload);
+  var images = JSON.parse($._Images) || [];
+  return images.map(({url, naturalHeight, naturalWidth, width, height}) => ({url, naturalHeight: Number.parseInt(naturalHeight) || 0, naturalWidth: Number.parseInt(naturalWidth) || 0, width: Number.parseInt(width) || 0, height: Number.parseInt(height) || 0}));
+} catch (e) {}
+return null;
+''';
+
+SELECT
+  a.client,
+  imageType,
+  count(0) as count,
+  APPROX_QUANTILES(if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 1000)[OFFSET(100)] AS pixels_p10,
+  APPROX_QUANTILES(if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 1000)[OFFSET(250)] AS pixels_p25,
+  APPROX_QUANTILES(if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 1000)[OFFSET(500)] AS pixels_p50,
+  APPROX_QUANTILES(if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 1000)[OFFSET(750)] AS pixels_p75,
+  APPROX_QUANTILES(if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 1000)[OFFSET(900)] AS pixels_p90,
+  APPROX_QUANTILES(bytes, 1000)[OFFSET(100)] AS bytes_p10,
+  APPROX_QUANTILES(bytes, 1000)[OFFSET(250)] AS bytes_p25,
+  APPROX_QUANTILES(bytes, 1000)[OFFSET(500)] AS bytes_p50,
+  APPROX_QUANTILES(bytes, 1000)[OFFSET(750)] AS bytes_p75,
+  APPROX_QUANTILES(bytes, 1000)[OFFSET(900)] AS bytes_p90,
+  APPROX_QUANTILES(round(bytes/if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 4), 1000)[OFFSET(100)] AS bpp_p10,
+  APPROX_QUANTILES(round(bytes/if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 4), 1000)[OFFSET(250)] AS bpp_p25,
+  APPROX_QUANTILES(round(bytes/if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 4), 1000)[OFFSET(500)] AS bpp_p50,
+  APPROX_QUANTILES(round(bytes/if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 4), 1000)[OFFSET(750)] AS bpp_p75,
+  APPROX_QUANTILES(round(bytes/if(imageType = 'svg' AND pixels > 0, pixels, naturalPixels), 4), 1000)[OFFSET(900)] AS bpp_p90
+FROM
+(
+  SELECT
+    _TABLE_SUFFIX AS client,
+    p.url AS page,
+    image.url AS url,
+    image.width width,
+    image.height height,
+    image.naturalWidth naturalWidth,
+    image.naturalHeight naturalHeight,
+    ifnull(image.width, 0) * ifnull(image.height, 0) pixels,
+    ifnull(image.naturalWidth, 0) * ifnull(image.naturalHeight, 0) naturalPixels
+  FROM
+    `httparchive.pages.2020_08_01_*` p
+    CROSS JOIN UNNEST(getImages(payload)) AS image
+  WHERE
+    image.naturalHeight > 0
+    AND image.naturalWidth > 0
+--  LIMIT 1000
+) a
+LEFT JOIN
+(
+    SELECT
+      client,
+      page,
+      url,
+      nullif(if(regexp_contains(mimetype, r'(?i)^application|^applicaton|^binary|^image$|^multipart|^media|^$|^text/html|^text/plain|\d|array|unknown|undefined|\*|string|^img|^images|^text|\%2f|\(|ipg$|jpe$|jfif'), format, lower(regexp_replace(regexp_replace(mimetype, r'(?is).*image[/\\](?:x-)?|[\."]|[ +,;]+.*$', ''), r'(?i)pjpeg|jpeg', 'jpg'))), '') as imageType,
+      respSize as bytes
+    FROM `httparchive.almanac.requests`
+
+    WHERE
+      # many 404s and redirects show up as image/gif
+      status = 200
+
+      # we are trying to catch images. WPO populates the format for media but it uses a file extension guess.
+      #So we exclude mimetypes that aren't image or where the format couldn't be guessed by WPO
+      and (format <> '' OR mimetype like 'image%')
+
+      # many image/gifs are really beacons with 1x1 pixel, but svgs can get caught in the mix
+      and (respSize > 1500 OR regexp_contains(mimetype, r'svg'))
+
+      # strip favicon requests
+      and format <> 'ico'
+
+      # strip video mimetypes and other favicons
+      and not regexp_contains(mimetype, r'video|ico')
+-- limit 1000
+) b
+ON (b.client = a.client AND a.page = b.page AND a.url = b.url)
+
+WHERE
+  naturalPixels > 0
+  AND bytes > 0
+  AND imageType in ('jpg', 'png', 'webp', 'gif', 'svg')
+GROUP BY
+  client,
+  imageType
+ORDER BY
+  client desc;
---
+++ sql/2020/05_Media/pixel_volume.sql
@@ -0,0 +1,72 @@
+#standardSQL
+# 04_11b: pixel volume
+CREATE TEMPORARY FUNCTION getCssPixels(payload STRING)
+RETURNS INT64 LANGUAGE js AS '''
+try {
+  let data = JSON.parse(payload);
+  return data.reduce((a, c) => a + (c.width||0)*(c.height||0), 0) || 0;
+}
+catch (e) {}
+return null;
+''';
+
+CREATE TEMPORARY FUNCTION getNaturalPixels(payload STRING)
+RETURNS INT64 LANGUAGE js AS '''
+try {
+  let data = JSON.parse(payload);
+  return data.reduce((a, c) => a + (c.naturalWidth||0)*(c.naturalHeight||0), 0) || 0;
+}
+catch (e) {}
+return null;
+''';
+
+SELECT
+  client,
+  count(0) as count,
+  any_value(viewportHeight) viewportHeight,
+  any_value(viewportWidth) viewportWidth,
+  any_value(dpr) dpr,
+  any_value(viewportHeight) * any_value(viewportWidth) displaypx,
+  APPROX_QUANTILES(cssPixels, 1000)[OFFSET(100)] AS cssPixels_p10,
+  APPROX_QUANTILES(cssPixels, 1000)[OFFSET(250)] AS cssPixels_p25,
+  APPROX_QUANTILES(cssPixels, 1000)[OFFSET(500)] AS cssPixels_p50,
+  APPROX_QUANTILES(cssPixels, 1000)[OFFSET(750)] AS cssPixels_p75,
+  APPROX_QUANTILES(cssPixels, 1000)[OFFSET(900)] AS cssPixels_p90,
+  APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(100)] AS naturalPixels_p10,
+  APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(250)] AS naturalPixels_p25,
+  APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(500)] AS naturalPixels_p50,
+  APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(750)] AS naturalPixels_p75,
+  APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(900)] AS naturalPixels_p90,
+  Round(APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(100)] / (any_value(viewportHeight) * any_value(viewportWidth)), 2) AS pct_p10,
+  Round(APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(250)] / (any_value(viewportHeight) * any_value(viewportWidth)), 2) AS pct_p25,
+  Round(APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(500)] / (any_value(viewportHeight) * any_value(viewportWidth)), 2) AS pct_p50,
+  Round(APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(750)] / (any_value(viewportHeight) * any_value(viewportWidth)), 2) AS pct_p75,
+  Round(APPROX_QUANTILES(naturalPixels, 1000)[OFFSET(900)] / (any_value(viewportHeight) * any_value(viewportWidth)), 2) AS pct_p90
+FROM
+(
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url as page,
+    getCssPixels(json_extract_scalar(payload, '$._Images')) as cssPixels,
+    getNaturalPixels(json_extract_scalar(payload, '$._Images')) as naturalPixels,
+    CAST(json_extract_scalar(payload, '$._smallImageCount') AS Int64) as smallImageCount,
+    CAST(json_extract_scalar(payload, '$._bigImageCount') AS Int64) as bigImageCount,
+    CAST(json_extract_scalar(payload, '$._image_total') AS Int64) as imageBytes,
+    CAST(json_extract_scalar(json_extract_scalar(payload, '$._Dpi'), '$.dppx') AS Float64) as dpr,
+    CAST(json_extract_scalar(json_extract_scalar(payload, '$._Resolution'), '$.absolute.height') AS Float64) as viewportHeight,
+    CAST(json_extract_scalar(json_extract_scalar(payload, '$._Resolution'), '$.absolute.width') AS Float64) as viewportWidth
+  FROM
+    `httparchive.pages.2020_08_01_*`
+--  LIMIT 1000
+)
+WHERE
+  # it appears the _Images array is populated only from <img> tag requests and not CSS or favicon
+  # likewise the bigImageCount and smallImageCount only track images > 100,000 and < 10,000 respectively.
+  # Meaning images between 10KB and 100KB won't show up in the count
+  # https://github.com/WPO-Foundation/webpagetest/blob/master/www/breakdown.inc#L95
+  cssPixels > 0 AND naturalPixels > 0
+  AND (smallImageCount > 0 OR bigImageCount > 0)
+GROUP BY
+  client
+ORDER BY
+  client desc
