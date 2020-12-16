0a6a04b9492bf7cec91b08a7f0d3db1a2800dca3
Barry <barry@tunetheweb.com>
2020-12-16 16:20:00 +0000

    Alternative improvement to figure menus

---
+++ src/tools/generate/generate_table_figure_dropdowns.js
@@ -0,0 +1,27 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+/* Grab all the <figcaptions> from tables and add a figure_link in the table-wrap div */
+const table_dropdowns = (html) => {
+  const dom = new JSDOM(`<!DOCTYPE html><body>${html}</body>`);
+
+  const figures = dom.window.document.querySelectorAll('figure');
+  [...figures].forEach(figure => {
+      const figcaption = figure.querySelector('figcaption');
+      if (figcaption) {
+        let figure_dropdown = figcaption.innerHTML.replace(/figure_link/, 'figure_dropdown');
+        figure_dropdown = figure_dropdown.replace(/caption=".*?"(,\s)+/, ' ');
+        figure_dropdown_node = dom.window.document.createTextNode(figure_dropdown);
+        const table_wrap = figure.querySelector('.table-wrap');
+        if (table_wrap) {
+          table_wrap.appendChild(figure_dropdown_node);
+        }
+      }
+  });
+
+  return dom.window.document.querySelector('body').innerHTML;
+};
+
+module.exports = {
+  table_dropdowns
+};
