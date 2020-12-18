8a6d616f9627e73be1c80c6020dae6f1469a8a05
Barry Pollard <barry_pollard@hotmail.com>
2020-12-18 15:51:41 +0000

    Fix dropdown JS issue raised by CodeQL (#1801)

--- src/tools/generate/generate_table_figure_dropdowns.js
+++ src/tools/generate/generate_table_figure_dropdowns.js
@@ -11,7 +11,7 @@ const generate_table_figure_dropdowns = (html) => {
       const table_wrap = figure.querySelector('.table-wrap');
       if (figcaption && table_wrap) {
         let figure_dropdown = figcaption.innerHTML.replace(/figure_link/, 'figure_dropdown');
-        figure_dropdown_node = dom.window.document.createTextNode(figure_dropdown);
+        let figure_dropdown_node = dom.window.document.createTextNode(figure_dropdown);
         if (figure_dropdown) {
           table_wrap.appendChild(figure_dropdown_node);
         }
