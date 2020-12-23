459ca95f121fbd0fd4ec18601a9cfaef1ae1d2fd
rockeynebhwani <rockeynebhwani@users.noreply.github.com>
2020-12-23 02:58:48 +0000

    Updated Top eCommerce vendors query
    - Added Distinct in select clause
    - Removed secondary signal which were added in Wappalyzer to increase eCommerce coverage (these are not relevant for vendor % market share)

--- sql/2020/16_Ecommerce/top_vendors.sql
+++ sql/2020/16_Ecommerce/top_vendors.sql
@@ -1,26 +1,31 @@
 #standardSQL
 # 13_01: Top ecommerce vendors by device
+# Excluding apps which are not eCommerce platforms/vendors themselves but are used to identify eCommerce sites. These are signals added in Wappalyzer in 2020 to get better idea on % of eCommerce sites but these are not relevant for vendor % market share analysis
 SELECT
   _TABLE_SUFFIX AS client,
-  app,
-  COUNT(0) AS freq,
+  COUNT(DISTINCT url) AS freq,
   total,
-  ROUND(COUNT(0) * 100 / total, 2) AS pct
+  COUNT(DISTINCT url) / total AS pct
 FROM
   `httparchive.technologies.2020_08_01_*`
 JOIN
-  (SELECT _TABLE_SUFFIX, COUNT(0) AS total 
-    FROM `httparchive.summary_pages.2020_08_01_*` 
-    GROUP BY _TABLE_SUFFIX)
+  (SELECT 
+      _TABLE_SUFFIX,
+      COUNT(DISTINCT url) AS total
+    FROM 
+      `httparchive.summary_pages.2020_08_01_*` 
+    GROUP BY 
+      _TABLE_SUFFIX)
 USING (_TABLE_SUFFIX)
 WHERE
   category = 'Ecommerce'
   and 
-  app != 'Cart Functionality'
+  (app != 'Cart Functionality' AND 
+   app != 'Google Analytics Enhanced eCommerce')
 GROUP BY
   client,
-  total,
-  app
+  total
 ORDER BY
-  freq / total DESC
-LIMIT 100
+  client desc,
+  pct DESC
+LIMIT 1000
