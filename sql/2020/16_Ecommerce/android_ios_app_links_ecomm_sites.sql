4c6959cc4a801e531fdf834b38ba108f48de8f5c
rockeynebhwani <rockeynebhwani@users.noreply.github.com>
2020-12-24 19:37:18 +0000

    Native app links association Query and Median LH scores per category for eCommerce (#1836)
    * Update pct_ecommsites_bydevice_compare20192020.sql
    
    Fix for https://github.com/HTTPArchive/almanac.httparchive.org/issues/1810
    
    * Some more fixes for 2019/2020 comparison query
    
    Forgot to add distinct in 2019 query and also looks like Jason may not have realised that 2019 run was against July tables.. so changing 2019 query to run against Jul-2019 table so that numbers match with 2019 chapter
    
    * eCommerce Query Modified
    
    Modified the query to identified eCommerce sites with AMP. Previous query was giving wrong result. Test with domain - https://direct.asda.com/
    
    * New query for WebPush stats for eCommerce origins
    
    Result from this query updated on eCommerce chapter sheet - https://docs.google.com/spreadsheets/d/1Hvsh_ZBKg2vWhouJ8vIzLmp0nLIMzrT2mr6RQbIkxqY/edit#gid=2129008669
    
    * CMP Stats for eCommerce origins
    
    Key findings -
    Higher usage compared to ALL origins - Result of query @ https://docs.google.com/spreadsheets/d/1Hvsh_ZBKg2vWhouJ8vIzLmp0nLIMzrT2mr6RQbIkxqY/edit#gid=1374272999
    
    Top CMPs are same as ALL origins. Results here - https://docs.google.com/spreadsheets/d/1Hvsh_ZBKg2vWhouJ8vIzLmp0nLIMzrT2mr6RQbIkxqY/edit#gid=1289324743
    
    * Fixing extension from .txt to .sql
    
    Result of the webpush SQL here - https://docs.google.com/spreadsheets/d/1Hvsh_ZBKg2vWhouJ8vIzLmp0nLIMzrT2mr6RQbIkxqY/edit#gid=2129008669
    
    * eCommerce quries for accessibility
    
    % usage of third party A11Y solutions... results here - https://docs.google.com/spreadsheets/d/1Hvsh_ZBKg2vWhouJ8vIzLmp0nLIMzrT2mr6RQbIkxqY/edit#gid=551118862
    
    % share of each A11Y solutions - https://docs.google.com/spreadsheets/d/1Hvsh_ZBKg2vWhouJ8vIzLmp0nLIMzrT2mr6RQbIkxqY/edit#gid=214256163
    
    * Update sql/2020/16_Ecommerce/percent_of_ecommsites_using_a11y_solutions.sql
    
    Thanks.. I didn't realise this
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    
    * Update sql/2020/16_Ecommerce/percent_of_ecommsites_using_cmp.sql
    
    Thanks.. Didn't realize this
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    
    * Update sql/2020/16_Ecommerce/percent_of_mobile_ecommsites_using_each_cmp.sql
    
    Thanks.. Didn't realize this
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    
    * Incorporated Barry's review comments in various files
    
    Results sheet updated with updated queries
    
    * Modified AMP query as per Barry's suggestion
    
    * Updated Top eCommerce vendors query
    
    - Added Distinct in select clause
    - Removed secondary signal which were added in Wappalyzer to increase eCommerce coverage (these are not relevant for vendor % market share)
    
    * More changes
    
    1 - Forgot to add APP in SELECT clause for top vendors query
    2 - added new query for webpush adoption by ecomm sites
    3 - changed from sep-2020 to aug-2020 for webpush scores query
    
    * Formatting Nits
    
    * Native app links association Query and Median LH scores per category for eCommerce
    
    Results updated in
    https://docs.google.com/spreadsheets/d/1Hvsh_ZBKg2vWhouJ8vIzLmp0nLIMzrT2mr6RQbIkxqY/edit#gid=436801065
    
    and
    
    https://docs.google.com/spreadsheets/d/1Hvsh_ZBKg2vWhouJ8vIzLmp0nLIMzrT2mr6RQbIkxqY/edit#gid=1467434719
    
    * Update sql/2020/16_Ecommerce/android_ios_app_links_ecomm_sites.sql
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>
    
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>

---
+++ sql/2020/16_Ecommerce/android_ios_app_links_ecomm_sites.sql
@@ -0,0 +1,32 @@
+#standardSQL
+# 13_21: Native app links association for ecommerce sites. 
+# This query uses custom metric 'ecommerce' - https://github.com/HTTPArchive/legacy.httparchive.org/blob/master/custom_metrics/ecommerce.js
+SELECT
+  client,
+  COUNTIF(android_app_links) AS android_app_links,
+  COUNTIF(ios_universal_links) AS ios_universal_links,
+  COUNT(0) AS total,
+  COUNTIF(android_app_links) / COUNT(0) AS pct_android_app_links,
+  COUNTIF(ios_universal_links) / COUNT(0) AS pct_ios_universal_links
+FROM (
+  SELECT DISTINCT
+    _TABLE_SUFFIX as client,
+    url
+  FROM
+    `httparchive.technologies.2020_08_01_*`
+  WHERE
+    category = 'Ecommerce')
+JOIN (
+  SELECT
+    _TABLE_SUFFIX as client,
+    url,
+    JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._ecommerce'), '$.AndroidAppLinks') = '1' AS android_app_links,
+    JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._ecommerce'), '$.iOSUniveralLinks') = '1' AS ios_universal_links
+  FROM
+    `httparchive.pages.2020_08_01_*`)
+USING
+  (client, url)
+GROUP BY
+  client
+ORDER BY
+  client
