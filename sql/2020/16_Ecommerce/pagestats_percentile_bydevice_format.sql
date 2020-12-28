a9fc1a6a8ea532f7d9927aa4985f97d78204c26c
rockeynebhwani <rockeynebhwani@users.noreply.github.com>
2020-12-28 03:26:29 +0000

    More eCommerce query corrections

--- sql/2020/16_Ecommerce/pagestats_percentile_bydevice_format.sql
+++ sql/2020/16_Ecommerce/pagestats_percentile_bydevice_format.sql
@@ -15,15 +15,13 @@ FROM (
   FROM
     `httparchive.almanac.summary_requests`
   JOIN (
-    SELECT 
+    SELECT DISTINCT
       _TABLE_SUFFIX AS client, 
       url AS page
     FROM `httparchive.technologies.2020_08_01_*`
     WHERE category = 'Ecommerce')
   USING
     (client, page)
-  WHERE
-    date = '2020-08-01' 
   GROUP BY
     client,
     type,
