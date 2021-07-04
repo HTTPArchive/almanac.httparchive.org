f71db380accb02f51b072994dbf24ed6f5385018
Nitin Pasumarthy <nipasuma@linkedin.com>
2021-07-03 22:35:14 -0700

    Find total occurences of every invalid preload type, besides total count

--- sql/2021/resource-hints/invalid-resource-types.sql
+++ sql/2021/resource-hints/invalid-resource-types.sql
@@ -1,23 +1,27 @@
 #standardSQL
 # returns the number of valid and invalid preload resource types
 CREATE TEMPORARY FUNCTION getInvalidTypes(almanac_string STRING)
-RETURNS STRING LANGUAGE js AS '''
+RETURNS ARRAY<STRUCT<type STRING, num_occurences NUMERIC>> 
+LANGUAGE js AS '''
 try {
     // obtained from https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/preload#what_types_of_content_can_be_preloaded
     var validResourceTypes = ['audio', 'document', 'embed', 'fetch', 'font', 'image', 'object', 'script', 'style', 'track', 'worker', 'video']
     var almanac = JSON.parse(almanac_string)
-    if (Array.isArray(almanac) || typeof almanac != 'object') return '{}';
+    if (Array.isArray(almanac) || typeof almanac != 'object') return [];
+
     var nodes = almanac["link-nodes"]["nodes"]
     nodes = typeof nodes == 'string' ? JSON.parse(nodes) : nodes
+    
     var validTypesCnt = 0
     var invalidTypesCnt = 0
     var invalidTypes = {}
+
     for (var node of nodes) {
       if (node['rel'].toLowerCase() === 'preload') {
         if (validResourceTypes.indexOf(node['as'].toLowerCase()) >= 0) {
           validTypesCnt += 1
         } else {
-          if(node['as'].toLowerCase() in invalidTypes) {
+          if (node['as'].toLowerCase() in invalidTypes) {
             invalidTypes[node['as'].toLowerCase()] += 1
           } else {
             invalidTypes[node['as'].toLowerCase()] = 1
@@ -26,34 +30,37 @@ try {
         }
       }
     }
-    return JSON.stringify({ validTypesCnt, invalidTypesCnt, as: invalidTypes });
-  }
-  catch {
-    return '{}'
-  }
+    // return JSON.stringify({ validTypesCnt, invalidTypesCnt, ...invalidTypes });
+    // return { validTypesCnt, invalidTypesCnt, ...invalidTypes };
+    var result = [
+        {"type": "validTypesCnt", num_occurences: validTypesCnt},
+        {"type": "invalidTypesCnt", num_occurences: invalidTypesCnt}
+    ]
+    for(var type in invalidTypes) {
+        result.push({
+            type,
+            num_occurences: invalidTypes[type]
+        })
+    }
+    return result
+} catch {
+  return []
+}
 ''';
-SELECT
-    client,
-    types,
-    COUNT(0) as count
+
+SELECT client, type, SUM(num_occurences) AS total_occurences
 FROM (
-    SELECT
-        client,
-        CAST(JSON_EXTRACT_SCALAR(invalidTypes, '$.invalidTypesCnt') AS NUMERIC) AS invalid,
-        JSON_QUERY(invalidTypes, '$.as') as types,
-    FROM (
-        SELECT
-            _TABLE_SUFFIX AS client,
-            JSON_QUERY(payload, '$._almanac') AS almanac, 
-            getInvalidTypes(JSON_EXTRACT_SCALAR(payload, '$._almanac')) AS invalidTypes
-        FROM
-            `httparchive.sample_data.pages*`
-    )
+  SELECT 
+      _TABLE_SUFFIX AS client,
+      url,
+      JSON_QUERY(payload, '$._almanac') AS almanac, 
+      # getInvalidTypes(JSON_EXTRACT_SCALAR(payload, '$._almanac'))
+      rd.type, rd.num_occurences
+  FROM
+      # `httparchive.sample_data.pages*`
+      `httparchive.pages.2021_06_01_*`
+      , UNNEST(getInvalidTypes(JSON_EXTRACT_SCALAR(payload, '$._almanac'))) AS rd
+  WHERE
+      payload IS NOT NULL
 )
-WHERE
-    invalid > 0
-GROUP BY 
-    client,
-    types
-ORDER BY 
-    count DESC
+GROUP BY client, type
