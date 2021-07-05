46d70094332a4b7e68148b3dcae540c9bd984493
Kevin Farrugia <hello@imkev.dev>
2021-07-05 17:43:09 +0200

    Added sorting by total_occurrences; Formatted document

--- sql/2021/resource-hints/invalid-resource-types.sql
+++ sql/2021/resource-hints/invalid-resource-types.sql
@@ -1,21 +1,19 @@
 #standardSQL
+
 # returns the number of valid and invalid preload resource types
-CREATE TEMPORARY FUNCTION getInvalidTypes(almanac_string STRING)
-RETURNS ARRAY<STRUCT<type STRING, num_occurences NUMERIC>> 
+CREATE TEMPORARY FUNCTION getInvalidTypes(almanac_string STRING) RETURNS ARRAY<STRUCT<type STRING, num_occurences NUMERIC>> 
 LANGUAGE js AS '''
 try {
     // obtained from https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/preload#what_types_of_content_can_be_preloaded
     var validResourceTypes = ['audio', 'document', 'embed', 'fetch', 'font', 'image', 'object', 'script', 'style', 'track', 'worker', 'video']
     var almanac = JSON.parse(almanac_string)
     if (Array.isArray(almanac) || typeof almanac != 'object') return [];
-
     var nodes = almanac["link-nodes"]["nodes"]
     nodes = typeof nodes == 'string' ? JSON.parse(nodes) : nodes
     
     var validTypesCnt = 0
     var invalidTypesCnt = 0
     var invalidTypes = {}
-
     for (var node of nodes) {
       if (node['rel'].toLowerCase() === 'preload') {
         if (validResourceTypes.indexOf(node['as'].toLowerCase()) >= 0) {
@@ -30,12 +28,12 @@ try {
         }
       }
     }
-    // return JSON.stringify({ validTypesCnt, invalidTypesCnt, ...invalidTypes });
-    // return { validTypesCnt, invalidTypesCnt, ...invalidTypes };
+
     var result = [
         {"type": "validTypesCnt", num_occurences: validTypesCnt},
         {"type": "invalidTypesCnt", num_occurences: invalidTypesCnt}
     ]
+
     for(var type in invalidTypes) {
         result.push({
             type,
@@ -44,23 +42,29 @@ try {
     }
     return result
 } catch {
-  return []
+    return []
 }
 ''';
 
-SELECT client, type, SUM(num_occurences) AS total_occurences
+SELECT
+    client,
+    type,
+    SUM(num_occurences) AS total_occurences
 FROM (
-  SELECT 
-      _TABLE_SUFFIX AS client,
-      url,
-      JSON_QUERY(payload, '$._almanac') AS almanac, 
-      # getInvalidTypes(JSON_EXTRACT_SCALAR(payload, '$._almanac'))
-      rd.type, rd.num_occurences
-  FROM
-      # `httparchive.sample_data.pages*`
-      `httparchive.pages.2021_06_01_*`
-      , UNNEST(getInvalidTypes(JSON_EXTRACT_SCALAR(payload, '$._almanac'))) AS rd
-  WHERE
-      payload IS NOT NULL
+    SELECT 
+        _TABLE_SUFFIX AS client,
+        url,
+        JSON_QUERY(payload, '$._almanac') AS almanac,
+        rd.type,
+        rd.num_occurences
+    FROM
+        `httparchive.pages.2021_06_01_*`,
+        UNNEST(getInvalidTypes(JSON_EXTRACT_SCALAR(payload, '$._almanac'))) AS rd
+    WHERE
+        payload IS NOT NULL
 )
-GROUP BY client, type
+GROUP BY
+    client,
+    type
+ORDER BY
+    total_occurences DESC
