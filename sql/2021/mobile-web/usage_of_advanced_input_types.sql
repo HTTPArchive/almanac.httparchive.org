dfcb27669afdc6c9a1b547d74f2d72907da802b1
David Fox <david@davidjfox.com>
2021-08-15 15:42:38 -0500

    Naming change

--- sql/2021/mobile-web/usage_of_advanced_input_types.sql
+++ sql/2021/mobile-web/usage_of_advanced_input_types.sql
@@ -3,31 +3,31 @@
 # color, date, datetime-local, email, month, number, range, reset, search, tel, time, url, week, datalist
 CREATE TEMPORARY FUNCTION getInputStats(payload STRING)
 RETURNS STRUCT<found_advanced_types BOOLEAN, total_inputs INT64> LANGUAGE js AS '''
-  try {
-    const almanac = JSON.parse(payload);
-    const found_index = almanac.input_elements.findIndex(node => {
-      if(node.type && node.type.match(/(color|date|datetime-local|email|month|number|range|reset|search|tel|time|url|week|datalist)/i)) {
-        return true;
-      }
-    });
+try {
+  const almanac = JSON.parse(payload);
+  const found_index = almanac.input_elements.findIndex(node => {
+    if(node.type && node.type.match(/(color|date|datetime-local|email|month|number|range|reset|search|tel|time|url|week|datalist)/i)) {
+      return true;
+    }
+  });
 
-    return {
-      found_advanced_types: found_index >= 0,
-      total_inputs: almanac.input_elements.length,
-    };
-  } catch (e) {
-    return {
-      found_advanced_types: false,
-      total_inputs: 0,
-    };
-  }
+  return {
+    found_advanced_types: found_index >= 0,
+    total_inputs: almanac.input_elements.length,
+  };
+} catch (e) {
+  return {
+    found_advanced_types: false,
+    total_inputs: 0,
+  };
+}
 ''';
 
 SELECT
-    COUNT(0) as count,
-    COUNTIF(input_stats.total_inputs > 0) AS total_applicable_sites,
-    COUNTIF(input_stats.found_advanced_types) AS total_pages_using,
-    COUNTIF(input_stats.found_advanced_types) / COUNTIF(input_stats.total_inputs > 0) AS occurence_perc
+  COUNT(0) as total_sites,
+  COUNTIF(input_stats.total_inputs > 0) AS total_applicable_sites,
+  COUNTIF(input_stats.found_advanced_types) AS total_pages_using,
+  COUNTIF(input_stats.found_advanced_types) / COUNTIF(input_stats.total_inputs > 0) AS occurence_perc
 FROM (
   SELECT
     getInputStats(JSON_EXTRACT_SCALAR(payload, '$._almanac')) as input_stats
