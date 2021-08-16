f73d9734e0a9999bb1777b3347101fac65a1dae9
Barry <barry@tunetheweb.com>
2021-08-16 23:13:02 +0100

    Group rankings properly

--- sql/2021/pwa/sw_adoption_over_time_ranking.sql
+++ sql/2021/pwa/sw_adoption_over_time_ranking.sql
@@ -3,13 +3,8 @@
 SELECT
   SUBSTRING(yyyymmdd, 0, 4) || '-' || SUBSTRING(yyyymmdd, 5, 2) || '-' || RIGHT(yyyymmdd, 2) AS date,
   client,
-  CAST(rank AS STRING) AS rank,
-  CASE rank
-    WHEN 1000 THEN '<= 1,000'
-    WHEN 10000000 THEN '> 1,000,000'
-    ELSE
-      FORMAT("%'d", CAST(rank / 10 + 1 AS INT64)) || ' - ' || FORMAT("%'d", rank)
-  END AS rank_text,
+  rank_grouping,
+  FORMAT("%'d", CAST(rank_grouping AS INT64)) AS ranking,
   COUNT(0) AS freq,
   total,
   COUNT(0) / total AS pct
@@ -25,24 +20,27 @@ FROM
     WHERE
       feature = 'ServiceWorkerControlledPage' AND
       yyyymmdd >= '2021-05-01'
-  )
+  ),
+  UNNEST([1000,10000,100000,1000000]) as rank_grouping
 JOIN (
   SELECT
     REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '') AS yyyymmdd,
     SUBSTR(_TABLE_SUFFIX, 12) AS client,
-    rank,
+    rank_grouping,
     COUNT(0) AS total
   FROM
-    `httparchive.summary_pages.*`
+    `httparchive.summary_pages.*`,
+     UNNEST([1000,10000,100000,1000000]) as rank_grouping
   WHERE
-    _TABLE_SUFFIX > '2021_05_01'
+    _TABLE_SUFFIX > '2021_05_01' AND
+    rank <= rank_grouping
   GROUP BY
     yyyymmdd,
-    client,
-    rank
+    rank_grouping,
+    client
 )
 USING
-  (yyyymmdd, client, rank)
+  (yyyymmdd, client, rank_grouping)
 JOIN (
   SELECT
     REPLACE(SUBSTR(_TABLE_SUFFIX, 0, 10), '_', '') AS yyyymmdd,
@@ -56,18 +54,18 @@ JOIN (
 )
 USING
   (yyyymmdd, client, url, rank)
+WHERE rank <= rank_grouping
 GROUP BY
   yyyymmdd,
   client,
-  rank,
-  rank_text,
-  total
+  total,
+  rank_grouping
 UNION ALL
 SELECT DISTINCT
   SUBSTRING(yyyymmdd, 0, 4) || '-' || SUBSTRING(yyyymmdd, 5, 2) || '-' || RIGHT(yyyymmdd, 2) AS date,
   client,
-  'all' AS rank,
-  'all' AS rank_text,
+  10000000 AS rank_grouping,
+  'all' AS ranking,
   num_urls AS freq,
   total_urls AS total,
   num_urls / total_urls AS pct
@@ -78,5 +76,5 @@ WHERE
   yyyymmdd >= '20210501'
 ORDER BY
   date DESC,
-  rank,
+  pct,
   client
