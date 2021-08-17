e1d6c171042d67a07f740601deb6ae8c192bdc6a
Rajiv Ramnath <rajiv_ramnath@hotmail.com>
2021-08-16 20:14:09 -0400

    domains that are marked as payment processors but not ecommerce

---
+++ sql/2021/ecommerce/payment_but_not_ecomm.sql
@@ -0,0 +1,18 @@
+#standardSQL
+# This query will find domains that are payment processors but not marked as ecommerce. Limited to 100 only as a sample
+SELECT DISTINCT url
+FROM
+  `httparchive.technologies.2021_07_01_*` ha1
+WHERE
+  ha1.category = "Payment processors"
+  AND NOT EXISTS (
+  SELECT
+    url,
+    category
+  FROM
+    `httparchive.technologies.2021_07_01_*` ha2
+  WHERE
+    ha1.url = ha2.url
+    AND (ha2.category = 'Ecommerce') )
+LIMIT
+  100
