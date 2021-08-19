3e4bc5af4d0010af5937550f0d4e1da0471adcb5
Barry <barry@tunetheweb.com>
2021-08-19 12:19:38 +0100

    Update Events to look at onEvents

--- sql/2021/pwa/sw_events.sql
+++ sql/2021/pwa/sw_events.sql
@@ -1,15 +1,36 @@
 #standardSQL
 # SW events
 
-CREATE TEMPORARY FUNCTION getSWEvents(swEventListenersInfo STRING)
+CREATE TEMPORARY FUNCTION getSWEvents(swEventListenersInfo STRING, swPropertiesInfo STRING)
 RETURNS ARRAY<STRING> LANGUAGE js AS '''
 try {
-  var swEvents = Object.values(JSON.parse(swEventListenersInfo));
-  if (typeof swEvents != 'string') {
-    swEvents = swEvents.toString();
+  var swEventsListeners = new Array(0);
+  var swEventsOn = new Array(0);
+
+  /* get the addEventListener(XXXX, ...) events */
+  if (swEventListenersInfo != '[]') {
+    swEventsListeners = Object.values(JSON.parse(swEventListenersInfo));
+    if (typeof swEventsListeners != 'string') {
+      swEventsListeners = swEventsListeners.toString();
+    }
+    swEventsListeners = swEventsListeners.trim().split(',');
+    swEventsListeners = Array.from(new Set(swEventsListeners));
   }
-  swEvents = swEvents.trim().split(',');
-  return Array.from(new Set(swEvents));
+
+  /* get the onXXXX events */
+  if (swPropertiesInfo != '[]') {
+    var swEventsOn = Object.values(JSON.parse(swPropertiesInfo));
+    if (typeof swEventsOn != 'string') {
+      swEventsOn = swEventsOn.toString();
+    }
+    swEventsOn = swEventsOn.trim().split(',');
+    swEventsOn = Array.from(new Set(swEventsOn));
+  }
+
+  /* Combine the two */
+  var swEvents = swEventsListeners.concat(swEventsOn);
+  /* Filter out any duplicates - note BigQuery doesn't have Array.unique() :-( */
+  swEvents = swEvents.filter((item, pos) => swEvents.indexOf(item) === pos)
 } catch (e) {
   return [e];
 }
@@ -23,7 +44,7 @@ SELECT
   COUNT(DISTINCT url) / total AS pct
 FROM
   `httparchive.pages.2021_07_01_*`,
-  UNNEST(getSWEvents(JSON_EXTRACT(payload, '$._pwa.swEventListenersInfo'))) AS event
+  UNNEST(getSWEvents(JSON_EXTRACT(payload, '$._pwa.swEventListenersInfo'), JSON_EXTRACT(payload, '$._pwa.swPropertiesInfo'))) AS event
 JOIN
   (
     SELECT
@@ -41,7 +62,10 @@ USING (_TABLE_SUFFIX)
 WHERE
   JSON_EXTRACT(payload, '$._pwa') != "[]" AND
   JSON_EXTRACT(payload, '$._pwa.serviceWorkerHeuristic') = "true" AND
-  JSON_EXTRACT(payload, '$._pwa.swEventListenersInfo') != "[]"
+  (
+    JSON_EXTRACT(payload, '$._pwa.swEventListenersInfo') != "[]" OR
+    JSON_EXTRACT(payload, '$._pwa.swPropertiesInfo') != "[]"
+  )
 GROUP BY
   client,
   total,
