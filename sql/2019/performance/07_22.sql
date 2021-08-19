f6107901b229b1e201700bf3bf241356109c851a
Barry Pollard <barry_pollard@hotmail.com>
2021-08-19 19:07:40 +0100

    More SQL linting fixes (#2315)

--- sql/2019/performance/07_22.sql
+++ sql/2019/performance/07_22.sql
@@ -7,13 +7,13 @@ SELECT
   APPROX_QUANTILES(paint_cpu_time, 1000)[OFFSET(percentile * 10)] AS paint_cpu_time
 FROM (
   SELECT
-  _TABLE_SUFFIX AS client,
-  (
-    CAST(IFNULL(JSON_EXTRACT(payload, "$['_cpu.Paint']"), "0") AS INT64) +
-    CAST(IFNULL(JSON_EXTRACT(payload, "$['_cpu.UpdateLayerTree']"), "0") AS INT64)
-  ) AS paint_cpu_time
+    _TABLE_SUFFIX AS client,
+    (
+      CAST(IFNULL(JSON_EXTRACT(payload, "$['_cpu.Paint']"), "0") AS INT64) +
+      CAST(IFNULL(JSON_EXTRACT(payload, "$['_cpu.UpdateLayerTree']"), "0") AS INT64)
+    ) AS paint_cpu_time
   FROM
-   `httparchive.pages.2019_07_01_*`),
+    `httparchive.pages.2019_07_01_*`),
   UNNEST([10, 25, 50, 75, 90]) AS percentile
 GROUP BY
   percentile,
