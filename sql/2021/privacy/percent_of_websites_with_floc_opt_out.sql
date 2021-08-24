b69b9e4e56f60395cf391119c88a24c67e78370d
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-08-24 15:53:26 +0200

    FLoC opt-out

---
+++ sql/2021/privacy/percent_of_websites_with_floc_opt_out.sql
@@ -0,0 +1,32 @@
+#standardSQL
+# Pages that opt out of FLoC
+
+WITH response_headers AS (
+  SELECT
+    client,
+    page,
+    rank,
+    JSON_VALUE(response_header, '$.name') name,
+    JSON_VALUE(response_header, '$.value') value
+  FROM
+    `httparchive.almanac.summary_response_bodies`,
+    UNNEST(JSON_QUERY_ARRAY(response_headers)) response_header
+  WHERE
+    date = '2021-07-01'
+  AND
+    firstHtml = true
+)
+
+SELECT 
+  client,
+  rank,
+  COUNT(DISTINCT page) AS nb_websites,
+  ROUND(COUNT(0) / (SELECT COUNT(0) FROM response_headers), 2) AS pct_websites
+FROM
+  response_headers 
+WHERE    
+  name = 'Permissions-Policy'
+  AND
+  value LIKE 'interest-cohort=()' # value could contain other policies
+GROUP BY
+  1, 2
