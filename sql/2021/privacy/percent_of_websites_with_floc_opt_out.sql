bd4724543a3dfd8ccc8ae72e3ac3f4b44f2875ea
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-08-24 17:11:00 +0200

    Search meta tags as well for FLoC opt-out

--- sql/2021/privacy/percent_of_websites_with_floc_opt_out.sql
+++ sql/2021/privacy/percent_of_websites_with_floc_opt_out.sql
@@ -6,8 +6,8 @@ WITH response_headers AS (
     client,
     page,
     rank,
-    LOWER(JSON_VALUE(response_header, '$.name')) AS name,
-    LOWER(JSON_VALUE(response_header, '$.value')) AS value
+    LOWER(JSON_VALUE(response_header, '$.name')) AS header_name,
+    LOWER(JSON_VALUE(response_header, '$.value')) AS header_value
   FROM
     `httparchive.almanac.summary_response_bodies`,
     UNNEST(JSON_QUERY_ARRAY(response_headers)) response_header
@@ -15,6 +15,25 @@ WITH response_headers AS (
     date = '2021-07-01'
   AND
     firstHtml = TRUE
+),
+
+meta_tags AS (
+  SELECT
+    client,
+    url AS page,
+    LOWER(JSON_VALUE(meta_node, '$.http-equiv')) AS tag_name,
+    LOWER(JSON_VALUE(meta_node, '$.content')) AS tag_value
+  FROM (
+    SELECT
+      _TABLE_SUFFIX AS client,
+      url,
+      JSON_VALUE(payload, "$._almanac") AS metrics
+    FROM
+      `httparchive.pages.2021_07_01_*`
+    ),
+    UNNEST(JSON_QUERY_ARRAY(metrics, "$.meta-nodes.nodes")) meta_node
+  WHERE
+    JSON_VALUE(meta_node, '$.http-equiv') IS NOT NULL
 ),
 
 total_nb_pages AS (
@@ -38,10 +57,10 @@ SELECT
   COUNT(DISTINCT page) AS nb_websites,
   ROUND(COUNT(DISTINCT page) / MIN(total_nb_pages.total_nb_pages), 2) AS pct_websites
 FROM
-  response_headers JOIN total_nb_pages USING (client, rank)
+  response_headers FULL OUTER JOIN meta_tags USING (client, page) JOIN total_nb_pages USING (client, rank)
 WHERE
-  name = 'permissions-policy'
-  AND
-  value LIKE 'interest-cohort=()' # value could contain other policies
+  (header_name = 'permissions-policy' AND header_value LIKE 'interest-cohort=()')
+  OR
+  (tag_name = 'permissions-policy' AND tag_value LIKE 'interest-cohort=()') # value could contain other policies
 GROUP BY
   1, 2
