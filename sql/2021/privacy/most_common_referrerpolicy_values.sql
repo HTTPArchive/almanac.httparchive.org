6a541072deeff3a949e0e549ac0fa11862933e33
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-08-24 15:52:14 +0200

    Referrer-Policy

---
+++ sql/2021/privacy/most_common_referrerpolicy_values.sql
@@ -0,0 +1,67 @@
+#standardSQL
+# Most common values for Referrer-Policy (at site level)
+
+WITH pages_privacy AS (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url,
+    JSON_VALUE(payload, "$._privacy") AS metrics
+  FROM
+    `httparchive.pages.2021_07_01_*`
+),
+
+total_nb_pages AS (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    COUNT(*) AS total_nb_pages
+  FROM
+    `httparchive.pages.2021_07_01_*`
+  GROUP BY
+    1
+),
+
+referrer_policy_custom_metrics AS (
+  SELECT
+    client,
+    url,
+    JSON_VALUE(metrics, "$.referrerPolicy.entire_document_policy") AS entire_document_policy_meta,
+   FROM
+    pages_privacy
+),
+
+response_headers AS (
+  SELECT
+    client,
+    page,
+    JSON_VALUE(response_header, '$.name') name,
+    JSON_VALUE(response_header, '$.value') value
+  FROM
+    `httparchive.almanac.summary_response_bodies`,
+    UNNEST(JSON_QUERY_ARRAY(response_headers)) response_header
+  WHERE
+    date = '2021-07-01'
+  AND
+    firstHtml = true
+),
+
+referrer_policy_headers AS (
+  SELECT
+    client,
+    page AS url,
+    value AS entire_document_policy_header,
+  FROM
+    response_headers 
+  WHERE    
+    name = 'Referrer-Policy'
+)
+
+SELECT
+  client,
+  COALESCE(entire_document_policy_header, entire_document_policy_meta) AS entire_document_policy,
+  COUNT(0) AS nb_websites_with_values,
+  ROUND(COUNT(0) / MIN(total_nb_pages.total_nb_pages), 2) AS pct_websites_with_values
+FROM
+  referrer_policy_custom_metrics FULL OUTER JOIN referrer_policy_headers USING (client, url) 
+  JOIN total_nb_pages USING (client)
+GROUP BY
+  1, 2
---
+++ sql/2021/privacy/percent_of_websites_with_referrerpolicy.sql
@@ -0,0 +1,77 @@
+#standardSQL
+# Websites using Referrer-Policy
+
+WITH pages_privacy AS (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url,
+    JSON_VALUE(payload, "$._privacy") AS metrics
+  FROM
+    `httparchive.pages.2021_07_01_*`
+),
+
+referrer_policy_custom_metrics AS (
+  SELECT
+    client,
+    url,
+    JSON_VALUE(metrics, "$.referrerPolicy.entire_document_policy") AS entire_document_policy_meta,
+    JSON_QUERY_ARRAY(metrics, "$.referrerPolicy.individual_requests") AS individual_requests,
+    JSON_QUERY_ARRAY(metrics, "$.referrerPolicy.link_relations") AS link_relations,
+   FROM
+    pages_privacy
+),
+
+response_headers AS (
+  SELECT
+    client,
+    page,
+    JSON_VALUE(response_header, '$.name') name,
+    JSON_VALUE(response_header, '$.value') value
+  FROM
+    `httparchive.almanac.summary_response_bodies`,
+    UNNEST(JSON_QUERY_ARRAY(response_headers)) response_header
+  WHERE
+    date = '2021-07-01'
+  AND
+    firstHtml = true
+),
+
+referrer_policy_headers AS (
+  SELECT
+    client,
+    page AS url,
+    value AS entire_document_policy_header,
+  FROM
+    response_headers 
+  WHERE    
+    name = 'Referrer-Policy'
+)
+
+SELECT
+  *,
+  ROUND(nb_websites_with_entire_document_policy_meta / nb_websites, 2) AS pct_websites_with_entire_document_policy_meta,
+  ROUND(nb_websites_with_entire_document_policy_header / nb_websites, 2) AS pct_websites_with_entire_document_policy_header,
+  ROUND(nb_websites_with_entire_document_policy / nb_websites, 2) AS pct_websites_with_entire_document_policy,
+  ROUND(nb_websites_with_any_individual_requests / nb_websites, 2) AS pct_websites_with_any_individual_requests,
+  ROUND(nb_websites_with_any_link_relations / nb_websites, 2) AS pct_websites_with_any_link_relations,
+  ROUND(nb_websites_with_any_referrer_policy / nb_websites, 2) AS pct_websites_with_any_referrer_policy
+FROM (
+  SELECT
+    client,
+    COUNTIF(entire_document_policy_meta IS NOT NULL) AS nb_websites_with_entire_document_policy_meta,
+    COUNTIF(entire_document_policy_header IS NOT NULL) AS nb_websites_with_entire_document_policy_header,
+    COUNTIF(
+      entire_document_policy_meta IS NOT NULL OR entire_document_policy_header IS NOT NULL
+    ) AS nb_websites_with_entire_document_policy,
+    COUNTIF(ARRAY_LENGTH(individual_requests) > 0) AS nb_websites_with_any_individual_requests,
+    COUNTIF(ARRAY_LENGTH(link_relations) > 0) AS nb_websites_with_any_link_relations,
+    COUNTIF(
+      entire_document_policy_meta IS NOT NULL OR entire_document_policy_header IS NOT NULL OR
+      ARRAY_LENGTH(individual_requests) > 0 OR ARRAY_LENGTH(link_relations) > 0
+    ) AS nb_websites_with_any_referrer_policy,
+    COUNT(0) AS nb_websites
+  FROM
+    referrer_policy_custom_metrics FULL OUTER JOIN referrer_policy_headers USING (client, url)
+  GROUP BY
+    1
+)
