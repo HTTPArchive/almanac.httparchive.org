427ac6d354566095cb258e067a37726c0305588f
Barry Pollard <barry_pollard@hotmail.com>
2021-08-25 20:26:03 +0100

    Upgrade SQLFluff and enable extra rules (#2321)
    * Upgrade SQLFluff and enable extra rules
    
    * Better comment
    
    * Switch to Docker version
    
    * Additional fixes

--- sql/2019/performance/07_07.sql
+++ sql/2019/performance/07_07.sql
@@ -10,7 +10,7 @@ FROM (
     ROUND(avg_ttfb * 100, 2) AS avg,
     ROUND(slow_ttfb * 100, 2) AS slow,
     ROW_NUMBER() OVER (ORDER BY fast_ttfb DESC) AS row,
-    COUNT(0) OVER (PARTITION BY 0) AS n
+    COUNT(0) OVER () AS n
   FROM
     `chrome-ux-report.materialized.metrics_summary`
   WHERE
