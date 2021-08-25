149e17b63fed16a1cb92287d46bd55252a1f5e08
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-08-25 20:30:58 +0200

    Privacy Sandbox experiments through Blink features

---
+++ sql/2021/privacy/nb_websites_with_privacy_sandbox_blink_usage.sql
@@ -0,0 +1,38 @@
+#standardSQL
+# Pages that use Privacy Sandbox-related features (based on Blink features)
+
+SELECT
+  client,
+  feature,
+  num_urls,
+  total_urls,
+  ROUND(pct_urls, 2) AS pct_urls
+FROM
+  `httparchive.blink_features.usage`
+WHERE
+  yyyymmdd = '20210701' AND
+  (
+    feature = 'InterestCohortAPI_interestCohort_Method'
+    OR
+    feature = 'V8Navigator_JoinAdInterestGroup_Method'
+    OR
+    feature = 'V8Navigator_LeaveAdInterestGroup_Method'
+    OR
+    feature = 'V8Navigator_UpdateAdInterestGroups_Method'
+    OR
+    feature = 'V8Navigator_RunAdAuction_Method'
+    OR
+    feature = 'ConversionRegistration'
+    OR
+    feature = 'ImpressionRegistration'
+    OR
+    feature = 'ConversionAPIAll'
+    OR
+    feature = 'SamePartyCookieAttribute'
+    OR
+    feature = 'V8Document_HasTrustToken_Method'
+    OR
+    feature = 'HTMLFencedFrameElement'
+  )
+ORDER BY
+  2 ASC, 1 ASC
