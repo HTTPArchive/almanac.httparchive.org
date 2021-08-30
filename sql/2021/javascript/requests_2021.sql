8ca1031bd81b73fbcb859f5be6be3a12500ebde9
Nishu Goel <nishu0505@gmail.com>
2021-08-31 01:56:52 +0530

    add requests, async, module queries

---
+++ sql/2021/javascript/requests_2021.sql
@@ -0,0 +1,16 @@
+#standardSQL
+# Sum of JS requests per page (2021)
+SELECT
+  percentile,
+  _TABLE_SUFFIX AS client,
+  APPROX_QUANTILES(reqJS, 1000)[OFFSET(percentile * 10)] AS js_requests
+FROM
+  `httparchive.latest.summary_pages_*`,
+  -- `httparchive.sample_data.summary_pages_*`
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
+  client
