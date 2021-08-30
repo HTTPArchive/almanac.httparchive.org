eac2b8d151e9dd777eaa342eb6899a88092dbf87
Thomas Steiner <tomac@google.com>
2021-08-30 09:22:10 +0200

    Add queries

---
+++ sql/2021/capabilities/mobile/shape_detection_barcodes.sql
@@ -0,0 +1,23 @@
+CREATE TEMP FUNCTION
+  getFuguAPIs(DATA STRING)
+  RETURNS ARRAY<STRING>
+  LANGUAGE js AS '''
+const $ = JSON.parse(data);
+return Object.keys($);
+''';
+SELECT
+  fuguAPI,
+  url,
+  payload
+FROM
+  `httparchive.pages.2021_07_01_mobile`,
+  UNNEST(getFuguAPIs(JSON_QUERY(payload,
+        '$."_fugu-apis"'))) AS fuguAPI
+WHERE
+  JSON_QUERY(payload,
+    '$."_fugu-apis"') != "[]"
+  AND fuguAPI = "Shape Detection (Barcodes)"
+GROUP BY
+  fuguAPI,
+  url,
+  payload;
