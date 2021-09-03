6645a624a5e2971ce50a6a259a996c7bc1f41297
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-09-03 12:16:04 +0200

    Most common cookies (from last year)

--- sql/2021/privacy/top100_cookies_set_from_header.sql
+++ sql/2021/privacy/top100_cookies_set_from_header.sql
@@ -1,8 +1,9 @@
 #standardSQL
-# Top100 domains that set cookies via response header
+# Top100 popular cookies and their origins
 
 CREATE TEMPORARY FUNCTION cookieNames(headers STRING)
-RETURNS ARRAY<STRING> DETERMINISTIC
+RETURNS ARRAY<STRING>
+DETERMINISTIC
 LANGUAGE js AS '''
 try {
   var headers = JSON.parse(headers);
@@ -15,14 +16,14 @@ try {
 } catch (e) {
   return null;
 }
-''' ;
+''';
 
 WITH request_headers AS (
   SELECT
     _TABLE_SUFFIX AS client,
     page,
-    NET.REG_DOMAIN(url) AS domain,
-    cookieNames(JSON_QUERY(payload, '$.response.headers')) AS cookie_names,
+    NET.REG_DOMAIN(url) AS request,
+    cookieNames(JSON_EXTRACT(payload, '$.response.headers')) AS cookie_names,
     COUNT(0) OVER (PARTITION BY _TABLE_SUFFIX) AS websites_per_client
   FROM
     `httparchive.requests.2021_07_01_*`
@@ -36,21 +37,25 @@ WITH request_headers AS (
 cookies AS (
   SELECT
     client,
-    domain,
+    request,
+    cookie,
     COUNT(DISTINCT page) AS websites_count,
     COUNT(DISTINCT page) / ANY_VALUE(websites_per_client) AS pct_websites
   FROM request_headers,
     UNNEST(cookie_names) AS cookie
   WHERE
-    cookie IS NOT NULL AND cookie != ""
+    cookie IS NOT NULL AND
+    cookie != ""
   GROUP BY
     client,
-    domain
+    request,
+    cookie
 )
 
 SELECT
   client,
-  domain,
+  request,
+  cookie,
   websites_count,
   pct_websites
 FROM cookies
