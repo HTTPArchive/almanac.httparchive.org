de19ca1fa5e10cfed50125f0dda606eac24ac171
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-09-12 01:17:00 +0800

    Formatted query.

--- sql/2021/javascript/ajax_request_per_page.sql
+++ sql/2021/javascript/ajax_request_per_page.sql
@@ -1,16 +1,17 @@
-
 SELECT
   client,
   percentile,
-  APPROX_QUANTILES(ajax_requests_total, 1000)[OFFSET(percentile * 10)] AS ajax_requests_total
-FROM
-  (
-    SELECT
-      _TABLE_SUFFIX AS client,
-      CAST(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'),'$.ajax_requests.total') AS INT64) AS ajax_requests_total
-    FROM
-      `httparchive.sample_data.pages_*`
-  ),
+  APPROX_QUANTILES(ajax_requests_total, 1000)[
+OFFSET
+  (percentile * 10)] AS ajax_requests_total
+FROM (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    CAST(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload,
+          '$._javascript'),
+        '$.ajax_requests.total') AS INT64) AS ajax_requests_total
+  FROM
+    `httparchive.sample_data.pages_*` ),
   UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
 WHERE
   ajax_requests_total > 0
