ccfc6d2f16c85429d7c56b68f330c4d99feb4cbe
David Fox <david@davidjfox.com>
2021-09-12 11:17:00 -0500

    Only count loading=lazy usages

--- sql/2021/mobile-web/lazy_loading_usage.sql
+++ sql/2021/mobile-web/lazy_loading_usage.sql
@@ -1,16 +1,34 @@
 #standardSQL
 # Usage of native lazy loading
+CREATE TEMPORARY FUNCTION usesLoadingLazy(payload STRING)
+RETURNS BOOLEAN LANGUAGE js AS '''
+try {
+  const almanac = JSON.parse(payload);
+
+  let found = false;
+  for (const node of almanac.images.imgs.nodes) {
+    if (node.loading === "lazy") {
+      found = true;
+      break;
+    }
+  }
+
+  return found;
+} catch (e) {
+  return false;
+}
+''';
 SELECT
   client,
   COUNTIF(total_img > 0) AS pages_with_images,
 
-  COUNTIF(total_loading_attribute > 0) AS pages_using_loading_attribute,
-  COUNTIF(total_loading_attribute > 0) / COUNTIF(total_img > 0) AS pct_pages_using_loading_attribute
+  COUNTIF(uses_loading_lazy) AS pages_using_loading_attribute,
+  COUNTIF(uses_loading_lazy) / COUNTIF(total_img > 0) AS pct_pages_using_loading_attribute
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
     SAFE_CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._almanac'), '$.images.imgs.total') AS INT64) AS total_img,
-    SAFE_CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._almanac'), '$.images.imgs.attribute_usage_count.loading') AS INT64) AS total_loading_attribute
+    usesLoadingLazy(JSON_EXTRACT_SCALAR(payload, '$._almanac')) AS uses_loading_lazy
   FROM
     `httparchive.pages.2021_07_01_*`
 )
