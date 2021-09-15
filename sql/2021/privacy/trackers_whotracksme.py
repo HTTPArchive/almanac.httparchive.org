e47e5a66b57043eb40ac2a9ce05dfd16486a0c08
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-09-15 16:03:30 +0200

    Python linting fixes

--- sql/2021/privacy/trackers_whotracksme.py
+++ sql/2021/privacy/trackers_whotracksme.py
@@ -1,15 +1,17 @@
 """
 Retrieve and extract trackers as identified by WhoTracks.me.
 
-First, download the latest 'tracker database' 
+First, download the latest 'tracker database'
 (https://github.com/ghostery/whotracks.me/tree/master/whotracksme/data/assets):
 `aws s3 cp --no-sign-request s3://data.whotracks.me/trackerdb.sql .`
 
-Then, execute this script, and copy the result into the tracker-related SQL queries.
+Then, execute this script.
+Finally, copy the result into the tracker-related SQL queries.
 """
 
 import json
 import sqlite3
+
 
 # https://github.com/ghostery/whotracks.me/blob/master/blog/generating_adblocker_filters.md#loading-the-data
 def load_tracker_db(loc=':memory:'):
@@ -18,6 +20,7 @@ def load_tracker_db(loc=':memory:'):
         with open('trackerdb.sql', 'rt') as f:
             connection.executescript(f.read())
     return connection
+
 
 tracker_domains = {}
 
@@ -30,5 +33,5 @@ with load_tracker_db() as connection:
     for (category, tracker, domain) in connection.execute(sql_query):
         tracker_domains[domain] = (category, tracker)
 
-formatted_json = [(k,)+v for k, v in tracker_domains.items()]
-print(json.dumps(formatted_json,separators=(',', ':')))
+formatted_json = [(k,) + v for k, v in tracker_domains.items()]
+print(json.dumps(formatted_json, separators=(',', ':')))
