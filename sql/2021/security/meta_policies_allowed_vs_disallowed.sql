db25019348ed26532bc83bf0b53c04a4a04c1008
Gertjan Franken <gertjan.franken@gmail.com>
2021-09-16 19:45:29 +0200

    User  instead of  for -tag

--- sql/2021/security/meta_policies_allowed_vs_disallowed.sql
+++ sql/2021/security/meta_policies_allowed_vs_disallowed.sql
@@ -4,7 +4,7 @@ SELECT
   COUNT(DISTINCT page) AS total_pages,
   COUNT(DISTINCT(CASE WHEN LOWER(JSON_VALUE(meta_node, '$.http-equiv')) = LOWER(policy) OR LOWER(JSON_VALUE(meta_node, '$.name')) = LOWER(policy) THEN page END)) AS count_policy,
   COUNT(DISTINCT(CASE WHEN LOWER(JSON_VALUE(meta_node, '$.http-equiv')) = LOWER(policy) OR LOWER(JSON_VALUE(meta_node, '$.name')) = LOWER(policy) THEN page END)) / COUNT(DISTINCT page) AS pct_policy,
-  policy IN ('Content-Security-Policy', 'Referrer-Policy') AS is_allowed_as_meta
+  policy IN ('Content-Security-Policy', 'referrer') AS is_allowed_as_meta
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
@@ -14,7 +14,7 @@ FROM (
     `httparchive.pages.2021_07_01_*`
 ),
 UNNEST(JSON_QUERY_ARRAY(metrics, "$.meta-nodes.nodes")) meta_node,
-UNNEST(['Content-Security-Policy', 'Content-Security-Policy-Report-Only', 'Cross-Origin-Embedder-Policy', 'Cross-Origin-Opener-Policy', 'Cross-Origin-Resource-Policy', 'Expect-CT', 'Feature-Policy', 'Permissions-Policy', 'Referrer-Policy', 'Report-To', 'Strict-Transport-Security', 'X-Content-Type-Options', 'X-Frame-Options', 'X-XSS-Protection']) AS policy
+UNNEST(['Content-Security-Policy', 'Content-Security-Policy-Report-Only', 'Cross-Origin-Embedder-Policy', 'Cross-Origin-Opener-Policy', 'Cross-Origin-Resource-Policy', 'Expect-CT', 'Feature-Policy', 'Permissions-Policy', 'Referrer-Policy', 'referrer', 'Report-To', 'Strict-Transport-Security', 'X-Content-Type-Options', 'X-Frame-Options', 'X-XSS-Protection']) AS policy
 GROUP BY
   client,
   policy
