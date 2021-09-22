5b566367180d06369294d3521cda4551c36b0d28
Gertjan Franken <gertjan.franken@gmail.com>
2021-09-22 20:07:36 +0200

    Update cryptominer_usage.sql to calculate pct

--- sql/2021/security/cryptominer_usage.sql
+++ sql/2021/security/cryptominer_usage.sql
@@ -1,16 +1,15 @@
 #standardSQL
-SELECT
-  LEFT(_TABLE_SUFFIX, 10) AS month,
-  IF(ENDS_WITH(_TABLE_SUFFIX, '_desktop'), 'desktop', 'mobile') AS client,
-  COUNT(0) AS freq
-FROM
-  `httparchive.technologies.*`
-WHERE
-  category = 'Cryptominers'
-  OR category = 'Cryptominer'
+  SELECT
+    LEFT(_TABLE_SUFFIX, 10) AS month,
+    IF(ENDS_WITH(_TABLE_SUFFIX, '_desktop'), 'desktop', 'mobile') AS client,
+    COUNTIF(category = 'Cryptominers' OR category = 'Cryptominer') freq,
+    COUNT(0) AS total,
+    COUNTIF(category = 'Cryptominers' OR category = 'Cryptominer') / COUNT(0) AS pct
+  FROM
+    `httparchive.technologies.*`
 GROUP BY
-  _TABLE_SUFFIX
+    _TABLE_SUFFIX
 ORDER BY
-  client,
-  month,
-  freq DESC
+    client,
+    month,
+    pct DESC
