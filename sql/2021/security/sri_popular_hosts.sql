387f33419586f5db210d0e8c01d843e00ea31816
Gertjan Franken <gertjan.franken@gmail.com>
2021-09-22 22:29:08 +0200

    Rewrite sri_popular_hosts.sql to avoid UNNEST - OVER combo

--- sql/2021/security/sri_popular_hosts.sql
+++ sql/2021/security/sri_popular_hosts.sql
@@ -1,11 +1,25 @@
 #standardSQL
 # Subresource integrity: most popular hosts for which SRI is used on script tags
+WITH totals AS (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    COUNT(0) AS total_sri_scripts
+  FROM
+     `httparchive.pages.2021_07_01_*`,
+    UNNEST( JSON_EXTRACT_ARRAY(JSON_EXTRACT_SCALAR(payload, '$._security'), "$.sri-integrity")) AS sri
+  WHERE
+    sri IS NOT NULL AND
+    JSON_EXTRACT_SCALAR(sri, '$.tagname') = 'script'
+  GROUP BY
+    client
+)
+
 SELECT
   client,
   NET.HOST(JSON_EXTRACT_SCALAR(sri, '$.src')) AS host,
-  SUM(COUNT(0)) OVER (PARTITION BY client) AS total_sri_scripts,
+  MIN(total_sri_scripts),
   COUNT(0) AS freq,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct,
+  COUNT(0) / MIN(total_sri_scripts) AS pct,
   SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS total_urls,
   COUNT(DISTINCT url) AS freq_urls,
   COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS pct_urls
@@ -17,6 +31,7 @@ FROM (
   FROM
     `httparchive.pages.2021_07_01_*`),
   UNNEST(sris) AS sri
+  JOIN totals USING (client)
 WHERE
   sri IS NOT NULL AND
   JSON_EXTRACT_SCALAR(sri, '$.tagname') = 'script'
