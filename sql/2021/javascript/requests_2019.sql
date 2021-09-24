812e8dc825386704b669b64b96515a6b9dd57648
Nishu Goel <nishu0505@gmail.com>
2021-09-25 02:49:36 +0530

    frameworks, sourcemaps queries

--- sql/2021/javascript/requests_2019.sql
+++ sql/2021/javascript/requests_2019.sql
@@ -6,7 +6,6 @@ SELECT
   APPROX_QUANTILES(reqJS, 1000)[OFFSET(percentile * 10)] AS js_requests
 FROM
   `httparchive.summary_pages.2019_08_01_*`,
-  -- `httparchive.latest.summary_pages_*`
   UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
 GROUP BY
   percentile,
