6b0e24500e92e5dfd91a50b87462c8a7a5b558cd
Max Ostapenko <bvz2001@gmail.com>
2021-09-26 22:49:34 +0200

    adjusting to a single select

--- sql/2021/privacy/number_of_websites_with_privacy_service.sql
+++ sql/2021/privacy/number_of_websites_with_privacy_service.sql
@@ -2,33 +2,18 @@
 # Percent of websites with any privacy-related service, based on wappalyzer categories
 # Cannot use `core_web_vitals.technologies`, as we cannot take the `OR` of websites
 
-WITH base AS (
-  SELECT
-    _TABLE_SUFFIX AS client,
-    url,
-    LOGICAL_OR(category = "Cookie compliance") AS with_cookie_compliance,
-    LOGICAL_OR(category = "Browser fingerprinting") AS with_browser_fingerprinting,
-    LOGICAL_OR(category = "Retargeting") AS with_retargeting,
-    LOGICAL_OR(category = "Geolocation") AS with_geolocation
-  FROM `httparchive.technologies.2021_07_01_*`
-  GROUP BY
-    client,
-    url
-)
-
 SELECT
-  client,
-  COUNT(url) AS total_pages,
-  COUNTIF(with_cookie_compliance) AS number_of_websites_with_cookie_compliance,
-  COUNTIF(with_cookie_compliance) / COUNT(url) AS pct_websites_with_cookie_compliance,
-  COUNTIF(with_browser_fingerprinting) AS number_of_websites_with_browser_fingerprinting,
-  COUNTIF(with_browser_fingerprinting) / COUNT(url) AS pct_websites_with_browser_fingerprinting,
-  COUNTIF(with_retargeting) AS number_of_websites_with_retargeting,
-  COUNTIF(with_retargeting) / COUNT(url) AS pct_websites_with_retargeting,
-  COUNTIF(with_geolocation) AS number_of_websites_with_geolocation,
-  COUNTIF(with_geolocation) / COUNT(url) AS pct_websites_with_geolocation
-FROM
-  base
+  _TABLE_SUFFIX AS client,
+  COUNT(DISTINCT url) AS total_pages,
+  COUNT(DISTINCT IF(category = "Cookie compliance", url, NULL)) AS number_of_websites_with_cookie_compliance,
+  COUNT(DISTINCT IF(category = "Cookie compliance", url, NULL)) / COUNT(DISTINCT url) AS pct_websites_with_cookie_compliance,
+  COUNT(DISTINCT IF(category = "Browser fingerprinting", url, NULL)) AS number_of_websites_with_browser_fingerprinting,
+  COUNT(DISTINCT IF(category = "Browser fingerprinting", url, NULL)) / COUNT(DISTINCT url) AS pct_websites_with_browser_fingerprinting,
+  COUNT(DISTINCT IF(category = "Retargeting", url, NULL)) AS number_of_websites_with_retargeting,
+  COUNT(DISTINCT IF(category = "Retargeting", url, NULL)) / COUNT(DISTINCT url) AS pct_websites_with_retargeting,
+  COUNT(DISTINCT IF(category = "Geolocation", url, NULL)) AS number_of_websites_with_geolocation,
+  COUNT(DISTINCT IF(category = "Geolocation", url, NULL)) / COUNT(DISTINCT url) AS pct_websites_with_geolocation
+FROM `httparchive.technologies.2021_07_01_*`
 GROUP BY
   client
 ORDER BY
