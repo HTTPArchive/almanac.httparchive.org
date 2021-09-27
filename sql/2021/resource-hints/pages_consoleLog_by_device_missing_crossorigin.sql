e918a372eb3d18ad61059c22e7a50b0b119ee72b
Kevin Farrugia <hello@imkev.dev>
2021-09-27 23:14:44 +0200

    Updated missing_crossorigin query to use REGEXP_CONTAINS

--- sql/2021/resource-hints/pages_consoleLog_by_device_missing_crossorigin.sql
+++ sql/2021/resource-hints/pages_consoleLog_by_device_missing_crossorigin.sql
@@ -7,10 +7,9 @@ CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS
 );
 
 SELECT
-  client,
-  COUNTIF(consoleLog LIKE '%A preload for % is found, but is not used because the request credentials mode does not match. Consider taking a look at crossorigin attribute.%') AS freq,
-  COUNT(0) AS total,
-  AS_PERCENT(COUNTIF(consoleLog LIKE '%A preload for % is found, but is not used because the request credentials mode does not match. Consider taking a look at crossorigin attribute.%'), SUM(COUNT(0)) OVER (PARTITION BY client)) AS pct_freq
+    client,
+    COUNTIF(REGEXP_CONTAINS(consoleLog, r'A preload for .* is found, but is not used because the request credentials mode does not match. Consider taking a look at crossorigin attribute.')) AS freq,
+    COUNT(0) AS total
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
