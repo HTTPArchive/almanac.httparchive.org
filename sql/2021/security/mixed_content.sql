f20a23d00c04c8da1ba70ba8bb9b96fa1abd8b34
Gertjan Franken <gertjan.franken@gmail.com>
2021-09-27 11:28:18 +0200

    Fix mixed_content.sql

--- sql/2021/security/mixed_content.sql
+++ sql/2021/security/mixed_content.sql
@@ -1,22 +1,20 @@
 #standardSQL
-# Prevalence of landing pages over HTTPS that initiate at least one request over HTTP and distribution over ranking.
+# Prevalence of landing pages over HTTPS that include at least one reference over HTTP, and distribution over ranking
 SELECT
-  COUNT(DISTINCT page) AS total_pages,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') THEN page END)) AS count_http_requests,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') THEN page END)) / COUNT(DISTINCT page) AS pct_http_requests,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 1000 THEN page END)) AS count_top_1k,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 1000 THEN page END)) / COUNT(DISTINCT page) AS pct_top_1k,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 10000 THEN page END)) AS count_top_10k,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 10000 THEN page END)) / COUNT(DISTINCT page) AS pct_top_10k,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 100000 THEN page END)) AS count_top_100k,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 100000 THEN page END)) / COUNT(DISTINCT page) AS pct_top_100k,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 1000000 THEN page END)) AS count_top_1m,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 1000000 THEN page END)) / COUNT(DISTINCT page) AS pct_top_1m,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 10000000 THEN page END)) AS count_top_10m,
-  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') AND rank = 10000000 THEN page END)) / COUNT(DISTINCT page) AS pct_top_10m
+  client,
+  rank_grouping,
+  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') THEN page END)) AS total_pages_over_https,
+  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') THEN page END)) AS count_pages_over_https_with_http_reference,
+  COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') AND REGEXP_CONTAINS(url, r'http://.*') THEN page END)) / COUNT(DISTINCT(CASE WHEN REGEXP_CONTAINS(page, r'https://.*') THEN page END)) AS pct_pages_over_https_with_http_reference
 FROM
-  `httparchive.almanac.requests`
+  `httparchive.almanac.requests`,
+  UNNEST([1000, 10000, 100000, 1000000, 10000000]) AS rank_grouping
 WHERE
-  date = '2021-07-01'
+  date = '2021-07-01' AND
+  rank <= rank_grouping
 GROUP BY
-  client
+  client,
+  rank_grouping
+ORDER BY
+  client,
+  rank_grouping
