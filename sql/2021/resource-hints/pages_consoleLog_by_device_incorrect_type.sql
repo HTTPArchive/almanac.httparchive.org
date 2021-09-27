78452e2045d36837ac1bf66738ad7eb2320fc7ff
Kevin Farrugia <hello@imkev.dev>
2021-09-27 23:31:43 +0200

    Added query that returns the number of pages which preload a resource of the incorrect script type

---
+++ sql/2021/resource-hints/pages_consoleLog_by_device_incorrect_type.sql
@@ -0,0 +1,25 @@
+#standardSQL
+# returns the number of pages which preload a resource of the incorrect script type
+
+# helper to create percent fields
+CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS (
+  ROUND(SAFE_DIVIDE(freq, total), 4)
+);
+
+SELECT
+    client,
+    COUNTIF(REGEXP_CONTAINS(consoleLog, r'A preload for (.*) is found, but is not used because the script type does not match.')) AS freq,
+    COUNT(0) AS total,    
+FROM (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    JSON_EXTRACT(payload, "$._consoleLog") AS consoleLog
+  FROM
+    `httparchive.pages.2021_07_01_*`
+  )
+WHERE
+  consoleLog IS NOT NULL
+GROUP BY
+  client
+ORDER BY
+  client
