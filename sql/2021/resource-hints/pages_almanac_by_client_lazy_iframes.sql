36e375b3531c5b9b2508d87e8bea61e28424b6be
Nitin Pasumarthy <nipasuma@linkedin.com>
2021-09-30 10:41:21 -0700

    lazy iframes

---
+++ sql/2021/resource-hints/pages_almanac_by_client_lazy_iframes.sql
@@ -0,0 +1,39 @@
+#standardSQL
+  # Count the number of lazily loaded iframes
+CREATE TEMPORARY FUNCTION
+  countLazyIframes(almanac_string STRING)
+  RETURNS INT64
+  LANGUAGE js AS ''' 
+try {
+    var almanac = JSON.parse(almanac_string)
+    if (Array.isArray(almanac) || typeof almanac != 'object') return null;
+    
+    var iframes = almanac["iframes"]["iframes"]["nodes"]
+    return iframes.filter(i => (i.loading || "").toLowerCase() === "lazy").length
+}
+catch {
+    return null
+}
+''' ;
+WITH
+  iframe_stats_tb AS (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    JSON_EXTRACT_SCALAR(payload,
+      '$._almanac') AS almanac,
+    countLazyIframes(JSON_EXTRACT_SCALAR(payload,
+        '$._almanac')) AS res
+  FROM
+    -- `httparchive.sample_data.pages*`
+    `httparchive.pages.2021_07_01_*` )
+SELECT
+  client,
+  res AS numLazyIframes,
+  COUNT(0) AS numPages
+FROM
+  iframe_stats_tb
+WHERE
+  res > 0
+GROUP BY
+  client,
+  res
