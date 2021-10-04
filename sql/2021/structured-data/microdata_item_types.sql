682cb34688271e7f2f317953dc60f1ec187a8bed
Greg Brimble <developer@gregbrimble.com>
2021-10-05 00:44:10 +0100

    Add Microdata Item Types query

---
+++ sql/2021/structured-data/microdata_item_types.sql
@@ -0,0 +1,34 @@
+# standardSQL
+  # Count Microdata item types
+CREATE TEMP FUNCTION
+  getMicrodataItemTypes(rendered STRING)
+  RETURNS ARRAY<STRING>
+  LANGUAGE js AS r"""
+  try {
+    rendered = JSON.parse(rendered);
+    return rendered.microdata_itemtypes.map(itemType => itemType.toLowerCase());
+  } catch (e) {
+      return [];
+  }
+""";
+WITH
+  rendered_data AS (
+  SELECT
+    getMicrodataItemTypes(rendered) AS microdataItemTypes
+  FROM (
+    SELECT
+      JSON_EXTRACT(JSON_VALUE(JSON_EXTRACT(payload,
+            '$._structured-data')),
+        '$.structured_data.rendered') AS rendered
+    FROM
+      `httparchive.pages.2021_07_01_*`))
+SELECT
+  microdataItemType,
+  COUNT(microdataItemType) AS count
+FROM
+  rendered_data,
+  UNNEST(microdataItemTypes) AS microdataItemType
+GROUP BY
+  microdataItemType
+ORDER BY
+  count DESC;
--- sql/2021/structured-data/rendered.sql
+++
@@ -1,8 +0,0 @@
-#standardSQL
-  # A dump of the entire structured data custom metric for each rendered page
-SELECT
-  JSON_EXTRACT(JSON_VALUE(JSON_EXTRACT(payload,
-        '$._structured-data')),
-    '$.structured_data.rendered') AS rendered
-FROM
-  `httparchive.pages.2021_07_01_*`;
