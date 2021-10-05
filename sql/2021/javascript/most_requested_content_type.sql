25343710bf8f1baf703ee1dd4b61c38c8e34c713
Nishu Goel <nishu0505@gmail.com>
2021-10-06 02:23:48 +0530

    link fix

--- sql/2021/javascript/most_requested_content_type.sql
+++ sql/2021/javascript/most_requested_content_type.sql
@@ -2,7 +2,7 @@ SELECT
   resp_content_type,
   COUNT(0) AS count,
   SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  ROUND((COUNT(0) * 100 / total), 2) AS pct,
+  ROUND((COUNT(0) * 100 / total), 2) AS pct
 FROM
   `httparchive.almanac.requests`
 WHERE
