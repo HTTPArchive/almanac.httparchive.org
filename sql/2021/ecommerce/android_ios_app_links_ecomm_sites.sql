881f25b3599a49d8a6b66b82c41010a3cdd36da4
Rajiv Ramnath <rajiv_ramnath@hotmail.com>
2021-10-05 09:12:09 -0400

    fixing the query to use well-known

--- sql/2021/ecommerce/android_ios_app_links_ecomm_sites.sql
+++ sql/2021/ecommerce/android_ios_app_links_ecomm_sites.sql
@@ -1,6 +1,9 @@
 #standardSQL
-# 13_21: Native app links association for ecommerce sites. 
-# This query uses custom metric 'ecommerce' - https://github.com/HTTPArchive/legacy.httparchive.org/blob/master/custom_metrics/well-known.js
+# This query uses custom metric '_well-known' - https://github.com/HTTPArchive/legacy.httparchive.org/blob/master/custom_metrics/well-known.js
+# Note that in this query, there is a subtle bug where the site could have empty /.well-known/assetlinks.json or /.well-known/apple-app-site-association files which will lead to over counting sites with native app links
+# an example is: https://www.allbirds.com/.well-known/assetlinks.json which has a payload of "[]"
+# To fix this, this would require response body parsing on well-known.js
+
 SELECT
   client,
   COUNTIF(android_app_links) AS android_app_links,
@@ -20,8 +23,8 @@ JOIN (
   SELECT
     _TABLE_SUFFIX AS client,
     url,
-    JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._ecommerce'), '$.AndroidAppLinks') = '1' AS android_app_links,
-    JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._ecommerce'), '$.iOSUniveralLinks') = '1' AS ios_universal_links
+    JSON_VALUE(JSON_EXTRACT_SCALAR(payload, '$._well-known'), '$."/.well-known/assetlinks.json".found') = 'true' AS android_app_links,
+    JSON_VALUE(JSON_EXTRACT_SCALAR(payload, '$._well-known'), '$."/.well-known/apple-app-site-association".found') = 'true' AS ios_universal_links
   FROM
     `httparchive.pages.2021_07_01_*`)
 USING
