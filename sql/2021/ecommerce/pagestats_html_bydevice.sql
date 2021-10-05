2784e9c0aa3adf771a5b77749f2d23b108cb38e2
Rajiv Ramnath <rajiv_ramnath@hotmail.com>
2021-10-05 18:09:20 -0400

    fixing this query to use UNNEST and remove some redundancy

--- sql/2021/ecommerce/pagestats_html_bydevice.sql
+++ sql/2021/ecommerce/pagestats_html_bydevice.sql
@@ -2,20 +2,21 @@
 # 13_07: Distribution of HTML kilobytes per page
 SELECT
   _TABLE_SUFFIX AS client,
-  APPROX_QUANTILES(bytesHtml, 1000)[OFFSET(100)] / 1024 AS p10,
-  APPROX_QUANTILES(bytesHtml, 1000)[OFFSET(250)] / 1024 AS p25,
-  APPROX_QUANTILES(bytesHtml, 1000)[OFFSET(500)] / 1024 AS p50,
-  APPROX_QUANTILES(bytesHtml, 1000)[OFFSET(750)] / 1024 AS p75,
-  APPROX_QUANTILES(bytesHtml, 1000)[OFFSET(900)] / 1024 AS p90,
-  APPROX_QUANTILES(bytesHtml, 1000)[OFFSET(1000)] / 1024 AS p100
+  percentile,
+  APPROX_QUANTILES(bytesHtml, 1000)[OFFSET(percentile * 10)] AS requests
 FROM
   `httparchive.summary_pages.2021_07_01_*`
 JOIN
   `httparchive.technologies.2021_07_01_*`
 USING (_TABLE_SUFFIX, url)
+UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
 WHERE
   category = 'Ecommerce' AND
   (app != 'Cart Functionality' AND
    app != 'Google Analytics Enhanced eCommerce')
 GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
   client
