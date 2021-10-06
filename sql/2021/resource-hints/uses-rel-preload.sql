fc7b1f5be7037b3a8ba4cc887756a205062e0f89
Kevin Farrugia <hello@imkev.dev>
2021-10-06 11:06:16 +0200

    Resolved syntax error

--- sql/2021/resource-hints/uses-rel-preload.sql
+++ sql/2021/resource-hints/uses-rel-preload.sql
@@ -5,7 +5,7 @@
 SELECT
   JSON_EXTRACT_SCALAR(report, "$.audits.uses-rel-preload.score") AS score,
   COUNT(0) AS freq,
-  SUM(COUNT(0)) AS total,
+  SUM(COUNT(0)) OVER () AS total,
   COUNT(0) / SUM(COUNT(0)) OVER () AS pct
 FROM
   `httparchive.lighthouse.2021_07_01_mobile`
