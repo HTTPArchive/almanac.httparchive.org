2a4e72c946dfa85a770f934f8d88d9e5fd74c4f9
Greg Brimble <developer@gregbrimble.com>
2021-10-07 02:02:29 +0100

    JSON-LD relationships

---
+++ sql/2021/structured-data/jsonld_relationships.sql
@@ -0,0 +1,55 @@
+# standardSQL
+  # Count JSON-LD relationships
+CREATE TEMP FUNCTION
+  getJSONLDRelationships(rendered STRING)
+  RETURNS ARRAY<STRING>
+  LANGUAGE js AS r"""
+  try {
+    const arrayify = (value) => Array.isArray(value) ? value : [value];
+
+    const getRelationships = (o) => {
+      if (Array.isArray(o)) return o.map(child => getRelationships(child)).flat();
+
+      if (o instanceof Object) {
+        return Object.entries(o).map(([k, value]) => {
+          if (!k.startsWith('@')) return [k, ...getRelationships(value)];
+          return getRelationships(value);
+        }).flat();
+      }
+
+      return [];
+    }
+
+    rendered = JSON.parse(rendered);
+    const jsonld_scripts = rendered.jsonld_scripts
+    return jsonld_scripts.map(jsonld_script => {
+      jsonld_script = JSON.parse(jsonld_script)
+      return getRelationships(jsonld_script)
+    }).flat();
+  } catch (e) {
+    return [];
+  }
+""";
+WITH
+  rendered_data AS (
+  SELECT
+    getJSONLDRelationships(rendered) AS jsonld_relationships
+  FROM (
+    SELECT
+      JSON_EXTRACT(JSON_VALUE(JSON_EXTRACT(payload,
+            '$._structured-data')),
+        '$.structured_data.rendered') AS rendered
+    FROM
+      `httparchive.pages.2021_07_01_*` LIMIT 1000)
+)
+
+SELECT
+  jsonld_relationship,
+  COUNT(jsonld_relationship) AS count
+FROM
+  rendered_data,
+  UNNEST(jsonld_relationships) AS jsonld_relationship
+GROUP BY
+  jsonld_relationship
+ORDER BY
+  count DESC
