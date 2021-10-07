fc7e71f3b3fa310b866edc24ff62320720469599
Greg Brimble <developer@gregbrimble.com>
2021-10-07 01:03:35 +0100

    Add JSON-LD Types

---
+++ sql/2021/structured-data/jsonld_types.sql
@@ -0,0 +1,53 @@
+# standardSQL
+  # Count JSON-LD types
+CREATE TEMP FUNCTION
+  getJSONLDTypes(rendered STRING)
+  RETURNS ARRAY<STRING>
+  LANGUAGE js AS r"""
+  try {
+    const getDeep = (key, o) => {
+      if (Array.isArray(o)) return o.map(child => getDeep(key, child)).flat();
+
+      if (o instanceof Object) {
+        return Object.entries(o).map(([k, value]) => {
+          if (k === key) return [value, ...getDeep(value)];
+          return getDeep(value);
+        }).flat();
+      }
+
+      return [];
+    }
+
+    rendered = JSON.parse(rendered);
+    const jsonld_scripts = rendered.jsonld_scripts
+    return jsonld_scripts.map(jsonld_script => {
+      jsonld_script = JSON.parse(jsonld_script)
+      return getDeep('@type', jsonld_script)
+    }).flat()
+  } catch (e) {
+    return [];
+  }
+""";
+WITH
+  rendered_data AS (
+  SELECT
+    getJSONLDTypes(rendered) AS jsonld_types
+  FROM (
+    SELECT
+      JSON_EXTRACT(JSON_VALUE(JSON_EXTRACT(payload,
+            '$._structured-data')),
+        '$.structured_data.rendered') AS rendered
+    FROM
+      `httparchive.pages.2021_07_01_*`)
+)
+
+SELECT
+  jsonld_type,
+  COUNT(jsonld_type) AS count
+FROM
+  rendered_data,
+  UNNEST(jsonld_types) AS jsonld_type
+GROUP BY
+  jsonld_type
+ORDER BY
+  count DESC
