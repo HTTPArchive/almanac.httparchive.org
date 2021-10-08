a706675d2451e5e4955e0734d125d580f58288f0
John Teague <25212653+logicalphase@users.noreply.github.com>
2021-10-07 20:14:17 -0500

    Change to use 2021_07_01 dataset

--- sql/2021/page-weight/average_resource_size.sql
+++ sql/2021/page-weight/average_resource_size.sql
@@ -9,6 +9,6 @@ SELECT
   AVG(bytesHtmlDoc) / 1024 AS html_doc_kbytes,
   AVG(bytesFont) / 1024 AS font_kbytes
 FROM
-  `httparchive.summary_pages.2021_08_01_*`
+  `httparchive.summary_pages.2021_07_01_*`
 GROUP BY
   client
