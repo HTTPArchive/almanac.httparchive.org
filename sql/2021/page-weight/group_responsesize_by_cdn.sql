bbcf2decd0183ef2b302d4ec59bbfec87c3df837
John Teague <25212653+logicalphase@users.noreply.github.com>
2021-10-07 20:16:01 -0500

    Change to use 2021_07_01 dataset

--- sql/2021/page-weight/group_responsesize_by_cdn.sql
+++ sql/2021/page-weight/group_responsesize_by_cdn.sql
@@ -3,7 +3,7 @@ SELECT
   AVG(respSize) / 1024 AS resp_size,
   _cdn_provider AS _cdn_provider
 FROM
-  `httparchive.summary_requests.2021_08_01_*`
+  `httparchive.summary_requests.2021_07_01_*`
 GROUP BY
   client,
 _cdn_provider
