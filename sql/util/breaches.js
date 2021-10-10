69349dffe22b72851b52290d50afc1fe0d30821b
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-10-10 19:09:04 -0400

    Remove more whitespace

--- sql/util/breaches.js
+++ sql/util/breaches.js
@@ -4,13 +4,13 @@
 // Usage:  wget https://haveibeenpwned.com/api/v2/breaches; node breaches.js  > breaches.tsv
 
 const fs = require("fs");
-                                
+
 var almanacDate = '2021-07-01';
-                                
+
 fs.readFile("breaches", function(err, data) {
     if (err) throw err;
     const breaches = JSON.parse(data);
-    
+
     console.log('date\tName\tTitle\tDomain\tBreachDate\tAddedDate\tModifiedDate\tPwnCount\tDescription\t'
     						+ 'LogoPath\tIsVerified\tIsFabricated\tIsSensitive\tIsRetired\tIsSpamList\tDataClasses');
 
@@ -18,8 +18,8 @@ fs.readFile("breaches", function(err, data) {
     	console.log(
     		almanacDate
     		+ '\t' + breaches[i].Name
-    		+ '\t' + breaches[i].Title 
-    		+ '\t' + breaches[i].Domain 
+    		+ '\t' + breaches[i].Title
+    		+ '\t' + breaches[i].Domain
     		+ '\t' + breaches[i].BreachDate
     		+ '\t' + breaches[i].AddedDate
     		+ '\t' + breaches[i].ModifiedDate
@@ -32,7 +32,6 @@ fs.readFile("breaches", function(err, data) {
     		+ '\t' + breaches[i].IsRetired
     		+ '\t' + breaches[i].IsSpamList
     		+ '\t' + JSON.stringify(breaches[i].DataClasses)
-    		);
-    	
-    } 
+    	);
+    }
 });
