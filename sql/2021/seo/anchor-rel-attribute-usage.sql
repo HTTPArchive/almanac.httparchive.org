2ba05fd7a35079d176b455542c6f4ac582375283
JR Oakes <jroakes@gmail.com>
2021-10-10 13:05:53 -0400

    removed rounding and partitioning

--- sql/2021/seo/anchor-rel-attribute-usage.sql
+++ sql/2021/seo/anchor-rel-attribute-usage.sql
@@ -2,18 +2,11 @@
 # Anchor rel attribute usage
 # Note: this query only reports if a rel attribute value was ever used on a page. It is not a per anchor report.
 
-# helper to create percent fields
-CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS (
-  ROUND(SAFE_DIVIDE(freq, total), 4)
-);
-
-# returns all the data we need from _wpt_bodies
-CREATE TEMPORARY FUNCTION get_wpt_bodies_info(wpt_bodies_string STRING)
+CREATE TEMPORARY FUNCTION getRelStatsWptBodies(wpt_bodies_string STRING)
 RETURNS STRUCT<
   rel ARRAY<STRING>
 > LANGUAGE js AS '''
 var result = {};
-
 //Function to retrieve only keys if value is >0
 function getKey(dict){
   const arr = [],
@@ -25,16 +18,12 @@ function getKey(dict){
   }
   return arr;
 }
-
 try {
     var wpt_bodies = JSON.parse(wpt_bodies_string);
-
     if (Array.isArray(wpt_bodies) || typeof wpt_bodies != 'object') return result;
-
     if (wpt_bodies.anchors && wpt_bodies.anchors.rendered && wpt_bodies.anchors.rendered.rel_attributes) {
       result.rel = getKey(wpt_bodies.anchors.rendered.rel_attributes);
     }
-
 } catch (e) {}
 return result;
 ''';
@@ -44,13 +33,13 @@ SELECT
   rel,
   total,
   COUNT(0) AS count,
-  AS_PERCENT(COUNT(0), SUM(COUNT(0)) OVER (PARTITION BY client)) AS pct
+  SAFE_DIVIDE(COUNT(0),total) AS pct
 FROM
   (
     SELECT
       _TABLE_SUFFIX AS client,
       total,
-      get_wpt_bodies_info(JSON_EXTRACT_SCALAR(payload, '$._wpt_bodies')) AS wpt_bodies_info
+      getRelStatsWptBodies(JSON_EXTRACT_SCALAR(payload, '$._wpt_bodies')) AS wpt_bodies_info
     FROM
       `httparchive.pages.2021_07_01_*`
     JOIN
