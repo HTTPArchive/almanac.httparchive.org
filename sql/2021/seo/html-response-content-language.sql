d9356f8725d56c231d3478ad647be8baa65435f5
JR Oakes <jroakes@gmail.com>
2021-10-11 07:04:09 -0400

    removed rounding

--- sql/2021/seo/html-response-content-language.sql
+++ sql/2021/seo/html-response-content-language.sql
@@ -1,15 +1,11 @@
 #standardSQL
 # HTML response: content language
 
-CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS (
-  ROUND(SAFE_DIVIDE(freq, total), 4)
-);
-
 SELECT
   _TABLE_SUFFIX AS client,
   LOWER(resp_content_language) AS resp_content_language,
   COUNT(0) AS freq,
-  AS_PERCENT(COUNT(0), SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX)) AS pct
+  SAFE_DIVIDE(COUNT(0), SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX)) AS pct
 FROM
   `httparchive.summary_requests.2021_07_01_*`
 WHERE
