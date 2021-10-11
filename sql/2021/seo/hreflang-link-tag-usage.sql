4a93b993bb0c470dd1e178d834d6226557757e5f
JR Oakes <jroakes@gmail.com>
2021-10-11 10:25:05 -0400

    formatting

--- sql/2021/seo/hreflang-link-tag-usage.sql
+++ sql/2021/seo/hreflang-link-tag-usage.sql
@@ -28,25 +28,27 @@ SELECT
   NORMALIZE_AND_CASEFOLD(hreflang) AS hreflang,
   total,
   COUNT(0) AS count,
-  SAFE_DIVIDE(COUNT(0), total) AS pct
-
-FROM
-  (
+  SAFE_DIVIDE(COUNT(0),
+    total) AS pct
+FROM (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    total,
+    getHreflangWptBodies(JSON_EXTRACT_SCALAR(payload,
+        '$._wpt_bodies')) AS hreflang_wpt_bodies_info
+  FROM
+    `httparchive.pages.2021_07_01_*`
+  JOIN (
     SELECT
-      _TABLE_SUFFIX AS client,
-      total,
-      getHreflangWptBodies(JSON_EXTRACT_SCALAR(payload, '$._wpt_bodies')) AS hreflang_wpt_bodies_info
+      _TABLE_SUFFIX,
+      COUNT(0) AS total
     FROM
       `httparchive.pages.2021_07_01_*`
-    JOIN
-      (SELECT _TABLE_SUFFIX, COUNT(0) AS total
-       FROM
-         `httparchive.pages.2021_07_01_*`
-       GROUP BY
-         _TABLE_SUFFIX) # to get an accurate total of pages per device. also seems fast
-      USING 
-        (_TABLE_SUFFIX)), 
-      UNNEST(hreflang_wpt_bodies_info.hreflangs) AS hreflang
+    GROUP BY
+      _TABLE_SUFFIX) # TO get an accurate total OF pages per device. also seems fast
+  USING
+    (_TABLE_SUFFIX)),
+  UNNEST(hreflang_wpt_bodies_info.hreflangs) AS hreflang
 GROUP BY
   total,
   hreflang,
