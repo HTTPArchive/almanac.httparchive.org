d7cb464c4aa2dc3eaa21644066cd9fe6fdd1bd95
JR Oakes <jroakes@gmail.com>
2021-10-11 07:43:43 -0400

    naming and formatting issues

--- sql/2021/seo/pages-containing-a-video-element.sql
+++ sql/2021/seo/pages-containing-a-video-element.sql
@@ -1,13 +1,9 @@
 #standardSQL
 # Pages containing a video element
 
-# helper to create percent fields
-CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS (
-  ROUND(SAFE_DIVIDE(freq, total), 4)
-);
 
 # returns all the data we need from _almanac
-CREATE TEMPORARY FUNCTION get_almanac_info(almanac_string STRING)
+CREATE TEMPORARY FUNCTION getVideosAlmanacInfo(almanac_string STRING)
 RETURNS STRUCT<
   videos_total INT64
 > LANGUAGE js AS '''
@@ -31,14 +27,14 @@ SELECT
   COUNT(0) AS total,
 
   # Pages with videos
-  COUNTIF(almanac_info.videos_total > 0) AS has_videos,
-  AS_PERCENT(COUNTIF(almanac_info.videos_total > 0), COUNT(0)) AS pct_has_videos
+  COUNTIF(videos_almanac_info.videos_total > 0) AS has_videos,
+  SAFE_DIVIDE(COUNTIF(videos_almanac_info.videos_total > 0), COUNT(0)) AS pct_has_videos
 
 FROM
   (
     SELECT
       _TABLE_SUFFIX AS client,
-      get_almanac_info(JSON_EXTRACT_SCALAR(payload, '$._almanac')) AS almanac_info
+      getVideosAlmanacInfo(JSON_EXTRACT_SCALAR(payload, '$._almanac')) AS videos_almanac_info
     FROM
       `httparchive.pages.2021_07_01_*`
   )
