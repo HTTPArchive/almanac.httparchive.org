20ecafea76fdec525d2fe201f3185d1fcca31042
JR Oakes <jroakes@gmail.com>
2021-10-11 07:03:13 -0400

    updated rounding and function name

--- sql/2021/seo/hreflang-link-tag-usage.sql
+++ sql/2021/seo/hreflang-link-tag-usage.sql
@@ -1,13 +1,8 @@
 #standardSQL
 # hreflang link tag usage
 
-# helper to create percent fields
-CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS (
-  ROUND(SAFE_DIVIDE(freq, total), 4)
-);
-
 # returns all the data we need from _wpt_bodies
-CREATE TEMPORARY FUNCTION get_wpt_bodies_info(wpt_bodies_string STRING)
+CREATE TEMPORARY FUNCTION getHreflangWptBodies(wpt_bodies_string STRING)
 RETURNS STRUCT<
   hreflangs ARRAY<STRING>
 > LANGUAGE js AS '''
@@ -33,23 +28,23 @@ SELECT
   NORMALIZE_AND_CASEFOLD(hreflang) AS hreflang,
   total,
   COUNT(0) AS count,
-  AS_PERCENT(COUNT(0), total) AS pct
+  SAFE_DIVIDE(COUNT(0), total) AS pct
 
 FROM
   (
     SELECT
       _TABLE_SUFFIX AS client,
       total,
-      get_wpt_bodies_info(JSON_EXTRACT_SCALAR(payload, '$._wpt_bodies')) AS wpt_bodies_info
+      getHreflangWptBodies(JSON_EXTRACT_SCALAR(payload, '$._wpt_bodies')) AS hreflang_wpt_bodies_info
     FROM
       `httparchive.pages.2021_07_01_*`
     JOIN
       (SELECT _TABLE_SUFFIX, COUNT(0) AS total
-                                   FROM
+       FROM
         `httparchive.pages.2021_07_01_*`
-        GROUP BY _TABLE_SUFFIX) # to get an accurate total of pages per device. also seems fast
+       GROUP BY _TABLE_SUFFIX) # to get an accurate total of pages per device. also seems fast
     USING (_TABLE_SUFFIX)
-  ), UNNEST(wpt_bodies_info.hreflangs) AS hreflang
+  ), UNNEST(hreflang_wpt_bodies_info.hreflangs) AS hreflang
 GROUP BY
   total,
   hreflang,
