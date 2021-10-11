1ce28be8a7bd138f93274f18f580fb95cbd7ae7e
JR Oakes <jroakes@gmail.com>
2021-10-11 07:15:06 -0400

    naming and formatting issues

--- sql/2021/seo/lighthouse-seo-stats.sql
+++ sql/2021/seo/lighthouse-seo-stats.sql
@@ -3,11 +3,6 @@
 
 # live run is about $9
 
-# helper to create percent fields
-CREATE TEMP FUNCTION AS_PERCENT (freq FLOAT64, total FLOAT64) RETURNS FLOAT64 AS (
-  ROUND(SAFE_DIVIDE(freq, total), 4)
-);
-
 CREATE TEMPORARY FUNCTION isCrawlableDetails(report STRING)
 RETURNS STRUCT<
   disallow BOOL,
@@ -33,52 +28,52 @@ SELECT
   COUNT(0) AS total,
 
   COUNTIF(is_canonical) AS is_canonical,
-  AS_PERCENT(COUNTIF(is_canonical), COUNT(0)) AS pct_is_canonical,
+  SAFE_DIVIDE(COUNTIF(is_canonical), COUNT(0)) AS pct_is_canonical,
 
   COUNTIF(has_title) AS has_title,
-  AS_PERCENT(COUNTIF(has_title), COUNT(0)) AS pct_has_title,
+  SAFE_DIVIDE(COUNTIF(has_title), COUNT(0)) AS pct_has_title,
 
   COUNTIF(has_meta_description) AS has_meta_description,
-  AS_PERCENT(COUNTIF(has_meta_description), COUNT(0)) AS pct_has_meta_description,
+  SAFE_DIVIDE(COUNTIF(has_meta_description), COUNT(0)) AS pct_has_meta_description,
 
   COUNTIF(has_title AND has_meta_description) AS has_title_and_meta_description,
-  AS_PERCENT(COUNTIF(has_title AND has_meta_description), COUNT(0)) AS pct_has_title_and_meta_description,
+  SAFE_DIVIDE(COUNTIF(has_title AND has_meta_description), COUNT(0)) AS pct_has_title_and_meta_description,
 
   COUNTIF(img_alt_on_all) AS img_alt_on_all,
-  AS_PERCENT(COUNTIF(img_alt_on_all), COUNT(0)) AS pct_img_alt_on_all,
+  SAFE_DIVIDE(COUNTIF(img_alt_on_all), COUNT(0)) AS pct_img_alt_on_all,
 
   COUNTIF(link_text_descriptive) AS link_text_descriptive,
-  AS_PERCENT(COUNTIF(link_text_descriptive), COUNT(0)) AS pct_link_text_descriptive,
+  SAFE_DIVIDE(COUNTIF(link_text_descriptive), COUNT(0)) AS pct_link_text_descriptive,
 
   COUNTIF(legible_font_size) AS legible_font_size,
-  AS_PERCENT(COUNTIF(legible_font_size), COUNT(0)) AS pct_legible_font_size,
+  SAFE_DIVIDE(COUNTIF(legible_font_size), COUNT(0)) AS pct_legible_font_size,
 
   COUNTIF(heading_order_valid) AS heading_order_valid,
-  AS_PERCENT(COUNTIF(heading_order_valid), COUNT(0)) AS pct_heading_order_valid,
+  SAFE_DIVIDE(COUNTIF(heading_order_valid), COUNT(0)) AS pct_heading_order_valid,
 
   COUNTIF(robots_txt_valid) AS robots_txt_valid,
-  AS_PERCENT(COUNTIF(robots_txt_valid), COUNT(0)) AS pct_robots_txt_valid,
+  SAFE_DIVIDE(COUNTIF(robots_txt_valid), COUNT(0)) AS pct_robots_txt_valid,
 
   COUNTIF(is_crawlable) AS is_crawlable,
-  AS_PERCENT(COUNTIF(is_crawlable), COUNT(0)) AS pct_is_crawlable,
+  SAFE_DIVIDE(COUNTIF(is_crawlable), COUNT(0)) AS pct_is_crawlable,
 
   COUNTIF(is_crawlable_details.noindex) AS noindex,
-  AS_PERCENT(COUNTIF(is_crawlable_details.noindex), COUNT(0)) AS pct_noindex,
+  SAFE_DIVIDE(COUNTIF(is_crawlable_details.noindex), COUNT(0)) AS pct_noindex,
 
   COUNTIF(is_crawlable_details.disallow) AS disallow,
-  AS_PERCENT(COUNTIF(is_crawlable_details.disallow), COUNT(0)) AS pct_disallow,
+  SAFE_DIVIDE(COUNTIF(is_crawlable_details.disallow), COUNT(0)) AS pct_disallow,
 
   COUNTIF(is_crawlable_details.disallow AND is_crawlable_details.noindex) AS disallow_noindex,
-  AS_PERCENT(COUNTIF(is_crawlable_details.disallow AND is_crawlable_details.noindex), COUNT(0)) AS pct_disallow_noindex,
+  SAFE_DIVIDE(COUNTIF(is_crawlable_details.disallow AND is_crawlable_details.noindex), COUNT(0)) AS pct_disallow_noindex,
 
   COUNTIF(NOT(is_crawlable_details.disallow) AND NOT(is_crawlable_details.noindex)) AS allow_index,
-  AS_PERCENT(COUNTIF(NOT(is_crawlable_details.disallow) AND NOT(is_crawlable_details.noindex)), COUNT(0)) AS pct_allow_index,
+  SAFE_DIVIDE(COUNTIF(NOT(is_crawlable_details.disallow) AND NOT(is_crawlable_details.noindex)), COUNT(0)) AS pct_allow_index,
 
   COUNTIF(is_crawlable_details.disallow AND NOT(is_crawlable_details.noindex)) AS disallow_index,
-  AS_PERCENT(COUNTIF(is_crawlable_details.disallow AND NOT(is_crawlable_details.noindex)), COUNT(0)) AS pct_disallow_index,
+  SAFE_DIVIDE(COUNTIF(is_crawlable_details.disallow AND NOT(is_crawlable_details.noindex)), COUNT(0)) AS pct_disallow_index,
 
   COUNTIF(NOT(is_crawlable_details.disallow) AND is_crawlable_details.noindex) AS allow_noindex,
-  AS_PERCENT(COUNTIF(NOT(is_crawlable_details.disallow) AND is_crawlable_details.noindex), COUNT(0)) AS pct_allow_noindex
+  SAFE_DIVIDE(COUNTIF(NOT(is_crawlable_details.disallow) AND is_crawlable_details.noindex), COUNT(0)) AS pct_allow_noindex
 FROM (
   SELECT
     JSON_EXTRACT_SCALAR(report, '$.audits.is-crawlable.score') = '1' AS is_crawlable,
