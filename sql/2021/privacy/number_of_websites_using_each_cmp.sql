e3bc96d8e07026a835d9d36d631eaf9542cbc601
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-10-12 07:35:06 -0400

    Revert date + add comment on non-usage

--- sql/2021/privacy/number_of_websites_using_each_cmp.sql
+++ sql/2021/privacy/number_of_websites_using_each_cmp.sql
@@ -2,13 +2,15 @@
 # Percent of websites using a specific CMP (Based on wappalyzer 'Cookie compliance' category)
 # Alternatively, `core_web_vitals.technologies` could be used, but then we do not have
 #  access to the total number of websites
+# Note: we did not use the results of this query in 2021, since Wappalyzer definitions/detections
+#  were unreliable. (https://github.com/HTTPArchive/almanac.httparchive.org/issues/2292)
 
 WITH totals AS (
   SELECT
     _TABLE_SUFFIX,
     COUNT(DISTINCT url) AS total_websites
   FROM
-    `httparchive.technologies.2021_09_01_*`
+    `httparchive.technologies.2021_07_01_*`
   GROUP BY
     _TABLE_SUFFIX
 )
@@ -20,7 +22,7 @@ SELECT
   COUNT(DISTINCT url) AS number_of_websites,
   COUNT(DISTINCT url) / total_websites AS percent_of_websites
 FROM
-  `httparchive.technologies.2021_09_01_*`
+  `httparchive.technologies.2021_07_01_*`
 JOIN totals USING (_TABLE_SUFFIX)
 WHERE
   category = "Cookie compliance" AND
