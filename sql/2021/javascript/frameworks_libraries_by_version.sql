0e9b16e5f01a06fe36be500f3f45cf3887eb20d1
Nishu Goel <nishu0505@gmail.com>
2021-10-13 00:47:37 +0530

    JavaScript 2021 queries (#2323)
    * add requests, async, module queries
    
    * Added ajax request per page percentile based query.
    
    * Added most requested content-types.
    
    * Formatted query.
    
    * Added query for iframe per page.
    
    * Percentage usage of different ajax APIs.
    
    * Added h1 vs h2 vs h3 script.
    
    * Added web_components_sepc sql.
    
    * linting
    
    * consistent syntax to count number of rows
    
    * frameworks, sourcemaps queries
    
    * 1p vs 3p
    
    * adds unminified, preload/prefetch queries
    
    * PR feedback accomodate & fixes
    
    * link fix
    
    * Feedback II update
    
    * Fixed review comment related to web_component_specs.
    
    Co-authored-by: Pankaj Parkar <parkarpankaj3@gmail.com>
    Co-authored-by: Max Ostapenko <bvz2001@gmail.com>

---
+++ sql/2021/javascript/frameworks_libraries_by_version.sql
@@ -0,0 +1,32 @@
+#standardSQL
+# Top JS frameworks and libraries by version
+SELECT
+  _TABLE_SUFFIX AS client,
+  category,
+  app,
+  info AS version,
+  COUNT(DISTINCT url) AS pages,
+  total,
+  COUNT(DISTINCT url) / total AS pct
+FROM
+  `httparchive.technologies.2021_07_01_*`
+JOIN (
+  SELECT
+    _TABLE_SUFFIX,
+    COUNT(0) AS total
+  FROM
+    `httparchive.summary_pages.2021_07_01_*`
+  GROUP BY
+    _TABLE_SUFFIX)
+USING
+  (_TABLE_SUFFIX)
+WHERE
+  app IN ('jQuery', 'jQuery Migrate', 'jQuery UI', 'Modernizr', 'FancyBox', 'Slick', 'Lightbox', 'Moment.js', 'Underscore.js', 'Lodash', 'React')
+GROUP BY
+  client,
+  category,
+  app,
+  info,
+  total
+ORDER BY
+  pct DESC
---
+++ sql/2021/javascript/percentage_usage_of_different_ajax_apis.sql
@@ -0,0 +1,30 @@
+SELECT
+  client,
+  percentile,
+  APPROX_QUANTILES(xmlhttprequest_total, 1000)[
+    OFFSET(percentile * 10)] AS xmlhttprequest_total,
+  APPROX_QUANTILES(fetch_total, 1000)[
+    OFFSET(percentile * 10)] AS fetch_total,
+  APPROX_QUANTILES(beacon_total, 1000)[
+    OFFSET(percentile * 10)] AS beacon_total
+FROM (
+    SELECT
+      _TABLE_SUFFIX AS client,
+      JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload,
+        '$._javascript'),
+        '$.ajax_requests.xmlhttprequest') AS xmlhttprequest_total,
+      JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload,
+          '$._javascript'),
+        '$.ajax_requests.fetch') AS fetch_total,
+      JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload,
+          '$._javascript'),
+        '$.ajax_requests.beacon') AS beacon_total
+    FROM
+      `httparchive.pages.2021_07_01_*`),
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
+  client
