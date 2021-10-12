4b8ce820b8caa19c1fe1bc703efec21019e4aa5a
Nishu Goel <nishu0505@gmail.com>
2021-10-13 00:47:37 +0530

    JavaScript 2021 queries (#2323)
    * add requests, async, module queries
    
    * Added ajax request per page percentile based query.
    
    * Added most requested content-types.
    
    * Formatted query.
    
    * Added query for iframe per page.
    
    * Percentage usage of different ajax APIs.
    
    * Added h1 vs h2 vs h3 script.
    
    * Added web_components_sepc sql.
    
    * linting
    
    * consistent syntax to count number of rows
    
    * frameworks, sourcemaps queries
    
    * 1p vs 3p
    
    * adds unminified, preload/prefetch queries
    
    * PR feedback accomodate & fixes
    
    * link fix
    
    * Feedback II update
    
    * Fixed review comment related to web_component_specs.
    
    Co-authored-by: Pankaj Parkar <parkarpankaj3@gmail.com>
    Co-authored-by: Max Ostapenko <bvz2001@gmail.com>

---
+++ sql/2021/javascript/most_requested_content_type.sql
@@ -0,0 +1,13 @@
+SELECT
+  resp_content_type,
+  COUNT(0) AS count,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
+FROM
+  `httparchive.almanac.requests`
+WHERE
+  date = '2021-07-01'
+GROUP BY
+  client
+ORDER BY
+  client DESC
