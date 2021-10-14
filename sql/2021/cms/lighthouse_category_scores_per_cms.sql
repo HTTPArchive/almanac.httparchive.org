4677b02216f271ad8cb90b04007ba56f80ecb054
TosinArasi <89331243+TosinArasi@users.noreply.github.com>
2021-10-14 14:10:12 -0400

    Update sql/2021/cms/lighthouse_category_scores_per_cms.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2021/cms/lighthouse_category_scores_per_cms.sql
+++ sql/2021/cms/lighthouse_category_scores_per_cms.sql
@@ -2,8 +2,7 @@
 SELECT
   app AS cms,
   COUNT(DISTINCT url) AS freq,
-  # See https://github.com/HTTPArchive/almanac.httparchive.org/pull/1087#issuecomment-684983795
-  #APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.performance.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_performance,
+  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.performance.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_performance,
   APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.accessibility.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_accessibility,
   APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.pwa.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_pwa,
   APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.seo.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_seo,
