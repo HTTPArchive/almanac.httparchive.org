492b4834dd13e1191a6c591a1bd413af1f95e74d
TosinArasi <89331243+TosinArasi@users.noreply.github.com>
2021-10-14 14:12:28 -0400

    Update sql/2021/cms/cms_passing_core_web_vitals.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2021/cms/cms_passing_core_web_vitals.sql
+++ sql/2021/cms/cms_passing_core_web_vitals.sql
@@ -31,7 +31,7 @@ SELECT
   SAFE_DIVIDE(
     COUNT(DISTINCT IF(
       IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AND
-      IS_GOOD(fast_fid, avg_fid, slow_fid) AND
+      (NOT IS_NON_ZERO(fast_fid, avg_fid, slow_fid) OR IS_GOOD(fast_fid, avg_fid, slow_fid)) AND
       IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL)),
     COUNT(DISTINCT IF(
       IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp) AND
