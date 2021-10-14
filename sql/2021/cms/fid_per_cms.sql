24dd971b2b62901efe3e4816b83d132994ff5ce8
TosinArasi <89331243+TosinArasi@users.noreply.github.com>
2021-10-14 13:31:20 -0400

    Update fid_per_cms.sql

--- sql/2021/cms/fid_per_cms.sql
+++ sql/2021/cms/fid_per_cms.sql
@@ -7,23 +7,6 @@ CREATE TEMP FUNCTION IS_NON_ZERO(good FLOAT64, needs_improvement FLOAT64, poor F
   good + needs_improvement + poor > 0
 );
 
-WITH geo_summary AS (
-  SELECT
-    *,
-  FROM
-    `chrome-ux-report.materialized.country_summary`
-  WHERE
-    yyyymm = 202107 AND
-    device IN ('desktop', 'phone')
-UNION ALL
-  SELECT
-    *,
-  FROM
-    `chrome-ux-report.materialized.country_summary`
-  WHERE
-    yyyymm = 202107 AND
-    device IN ('desktop', 'phone'))
-
 SELECT
   date,
   ARRAY_TO_STRING(ARRAY_AGG(DISTINCT category IGNORE NULLS ORDER BY category), ', ') AS categories,
@@ -31,8 +14,7 @@ SELECT
   client,
   COUNT(DISTINCT url) AS origins,
   COUNT(DISTINCT IF(good_fid, url, NULL)) AS origins_with_good_fid,
-  COUNT(DISTINCT IF(any_fid, url, NULL)) AS origins_with_any_fid,
-
+  COUNT(DISTINCT IF(any_fid, url, NULL)) AS origins_with_any_fid
 FROM (
   SELECT
     yyyymm,
@@ -40,10 +22,9 @@ FROM (
     CONCAT(origin, '/') AS url,
     IF(device = 'desktop', 'desktop', 'mobile') AS client,
     IS_NON_ZERO(fast_fid, avg_fid, slow_fid) AS any_fid,
-    IS_GOOD(fast_fid, avg_fid, slow_fid) AS good_fid,
-    (IS_GOOD(fast_fid, avg_fid, slow_fid) OR fast_fid IS NULL)
+    IS_GOOD(fast_fid, avg_fid, slow_fid) AS good_fid
   FROM
-    geo_summary
+    `chrome-ux-report.materialized.device_summary`
 ) JOIN (
   SELECT DISTINCT
     CAST('2021-07-01' AS DATE) AS date,
