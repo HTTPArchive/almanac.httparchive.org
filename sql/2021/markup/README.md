4b833b79a0c33884fbc05964cfd5fe65645e1637
Kevin Farrugia <kevinfarrugia@users.noreply.github.com>
2021-10-14 17:34:04 +0200

    Markup 2021 queries (#2265)
    * Updated README.md
    
    * Added markup 2021 SQL queries (WIP)
    
    * Updated summary pages by device to include trends from previous years
    
    * Added queries for wpt_bodies_by_device
    
    * Updated README.md for markup-2021-sql
    
    * Updated README.md for markup-2021-sql
    
    * Linting fixes
    
    * Refactored queries to align with other chapters
    
    * Added markup SQL
    
    * Lint fix
    
    * Added markup attribute SQL
    
    * Added markup meta SQL
    
    * Added button and favicon SQL
    
    * Added forms SQL
    
    * Updated following review feedback
    
    * Resolved lint issues
    
    * Removed headings from wpt_bodies sql
    
    * Updated follow code review
    
    * Lint fixes
    
    Co-authored-by: Barry <barry@tunetheweb.com>

--- sql/2021/markup/README.md
+++ sql/2021/markup/README.md
@@ -8,3 +8,18 @@
 
   Analysts: if helpful, you can use this README to give additional info about the queries.
 -->
+
+Resources:
+
+- [Planning doc](https://docs.google.com/document/d/1x7iV0Tv3hxaruwlNHdhduPpzwZc9dnqMOZW1q9nGKjI/edit)
+- [2020 chapter](https://almanac.httparchive.org/en/2020/markup)
+- [2019 chapter](https://almanac.httparchive.org/en/2019/markup)
+- [Results](https://docs.google.com/spreadsheets/d/1Ta5Ck7y3W6LCn2CeGtW7dAdLF6Lh5wV2UBQJZTz3W5Q/edit?usp=sharing)
+
+## File Naming
+
+File names start with the table being queried. Next is the custom metric if appropriate. Then "by" followed by how the query is grouped. Finally anything more specific needed.
+
+## Results
+
+Results are saved in a Google Sheet with one sheet per query. The sheet name should match the query to make it simpler for analysts to follow in the future.
---
+++ sql/2021/markup/top_obsolete_elements.sql
@@ -0,0 +1,48 @@
+#standardSQL
+# Top obsolete elements
+
+CREATE TEMPORARY FUNCTION get_element_types(element_count_string STRING)
+RETURNS ARRAY<STRING> LANGUAGE js AS '''
+try {
+    if (!element_count_string) return []; // 2019 had a few cases
+
+    var element_count = JSON.parse(element_count_string); // should be an object with element type properties with values of how often they are present
+
+    if (Array.isArray(element_count)) return [];
+    if (typeof element_count != 'object') return [];
+
+    return Object.keys(element_count);
+} catch (e) {
+    return [];
+}
+''';
+
+CREATE TEMPORARY FUNCTION is_obsolete(element STRING) AS (
+  element IN ("applet", "acronym", "bgsound", "dir", "frame", "frameset", "noframes", "isindex", "keygen", "listing", "menuitem", "nextid", "noembed", "plaintext", "rb", "rtc", "strike", "xmp", "basefont", "big", "blink", "center", "font", "marquee", "multicol", "nobr", "spacer", "tt")
+);
+
+SELECT
+  _TABLE_SUFFIX AS client,
+  element_type AS obsolete_element_type,
+  COUNT(0) AS freq,
+  COUNT(DISTINCT url) AS pages,
+  total AS total_pages,
+  COUNT(DISTINCT url) / total AS pct_pages_with_obsolete_elements,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS ratio_compared_to_all_obsolete_elements
+FROM
+  `httparchive.pages.2021_07_01_*`
+JOIN
+  (SELECT _TABLE_SUFFIX, COUNT(0) AS total FROM
+    `httparchive.pages.2021_07_01_*`
+    GROUP BY _TABLE_SUFFIX)
+USING (_TABLE_SUFFIX),
+  UNNEST(get_element_types(JSON_EXTRACT_SCALAR(payload, '$._element_count'))) AS element_type
+WHERE
+  is_obsolete(element_type)
+GROUP BY
+  client,
+  total,
+  obsolete_element_type
+ORDER BY
+  client,
+  pct_pages_with_obsolete_elements DESC
