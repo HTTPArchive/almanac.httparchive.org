a707bb5b738346916d74ad2dbc6d33006584030b
Rajiv Ramnath <1437727+rrajiv@users.noreply.github.com>
2021-10-14 21:07:03 -0400

    Ecommerce 2021 queries (#2300)
    * Recycle 2020 ecommerce queries
    
    Updating the 2020 queries to make it ready for 2021
    
    * Fix linting errors
    
    * domains that are marked as payment processors but not ecommerce
    
    * update all the new 2021 files to use 2021_07_01 or 2020_08_01 or 2019_07_01 where needed
    
    * fixing the queries after running the updated linter
    
    * update the readme
    
    * add some new queries; update the readme
    
    * Update all_categories.sql
    
    fix a typo changing from `app` to `category`
    
    * add hreflang queries
    
    * rename file
    
    * update the README
    
    * add ecomm + csp query
    
    * add aug and sep versions of cmp query
    
    * add app links query update
    
    * updated readme
    
    * update readme
    
    * update the top vendors file to use ranks
    
    * fixing the query to use well-known
    
    * Update README.md
    
    Updating list of queries used, lists created for testing and unused items
    
    * remove unused/informational queries
    
    * fix this to use production tables instead of sample data
    
    * fix to not use the ROUND function
    
    * update README
    
    * fix to stop using ROUND
    
    * fixing this query to use UNNEST and remove some redundancy
    
    * fix linter issue
    
    * fix query and fix linting again
    
    * fix the ecomm category matches
    
    * Update sql/2021/ecommerce/core_web_vitals_passingmetrics_byvendor_bydevice.sql
    
    Updating the query as its possible FID could be null
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    Co-authored-by: Barry <barry@tunetheweb.com>
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

---
+++ sql/2021/ecommerce/pagestats_percentiles_bydevice.sql
@@ -0,0 +1,34 @@
+#standardSQL
+# 13_09d: Requests and weight of all content on ecom pages
+SELECT
+  percentile,
+  client,
+  APPROX_QUANTILES(requests, 1000)[OFFSET(percentile * 10)] AS requests,
+  APPROX_QUANTILES(bytes, 1000)[OFFSET(percentile * 10)] / 1024 / 1024 AS mbytes
+FROM (
+  SELECT
+    client,
+    COUNT(0) AS requests,
+    SUM(respSize) AS bytes
+  FROM
+    `httparchive.almanac.requests`
+  JOIN (
+    SELECT DISTINCT
+      _TABLE_SUFFIX AS client,
+      url AS page
+    FROM `httparchive.technologies.2021_07_01_*`
+    WHERE category = 'Ecommerce' AND (app != 'Cart Functionality' AND app != 'Google Analytics Enhanced eCommerce'))
+  USING
+    (client, page)
+  WHERE
+    date = '2021-07-01'
+  GROUP BY
+    client,
+    page),
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
+  client
