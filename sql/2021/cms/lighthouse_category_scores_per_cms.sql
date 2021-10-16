e513ad221bf9e4993bb62ec00ba880dbe5d72408
Rick Viscomi <rviscomi@users.noreply.github.com>
2021-10-16 02:43:54 -0400

    CMS 2021 queries (part 2) (#2376)
    * rank
    
    * edits
    
    * geo, rank fixes
    
    * cms adoption metrics
    
    * linter
    
    * resources
    
    * cwv by geo and LH
    
    * linter
    
    * fix wp resources

--- sql/2021/cms/lighthouse_category_scores_per_cms.sql
+++ sql/2021/cms/lighthouse_category_scores_per_cms.sql
@@ -1,20 +1,29 @@
-#lighthouse category scores per cms
+#standardSQL
+# Lighthouse category scores per CMS
 SELECT
-  app AS cms,
+  cms,
   COUNT(DISTINCT url) AS freq,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.performance.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_performance,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.accessibility.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_accessibility,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.pwa.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_pwa,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.seo.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_seo,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.best-practices.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_best_practices
-FROM
-  `httparchive.lighthouse.2021_07_01_mobile`
-JOIN
-  `httparchive.technologies.2021_07_01_mobile`
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.performance.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_performance,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.accessibility.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_accessibility,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.pwa.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_pwa,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.seo.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_seo,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$."best-practices".score') AS NUMERIC), 1000)[OFFSET(500)] AS median_best_practices
+FROM (
+  SELECT
+    url,
+    JSON_EXTRACT(report, '$.categories') AS categories
+  FROM
+    `httparchive.lighthouse.2021_07_01_mobile`)
+JOIN (
+  SELECT DISTINCT
+    app AS cms,
+    url
+  FROM
+    `httparchive.technologies.2021_07_01_mobile`
+  WHERE
+    category = 'CMS')
 USING
   (url)
-WHERE
-  category = 'CMS'
 GROUP BY
   cms
 ORDER BY
--- sql/2021/cms/top_cms.sql
+++ sql/2021/cms/top_cms.sql
@@ -4,9 +4,9 @@ SELECT
   _TABLE_SUFFIX AS client,
   2021 AS year,
   app AS cms,
-  COUNT(0) AS freq,
+  COUNT(DISTINCT url) AS freq,
   total,
-  COUNT(0) / total AS pct
+  COUNT(DISTINCT url) / total AS pct
 FROM
   `httparchive.technologies.2021_07_01_*`
 JOIN (
@@ -30,9 +30,9 @@ SELECT
   _TABLE_SUFFIX AS client,
   2020 AS year,
   app AS cms,
-  COUNT(0) AS freq,
+  COUNT(DISTINCT url) AS freq,
   total,
-  COUNT(0) / total AS pct
+  COUNT(DISTINCT url) / total AS pct
 FROM
   `httparchive.technologies.2020_08_01_*`
 JOIN (
@@ -41,6 +41,32 @@ JOIN (
     COUNT(0) AS total
   FROM
     `httparchive.summary_pages.2020_08_01_*`
+  GROUP BY
+    _TABLE_SUFFIX)
+USING
+  (_TABLE_SUFFIX)
+WHERE
+  category = 'CMS'
+GROUP BY
+  client,
+  total,
+  cms
+UNION ALL
+SELECT
+  _TABLE_SUFFIX AS client,
+  2019 AS year,
+  app AS cms,
+  COUNT(DISTINCT url) AS freq,
+  total,
+  COUNT(DISTINCT url) / total AS pct
+FROM
+  `httparchive.technologies.2019_07_01_*`
+JOIN (
+  SELECT
+    _TABLE_SUFFIX,
+    COUNT(0) AS total
+  FROM
+    `httparchive.summary_pages.2019_07_01_*`
   GROUP BY
     _TABLE_SUFFIX)
 USING
