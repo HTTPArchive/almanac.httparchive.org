0b7f4be7bd406423b539893a0ce712131eb129c9
Rick Viscomi <rviscomi@gmail.com>
2021-10-17 02:12:16 -0400

    fix optional fid

--- sql/2021/cms/core_web_vitals.sql
+++ sql/2021/cms/core_web_vitals.sql
@@ -30,7 +30,7 @@ SELECT
   SAFE_DIVIDE(
     COUNT(DISTINCT IF(
       IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AND
-      (NOT IS_NON_ZERO(fast_fid, avg_fid, slow_fid) OR IS_GOOD(fast_fid, avg_fid, slow_fid)) AND
+      IS_GOOD(fast_fid, avg_fid, slow_fid) IS NOT FALSE AND
       IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL)),
     COUNT(DISTINCT IF(
       IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp) AND
