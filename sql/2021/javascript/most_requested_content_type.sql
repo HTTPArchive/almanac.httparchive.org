4642675507e1c864b15ce429094ac41e91c4ab55
Nishu Goel <nishu0505@gmail.com>
2021-10-19 02:48:11 +0530

    update content_type and webcomponent query

--- sql/2021/javascript/most_requested_content_type.sql
+++ sql/2021/javascript/most_requested_content_type.sql
@@ -1,13 +1,14 @@
 SELECT
-  resp_content_type,
-  COUNT(0) AS count,
-  SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
-FROM
-  `httparchive.almanac.requests`
-WHERE
-  date = '2021-07-01'
+    client,
+    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%image%') as images,
+    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%font%') as font, 
+    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%css%') as css, 
+    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%javascript%') as javascript, 
+    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%json%') as json,
+    count(*) as total
+  FROM
+    `httparchive.almanac.requests`
 GROUP BY
   client
 ORDER BY
-  client DESC
+  client
