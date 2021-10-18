0ad2835976ed1afaca4d15e5c15fdbb983962ec2
Eric Portis <e@ericportis.com>
2021-10-18 15:46:49 -0700

    Simplify query, based on advice from Kevin Farrugia

--- sql/2021/media/percent_of_pages_with_picture.sql
+++ sql/2021/media/percent_of_pages_with_picture.sql
@@ -1,31 +1,14 @@
-WITH number_of_pictures_per_page AS (
 SELECT
-    _TABLE_SUFFIX AS client,
-   url,
-   CAST( JSON_VALUE( JSON_VALUE( payload, "$._media" ), "$.num_picture_img") AS INT64 ) AS num_picture_img
-FROM
-  `httparchive.sample_data.pages_*`
-),
-total_number_of_pages AS(
-SELECT
-    client,
-    COUNT(0) AS total_pages
-FROM number_of_pictures_per_page
-GROUP BY client
-),
-number_of_pages_with_picture_elements AS (
-SELECT
-    client,
-    COUNT(0) AS total_pages_with_picture
-FROM number_of_pictures_per_page
-WHERE num_picture_img > 0
-GROUP BY client
+  client,
+  COUNTIF(numOfImages > 0) AS number_of_pages_with_picture_elements,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total_number_of_pages,
+  COUNTIF(numOfImages > 0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS percent_of_pages_with_picture_elements
+FROM (
+    SELECT
+        _TABLE_SUFFIX AS client,
+        CAST(JSON_VALUE(JSON_VALUE(payload, "$._media" ), "$.num_picture_img") AS INT64) AS numOfImages
+    FROM
+        `httparchive.sample_data.pages_*`
 )
-SELECT
-    client,
-    total_pages,
-    total_pages_with_picture,
-    SAFE_DIVIDE( total_pages_with_picture, total_pages ) AS percent_of_pages_with_picture
-FROM total_number_of_pages
-    JOIN number_of_pages_with_picture_elements
-    USING (client)
+GROUP BY
+  client
