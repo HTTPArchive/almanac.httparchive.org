02710e23039aa2aedae314b222b523bd69330dd6
Barry <barry@tunetheweb.com>
2021-10-19 15:44:06 +0100

    Run sqlfluff fix on the queries

--- sql/2021/media/video_attribute_count_mobile.sql
+++ sql/2021/media/video_attribute_count_mobile.sql
@@ -1,30 +1,30 @@
-WITH video_attributes AS(
+WITH video_attributes AS (
 
-WITH videonotes AS (
-SELECT
-    url as pageURL,
-    JSON_VALUE( payload, "$._media" ) as media,
-    CAST( JSON_VALUE(JSON_VALUE( payload, "$._media" ),"$.num_video_nodes") AS INT64) as num_video_nodes,
-     ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ),"$.video_durations")  ) as video_duration,
-    ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ),"$.video_display_style")) as video_display_style,
-    ARRAY_TO_STRING( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ),"$.video_attributes_values_counts")," ") as video_attributes_values_counts,
-    ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ),"$.video_source_format_count") ) as video_source_format_count,
-    ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ),"$.video_source_format_type") ) as video_source_format_type,
-FROM `httparchive.summary_pages.2021_07_01_mobile`
-) 
+  WITH videonotes AS (
+    SELECT
+      url AS pageURL,
+      JSON_VALUE( payload, "$._media" ) AS media,
+      CAST( JSON_VALUE(JSON_VALUE( payload, "$._media" ), "$.num_video_nodes") AS INT64) AS num_video_nodes,
+      ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ), "$.video_durations") ) AS video_duration,
+      ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ), "$.video_display_style")) AS video_display_style,
+      ARRAY_TO_STRING( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ), "$.video_attributes_values_counts"), " ") AS video_attributes_values_counts,
+      ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ), "$.video_source_format_count") ) AS video_source_format_count,
+      ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ), "$.video_source_format_type") ) AS video_source_format_type
+    FROM `httparchive.summary_pages.2021_07_01_mobile`
+  )
 
 
-SELECT pageURL,
-        JSON_VALUE(video_attributes_values_counts, "$.attribute") as attribute,
-        JSON_VALUE(video_attributes_values_counts, "$.value") as value,
-       cast( JSON_VALUE(video_attributes_values_counts, "$.count") as int64) as cnt,
-       video_attributes_values_counts
-    FROM videonotes
-where num_video_nodes >0
+  SELECT pageURL,
+    JSON_VALUE(video_attributes_values_counts, "$.attribute") AS attribute,
+    JSON_VALUE(video_attributes_values_counts, "$.value") AS value,
+    cast( JSON_VALUE(video_attributes_values_counts, "$.count") AS int64) AS cnt,
+    video_attributes_values_counts
+  FROM videonotes
+  WHERE num_video_nodes > 0
 
 )
 
-Select attribute, value, sum(cnt)
-from video_attributes
-group by attribute, value
-order by attribute asc
+SELECT attribute, value, sum(cnt)
+FROM video_attributes
+GROUP BY attribute, value
+ORDER BY attribute ASC
--- sql/2021/media/video_source_formats_mobile.sql
+++ sql/2021/media/video_source_formats_mobile.sql
@@ -1,30 +1,30 @@
 WITH videonotes AS (
-    SELECT
+  SELECT
     pageURL,
     num_video_nodes,
     video_source_format_type,
-    source_formats, 
+    source_formats,
     video_source_format_type,
     source_format_count
   FROM (
-        SELECT
-            url as pageURL,
-            JSON_VALUE( payload, "$._media" ) as media,
-            CAST( JSON_VALUE(JSON_VALUE( payload, "$._media" ),"$.num_video_nodes") AS INT64) as num_video_nodes,
-            ( JSON_QUERY(JSON_VALUE( payload, "$._media" ),"$.video_durations")  ) as video_duration,
-            ( JSON_QUERY(JSON_VALUE( payload, "$._media" ),"$.video_display_style")) as video_display_style,
-            ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ),"$.video_attributes_values_counts") ) as video_attributes_values_counts,
-            ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ),"$.video_source_format_count") ) as video_source_format_count,
-            ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ),"$.video_source_format_type") ) as video_source_format_type,
-        FROM `httparchive.summary_pages.2021_07_01_mobile`
-        ) 
-    CROSS JOIN UNNEST(video_source_format_type) AS source_formats
-    CROSS JOIN UNNEST(video_source_format_count) AS source_format_count
+      SELECT
+        url AS pageURL,
+        JSON_VALUE( payload, "$._media" ) AS media,
+        CAST( JSON_VALUE(JSON_VALUE( payload, "$._media" ), "$.num_video_nodes") AS INT64) AS num_video_nodes,
+        ( JSON_QUERY(JSON_VALUE( payload, "$._media" ), "$.video_durations") ) AS video_duration,
+        ( JSON_QUERY(JSON_VALUE( payload, "$._media" ), "$.video_display_style")) AS video_display_style,
+        ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ), "$.video_attributes_values_counts") ) AS video_attributes_values_counts,
+        ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ), "$.video_source_format_count") ) AS video_source_format_count,
+        ( JSON_QUERY_ARRAY(JSON_VALUE( payload, "$._media" ), "$.video_source_format_type") ) AS video_source_format_type
+      FROM `httparchive.summary_pages.2021_07_01_mobile`
+    )
+  CROSS JOIN UNNEST(video_source_format_type) AS source_formats
+  CROSS JOIN UNNEST(video_source_format_count) AS source_format_count
 )
 
 
-SELECT  source_formats,count(source_formats) as numberofoccurances
-from videonotes
-WHERE num_video_nodes>0
-group by  source_formats
-order by numberofoccurances desc
+SELECT source_formats, count(source_formats) AS numberofoccurances
+FROM videonotes
+WHERE num_video_nodes > 0
+GROUP BY source_formats
+ORDER BY numberofoccurances DESC
