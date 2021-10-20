11028b8a19aeeda3306d2260f45184b3f07ada0e
Nishu Goel <nishu0505@gmail.com>
2021-10-20 12:55:49 +0530

    add review updates

--- sql/2021/javascript/most_requested_content_type.sql
+++ sql/2021/javascript/most_requested_content_type.sql
@@ -1,14 +1,22 @@
 SELECT
-    client,
-    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%image%') as images,
-    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%font%') as font, 
-    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%css%') as css, 
-    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%javascript%') as javascript, 
-    (select count(*) from `httparchive.almanac.requests` WHERE date = '2021-07-01' and resp_content_type like '%json%') as json,
-    count(*) as total
-  FROM
-    `httparchive.almanac.requests`
+  client,
+  CASE
+    WHEN resp_content_type LIKE '%image%' THEN 'images'
+    WHEN resp_content_type LIKE '%font%' THEN 'font'
+    WHEN resp_content_type LIKE '%css%' THEN 'css'
+    WHEN resp_content_type LIKE '%javascript%' THEN 'javascript'
+    WHEN resp_content_type LIKE '%json%' THEN 'json'
+    ELSE 'other'
+  END AS content_type,
+  COUNT(0) AS count,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
+FROM
+  `httparchive.almanac.requests`
+WHERE
+  date = '2021-07-01'
 GROUP BY
-  client
+  client,
+  content_type
 ORDER BY
-  client
+  pct DESC
--- sql/2021/javascript/web_components_specs.sql
+++ sql/2021/javascript/web_components_specs.sql
@@ -1,51 +1,21 @@
 SELECT
-    client,
-    percentile,
-    APPROX_QUANTILES(custom_elements,
-        1000)[OFFSET(percentile * 10)] AS custom_elements,
-    APPROX_QUANTILES(shadow_roots,
-        1000)[OFFSET(percentile * 10)] AS shadow_roots,
-    APPROX_QUANTILES(template,
-        1000)[OFFSET(percentile * 10)] AS template
-FROM
-    (SELECT
-        _table_suffix AS client,
-        (CASE
-                 WHEN
-            ARRAY_LENGTH(
-                JSON_QUERY_ARRAY(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload,
-                                                                       '$._javascript'
-                                                                  ),
-                                                                       '$.web_component_specs.custom_elements'
-                                                                  ))) > 1 THEN 1
-                 ELSE 0
-             END) AS custom_elements,
-        (CASE
-                 WHEN
-            ARRAY_LENGTH(
-                JSON_QUERY_ARRAY(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload,
-                                                                       '$._javascript'
-                                                                  ),
-                                                                       '$.web_component_specs.shadow_roots'
-                                                                  ))) > 1 THEN 1
-                 ELSE 0
-             END) AS shadow_roots,
-        (CASE
-                 WHEN
-            ARRAY_LENGTH(
-                JSON_QUERY_ARRAY(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload,
-                                                                       '$._javascript'
-                                                                  ),
-                                                                       '$.web_component_specs.template'
-                                                                  ))) > 1 THEN 1
-                 ELSE 0
-             END) AS template
-    FROM
-    `httparchive.pages.2021_09_01_*` ),
-    UNNEST([10, 25, 50, 75, 90, 100])
+  client,
+  percentile,
+  APPROX_QUANTILES(custom_elements, 1000)[OFFSET(percentile * 10)] AS custom_elements,
+  APPROX_QUANTILES(shadow_roots, 1000)[OFFSET(percentile * 10)] AS shadow_roots,
+  APPROX_QUANTILES(template, 1000)[OFFSET(percentile * 10)] AS template
+FROM (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    ARRAY_LENGTH(JSON_EXTRACT_ARRAY(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.web_component_specs.custom_elements')) AS custom_elements,
+    ARRAY_LENGTH(JSON_EXTRACT_ARRAY(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.web_component_specs.shadow_roots')) AS shadow_roots,
+    ARRAY_LENGTH(JSON_EXTRACT_ARRAY(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.web_component_specs.template')) AS template
+  FROM
+    `httparchive.pages.2021_09_01_*`),
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
 GROUP BY
-    percentile,
-    client
+  percentile,
+  client
 ORDER BY
-    percentile,
-    client
+  percentile,
+  client
