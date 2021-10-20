e34bd7dce6c1f8a3a416e6241b0781203454272e
Kevin Farrugia <kevin.farrugia@incredible-web.com>
2021-10-20 23:42:26 +0200

    Updated correlation CWV to use pages CrUX data

--- sql/2021/resource-hints/correlation_cwv_preload.sql
+++ sql/2021/resource-hints/correlation_cwv_preload.sql
@@ -1,23 +1,3 @@
-#standardSQL
-# Correlation between CWV and preload resource hint
-
-CREATE TEMP FUNCTION IS_GOOD (good FLOAT64, needs_improvement FLOAT64, poor FLOAT64) RETURNS BOOL AS (
-  good / (good + needs_improvement + poor) >= 0.75
-);
-
-CREATE TEMP FUNCTION IS_NI (good FLOAT64, needs_improvement FLOAT64, poor FLOAT64) RETURNS BOOL AS (
-  good / (good + needs_improvement + poor) < 0.75 AND
-  poor / (good + needs_improvement + poor) < 0.25
-);
-
-CREATE TEMP FUNCTION IS_POOR (good FLOAT64, needs_improvement FLOAT64, poor FLOAT64) RETURNS BOOL AS (
-  poor / (good + needs_improvement + poor) >= 0.25
-);
-
-CREATE TEMP FUNCTION IS_NON_ZERO (good FLOAT64, needs_improvement FLOAT64, poor FLOAT64) RETURNS BOOL AS (
-  good + needs_improvement + poor > 0
-);
-
 CREATE TEMPORARY FUNCTION getResourceHints(payload STRING)
 RETURNS STRUCT<preload INT64, prefetch INT64, preconnect INT64, prerender INT64, `dns-prefetch` INT64, `modulepreload` INT64>
 LANGUAGE js AS '''
@@ -28,157 +8,61 @@ try {
   return hints.reduce((results, hint) => {
     // Null values are omitted from BigQuery aggregations.
     // This means only pages with at least one hint are considered.
-    results[hint] = almanac['link-nodes'].nodes.filter(link => link.rel.toLowerCase() == hint).length || 0;
+    results[hint] = almanac['link-nodes'].nodes.filter(link => link.rel.toLowerCase() == hint).length || null;
     return results;
   }, {});
 } catch (e) {
   return hints.reduce((results, hint) => {
-    results[hint] = 0;
+    results[hint] = null;
     return results;
   }, {});
 }
 ''' ;
 
+CREATE TEMPORARY FUNCTION getGoodCwv(payload STRING)
+RETURNS STRUCT<cumulative_layout_shift BOOLEAN, first_contentful_paint BOOLEAN, first_input_delay BOOLEAN, largest_contentful_paint BOOLEAN>
+LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(payload);
+  var crux = $._CrUX;
+
+  if (crux) {
+    return Object.keys(crux.metrics).reduce((acc, n) => ({
+      ...acc,
+      [n]: crux.metrics[n].histogram[0].density > 0.75
+    }), {})
+  }  
+  
+  return {};
+} catch (e) {
+  return {};
+}
+''' ;
 
 SELECT
-  device,
-  COUNT(0) AS freq,
-  IF(hints.preload >= 20, 20, hints.preload) AS preload,
-  SUM(COUNT(0)) OVER (PARTITION BY device) AS total,
-
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_GOOD(fast_fid, avg_fid, slow_fid) IS NOT FALSE AND
-        IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AND
-        IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp) AND
-        IS_NON_ZERO(small_cls, medium_cls, large_cls), origin, NULL))) AS pct_cwv_good,
-
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_GOOD(fast_lcp, avg_lcp, slow_lcp), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp), origin, NULL))) AS pct_lcp_good,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_NI(fast_lcp, avg_lcp, slow_lcp), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp), origin, NULL))) AS pct_lcp_ni,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_POOR(fast_lcp, avg_lcp, slow_lcp), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp), origin, NULL))) AS pct_lcp_poor,
-
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_GOOD(fast_fid, avg_fid, slow_fid), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_fid, avg_fid, slow_fid), origin, NULL))) AS pct_fid_good,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_NI(fast_fid, avg_fid, slow_fid), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_fid, avg_fid, slow_fid), origin, NULL))) AS pct_fid_ni,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_POOR(fast_fid, avg_fid, slow_fid), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_fid, avg_fid, slow_fid), origin, NULL))) AS pct_fid_poor,
-
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(small_cls, medium_cls, large_cls), origin, NULL))) AS pct_cls_good,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_NI(small_cls, medium_cls, large_cls), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(small_cls, medium_cls, large_cls), origin, NULL))) AS pct_cls_ni,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_POOR(small_cls, medium_cls, large_cls), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(small_cls, medium_cls, large_cls), origin, NULL))) AS pct_cls_poor,
-
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_GOOD(fast_fcp, avg_fcp, slow_fcp), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_fcp, avg_fcp, slow_fcp), origin, NULL))) AS pct_fcp_good,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_NI(fast_fcp, avg_fcp, slow_fcp), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_fcp, avg_fcp, slow_fcp), origin, NULL))) AS pct_fcp_ni,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_POOR(fast_fcp, avg_fcp, slow_fcp), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_fcp, avg_fcp, slow_fcp), origin, NULL))) AS pct_fcp_poor,
-
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_GOOD(fast_ttfb, avg_ttfb, slow_ttfb), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_ttfb, avg_ttfb, slow_ttfb), origin, NULL))) AS pct_ttfb_good,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_NI(fast_ttfb, avg_ttfb, slow_ttfb), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_ttfb, avg_ttfb, slow_ttfb), origin, NULL))) AS pct_ttfb_ni,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(
-        IS_POOR(fast_ttfb, avg_ttfb, slow_ttfb), origin, NULL)),
-    COUNT(DISTINCT IF(
-        IS_NON_ZERO(fast_ttfb, avg_ttfb, slow_ttfb), origin, NULL))) AS pct_ttfb_poor
+    device,
+    CASE
+        WHEN hints.preload IS NULL THEN 0
+        WHEN hints.preload >= 20 THEN 20
+        ELSE hints.preload
+    END AS preload,
+    COUNT(0) AS freq,
+    SUM(COUNT(0)) OVER (PARTITION BY device) AS total,
+    COUNTIF(CrUX.largest_contentful_paint) AS good_largest_contentful_paint,
+    COUNTIF(CrUX.largest_contentful_paint) / COUNT(0) AS pct_good_largest_contentful_paint
 FROM (
-  SELECT
-    _TABLE_SUFFIX AS device,
-    RTRIM(url, "/") AS origin,
-    getResourceHints(payload) AS hints
-  FROM
-    `httparchive.pages.2021_07_01_*`
+    SELECT
+        _TABLE_SUFFIX AS device,
+        getResourceHints(payload) AS hints,
+        getGoodCwv(payload) AS CrUX
+    FROM
+        `httparchive.pages.2021_07_01_*`
 )
-JOIN (
-  SELECT
-    date,
-    origin,
-    IF(device = "phone", "mobile", device) AS device,
-    rank,
-
-    fast_fid,
-    avg_fid,
-    slow_fid,
-
-    fast_lcp,
-    avg_lcp,
-    slow_lcp,
-
-    small_cls,
-    medium_cls,
-    large_cls,
-
-    fast_fcp,
-    avg_fcp,
-    slow_fcp,
-
-    fast_ttfb,
-    avg_ttfb,
-    slow_ttfb
-  FROM
-    `chrome-ux-report.materialized.device_summary`
-  WHERE
-    device IN ('desktop', 'phone') AND
-    date IN ('2021-07-01')
-)
-USING
-  (device, origin)
+WHERE
+    CrUX IS NOT NULL
 GROUP BY 
-  device,
-  preload
+    device,
+    preload
 ORDER BY 
-  device,
-  preload
+    device,
+    preload
