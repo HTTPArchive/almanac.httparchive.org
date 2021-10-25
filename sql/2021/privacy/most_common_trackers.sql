0648b42989c953b9b611287932e6a30679e30d6d
Victor Le Pochat <victor.lepochat@kuleuven.be>
2021-10-25 12:14:52 -0400

    Trackers: select specific categories, add percentages

--- sql/2021/privacy/most_common_trackers.sql
+++ sql/2021/privacy/most_common_trackers.sql
@@ -1,6 +1,15 @@
 #standardSQL
 # Pages that deploy a certain tracker (as defined by WhoTracks.me, i.e., one tracker can run on multiple domains)
 
+CREATE TEMP FUNCTION isTrackerCategory(category STRING)
+RETURNS BOOL
+AS (
+  category = 'advertising' OR
+  category = 'pornvertising' OR
+  category = 'site_analytics' OR
+  category = 'social_media'
+);
+
 WITH whotracksme AS (
   SELECT
     domain,
@@ -10,12 +19,28 @@ WITH whotracksme AS (
     `httparchive.almanac.whotracksme`
   WHERE
     date = '2021-07-01'
+),
+
+totals AS (
+  SELECT
+    client,
+    COUNT(DISTINCT page) AS total_websites
+  FROM
+    `httparchive.almanac.requests`
+  WHERE
+    date = '2021-07-01'
+  GROUP BY
+    client
 )
 
 SELECT
   client,
   tracker,
-  COUNT(DISTINCT page) AS number_of_websites
+  category,
+  isTrackerCategory(category) AS isTracker,
+  COUNT(DISTINCT page) AS number_of_websites,
+  total_websites,
+  COUNT(DISTINCT page) / total_websites AS pct_websites
 FROM
   `httparchive.almanac.requests`
 JOIN
@@ -24,12 +49,18 @@ ON (
   NET.HOST(urlShort) = domain OR
   ENDS_WITH(NET.HOST(urlShort), CONCAT('.', domain))
 )
+JOIN
+  totals
+USING (client)
 WHERE
   date = "2021-07-01" AND
   NET.REG_DOMAIN(page) != NET.REG_DOMAIN(urlShort) -- third party
 GROUP BY
   client,
-  tracker
+  tracker,
+  category,
+  isTracker,
+  total_websites
 ORDER BY
   client,
   number_of_websites DESC
