ef46ad539dab62f8c9e71183e9b003d8216c3ef7
Barry <barry@tunetheweb.com>
2021-10-25 01:08:39 +0100

    More ranking queries

--- sql/2021/third-parties/percent_of_third_party_requests_and_bytes_by_category_and_content_type.sql
+++ sql/2021/third-parties/percent_of_third_party_requests_and_bytes_by_category_and_content_type.sql
@@ -71,11 +71,11 @@ SELECT
   category,
   contentType,
   SUM(requests) AS requests,
-  AVG(requests) AS avg_requests_per_page,
-  SAFE_DIVIDE(SUM(requests), SUM(total_page_requests)) AS avg_pct_requests_per_page,
-  AVG(body_size) AS avg_body_size_per_page,
-  SAFE_DIVIDE(SUM(body_size), SUM(total_page_size)) AS avg_pct_body_size_per_page
-FROM requests_per_page_and_category
+  SAFE_DIVIDE(SUM(requests), SUM(SUM(requests)) OVER (PARTITION by client, category)) AS pct_requests,
+  SUM(body_size) AS body_size,
+  SAFE_DIVIDE(SUM(body_size), SUM(SUM(body_size)) OVER (PARTITION by client, category)) AS pct_body_size
+FROM
+  requests_per_page_and_category
 GROUP BY
   client,
   category,
