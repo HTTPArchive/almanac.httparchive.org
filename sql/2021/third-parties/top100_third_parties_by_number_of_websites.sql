78ea8ea14f748a9f88d22cfcb48700eaf4bc8ced
Barry Pollard <barry_pollard@hotmail.com>
2021-10-31 09:54:28 +0000

    More changes (#2420)

--- sql/2021/third-parties/top100_third_parties_by_number_of_websites.sql
+++ sql/2021/third-parties/top100_third_parties_by_number_of_websites.sql
@@ -13,10 +13,12 @@ WITH requests AS (
 totals AS (
   SELECT
     _TABLE_SUFFIX AS client,
-    COUNT(0) AS total_pages
+    COUNT(DISTINCT pageid) AS total_pages,
+    COUNT(0) AS total_requests
   FROM
-    `httparchive.summary_pages.2021_07_01_*`
-  GROUP BY _TABLE_SUFFIX
+    `httparchive.summary_requests.2021_07_01_*`
+  GROUP BY
+    _TABLE_SUFFIX
 ),
 
 third_party AS (
@@ -47,6 +49,9 @@ SELECT
   COUNT(DISTINCT page) AS pages,
   total_pages,
   COUNT(DISTINCT page) / total_pages AS pct_pages,
+  COUNT(0) AS requests,
+  total_requests,
+  COUNT(0) / total_requests AS pct_requests,
   DENSE_RANK() OVER (PARTITION BY client ORDER BY COUNT(DISTINCT page) DESC) AS sorted_order
 FROM
   requests
@@ -62,6 +67,7 @@ WHERE
 GROUP BY
   client,
   total_pages,
+  total_requests,
   canonicalDomain
 QUALIFY
   sorted_order <= 100
