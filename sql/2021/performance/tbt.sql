bd58878e002285797f3e2891038436633f05f3a5
Sia Karamalegos <siakaramalegos@gmail.com>
2021-11-01 16:12:54 -0400

    Remove debugging lines

--- sql/2021/performance/tbt.sql
+++ sql/2021/performance/tbt.sql
@@ -17,22 +17,14 @@ WITH
 tbt_stats AS (
   SELECT
     url,
-    report,
-    CAST(JSON_EXTRACT_SCALAR(report, "$.audits.total-blocking-time.numericValue") AS FLOAT64) AS lighthouseTbt,
-    CAST(JSON_EXTRACT_SCALAR(report, "$.audits.total-blocking-time.score") AS FLOAT64) AS lighthouseTbtScore,
     getTbtThreshold(CAST(JSON_EXTRACT_SCALAR(report, "$.audits.total-blocking-time.score") AS FLOAT64)) AS threshold
   FROM
-    --`httparchive.pages.2021_07_01_*`
-    `httparchive.sample_data.lighthouse_mobile_10k`
+    `httparchive.lighthouse.2021_07_01_mobile`
 )
 
 SELECT
   threshold,
   COUNT(DISTINCT url) AS pages,
-  --lighthouseTbt,
-  --lighthouseTbtScore,
-  --url,
-  --report,
 FROM
   tbt_stats
 GROUP BY
