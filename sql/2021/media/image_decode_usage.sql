51ca1d890e29fad3c6871028c73d00a1bc7ace45
Eric Portis <e@ericportis.com>
2021-11-02 16:37:28 -0700

    Add extra totals columns, query July data

--- sql/2021/media/image_decode_usage.sql
+++ sql/2021/media/image_decode_usage.sql
@@ -4,16 +4,15 @@
 # returns all the data we need from _markup
 CREATE TEMPORARY FUNCTION get_decode_info(images_string STRING)
 RETURNS STRUCT<
-  total INT64,  
+  total INT64,
   decode_lazy INT64
 > LANGUAGE js AS '''
 var result = {};
 try {
     var images = JSON.parse(images_string);
-	
+
     result.total = images.length;
     result.decode_lazy = 0;
-
     for(let index in images){
         if(images[index].decoding==='lazy'){
             result.decode_lazy += 1
@@ -25,17 +24,20 @@ return result;
 
 SELECT
   client,
+  COUNT(0) AS pages_total,
   SAFE_DIVIDE(COUNTIF(images_info.total > 0), COUNT(0)) AS pages_with_img_pct,
+  COUNTIF(images_info.decode_lazy > 0) AS pages_with_decode_lazy,
   SAFE_DIVIDE(COUNTIF(images_info.decode_lazy > 0), COUNT(0)) AS pages_with_decode_lazy_pct,
   SUM(images_info.total) AS img_total,
-  SAFE_DIVIDE(SUM(images_info.decode_lazy), SUM(images_info.total)) AS decode_lazy_pct
+  SUM(images_info.decode_lazy) AS imgs_with_decode_lazy,
+  SAFE_DIVIDE(SUM(images_info.decode_lazy), SUM(images_info.total)) AS imgs_with_decode_lazy_pct
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
     url,
     get_decode_info(JSON_EXTRACT_SCALAR(payload, '$._Images')) AS images_info
   FROM
-    `httparchive.pages.2021_08_01_*`)
+    `httparchive.pages.2021_07_01_*`)
 GROUP BY
   client
 ORDER BY
