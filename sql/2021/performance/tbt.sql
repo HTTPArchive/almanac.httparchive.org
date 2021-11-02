f2ba82fdcfd4e63b1f29ce5d0d2b6c6393d17345
Sia Karamalegos <siakaramalegos@gmail.com>
2021-11-02 10:58:10 -0400

    Switch to thresholds of 0.9 and 0.5

--- sql/2021/performance/tbt.sql
+++ sql/2021/performance/tbt.sql
@@ -1,31 +1,27 @@
 #standardSQL
 # TBT data by threshold
 
-CREATE TEMP FUNCTION getTbtThreshold (lighthouseTbtScore FLOAT64) RETURNS STRING LANGUAGE js AS '''
-  if (lighthouseTbtScore >= 0.75) {
-      return 'GOOD'
-  } else if (lighthouseTbtScore >= 0.25) {
-      return 'NI'
-  } else if (lighthouseTbtScore >= 0) {
-      return 'POOR'
-  } else {
-      return 'NA'
-  }
-''';
-
-WITH
-tbt_stats AS (
+WITH tbt_stats AS (
   SELECT
     url,
-    getTbtThreshold(CAST(JSON_EXTRACT_SCALAR(report, "$.audits.total-blocking-time.score") AS FLOAT64)) AS threshold
+    CAST(JSON_EXTRACT_SCALAR(report, "$.audits.total-blocking-time.numericValue") AS FLOAT64) AS tbtValue,
+    CAST(JSON_EXTRACT_SCALAR(report, "$.audits.total-blocking-time.score") AS FLOAT64) AS tbtScore
   FROM
     `httparchive.lighthouse.2021_07_01_mobile`
 )
 
 SELECT
-  threshold,
-  COUNT(DISTINCT url) AS pages
+  CASE
+    WHEN tbtScore < 0.5 THEN 'POOR'
+    WHEN tbtScore < 0.9 THEN 'NI'
+    ELSE 'GOOD'
+  END AS tbt,
+  COUNT(0) AS pages,
+  SUM(COUNT(0)) OVER () AS total,
+  COUNT(0) / SUM(COUNT(0)) OVER () AS pct
 FROM
   tbt_stats
 GROUP BY
-  threshold
+  tbt
+ORDER BY
+  tbt
