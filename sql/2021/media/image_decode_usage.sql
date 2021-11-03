3637dd52b4926d3e66e352445a42ac3b7d77464a
Eric Portis <e@ericportis.com>
2021-11-02 17:03:50 -0700

    Fix for loop; 'lazy' → 'async'

--- sql/2021/media/image_decode_usage.sql
+++ sql/2021/media/image_decode_usage.sql
@@ -5,32 +5,37 @@
 CREATE TEMPORARY FUNCTION get_decode_info(images_string STRING)
 RETURNS STRUCT<
   total INT64,
-  decode_lazy INT64
+  decode_async INT64
 > LANGUAGE js AS '''
-var result = {};
-try {
-    var images = JSON.parse(images_string);
 
-    result.total = images.length;
-    result.decode_lazy = 0;
-    for(let index in images){
-        if(images[index].decoding==='lazy'){
-            result.decode_lazy += 1
-        }
+let result = {};
+let images = JSON.parse(images_string);
+if (!Array.isArray(images)) {
+    return {};
+}
+
+result.total = images.length;
+result.decode_async = 0;
+
+for(let img of images) {
+    if(img.decoding === 'async'){
+        result.decode_async += 1
     }
-} catch (e) {}
+}
+
 return result;
+
 ''';
 
 SELECT
   client,
   COUNT(0) AS pages_total,
   SAFE_DIVIDE(COUNTIF(images_info.total > 0), COUNT(0)) AS pages_with_img_pct,
-  COUNTIF(images_info.decode_lazy > 0) AS pages_with_decode_lazy,
-  SAFE_DIVIDE(COUNTIF(images_info.decode_lazy > 0), COUNT(0)) AS pages_with_decode_lazy_pct,
+  COUNTIF(images_info.decode_async > 0) AS pages_with_decode_async,
+  SAFE_DIVIDE(COUNTIF(images_info.decode_async > 0), COUNT(0)) AS pages_with_decode_async_pct,
   SUM(images_info.total) AS img_total,
-  SUM(images_info.decode_lazy) AS imgs_with_decode_lazy,
-  SAFE_DIVIDE(SUM(images_info.decode_lazy), SUM(images_info.total)) AS imgs_with_decode_lazy_pct
+  SUM(images_info.decode_async) AS imgs_with_decode_async,
+  SAFE_DIVIDE(SUM(images_info.decode_async), SUM(images_info.total)) AS imgs_with_decode_async_pct
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
