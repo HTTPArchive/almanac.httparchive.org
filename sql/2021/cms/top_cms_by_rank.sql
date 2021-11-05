31e0cc1f2f741e32a532e5dbf2441be9ed7b53b4
Rick Viscomi <rviscomi@users.noreply.github.com>
2021-11-04 23:48:42 -0400

    Update top_cms_by_rank.sql

--- sql/2021/cms/top_cms_by_rank.sql
+++ sql/2021/cms/top_cms_by_rank.sql
@@ -19,6 +19,18 @@ FROM (
 JOIN (
   SELECT
     _TABLE_SUFFIX AS client,
+    url,
+    rank_magnitude AS rank
+  FROM
+    `httparchive.summary_pages.2021_07_01_*`,
+    UNNEST([1e3, 1e4, 1e5, 1e6, 1e7]) AS rank_magnitude
+  WHERE
+    rank <= rank_magnitude)
+USING
+  (client, url)
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
     rank_magnitude AS rank,
     COUNT(0) AS total
   FROM
@@ -30,13 +42,10 @@ JOIN (
     _TABLE_SUFFIX,
     rank_magnitude)
 USING
-  (client, rank),
-  UNNEST([1e3, 1e4, 1e5, 1e6, 1e7]) AS rank_magnitude
-WHERE
-  rank <= rank_magnitude
+  (client, rank)
 GROUP BY
   client,
-  geo,
+  cms,
   rank
 ORDER BY
   rank,
