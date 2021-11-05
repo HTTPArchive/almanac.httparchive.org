16d89f36a7a68b2346346dd22e9bc2af4fbddcba
Akshay Ranganath <akshay.ranganath@cloudinary.com>
2021-11-05 13:30:08 -0700

    Adding a SQL to count cross domain image requests

---
+++ sql/2021/media/img_xdomain.sql
@@ -0,0 +1,36 @@
+#standardSQL
+# usage of alt text in images
+
+# returns all the data we need from _markup
+CREATE TEMPORARY FUNCTION get_images(images_string STRING)
+RETURNS ARRAY<STRUCT<url STRING>>
+LANGUAGE js AS '''
+var result = [];
+try {
+    var images = JSON.parse(images_string);
+    for(index in images){            
+      result.push({ 
+          url: images[index].url          
+      })
+    }         
+} catch (e) {}
+return result;
+''';
+SELECT
+  client,
+  COUNT(DISTINCT pageUrl) AS pages,
+  count(0) AS images,
+  SAFE_DIVIDE(COUNTIF(pageDomain = imageDomain), COUNT(0)) AS img_xdomain_pct,
+  SAFE_DIVIDE(COUNTIF(pageDomain != imageDomain), COUNT(0)) AS img_samedomain_pct
+FROM (
+  SELECT
+  _TABLE_SUFFIX AS client,
+    a.url AS pageUrl,
+    FORMAT("%T", NET.REG_DOMAIN(a.url)) as pageDomain,  
+    FORMAT("%T", NET.REG_DOMAIN(imageurl.url)) as imageDomain    
+  FROM
+    `httparchive.pages.2021_07_01_*` a,
+    UNNEST(get_images(JSON_EXTRACT_SCALAR(payload, '$._Images'))) AS imageurl    
+    )
+GROUP BY
+  client
