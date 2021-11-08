25b1591875bf8da6f6881d7b3520ff54e97391c7
Doug Sillars <doug.sillars@gmail.com>
2021-11-08 11:17:00 -0500

    Media 2021 queries - video (#2392)
    * video initial queries
    
    * Run sqlfluff fix on the queries
    
    * more formating
    
    * more formating
    
    * Final linting fixes?
    
    * git is hard
    
    * upated tables
    
    * fixed last query
    
    * linter fixes- i hope
    
    * linter fixes- i hope
    
    * linter fixes- i hope
    
    * linter fixes- i hope
    
    Co-authored-by: Barry <barry@tunetheweb.com>

---
+++ sql/2021/media/siteswithvideo_mobile.sql
@@ -0,0 +1,16 @@
+SELECT
+  count(pageURL)
+FROM (
+  SELECT
+    url AS pageURL,
+    JSON_VALUE( payload, "$._media" ) AS media,
+    CAST( JSON_VALUE(JSON_VALUE(payload, "$._media"), "$.num_video_nodes") AS INT64) AS num_video_nodes,
+    ( JSON_QUERY_ARRAY(JSON_VALUE(payload, "$._media"), "$.video_durations")) AS video_duration,
+    ( JSON_QUERY(JSON_VALUE(payload, "$._media"), "$.video_display_style")) AS video_display_style,
+    ( JSON_QUERY_ARRAY(JSON_VALUE(payload, "$._media"), "$.video_attributes_values_counts")) AS video_attributes_values_counts,
+    ( JSON_QUERY_ARRAY(JSON_VALUE(payload, "$._media"), "$.video_source_format_count")) AS video_source_format_count,
+    ( JSON_QUERY_ARRAY(JSON_VALUE(payload, "$._media"), "$.video_source_format_type")) AS video_source_format_type
+  FROM `httparchive.pages.2021_07_01_mobile`
+)
+WHERE
+  num_video_nodes > 0
