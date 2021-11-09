f3f9490f2bcee058de203fb9b49d3f569d8bc44c
Rick Viscomi <rviscomi@users.noreply.github.com>
2021-11-08 20:41:29 -0500

    Update group_responsesize_by_cdn.sql

--- sql/2021/page-weight/group_responsesize_by_cdn.sql
+++ sql/2021/page-weight/group_responsesize_by_cdn.sql
@@ -1,9 +1,13 @@
 SELECT
   _TABLE_SUFFIX AS client,
-  AVG(respSize) / 1024 AS resp_size,
-  _cdn_provider AS _cdn_provider
+  cdn,
+  COUNT(0) AS requests,
+  AVG(respSize) / 1024 AS resp_kbytes
 FROM
-  `httparchive.summary_requests.2021_07_01_*`
+  `httparchive.summary_requests.2021_07_01_*`,
+  UNNEST(SPLIT(_cdn_provider, ', ')) AS cdn
 GROUP BY
   client,
-  _cdn_provider
+  cdn
+ORDER BY
+  requests DESC
