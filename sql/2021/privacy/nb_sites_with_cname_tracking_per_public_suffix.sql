9b2eca657302e148a7887359d89e097be80dfe03
Yana Dimova <ydimova@gmail.com>
2021-11-15 17:04:41 +0100

    Update without linting

--- sql/2021/privacy/nb_sites_with_cname_tracking_per_public_suffix.sql
+++ sql/2021/privacy/nb_sites_with_cname_tracking_per_public_suffix.sql
@@ -1,16 +1,6 @@
-WITH websites_using_cname_tracking AS (
-    SELECT 
-        DISTINCT net.REG_DOMAIN(d) AS d
-    FROM `bamboo-chariot-267911.cname_analysis.results_tracking_2021_08_01`, unnest(domains) as d
-),
-
-suffixes AS (
-    SELECT net.PUBLIC_SUFFIX(d) AS suffix FROM websites_using_cname_tracking
-)
-
-SELECT
-    suffix,
-    COUNT(suffix) AS nb_websites_with_suffix
+WITH websites_using_cname_tracking as (SELECT DISTINCT NET.REG_DOMAIN(d) as d FROM `bamboo-chariot-267911.cname_analysis.results_tracking_2021_08_01`, unnest(domains) as d),
+ suffixes as (SELECT NET.PUBLIC_SUFFIX(d) as suffix FROM websites_using_cname_tracking)
+SELECT suffix, COUNT(suffix) as nb_websites_with_suffix
 FROM suffixes
 GROUP BY suffix
 ORDER BY nb_websites_with_suffix DESC
