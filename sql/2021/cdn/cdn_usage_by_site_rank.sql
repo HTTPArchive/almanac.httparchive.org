4a58ded45d11ab6ac1ef9eb7f9c98fbd9e592678
paulcalvano <paulcalvano@yahoo.com>
2021-11-15 02:13:16 -0500

    more 2021 CDN queries

---
+++ sql/2021/cdn/cdn_usage_by_site_rank.sql
@@ -0,0 +1,19 @@
+#standardSQL
+# cdn_usage_by_site_rank.sql : Distribution of HTML pages served by CDN vs Origin by rank
+
+SELECT client , cdn, rank,
+  COUNT(0) AS num_requests,
+  SUM(COUNT(0)) OVER (PARTITION BY client, cdn, rank) AS total_compressed,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client, cdn, rank) AS pct
+FROM (
+  SELECT
+    client,
+    IF(IFNULL(NULLIF(REGEXP_EXTRACT(_cdn_provider, r'^([^,]*).*'), ''), 'ORIGIN') = "ORIGIN", "ORIGIN", "CDN") AS cdn,
+    rank
+  FROM
+    `httparchive.almanac.requests`
+  WHERE
+    date = '2021-07-01'
+ ) 
+ GROUP BY client, cdn, rank
+ORDER BY client, rank, cdn
