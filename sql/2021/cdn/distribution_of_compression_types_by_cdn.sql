c8418cb578888989424117186f82b6d83dba84a7
Paul Calvano <paulcalvano@yahoo.com>
2021-11-15 14:25:47 -0500

    Update sql/2021/cdn/distribution_of_compression_types_by_cdn.sql
    Co-authored-by: Barry Pollard <barry_pollard@hotmail.com>

--- sql/2021/cdn/distribution_of_compression_types_by_cdn.sql
+++ sql/2021/cdn/distribution_of_compression_types_by_cdn.sql
@@ -21,5 +21,11 @@ FROM (
     date = '2021-07-01' AND
     resp_content_encoding != ''
   )
-GROUP BY client, cdn, compression_type
-ORDER BY client, cdn, compression_type
+GROUP BY
+  client,
+  cdn,
+  compression_type
+ORDER BY
+  client,
+  cdn,
+  compression_type
