169161848d158f5acc77ad043f93bdafbe141cba
Yana Dimova <ydimova@gmail.com>
2021-11-15 17:04:09 +0100

    Update without linting

--- sql/2021/privacy/nb_sites_with_cname_tracking.sql
+++ sql/2021/privacy/nb_sites_with_cname_tracking.sql
@@ -1,26 +1,18 @@
-SELECT
-    COUNT(DISTINCT NET.REG_DOMAIN(DOMAIN)) AS NB_DOMAINS,
-    CASE
-        WHEN
-            (
-                TRACKER = "sc.omtrdc.net" OR TRACKER = ".2o7.net"
-            ) THEN "Adobe Experience Cloud"
-        WHEN TRACKER = "pi.pardot.com" THEN "Pardot"
-        WHEN TRACKER = "hs.eloqua.com" THEN "Oracle Eloqua"
-        WHEN TRACKER = ".wizaly.com" THEN "Wizaly"
-        WHEN TRACKER = "k.keyade.com" THEN "Keyade"
-        WHEN TRACKER = "partner.intentmedia.net" THEN "Intent"
-        WHEN TRACKER = "dnsdelegation.io" THEN "Criteo"
-        WHEN
-            (
-                TRACKER = "afc4d9aa2a91d11e997c60ac8a4ec150-2082092489.eu-central-1.elb.amazonaws.com" OR TRACKER = "a88045584548111e997c60ac8a4ec150-1610510072.eu-central-1.elb.amazonaws.com"
-            ) THEN "Tracedock"
-        WHEN TRACKER = ".at-o.net" THEN "AT Internet"
-        WHEN TRACKER = ".affex.org" THEN "Ingenious Technologies"
-        WHEN (TRACKER = ".wt-eu02.net" OR TRACKER = ".webtrekk.net") THEN "Webtrekk"
-        WHEN TRACKER = ".actonsoftware.com" THEN "Act-On Software"
-        WHEN TRACKER = ".eulerian.net" THEN "Eulerian"
-    END AS COMPANY
-FROM `bamboo-chariot-267911.cname_analysis.results_tracking_2021_08_01`, unnest(domains) as domain
-GROUP BY COMPANY
-ORDER BY NB_DOMAINS DESC
+SELECT COUNT(DISTINCT NET.REG_DOMAIN(domain)) as nb_domains, CASE 
+    WHEN (tracker = "sc.omtrdc.net" or tracker = ".2o7.net")  THEN "Adobe Experience Cloud"
+    WHEN tracker = "pi.pardot.com" THEN "Pardot"
+    WHEN tracker = "hs.eloqua.com" THEN "Oracle Eloqua"
+    WHEN tracker = ".wizaly.com" THEN "Wizaly"
+    WHEN tracker = "k.keyade.com" THEN "Keyade"
+    WHEN tracker = "partner.intentmedia.net" THEN "Intent"
+    WHEN tracker = "dnsdelegation.io" THEN "Criteo"
+    WHEN (tracker = "afc4d9aa2a91d11e997c60ac8a4ec150-2082092489.eu-central-1.elb.amazonaws.com" or tracker = "a88045584548111e997c60ac8a4ec150-1610510072.eu-central-1.elb.amazonaws.com")  THEN "Tracedock"
+    WHEN tracker = ".at-o.net" THEN "AT Internet" 
+    WHEN tracker = ".affex.org" THEN "Ingenious Technologies"
+    WHEN (tracker = ".wt-eu02.net" or tracker = ".webtrekk.net")  THEN "Webtrekk"
+    WHEN tracker = ".actonsoftware.com" THEN "Act-On Software"
+    WHEN tracker = ".eulerian.net" THEN "Eulerian"
+  END as company,
+FROM `bamboo-chariot-267911.cname_analysis.results_tracking_2021_08_01`,unnest(domains) as domain
+GROUP BY company
+ORDER BY nb_domains DESC
