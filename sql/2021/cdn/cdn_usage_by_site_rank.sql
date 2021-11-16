6b29be66f9c6567b735f7838b6d74eca01ee10e4
Paul Calvano <paulcalvano@yahoo.com>
2021-11-15 22:44:09 -0500

    Update cdn_usage_by_site_rank.sql

--- sql/2021/cdn/cdn_usage_by_site_rank.sql
+++ sql/2021/cdn/cdn_usage_by_site_rank.sql
@@ -2,9 +2,7 @@
 # cdn_usage_by_site_rank.sql : Distribution of HTML pages served by CDN vs Origin by rank
 
 SELECT client, cdn, rank,
-  COUNT(0) AS num_requests,
-  SUM(COUNT(0)) OVER (PARTITION BY client, cdn, rank) AS total_compressed,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client, cdn, rank) AS pct
+  COUNT(0) AS num_requests
 FROM (
   SELECT
     client,
