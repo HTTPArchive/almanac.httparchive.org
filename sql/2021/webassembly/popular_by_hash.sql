7163d5859fb73d7de913c6658efad26dcde3f60c
Ingvar Stepanyan <me@rreverser.com>
2021-11-16 15:07:46 +0000

    WebAssembly SQL updates (#2504)
    * WebAssembly SQL updates
    
    * Lint fixes
    
    * Add client groupings and few more fixups
    
    * Update sql/util/wasm_initial_set.sql
    
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2021/webassembly/popular_by_hash.sql
+++ sql/2021/webassembly/popular_by_hash.sql
@@ -1,11 +1,13 @@
 SELECT
-  MIN(url) AS url,
+  client,
+  ANY_VALUE(url) AS url,
   COUNT(0) AS count
 FROM
   `httparchive.almanac.wasm_stats`
 WHERE
   date = '2021-09-01'
 GROUP BY
+  client,
   filename
 ORDER BY
   count DESC
--- sql/2021/webassembly/proposals.sql
+++ sql/2021/webassembly/proposals.sql
@@ -1,20 +1,22 @@
 SELECT
   client,
-  COUNT(0) AS total,
   COUNTIF(has_start) AS has_start,
-  COUNTIF(instr.proposals.atomics > 0) AS proposals_atomics,
-  COUNTIF(instr.proposals.bigint_externals > 0) AS proposals_bigint_externals,
-  COUNTIF(instr.proposals.bulk > 0) AS proposals_bulk,
-  COUNTIF(instr.proposals.multi_value > 0) AS proposals_multi_value,
-  COUNTIF(instr.proposals.mutable_externals > 0) AS proposals_mutable_externals,
-  COUNTIF(instr.proposals.non_trapping_conv > 0) AS proposals_non_trapping_conv,
-  COUNTIF(instr.proposals.ref_types > 0) AS proposals_ref_types,
-  COUNTIF(instr.proposals.sign_extend > 0) AS proposals_sign_extend,
-  COUNTIF(instr.proposals.simd > 0) AS proposals_simd,
-  COUNTIF(instr.proposals.tail_calls > 0) AS proposals_tail_calls
+  COUNTIF(instr.proposals.atomics > 0) AS atomics,
+  COUNTIF(instr.proposals.bigint_externals > 0) AS bigint_externals,
+  COUNTIF(instr.proposals.bulk > 0) AS bulk,
+  COUNTIF(instr.proposals.multi_value > 0) AS multi_value,
+  COUNTIF(instr.proposals.mutable_externals > 0) AS mutable_externals,
+  COUNTIF(instr.proposals.non_trapping_conv > 0) AS non_trapping_conv,
+  COUNTIF(instr.proposals.ref_types > 0) AS ref_types,
+  COUNTIF(instr.proposals.sign_extend > 0) AS sign_extend,
+  COUNTIF(instr.proposals.simd > 0) AS simd,
+  COUNTIF(instr.proposals.tail_calls > 0) AS tail_calls,
+  COUNT(0) AS total
 FROM
   `httparchive.almanac.wasm_stats`
 WHERE
   date = '2021-09-01'
 GROUP BY
-  client;
+  client
+ORDER BY
+  client
