fd81014e7f7fb82e0ae198602ea4cebba2bf1c62
Artem Denysov <denysov.artem@gmail.com>
2021-11-18 01:55:26 +0200

    Fix Jamstack LH scores (#2549)
    * fix jamstack LH scores
    
    * fix jamstack LH scores

--- sql/2021/jamstack/median_lighthouse_score.sql
+++ sql/2021/jamstack/median_lighthouse_score.sql
@@ -1,24 +1,32 @@
 #standardSQL
 # Lighthouse category scores per SSG
 SELECT
-  _TABLE_SUFFIX AS client,
-  app AS ssg,
-  COUNT(DISTINCT url) AS freq,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.performance.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_performance,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.accessibility.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_accessibility,
-  APPROX_QUANTILES(CAST(JSON_EXTRACT_SCALAR(report, '$.categories.pwa.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_pwa
-FROM
-  `httparchive.lighthouse.2021_07_01_*`
-JOIN
-  `httparchive.technologies.2021_07_01_*`
-USING
-  (_TABLE_SUFFIX, url)
-WHERE
-  LOWER(category) = "static site generator" OR
-  app = "Next.js" OR
-  app = "Nuxt.js"
-GROUP BY
   ssg,
-  client
+  COUNT(DISTINCT url) AS freq,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.performance.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_performance,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.accessibility.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_accessibility,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.pwa.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_pwa,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.seo.score') AS NUMERIC), 1000)[OFFSET(500)] AS median_seo,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$."best-practices".score') AS NUMERIC), 1000)[OFFSET(500)] AS median_best_practices
+FROM (
+  SELECT
+    url,
+    JSON_EXTRACT(report, '$.categories') AS categories
+  FROM
+    `httparchive.lighthouse.2021_07_01_mobile`)
+JOIN (
+  SELECT DISTINCT
+    app AS ssg,
+    url
+  FROM
+    `httparchive.technologies.2021_07_01_mobile`
+  WHERE
+    LOWER(category) = "static site generator" OR
+    app = "Next.js" OR
+    app = "Nuxt.js")
+USING
+  (url)
+GROUP BY
+  ssg
 ORDER BY
   freq DESC
