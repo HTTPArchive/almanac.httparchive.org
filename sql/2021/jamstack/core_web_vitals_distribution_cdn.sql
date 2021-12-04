2a169c1447a564b2b6e35bc218365d2afc239fca
Barry <barry@tunetheweb.com>
2021-12-04 23:12:14 +0000

    more query changes

--- sql/2021/jamstack/core_web_vitals_distribution_cdn.sql
+++ sql/2021/jamstack/core_web_vitals_distribution_cdn.sql
@@ -5,7 +5,7 @@
 # Performance of sites with millions of visitors as weighted the same as small sites.
 SELECT
   client,
-  app,
+  CDN,
   COUNT(DISTINCT origin) AS origins,
   SUM(fast_lcp) / (SUM(fast_lcp) + SUM(avg_lcp) + SUM(slow_lcp)) AS good_lcp,
   SUM(avg_lcp) / (SUM(fast_lcp) + SUM(avg_lcp) + SUM(slow_lcp)) AS ni_lcp,
@@ -29,6 +29,21 @@ FROM (
     date = '2021-07-01')
 JOIN (
   SELECT
+    CASE
+      WHEN REGEXP_EXTRACT(LOWER(CONCAT(respOtherHeaders, resp_x_powered_by, resp_via, resp_server)), '(x-github-request)') = 'x-github-request' THEN 'GitHub'
+      WHEN REGEXP_EXTRACT(LOWER(CONCAT(respOtherHeaders, resp_x_powered_by, resp_via, resp_server)), '(netlify)') = 'netlify' THEN 'Netlify'
+      WHEN REGEXP_EXTRACT(LOWER(CONCAT(respOtherHeaders, resp_x_powered_by, resp_via, resp_server)), '(x-nf-request-id)') IS NOT NULL THEN 'Netlify'
+      WHEN REGEXP_EXTRACT(LOWER(CONCAT(respOtherHeaders, resp_x_powered_by, resp_via, resp_server)), '(x-vercel-id)') IS NOT NULL THEN 'Vercel'
+      WHEN REGEXP_EXTRACT(LOWER(CONCAT(respOtherHeaders, resp_x_powered_by, resp_via, resp_server)), '(x-amz-cf-id)') IS NOT NULL THEN 'AWS'
+      WHEN REGEXP_EXTRACT(LOWER(CONCAT(respOtherHeaders, resp_x_powered_by, resp_via, resp_server)), '(x-azure-ref)') IS NOT NULL THEN 'Azure'
+      WHEN _cdn_provider = 'Microsoft Azure' THEN 'Azure'
+      WHEN _cdn_provider = 'DigitalOcean Spaces CDN' THEN 'DigitalOcean'
+      WHEN _cdn_provider = 'Vercel' THEN 'Vercel'
+      WHEN _cdn_provider = 'Amazon CloudFront' THEN 'AWS'
+      WHEN _cdn_provider = 'Akamai' THEN 'Akamai'
+      WHEN _cdn_provider = 'Cloudflare' THEN 'Cloudflare'
+      ELSE NULL
+    END AS CDN,
     client,
     page AS url
   FROM
@@ -42,7 +57,6 @@ JOIN (
   SELECT DISTINCT
     _TABLE_SUFFIX AS client,
     app,
-    url
   FROM
     `httparchive.technologies.2021_07_01_*`
   WHERE
@@ -51,8 +65,10 @@ JOIN (
     app = "Nuxt.js"
   )
 USING (client, url)
+WHERE
+  CDN IS NOT NULL
 GROUP BY
-  app,
+  CDN,
   client
 ORDER BY
   origins DESC
