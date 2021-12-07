def39da39f4636bc850e743d3438864b2855e195
Eric Portis <e@ericportis.com>
2021-12-06 16:49:15 -0800

    Images per page queries

---
+++ sql/2021/media/at_least_one_image_request.sql
@@ -0,0 +1,7 @@
+SELECT
+    _TABLE_SUFFIX AS client,
+    COUNTIF(reqImg > 0) AS atLeastOneImgReqCount,
+    COUNT(0) AS total,
+    SAFE_DIVIDE( COUNTIF(reqImg > 0), COUNT(0) ) as atLeastOneImgReqPct
+FROM `httparchive.summary_pages.2021_07_01_*`
+GROUP BY client
