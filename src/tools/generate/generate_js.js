7c10d953b6fe68b56d1a309add2b7a474cededb3
Barry <barry@tunetheweb.com>
2022-01-07 14:34:44 +0000

    More testing

--- src/tools/generate/generate_js.js
+++ src/tools/generate/generate_js.js
@@ -2,24 +2,10 @@ const fs = require('fs-extra');
 
 const generate_js = async () => {
   console.log(`\n Generating web-vitals.js`);
-
-  console.log(`\n Checking node`);
-  await fs.pathExists('./node_modules/web-vitals/dist/web-vitals.umd.js', (err, exists) => {
-    console.log(err) // => null
-    console.log(exists) // => false
-  })
-
-  console.log(`\n Copying web-vitals.js`);
   await fs.copy(
     './node_modules/web-vitals/dist/web-vitals.umd.js',
     './static/js/web-vitals.js',
   );
-
-  console.log(`\n Checking web-vitals.js`);
-  await fs.pathExists('./static/js/web-vitals.js', (err, exists) => {
-    console.log(err) // => null
-    console.log(exists) // => false
-  })
 };
 
 module.exports = {
