119630e23a070141557fd8156c458e8b289d3e51
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-06-08 08:50:56 -0400

    Update green_web_foundation.sql

--- sql/util/green_web_foundation.sql
+++ sql/util/green_web_foundation.sql
@@ -1,7 +1,7 @@
 SELECT
   DATE('2022-06-01') AS date,
-  NET.HOST(url) AS host,
-  NET.REG_DOMAIN(url) AS domain,
+  NET.HOST(LOWER(url)) AS host,
+  NET.REG_DOMAIN(LOWER(url)) AS domain,
   *
 FROM
   # This is the raw database dump from GWF.
