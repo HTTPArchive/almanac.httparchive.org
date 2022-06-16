922cb4a369e6c79b75cdcf307d65c77a354ae946
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-06-16 18:17:59 -0400

    Update green_web_foundation.sql

--- sql/util/green_web_foundation.sql
+++ sql/util/green_web_foundation.sql
@@ -1,8 +1,9 @@
+-- noqa: L044
 SELECT
   DATE('2022-06-01') AS date,
   NET.HOST(LOWER(url)) AS host,
   NET.REG_DOMAIN(LOWER(url)) AS domain,
-  * -- noqa: L044
+  *
 FROM
   # This is the raw database dump from GWF.
   `httparchive.almanac.green_web_foundation_raw`
