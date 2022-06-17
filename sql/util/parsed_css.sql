0de046eb72e91f4959696a6ba7788985b8df1e8f
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-06-17 00:58:00 -0400

    Update parsed_css.sql

--- sql/util/parsed_css.sql
+++ sql/util/parsed_css.sql
@@ -1,39 +1,78 @@
-CREATE TEMP FUNCTION parseCSS(stylesheet STRING)
-RETURNS STRING
-LANGUAGE js
-OPTIONS (library = "gs://httparchive/lib/parse-css.js")
-AS '''
-  try {
-    var css = parse(stylesheet)
-    return JSON.stringify(css);
-  } catch (e) {
-    '';
-  }
-''';
+CREATE TEMP FUNCTION parseCSS(stylesheet STRING) RETURNS STRING LANGUAGE js AS '''
+try {
+  var css = parse(stylesheet)
+  return JSON.stringify(css);
+} catch (e) {
+  return null;
+}
+'''
+OPTIONS (library = "gs://httparchive/lib/parse-css.js");
+
+WITH external_stylesheets AS (
+  SELECT
+    date,
+    client,
+    page,
+    url,
+    body AS styles
+  FROM
+    `httparchive.almanac.summary_response_bodies`
+  WHERE
+    date = '2022-06-01' AND
+    type = 'css' AND
+    LENGTH(body) < 3 * 1024 * 1024 # 3 MB
+), inline_styles AS (
+  SELECT
+    date,
+    client,
+    page,
+    'inline' AS url,
+    styles
+  FROM (
+    SELECT
+      date,
+      client,
+      page,
+      url,
+      body
+    FROM
+      `httparchive.almanac.summary_response_bodies`
+    WHERE
+      date = '2022-06-01' AND
+      firstHtml),
+    UNNEST(REGEXP_EXTRACT_ALL(body, '(?i)<style[^>]*>(.*)</style>')) AS styles
+  WHERE
+    styles IS NOT NULL AND
+    LENGTH(styles) > 0 AND
+    LENGTH(styles) < 3 * 1024 * 1024 # 3 MB
+), all_styles AS (
+  SELECT
+    *
+  FROM
+    external_stylesheets
+  UNION ALL
+  SELECT
+    *
+  FROM
+    inline_styles
+), parsed_css AS (
+  SELECT
+    date,
+    client,
+    page,
+    url,
+    parseCSS(styles) AS css
+  FROM
+    all_styles
+)
 
 SELECT
   date,
   client,
   page,
   url,
-  parseCSS(body) AS css
+  css
 FROM
-  `httparchive.almanac.summary_response_bodies`
+  parsed_css
 WHERE
-  date = '2022-06-01' AND
-  type = 'css' AND
-  LENGTH(body) < 3 * 1024 * 1024 # 3 MB
-UNION ALL
-SELECT
-  date,
-  client,
-  page,
-  'inline' AS url,
-  parseCSS(style) AS css
-FROM
-  (SELECT date, client, page, url, body FROM `httparchive.almanac.summary_response_bodies` WHERE DATE = '2022-06-01' AND firstHtml),
-  UNNEST(REGEXP_EXTRACT_ALL(body, '(?i)<style[^>]*>(.*)</style>')) AS style
-WHERE
-  style IS NOT NULL AND
-  LENGTH(style) > 0 AND
-  LENGTH(style) < 3 * 1024 * 1024 # 3 MB
+  css IS NOT NULL
