204573ecb06371a6fef78ed645d5ade51b1c6ab0
csliva <colt.sliva@gmail.com>
2022-06-19 23:33:51 -0400

    duplicating 2021 to 2022

---
+++ sql/2022/cms/cms_adoption_by_geo.sql
@@ -0,0 +1,52 @@
+#standardSQL
+# All CMS popularity per geo
+WITH geo_summary AS (
+  SELECT
+    `chrome-ux-report`.experimental.GET_COUNTRY(country_code) AS geo,
+    IF(device = 'desktop', 'desktop', 'mobile') AS client,
+    origin,
+    COUNT(DISTINCT origin) OVER (PARTITION BY country_code, IF(device = 'desktop', 'desktop', 'mobile')) AS total
+  FROM
+    `chrome-ux-report.materialized.country_summary`
+  WHERE
+    # We're intentionally using May 2021 CrUX data here.
+    # That's because there's a two month lag between CrUX and HA datasets.
+    # Since we're only JOINing with the CrUX dataset to see which URLs
+    # belong to different countries (as opposed to CWV field data)
+    # it's not necessary to look at the 202107 dataset.
+    yyyymm = 202105
+)
+
+SELECT
+  *
+FROM (
+  SELECT
+    client,
+    geo,
+    COUNT(0) AS pages,
+    ANY_VALUE(total) AS total,
+    COUNT(0) / ANY_VALUE(total) AS pct
+  FROM (
+    SELECT DISTINCT
+      geo,
+      client,
+      total,
+      CONCAT(origin, '/') AS url
+    FROM
+      geo_summary
+  ) JOIN (
+    SELECT DISTINCT
+      _TABLE_SUFFIX AS client,
+      url
+    FROM
+      `httparchive.technologies.2021_07_01_*`
+    WHERE
+      category = 'CMS'
+  ) USING (client, url)
+  GROUP BY
+    client,
+    geo)
+WHERE
+  pages > 1000
+ORDER BY
+  pages DESC
---
+++ sql/2022/cms/cms_adoption_by_rank.sql
@@ -0,0 +1,52 @@
+#standardSQL
+# CMS adoption per rank
+SELECT
+  client,
+  cms,
+  rank,
+  COUNT(DISTINCT url) AS pages,
+  ANY_VALUE(total) AS total,
+  COUNT(DISTINCT url) / ANY_VALUE(total) AS pct
+FROM (
+  SELECT DISTINCT
+    _TABLE_SUFFIX AS client,
+    app AS cms,
+    url
+  FROM
+    `httparchive.technologies.2021_07_01_*`
+  WHERE
+    category = 'CMS')
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url,
+    rank
+  FROM
+    `httparchive.summary_pages.2021_07_01_*`)
+USING
+  (client, url)
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    rank_magnitude AS rank,
+    COUNT(0) AS total
+  FROM
+    `httparchive.summary_pages.2021_07_01_*`,
+    UNNEST([1e3, 1e4, 1e5, 1e6, 1e7]) AS rank_magnitude
+  WHERE
+    rank <= rank_magnitude
+  GROUP BY
+    _TABLE_SUFFIX,
+    rank_magnitude)
+USING
+  (client, rank),
+  UNNEST([1e3, 1e4, 1e5, 1e6, 1e7]) AS rank_magnitude
+WHERE
+  rank <= rank_magnitude
+GROUP BY
+  client,
+  cms,
+  rank
+ORDER BY
+  rank,
+  pages DESC
