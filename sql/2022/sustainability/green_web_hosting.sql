42714b0d44ae0cb59d97a339dd2a5fa875f9104b
Fershad <itsfish@fershad.com>
2022-07-02 11:36:48 +0800

    add green web hosting query

---
+++ sql/2022/sustainability/green_web_hosting.sql
@@ -0,0 +1,24 @@
+#standardSQL
+# What percentage of URLs are hosted on a known green web hosting provider?
+
+WITH green AS (
+  SELECT
+    NET.HOST(url) AS host,
+    TRUE AS is_green
+  FROM
+    `httparchive.almanac.green_web_foundation`
+), pages AS (
+  SELECT
+    NET.HOST(url) AS host
+  FROM
+    `httparchive.summary_pages.2022_07_01_mobile`
+)
+
+SELECT
+  COUNTIF(is_green) / COUNT(0)
+FROM
+  pages
+LEFT JOIN
+  green
+USING
+  (host)
