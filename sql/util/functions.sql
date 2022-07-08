effdee393b7533442df6f1d5972024b1267588cb
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-07-07 20:04:20 -0400

    linter

--- sql/util/functions.sql
+++ sql/util/functions.sql
@@ -1,6 +1,6 @@
 # Rework CSS parsing
 CREATE OR REPLACE FUNCTION `httparchive.almanac.PARSE_CSS`(stylesheet STRING) RETURNS STRING LANGUAGE js
-OPTIONS (library=["gs://httparchive/lib/parse-css.js"]) AS R"""
+OPTIONS (library = ["gs://httparchive/lib/parse-css.js"]) AS R"""
 try {
   var css = parse(stylesheet)
   return JSON.stringify(css);
