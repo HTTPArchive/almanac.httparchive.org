a02cc2f443e5888dacf76056666bf614d4dfd2f6
Kevin Farrugia <hello@imkev.dev>
2022-07-21 13:56:17 +0200

    Added queries for CSS/JS and image optimization

---
+++ sql/2022/third-parties/distribution_of_lighthouse_unminified_js_by_3p.sql
@@ -0,0 +1,46 @@
+#standardSQL
+#Third-party pages with unminified JavaScript
+
+CREATE TEMPORARY FUNCTION getUnminifiedJavascriptUrls(audit STRING)
+RETURNS ARRAY<STRUCT<url STRING, wastedBytes INT64>> LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(audit);
+  return $.details.items.map(({url, wastedBytes}) => {
+    return {url, wastedBytes};
+  });
+} catch (e) {
+  return [];
+}
+''';
+
+WITH base AS (
+  SELECT
+    page,
+    SUM(IF(is_3p, potential_savings, 0)) AS potential_savings
+  FROM (
+    SELECT
+      lighthouse.url AS page,
+      NET.HOST(data.url) IS NOT NULL AND NET.HOST(data.url) IN (
+        SELECT domain
+        FROM `httparchive.almanac.third_parties`
+        WHERE date = '2022-06-01' AND category != 'hosting'
+      ) AS is_3p,
+      data.wastedBytes AS potential_savings
+    FROM
+      `httparchive.lighthouse.2022_06_01_mobile` AS lighthouse,
+      UNNEST(getUnminifiedJavascriptUrls(JSON_EXTRACT(report, "$.audits['unminified-javascript']"))) AS data
+  )
+  GROUP BY
+    page
+)
+
+SELECT
+  percentile,
+  APPROX_QUANTILES(potential_savings, 1000)[OFFSET(percentile * 10)] AS potential_savings_bytes
+FROM 
+  base,
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+GROUP BY
+  percentile
+ORDER BY
+  percentile
---
+++ sql/2022/third-parties/lighthouse_unminified_js_by_3p.sql
@@ -0,0 +1,55 @@
+#standardSQL
+#Third-party pages with unminified JavaScript
+
+CREATE TEMPORARY FUNCTION getUnminifiedJavascriptUrls(audit STRING)
+RETURNS ARRAY<STRUCT<url STRING, wastedBytes INT64>> LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(audit);
+  return $.details.items.map(({url, wastedBytes}) => {
+    return {url, wastedBytes};
+  });
+} catch (e) {
+  return [];
+}
+''';
+
+WITH base AS (
+  SELECT
+    page,
+    domain,
+    SUM(IF(is_3p, potential_savings, 0)) AS potential_savings
+  FROM (
+    SELECT
+      NET.HOST(data.url) as domain,
+      lighthouse.url AS page,
+      NET.HOST(data.url) IS NOT NULL AND
+      NET.HOST(data.url) IN (
+        SELECT domain
+        FROM `httparchive.almanac.third_parties`
+        WHERE date = '2022-06-01' AND category != 'hosting'
+      ) AS is_3p,
+      data.wastedBytes AS potential_savings
+    FROM
+      `httparchive.lighthouse.2022_06_01_mobile` AS lighthouse,
+      UNNEST(getUnminifiedJavascriptUrls(JSON_EXTRACT(report, "$.audits['unminified-javascript']"))) AS data
+  )
+  GROUP BY
+    page,
+    domain
+)
+
+SELECT
+  domain,
+  SUM(potential_savings) AS total_potential_savings_bytes,
+  COUNT(DISTINCT page) AS total_pages,
+  SUM(potential_savings) / COUNT(DISTINCT page) AS total_potential_savings_bytes_per_page
+FROM 
+  base
+WHERE
+  potential_savings > 0
+GROUP BY
+  domain
+ORDER BY
+  total_pages DESC,
+  total_potential_savings_bytes_per_page DESC,
+  domain
