ba0ed23318aa00931fa31450c013a1f9f34e1afa
Akshay Ranganath <akshay.ranganath@cloudinary.com>
2022-07-22 13:13:24 -0700

    Updating the table name for 2022 report

--- sql/2022/media/video_ext.sql
+++ sql/2022/media/video_ext.sql
@@ -1,5 +1,5 @@
 SELECT _TABLE_SUFFIX AS client, ext, COUNT(ext) AS cnt
-FROM `httparchive.summary_requests.2021_07_01_*`
+FROM `httparchive.summary_requests.2022_06_01_*`
 WHERE mimetype LIKE '%video%'
 GROUP BY client, ext
 ORDER BY cnt DESC
