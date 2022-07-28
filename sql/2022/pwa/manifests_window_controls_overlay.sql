73f4d2cf1f8298f181ba8d6468c59b7f6f1af2db
Beth Pan <beth_panx@hotmail.com>
2022-07-28 11:34:16 -0700

    PWA 2022 queries (#2960)
    * Copied over queries from 2021
    
    * Update time for blink usage
    
    * Update clause to check for empty manifest values
    
    * Add members of manifests
    
    * Add fugu api and fix linting
    
    * Revert accessive update
    
    * Remove unused update
    
    * Update workbox null clause
    
    * Update datetime format
    
    * Update linting names
    
    * Update date range to query June data

---
+++ sql/2022/pwa/manifests_window_controls_overlay.sql
@@ -0,0 +1,30 @@
+#standardSQL
+# % manifests with window controls overlay display override for service worker pages and all pages
+
+CREATE TEMP FUNCTION hasWindowControlsOverlay(manifest STRING)
+RETURNS BOOLEAN LANGUAGE js AS '''
+try {
+  var manifest = Object.values(JSON.parse(manifest))[0];
+  var display_override = manifest.display_override;
+  var check = display_override.includes("window-controls-overlay");
+  return check;
+} catch (e) {
+  return null;
+}
+''';
+
+SELECT
+  'PWA Pages' AS type,
+  _TABLE_SUFFIX AS client,
+  hasWindowControlsOverlay(JSON_EXTRACT(payload, '$._pwa.manifests')) AS hasWindowControlsOverlay,
+  COUNT(0) AS freq,
+  SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS pct
+FROM
+  `httparchive.pages.2022_06_01_*`
+WHERE
+  JSON_EXTRACT(payload, '$._pwa.manifests') != '[]' AND JSON_EXTRACT(payload, '$._pwa.manifests') != '{}' AND
+  JSON_EXTRACT(payload, '$._pwa.serviceWorkerHeuristic') = 'true'
+GROUP BY
+  client,
+  hasWindowControlsOverlay
