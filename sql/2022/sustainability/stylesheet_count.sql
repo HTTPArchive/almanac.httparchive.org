4c6b05c2656aa998c3d04f78bba22731ee0694f9
Fershad <itsfish@fershad.com>
2022-08-01 12:00:02 +0800

    simplify stylesheet count

--- sql/2022/sustainability/stylesheet_count.sql
+++ sql/2022/sustainability/stylesheet_count.sql
@@ -1,5 +1,5 @@
 #standardSQL
-# The distribution of sytlesheets (both inline and external)
+# The percentage of stylesheets used inline v. external
 CREATE TEMPORARY FUNCTION getStylesheets(payload STRING)
 RETURNS STRUCT<remote INT64, inline INT64> LANGUAGE js AS '''
 try {
@@ -12,22 +12,18 @@ try {
 ''';
 
 SELECT
-  percentile,
   _TABLE_SUFFIX AS client,
   COUNTIF(stylesheets.remote = 1) / COUNT(0) AS pct_1_remote,
-  APPROX_QUANTILES(stylesheets.inline, 1000)[OFFSET(percentile * 10)] AS num_inline_stylesheets,
-  APPROX_QUANTILES(stylesheets.remote, 1000)[OFFSET(percentile * 10)] AS num_remote_stylesheets
+  SUM(stylesheets.inline) AS num_inline_stylesheets,
+  SUM(stylesheets.remote) AS num_inline_stylesheets,
 FROM (
   SELECT
     _TABLE_SUFFIX,
     url,
     getStylesheets(payload) AS stylesheets
   FROM
-    `httparchive.pages.2022_06_01_*`),
-  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+    `httparchive.sample_data.pages_*`)
 GROUP BY
-  percentile,
   client
 ORDER BY
-  percentile,
   client
