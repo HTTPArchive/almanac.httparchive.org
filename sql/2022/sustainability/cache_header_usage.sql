2bc6cf3fe647244c4d01c28c31716954c0a9b1b6
Fershad <itsfish@fershad.com>
2022-08-02 17:29:39 +0800

    removed percentile, simplified query

--- sql/2022/sustainability/cache_header_usage.sql
+++ sql/2022/sustainability/cache_header_usage.sql
@@ -1,16 +1,7 @@
 #standardSQL
-# The distribution of cache header adoption on websites by percentile and client.
-WITH summary_requests AS (
-  SELECT
-    _TABLE_SUFFIX AS client,
-    *
-  FROM
-    `httparchive.summary_requests.2022_06_01_*`
-)
-
+# The distribution of cache header adoption on websites by client.
 SELECT
   client,
-  percentile,
   COUNT(0) AS total_requests,
 
   COUNTIF(uses_cache_control) AS total_using_cache_control,
@@ -32,8 +23,7 @@ SELECT
   COUNTIF(NOT uses_cache_control AND uses_expires) / COUNT(0) AS pct_using_only_expires
 FROM (
   SELECT
-    client,
-    percentile,
+    _TABLE_SUFFIX AS client,
     TRIM(resp_expires) != '' AS uses_expires,
     TRIM(resp_cache_control) != '' AS uses_cache_control,
     REGEXP_CONTAINS(resp_cache_control, r'(?i)max-age\s*=\s*[0-9]+') AS uses_max_age,
@@ -43,12 +33,9 @@ FROM (
     REGEXP_CONTAINS(TRIM(resp_etag), '^W/".*"') AS uses_weak_etag,
     REGEXP_CONTAINS(TRIM(resp_etag), '^".*"') AS uses_strong_etag
   FROM
-    summary_requests,
-    UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+    `httparchive.summary_requests.2022_06_01_*`
 )
 GROUP BY
-  percentile,
   client
 ORDER BY
-  percentile,
   client
