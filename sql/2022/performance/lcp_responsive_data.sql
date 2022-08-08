72cc52c489a705ae9a65879e3936852cb114c573
Prathamesh Rasam <25prathamesh@gmail.com>
2022-08-08 12:43:45 +0530

    linting errors fixed

--- sql/2022/performance/lcp_responsive_data.sql
+++ sql/2022/performance/lcp_responsive_data.sql
@@ -26,8 +26,8 @@ CREATE TEMP FUNCTION checkResponsiveImages(responsivelist STRING, lcpImgUrl STRI
 SELECT
   _TABLE_SUFFIX AS client,
   COUNT(0) AS total,
-  SUM(IF(checkResponsiveImages(JSON_EXTRACT(report, '$.audits.uses-responsive-images.details.items'), JSON_EXTRACT(report, '$.audits.preload-lcp-image.details.items[0].url'), JSON_EXTRACT(report, '$.audits.largest-contentful-paint-element.details.items[0].node.path')) = true,1,0 )) AS check_responsive_images,
-  SUM(IF(checkResponsiveImages(JSON_EXTRACT(report, '$.audits.uses-responsive-images.details.items'), JSON_EXTRACT(report, '$.audits.preload-lcp-image.details.items[0].url'), JSON_EXTRACT(report, '$.audits.largest-contentful-paint-element.details.items[0].node.path')) = true,1,0 )) / COUNT(0) AS lcp_responsive_images_per
+  SUM(IF(checkResponsiveImages(JSON_EXTRACT(report, '$.audits.uses-responsive-images.details.items'), JSON_EXTRACT(report, '$.audits.preload-lcp-image.details.items[0].url'), JSON_EXTRACT(report, '$.audits.largest-contentful-paint-element.details.items[0].node.path')) = true, 1, 0 )) AS check_responsive_images,
+  SUM(IF(checkResponsiveImages(JSON_EXTRACT(report, '$.audits.uses-responsive-images.details.items'), JSON_EXTRACT(report, '$.audits.preload-lcp-image.details.items[0].url'), JSON_EXTRACT(report, '$.audits.largest-contentful-paint-element.details.items[0].node.path')) = true, 1, 0 )) / COUNT(0) AS lcp_responsive_images_per
 
 FROM `httparchive.lighthouse.2022_06_01_*`
 GROUP BY
