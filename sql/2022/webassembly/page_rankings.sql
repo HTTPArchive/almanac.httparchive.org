306afc74d95cbc1d8a88ec012dbb610381610ebb
Jamie Macdonald <jamie.whit.mac@gmail.com>
2022-08-08 15:56:47 +0100

    Fix page rankings sql to not use scratchspace

--- sql/2022/webassembly/page_rankings.sql
+++ sql/2022/webassembly/page_rankings.sql
@@ -1,25 +1,15 @@
 # standardSQL
 # Count of pages which use wasm at page ranking intervals.
 
-WITH wasm AS (
-  SELECT
-    *,
-    JSON_QUERY(payload, '$._wasm_stats') AS wasm_stats
-  FROM
-    `httparchive.almanac.requests`
-  WHERE
-    date = '2022-06-01' AND
-    (mimeType = 'application/wasm' OR ext = 'wasm')
-)
-
 SELECT
   _rank AS rank,
   COUNT(DISTINCT page) AS pages
 FROM
-  `httparchive.scratchspace.wasm_stats`,
+  `httparchive.almanac.requests`,
   UNNEST([1000, 10000, 100000, 1000000, 10000000]) AS _rank
 WHERE
-  wasm_stats IS NOT NULL AND
+  date = '2022-06-01' AND
+  (mimeType = 'application/wasm' OR ext = 'wasm') AND
   rank <= _rank
 GROUP BY
   rank
