ceec6990d557b77d266633720d49805201192c4b
Prathamesh Rasam <25prathamesh@gmail.com>
2022-08-08 12:28:52 +0530

    linting errors fixed

--- sql/2022/performance/lcp_responsive_data.sql
+++ sql/2022/performance/lcp_responsive_data.sql
@@ -1,6 +1,4 @@
-
-
-CREATE TEMP FUNCTION checkResponsiveImages(responsivelist STRING , lcpImgUrl STRING , nodePath STRING  ) RETURNS BOOLEAN LANGUAGE js AS '''
+CREATE TEMP FUNCTION checkResponsiveImages(responsivelist STRING, lcpImgUrl STRING, nodePath STRING) RETURNS BOOLEAN LANGUAGE js AS '''
   try {
     //we will check lcp elment is img
     const lastSegment = (JSON.parse(nodePath).split(",").reverse())[0];
@@ -26,10 +24,10 @@ CREATE TEMP FUNCTION checkResponsiveImages(responsivelist STRING , lcpImgUrl STR
 ''';
 
 SELECT
- _TABLE_SUFFIX AS client,
+  _TABLE_SUFFIX AS client,
   COUNT(0) AS total,
-  SUM(IF(checkResponsiveImages(JSON_EXTRACT(report, "$.audits.uses-responsive-images.details.items"),JSON_EXTRACT(report, "$.audits.preload-lcp-image.details.items[0].url"),JSON_EXTRACT(report, "$.audits.largest-contentful-paint-element.details.items[0].node.path")) = true,1,0 )) AS check_responsive_images,
-  SUM(IF(checkResponsiveImages(JSON_EXTRACT(report, "$.audits.uses-responsive-images.details.items"),JSON_EXTRACT(report, "$.audits.preload-lcp-image.details.items[0].url"),JSON_EXTRACT(report, "$.audits.largest-contentful-paint-element.details.items[0].node.path")) = true,1,0 )) / COUNT(0) AS lcp_responsive_images_per
+  SUM(IF(checkResponsiveImages(JSON_EXTRACT(report, '$.audits.uses-responsive-images.details.items'), JSON_EXTRACT(report, '$.audits.preload-lcp-image.details.items[0].url'), JSON_EXTRACT(report, '$.audits.largest-contentful-paint-element.details.items[0].node.path')) = true,1,0 )) AS check_responsive_images,
+  SUM(IF(checkResponsiveImages(JSON_EXTRACT(report, '$.audits.uses-responsive-images.details.items'), JSON_EXTRACT(report, '$.audits.preload-lcp-image.details.items[0].url'), JSON_EXTRACT(report, '$.audits.largest-contentful-paint-element.details.items[0].node.path')) = true,1,0 )) / COUNT(0) AS lcp_responsive_images_per
 
 FROM `httparchive.lighthouse.2022_06_01_*`
 GROUP BY
