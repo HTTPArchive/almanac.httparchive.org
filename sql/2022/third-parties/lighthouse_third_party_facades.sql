5d500b66b13e9603d2f352fc9c8643f88400ab84
Kevin Farrugia <hello@imkev.dev>
2022-08-10 15:34:08 +0200

    Fixed linting issues

--- sql/2022/third-parties/lighthouse_third_party_facades.sql
+++ sql/2022/third-parties/lighthouse_third_party_facades.sql
@@ -4,11 +4,11 @@ SELECT
   total,
   pct
 FROM (
-  SELECT 
-    _TABLE_SUFFIX as client,
+  SELECT
+    _TABLE_SUFFIX AS client,
     COUNTIF(SAFE_CAST(JSON_VALUE(report, '$.audits.third-party-facades.score') AS FLOAT64) < 0.9) AS fail,
     SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS total,
-    COUNTIF(SAFE_CAST(JSON_VALUE(report, '$.audits.third-party-facades.score') AS FLOAT64) < 0.9) / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) as pct
+    COUNTIF(SAFE_CAST(JSON_VALUE(report, '$.audits.third-party-facades.score') AS FLOAT64) < 0.9) / SUM(COUNT(0)) OVER (PARTITION BY _TABLE_SUFFIX) AS pct
   FROM
     `httparchive.lighthouse.2022_06_01_*`
   GROUP BY
