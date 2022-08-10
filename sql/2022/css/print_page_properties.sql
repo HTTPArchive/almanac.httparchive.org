e97e0112256d305b836b53ea0e04ec2a48f16bb6
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-08-10 01:27:09 -0400

    print styles

---
+++ sql/2022/css/print_page_properties.sql
@@ -0,0 +1,63 @@
+#standardSQL
+CREATE TEMPORARY FUNCTION getPageQueryProperties(css STRING)
+RETURNS ARRAY<STRING>
+LANGUAGE js
+OPTIONS (library = "gs://httparchive/lib/css-utils.js")
+AS '''
+try {
+  function compute(ast) {
+    let ret = {};
+
+    walkRules(ast, rule => {
+      walkDeclarations(rule.rules, ({property, value}) => {
+        incrementByKey(ret, property);
+      });
+    }, {
+      type: "page"
+    });
+
+    return sortObject(ret);
+  }
+
+  const ast = JSON.parse(css);
+  let properties = compute(ast);
+  return Object.keys(properties);
+} catch (e) {
+  return [];
+}
+''';
+
+SELECT
+  client,
+  property,
+  COUNT(DISTINCT page) AS pages,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM (
+  SELECT DISTINCT
+    client,
+    page,
+    LOWER(property) AS property
+  FROM
+    `httparchive.almanac.parsed_css`
+  LEFT JOIN
+    UNNEST(getPageQueryProperties(css)) AS property
+  WHERE
+    date = '2022-07-01' AND
+    property IS NOT NULL)
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.summary_pages.2022_07_01_*`
+  GROUP BY
+    client)
+USING
+  (client)
+GROUP BY
+  client,
+  total,
+  property
+ORDER BY
+  pct DESC
---
+++ sql/2022/css/print_page_pseudo_classes.sql
@@ -0,0 +1,91 @@
+#standardSQL
+CREATE TEMPORARY FUNCTION getSelectorParts(css STRING)
+RETURNS ARRAY<STRING> LANGUAGE js
+OPTIONS (library = "gs://httparchive/lib/css-utils.js")
+AS '''
+try {
+  function compute(ast) {
+    let ret = {
+      "pseudo-class": {}
+    };
+
+    walkRules(ast, rule => {
+      walkSelectors(rule, selector => {
+        let sast = parsel.parse(selector, {list: false});
+
+        parsel.walk(sast, node => {
+          if (node.type in ret) {
+            incrementByKey(ret[node.type], node.name);
+          }
+        }, {subtree: true});
+      });
+    }, {type: 'print'});
+
+    for (let type in ret) {
+      ret[type] = sortObject(ret[type]);
+    }
+
+    return ret;
+  }
+
+  function unzip(obj) {
+    return Object.entries(obj).filter(([name, value]) => {
+      return !isNaN(value);
+    }).map(([name, value]) => name);
+  }
+
+  const ast = JSON.parse(css);
+  let parts = compute(ast);
+  return unzip(parts['pseudo-class']);
+} catch (e) {
+  return null;
+}
+''';
+
+WITH totals AS (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    COUNT(0) AS total_pages
+  FROM
+    `httparchive.summary_pages.2022_07_01_*`
+  GROUP BY
+    client
+)
+
+SELECT
+  client,
+  pages,
+  total_pages,
+  pct_pages,
+  pseudo_class.value AS pseudo_class,
+  pseudo_class.count AS freq,
+  pseudo_class.count / pages AS pct
+FROM (
+  SELECT
+    client,
+    COUNT(DISTINCT page) AS pages,
+    ANY_VALUE(total_pages) AS total_pages,
+    COUNT(DISTINCT page) / ANY_VALUE(total_pages) AS pct_pages,
+    APPROX_TOP_COUNT(pseudo_class, 100) AS pseudo_classes
+  FROM (
+      SELECT DISTINCT
+        client,
+        page,
+        pseudo_class
+      FROM
+        `httparchive.almanac.parsed_css`
+      LEFT JOIN
+        UNNEST(getSelectorParts(css)) AS pseudo_class
+      WHERE
+        date = '2022-07-01')
+  JOIN
+    totals
+  USING
+    (client)
+  GROUP BY
+    client),
+  UNNEST(pseudo_classes) AS pseudo_class
+WHERE
+  pseudo_class.value IS NOT NULL
+ORDER BY
+  pct DESC
