0eb6328d676686c148bb3232303bc61c532ade7b
drohe <62556996+drohe@users.noreply.github.com>
2022-08-10 21:47:52 -0400

    Update minified_js_usage.sql
    hopefully fixing linter issues

--- sql/2022/page-weight/minified_js_usage.sql
+++ sql/2022/page-weight/minified_js_usage.sql
@@ -1,14 +1,8 @@
 SELECT 
     _TABLE_SUFFIX as client,
-    COUNTIF(
-        REGEXP_CONTAINS(report,r'unminified-javascript\/\).","score":null')
-    ) AS null_count,
-    COUNTIF(
-        REGEXP_CONTAINS(report,r'unminified-javascript\/\).","score":1')
-    ) AS pass_count,
-    COUNTIF(
-        REGEXP_CONTAINS(report,r'unminified-javascript\/\).","score":0')
-    ) AS fail_count
+    COUNTIF(JSON_VALUE(report, '$.audits.unminified-javascript.score') IS NULL) AS null_count,
+    COUNTIF(SAFE_CAST(JSON_VALUE(report, '$.audits.unminified-javascript.score') AS FLOAT64) >= 0.9) AS pass_count,
+    COUNTIF(SAFE_CAST(JSON_VALUE(report, '$.audits.unminified-javascript.score') AS FLOAT64) < 0.9) AS fail_count
 FROM
   `httparchive.lighthouse.2022_06_01_*`
 GROUP BY
@@ -18,3 +12,4 @@ ORDER BY
     null_count,
     pass_count,
     fail_count
+    
