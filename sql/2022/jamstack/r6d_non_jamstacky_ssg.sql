e11eb9146234711e5d8ac54bce7a1b5056b1d1aa
Laurie Voss <github@seldo.com>
2022-08-11 13:46:56 -0700

    Adding SSG and PaaS queries

--- sql/2022/jamstack/r6d_non_jamstacky_ssg.sql
+++ sql/2022/jamstack/r6d_non_jamstacky_ssg.sql
@@ -2,6 +2,6 @@ SELECT
   app,
   count(0) AS urls
 FROM `httparchive.technologies.2022_06_01_mobile`
-WHERE category = 'CDN'
+WHERE category = 'Static site generator'
 GROUP BY app
 ORDER BY urls DESC
