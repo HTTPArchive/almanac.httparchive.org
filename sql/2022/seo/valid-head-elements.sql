f85ea704c9a453a617c15b45fad0ed84f4035dbc
Barry Pollard <barrypollard@google.com>
2022-08-17 17:44:50 +0100

    Restrict Sample words in queries (#3071)
    * Restrict Sample words in queries
    
    * Add reason
    
    * Fix queries

--- sql/2022/seo/valid-head-elements.sql
+++ sql/2022/seo/valid-head-elements.sql
@@ -1,17 +1,24 @@
 #standardSQL
 # Counted metrics of invalid head elements in HTML
-# Add a table sample to reduce query cost
-# TABLESAMPLE SYSTEM (.05 PERCENT)
 
-WITH pages AS (SELECT
-  url,
-  JSON_QUERY(payload, '$._valid-head.invalidHead') AS invalidHead,
-  JSON_EXTRACT_ARRAY(payload, '$._valid-head.invalidElements') AS invalidElements,
-  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(payload, '$._valid-head.invalidElements')) AS invalidCount
-  FROM `httparchive.pages.2022_07_01_*` -- noqa: L062
+WITH pages AS (
+  SELECT
+    url,
+    JSON_QUERY(payload, '$._valid-head.invalidHead') AS invalidHead,
+    JSON_EXTRACT_ARRAY(payload, '$._valid-head.invalidElements') AS invalidElements,
+    ARRAY_LENGTH(JSON_EXTRACT_ARRAY(payload, '$._valid-head.invalidElements')) AS invalidCount
+  FROM
+    `httparchive.pages.2022_07_01_*` -- noqa: L062
 )
 
-SELECT element, COUNT(element) AS elementCount FROM pages
-CROSS JOIN UNNEST(invalidElements) AS element
-GROUP BY element
-ORDER BY elementCount DESC
+SELECT
+  element,
+  COUNT(element) AS elementCount
+FROM
+  pages
+CROSS JOIN
+  UNNEST(invalidElements) AS element
+GROUP BY
+  element
+ORDER BY
+  elementCount DESC
