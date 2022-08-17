442b14810632dca2f6f907c4ed84ff52f6414e4e
Barry Pollard <barrypollard@google.com>
2022-08-17 18:31:51 +0100

    Revert joining all sites to CrUX

--- sql/2022/jamstack/jamstack_ssgs.sql
+++ sql/2022/jamstack/jamstack_ssgs.sql
@@ -25,46 +25,12 @@ jamstack_totals AS (
     client
 ),
 
--- CrUX and HA use slightly different sets of sites for the same date
--- So the total sites we're looking at is the number of sites in the join
-total_eligible_sites AS (
-  SELECT
-    s.*,
-    p75_lcp,
-    p75_cls
-  FROM
-    (
-      SELECT DISTINCT
-        client,
-        date,
-        page AS url,
-        rank
-      FROM
-        `httparchive.almanac.requests`
-      WHERE
-        firstHtml
-    ) AS s
-  JOIN
-    `chrome-ux-report.materialized.device_summary` AS c
-  ON
-    url = CONCAT(origin, '/') AND
-    s.date = c.date AND
-    (
-      (s.client = 'mobile' AND c.device = 'phone')
-      OR
-      (s.client = 'desktop' AND c.device = 'desktop')
-      OR
-      c.device IS NULL
-    )
-  WHERE s.date = '2022-06-01'
-),
-
 all_sites_totals AS (
   SELECT
-    client,
+    _TABLE_SUFFIX AS client,
     COUNT(0) AS total_all_sites
   FROM
-    total_eligible_sites
+    `httparchive.pages.2022_06_01_*`
   GROUP BY
     client
 )
