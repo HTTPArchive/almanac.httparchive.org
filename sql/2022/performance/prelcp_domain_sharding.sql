cbfe6daac363c354667d87d27d27ed1af677cd8c
Kanmi Obasa <9829274+konfirmed@users.noreply.github.com>
2022-08-17 21:17:59 -0400

    Performance 2022 Queries (#2974)
    * Update README.md
    
    * Update README.md
    
    * WIP
    
    * gaming metric query by count & percent
    
    * fix linting error gaming metrics sql file
    
    * add list of all queries
    
    * update MD syntax
    
    * update comment on FID / INP Long Task Data
    
    * forgot to save - updating FID/INP comment once again
    
    * correct linter errors
    
    * Antipatterns - LCP Lazy Loaded
    
    * lin issue fix for gaming sql file
    
    * lin issue fix for gaming sql file
    
    * linter error
    
    * linter issue
    
    Linter error fix
    
    * Linter issue fixed
    
    * changed date from 2021 to 2022
    
    * lazy load LCP element by node & client query fix
    
    * lcp responsiveness data query file
    
    * linting errors fixed
    
    * linting errors fixed
    
    * LCP element (preload added)
    
    * linter error
    
    * Added fetch priority
    
    May need to review this as freq of fetchpriority looks sketchy
    
    * Copies 2021 queries
    
    * Switch to 2022 and add INP
    
    * Fix INP label
    
    * update query list
    
    * add note for synthetic filtering metric
    
    * Add LCP element query with fetchpriority
    
    * Trim trailing white space
    
    * Add sample data tables
    
    * Point to object instead of array
    
    * Query same table on join
    
    * Fix INP thresholds
    
    * fid disable zoom , ttfb by cms query performance chapter
    
    * readme update
    
    * linting issue fix for prev commit
    
    * linter
    
    * lcp preload
    
    * inp, ttfb, hosts
    
    * linter, lcp resource delay
    
    * fix lcp resource delay
    
    * linter
    
    * linter
    
    * lcp same-host
    
    Co-authored-by: Prathamesh Rasam <25prathamesh@gmail.com>
    Co-authored-by: Melissa Morel <mmorel@etsy.com>
    Co-authored-by: Sia Karamalegos <sia.karamalegos@shopify.com>
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

---
+++ sql/2022/performance/prelcp_domain_sharding.sql
@@ -0,0 +1,58 @@
+WITH requests AS (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    page,
+    NET.HOST(url) AS host,
+    CAST(JSON_QUERY(payload, '$._all_start') AS INT64) AS req_start
+  FROM
+    `httparchive.requests.2022_06_01_*`
+),
+
+lcp AS (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url AS page,
+    httparchive.core_web_vitals.GET_LAB_LCP(payload) AS lcp_time
+  FROM
+    `httparchive.pages.2022_06_01_*`
+),
+
+hosts AS (
+  SELECT
+    client,
+    COUNT(DISTINCT host) AS pre_lcp_hosts,
+    ANY_VALUE(lcp_time) AS lcp
+  FROM
+    lcp
+  JOIN
+    requests
+  USING
+    (client, page)
+  WHERE
+    req_start <= lcp_time
+  GROUP BY
+    client,
+    page
+)
+
+SELECT
+  percentile,
+  client,
+  CASE
+    WHEN lcp <= 2500 THEN 'good'
+    WHEN lcp > 4000 THEN 'poor'
+    ELSE 'needs improvement'
+  END AS lcp,
+  COUNT(0) AS pages,
+  APPROX_QUANTILES(pre_lcp_hosts, 1000)[OFFSET(percentile * 10)] AS pre_lcp_hosts
+FROM
+  hosts,
+  UNNEST([10, 25, 50, 75, 90]) AS percentile
+GROUP BY
+  percentile,
+  client,
+  lcp
+ORDER BY
+  percentile,
+  client,
+  lcp
