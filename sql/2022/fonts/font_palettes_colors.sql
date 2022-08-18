6ec2badd18f3ee73a5e7586e8edcd3289cb52999
Bram Stein <b.l.stein@gmail.com>
2022-08-18 21:59:21 +0200

    Update sql/2022/fonts/font_palettes_colors.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2022/fonts/font_palettes_colors.sql
+++ sql/2022/fonts/font_palettes_colors.sql
@@ -1,8 +1,9 @@
 SELECT
   client,
   SAFE_CAST(JSON_EXTRACT_SCALAR(payload, '$._font_details.color.numPaletteEntries') AS INT64) AS entries,
-  COUNT(0) AS total,
-  COUNT(0) * 1.0 / SUM(COUNT(0)) OVER(PARTITION BY client) AS pct
+  COUNT(0) AS freq,
+  SUM(COUNT(0)) OVER(PARTITION BY client) AS total,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
 FROM
   `httparchive.almanac.requests`
 WHERE
