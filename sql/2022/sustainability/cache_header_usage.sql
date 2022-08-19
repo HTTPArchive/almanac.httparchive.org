07fbf680b161be6c8f9d437ac32cd7a003f0f38a
Barry Pollard <barrypollard@google.com>
2022-08-19 22:02:34 +0100

    Fix caching query

--- sql/2022/sustainability/cache_header_usage.sql
+++ sql/2022/sustainability/cache_header_usage.sql
@@ -8,8 +8,8 @@ SELECT
   COUNTIF(uses_max_age) AS total_using_max_age,
   COUNTIF(uses_expires) AS total_using_expires,
   COUNTIF(uses_max_age AND uses_expires) AS total_using_max_age_and_expires,
-  COUNTIF(uses_cache_control AND uses_expires) AS total_using_both,
-  COUNTIF(NOT uses_cache_control AND NOT uses_expires) AS total_using_neither,
+  COUNTIF(uses_cache_control AND uses_expires) AS total_using_both_cc_and_expires,
+  COUNTIF(NOT uses_cache_control AND NOT uses_expires) AS total_using_neither_cc_and_expires,
   COUNTIF(uses_cache_control AND NOT uses_expires) AS total_using_only_cache_control,
   COUNTIF(NOT uses_cache_control AND uses_expires) AS total_using_only_expires,
 
@@ -17,19 +17,19 @@ SELECT
   COUNTIF(uses_max_age) / COUNT(0) AS pct_using_max_age,
   COUNTIF(uses_expires) / COUNT(0) AS pct_using_expires,
   COUNTIF(uses_max_age AND uses_expires) / COUNT(0) AS pct_using_max_age_and_expires,
-  COUNTIF(uses_cache_control AND uses_expires) / COUNT(0) AS pct_using_both,
-  COUNTIF(NOT uses_cache_control AND NOT uses_expires) / COUNT(0) AS pct_using_neither,
+  COUNTIF(uses_cache_control AND uses_expires) / COUNT(0) AS pct_using_both_cc_and_expires,
+  COUNTIF(NOT uses_cache_control AND NOT uses_expires) / COUNT(0) AS pct_using_neither_cc_nor_expires,
   COUNTIF(uses_cache_control AND NOT uses_expires) / COUNT(0) AS pct_using_only_cache_control,
   COUNTIF(NOT uses_cache_control AND uses_expires) / COUNT(0) AS pct_using_only_expires
 FROM (
   SELECT
     _TABLE_SUFFIX AS client,
-    TRIM(resp_expires) != '' AS uses_expires,
-    TRIM(resp_cache_control) != '' AS uses_cache_control,
+    TRIM(IFNULL(resp_expires, '')) != '' AS uses_expires,
+    TRIM(IFNULL(resp_cache_control, '')) != '' AS uses_cache_control,
     REGEXP_CONTAINS(resp_cache_control, r'(?i)max-age\s*=\s*[0-9]+') AS uses_max_age,
-    TRIM(resp_etag) = '' AS uses_no_etag,
-    TRIM(resp_etag) != '' AS uses_etag,
-    TRIM(resp_last_modified) != '' AS uses_last_modified,
+    TRIM(IFNULL(resp_etag, '')) = '' AS uses_no_etag,
+    TRIM(IFNULL(resp_etag, '')) != '' AS uses_etag,
+    TRIM(IFNULL(resp_last_modified, '')) != '' AS uses_last_modified,
     REGEXP_CONTAINS(TRIM(resp_etag), '^W/".*"') AS uses_weak_etag,
     REGEXP_CONTAINS(TRIM(resp_etag), '^".*"') AS uses_strong_etag
   FROM
