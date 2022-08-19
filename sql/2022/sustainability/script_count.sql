8ee3604336947b81fa6b25e1179f0f167c04a9e1
Fershad <itsfish@fershad.com>
2022-08-19 12:14:05 +0800

    fix error in queries, simplify returned results

--- sql/2022/sustainability/script_count.sql
+++ sql/2022/sustainability/script_count.sql
@@ -1,32 +1,32 @@
 #standardSQL
-# The percentage of scripts used inline v. external
-CREATE TEMPORARY FUNCTION getScripts(payload STRING)
-RETURNS STRUCT<total INT64, inline INT64, src INT64, async INT64, defer INT64, async_and_defer INT64, type_module INT64, nomodule INT64>
-LANGUAGE js AS '''
+# The number of stylesheets used inline v. external
+
+CREATE TEMPORARY FUNCTION getStylesheets(payload STRING)
+RETURNS STRUCT<remote INT64, inline INT64> LANGUAGE js AS '''
 try {
-  var $ = JSON.parse(payload);
-  var javascript = JSON.parse($._javascript);
-  return javascript.script_tags;
+  var $ = JSON.parse(payload)
+  var sass = JSON.parse($._sass);
+  return sass.stylesheets;
 } catch (e) {
-  return {};
+  return null;
 }
 ''';
 
 SELECT
-  client,
   percentile,
-  APPROX_QUANTILES(script.total, 1000)[OFFSET(percentile * 10)] AS total_scripts,
-  APPROX_QUANTILES(script.inline, 1000)[OFFSET(percentile * 10)] AS inline_script,
-  APPROX_QUANTILES(script.src, 1000)[OFFSET(percentile * 10)] AS external_script,
-  APPROX_QUANTILES(script.src / script.total, 1000)[OFFSET(percentile * 10)] AS pct_external_script,
-  APPROX_QUANTILES(script.inline / script.total, 1000)[OFFSET(percentile * 10)] AS pct_inline_script
+  _TABLE_SUFFIX AS client,
+  APPROX_QUANTILES(stylesheets.inline, 1000)[OFFSET(percentile * 10)] AS num_inline_stylesheets,
+  APPROX_QUANTILES(stylesheets.remote, 1000)[OFFSET(percentile * 10)] AS num_remote_stylesheets
 FROM (
   SELECT
-    _TABLE_SUFFIX AS client,
-    getScripts(payload) AS script
+    _TABLE_SUFFIX,
+    url,
+  percentile,
+    getStylesheets(payload) AS stylesheets
   FROM
-    `httparchive.pages.2022_06_01_*`),
-  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+    `httparchive.pages.2022_06_01_*`,
+    UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+)
 GROUP BY
   client,
   percentile
--- sql/2022/sustainability/stylesheet_count.sql
+++ sql/2022/sustainability/stylesheet_count.sql
@@ -1,5 +1,6 @@
 #standardSQL
-# The percentage of stylesheets used inline v. external
+# The number of stylesheets used inline v. external
+
 CREATE TEMPORARY FUNCTION getStylesheets(payload STRING)
 RETURNS STRUCT<remote INT64, inline INT64> LANGUAGE js AS '''
 try {
@@ -14,13 +15,13 @@ try {
 SELECT
   _TABLE_SUFFIX AS client,
   percentile,
-  APPROX_QUANTILES(SAFE_DIVIDE(stylesheets.remote, (stylesheets.inline + stylesheets.remote)), 1000)[OFFSET(percentile * 10)] AS pct_remote,
   APPROX_QUANTILES(stylesheets.inline, 1000)[OFFSET(percentile * 10)] AS num_inline_stylesheets,
   APPROX_QUANTILES(stylesheets.remote, 1000)[OFFSET(percentile * 10)] AS num_inline_stylesheets
 FROM (
   SELECT
     _TABLE_SUFFIX,
     url,
+    percentile,
     getStylesheets(payload) AS stylesheets
   FROM
     `httparchive.pages.2022_06_01_*`,
