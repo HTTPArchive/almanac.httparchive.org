6812c687709dd02191626d707e480cf730605303
camcash17 <32401001+camcash17@users.noreply.github.com>
2022-08-19 15:14:51 -0600

    Sustainability 2022 Queries (#2989)
    * adds cdn usage for hosting category
    
    * adds trailing new line to fix linting
    
    * adds trailing new line to fix linting
    
    * add page weight query
    
    * update to use 2022 dataset
    
    * update bytes value for consistency
    
    * add green web hosting query
    
    * add requests by type query
    
    * add emissions calculation for all request types
    
    * add image format query
    
    * add image lazy loading query
    
    * fix linting errors with sqlfluff
    
    * update dates to use June run
    
    * remove talismanrc file
    
    * update with feedback from barry
    
    * lint sql
    
    * add unminified Js query
    
    * add unminified css query
    
    * add text compression query
    
    * fix dates
    
    * add green third party request distribution query
    
    * add resource weight by SSG query
    
    * add resource weight by CMS query
    
    * add median emissions for all resource types
    
    * add resource weight by Ecommerce query
    
    * adds reference for unused js and video preload
    
    * add unused css bytes distribution
    
    * fix linting error
    
    * simplify cdn adoption query
    
    * add descriptive heading
    
    * add cache header adoption query
    
    * fix linting errors
    
    * fix linting errors
    
    * add video autoplay query
    
    * fix date
    
    * refine video preload query to distribute by percentile
    
    * fix syntax error for APPROX_QUANTILES
    
    * add stylesheet count query
    
    * simplify stylesheet count
    
    * add script count query
    
    * update date
    
    * update date
    
    * add responsive images query
    
    * fix linting errors
    
    * removed percentile, simplified query
    
    * remove percentile
    
    * update date
    
    * fix variable name
    
    * change variable name
    
    * add totals to output
    
    * refine script count query
    
    * refine script count query
    
    * updates order by and attempts to use percentiles over averages
    
    * fix linting errors
    
    * attempts to resolve sqlfluff linting error
    
    * update green-third-party-scripts
    
    * make the linter happy
    
    * complete rewrite of green third parties query
    
    * linter don't like no trailing commas
    
    * add percentile grouping to script & stylesheet counts
    
    * updates date for ssg bytes per type
    
    * updates stylesheet count to use percentile rather than count
    
    * fix error in queries, simplify returned results
    
    * linting. my bad.
    
    * Update sql/2022/sustainability/unused_css_bytes_distribution.sql
    
    * Update sql/2022/sustainability/unused_js_bytes_distribution.sql
    
    * Set No CDN label
    
    * Review feedback
    
    * Fix caching query
    
    Co-authored-by: Fershad <itsfish@fershad.com>
    Co-authored-by: Barry Pollard <barrypollard@google.com>

---
+++ sql/2022/sustainability/cms_bytes_per_type.sql
@@ -0,0 +1,64 @@
+#standardSQL
+# Median resource weights by CMS
+
+# Declare variables to calculate the carbon emissions of one byte
+# Source: https://sustainablewebdesign.org/calculating-digital-emissions/
+# The implementation below does not make the assumptions about returning visitors or caching that are present in the Sustainable Web Design model.
+
+DECLARE kw_per_GB NUMERIC DEFAULT 0.81;
+DECLARE global_grid_intensity NUMERIC DEFAULT 442;
+
+SELECT
+  client,
+  cms,
+  COUNT(0) AS pages,
+  APPROX_QUANTILES(total_kb, 1000)[OFFSET(500)] AS median_total_kb,
+  APPROX_QUANTILES(total_emissions, 1000)[OFFSET(500)] AS median_total_emissions,
+  APPROX_QUANTILES(html_kb, 1000)[OFFSET(500)] AS median_html_kb,
+  APPROX_QUANTILES(total_html_emissions, 1000)[OFFSET(500)] AS median_total_html_emissions,
+  APPROX_QUANTILES(js_kb, 1000)[OFFSET(500)] AS median_js_kb,
+  APPROX_QUANTILES(total_js_emissions, 1000)[OFFSET(500)] AS median_total_js_emissions,
+  APPROX_QUANTILES(css_kb, 1000)[OFFSET(500)] AS median_css_kb,
+  APPROX_QUANTILES(total_css_emissions, 1000)[OFFSET(500)] AS median_total_css_emissions,
+  APPROX_QUANTILES(img_kb, 1000)[OFFSET(500)] AS median_img_kb,
+  APPROX_QUANTILES(total_img_emissions, 1000)[OFFSET(500)] AS median_total_img_emissions,
+  APPROX_QUANTILES(font_kb, 1000)[OFFSET(500)] AS median_font_kb,
+  APPROX_QUANTILES(total_font_emissions, 1000)[OFFSET(500)] AS median_total_font_emissions
+FROM (
+  SELECT DISTINCT
+    _TABLE_SUFFIX AS client,
+    url,
+    app AS cms
+  FROM
+    `httparchive.technologies.2022_06_01_*`
+  WHERE
+    category = 'CMS'
+)
+JOIN (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    url,
+    bytesTotal / 1024 AS total_kb,
+    (bytesTotal / 1024 / 1024 / 1024) * kw_per_GB * global_grid_intensity AS total_emissions,
+    bytesHtml / 1024 AS html_kb,
+    (bytesHtml / 1024 / 1024 / 1024) * kw_per_GB * global_grid_intensity AS total_html_emissions,
+    bytesJS / 1024 AS js_kb,
+    (bytesJS / 1024 / 1024 / 1024) * kw_per_GB * global_grid_intensity AS total_js_emissions,
+    bytesCSS / 1024 AS css_kb,
+    (bytesCSS / 1024 / 1024 / 1024) * kw_per_GB * global_grid_intensity AS total_css_emissions,
+    bytesImg / 1024 AS img_kb,
+    (bytesImg / 1024 / 1024 / 1024) * kw_per_GB * global_grid_intensity AS total_img_emissions,
+    bytesFont / 1024 AS font_kb,
+    (bytesFont / 1024 / 1024 / 1024) * kw_per_GB * global_grid_intensity AS total_font_emissions
+  FROM
+    `httparchive.summary_pages.2022_06_01_*`
+)
+USING
+  (client, url)
+GROUP BY
+  client,
+  cms
+ORDER BY
+  pages DESC,
+  cms,
+  client
---
+++ sql/2022/sustainability/image_formats.sql
@@ -0,0 +1,41 @@
+#standardSQL
+# Percentage adoption of image formats
+# Ignore really small images < 1500 bytes (often pixels) that would not benefit from more efficient format
+
+CREATE TEMPORARY FUNCTION fixFormat(format STRING, mimeType STRING)
+RETURNS STRING
+LANGUAGE js AS '''
+if (mimeType === "image/avif") {
+  return "avif";
+} else if (mimeType === "image/webp" || format==="webp") {
+  return "webp";
+} else {
+  return format;
+}
+''';
+
+SELECT
+  client,
+  trueFormat,
+  COUNT(DISTINCT NET.HOST(url)) AS hosts,
+  COUNT(DISTINCT page) AS pages,
+  COUNT(0) AS freqImages,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS totalImages,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pctImages
+FROM (
+  SELECT
+    client,
+    page,
+    url,
+    fixFormat(format, mimeType) AS trueFormat
+  FROM
+    `httparchive.almanac.requests`
+  WHERE
+    date = '2022-06-01' AND
+    type = 'image' AND
+    respSize > 1500)
+GROUP BY
+  client,
+  trueFormat
+ORDER BY
+  pctImages DESC
