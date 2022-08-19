68a2d47ae985bc5a1792bff34740391ef69716a9
camcash17 <32401001+camcash17@users.noreply.github.com>
2022-08-19 15:14:51 -0600

    Sustainability 2022 Queries (#2989)
    * adds cdn usage for hosting category
    
    * adds trailing new line to fix linting
    
    * adds trailing new line to fix linting
    
    * add page weight query
    
    * update to use 2022 dataset
    
    * update bytes value for consistency
    
    * add green web hosting query
    
    * add requests by type query
    
    * add emissions calculation for all request types
    
    * add image format query
    
    * add image lazy loading query
    
    * fix linting errors with sqlfluff
    
    * update dates to use June run
    
    * remove talismanrc file
    
    * update with feedback from barry
    
    * lint sql
    
    * add unminified Js query
    
    * add unminified css query
    
    * add text compression query
    
    * fix dates
    
    * add green third party request distribution query
    
    * add resource weight by SSG query
    
    * add resource weight by CMS query
    
    * add median emissions for all resource types
    
    * add resource weight by Ecommerce query
    
    * adds reference for unused js and video preload
    
    * add unused css bytes distribution
    
    * fix linting error
    
    * simplify cdn adoption query
    
    * add descriptive heading
    
    * add cache header adoption query
    
    * fix linting errors
    
    * fix linting errors
    
    * add video autoplay query
    
    * fix date
    
    * refine video preload query to distribute by percentile
    
    * fix syntax error for APPROX_QUANTILES
    
    * add stylesheet count query
    
    * simplify stylesheet count
    
    * add script count query
    
    * update date
    
    * update date
    
    * add responsive images query
    
    * fix linting errors
    
    * removed percentile, simplified query
    
    * remove percentile
    
    * update date
    
    * fix variable name
    
    * change variable name
    
    * add totals to output
    
    * refine script count query
    
    * refine script count query
    
    * updates order by and attempts to use percentiles over averages
    
    * fix linting errors
    
    * attempts to resolve sqlfluff linting error
    
    * update green-third-party-scripts
    
    * make the linter happy
    
    * complete rewrite of green third parties query
    
    * linter don't like no trailing commas
    
    * add percentile grouping to script & stylesheet counts
    
    * updates date for ssg bytes per type
    
    * updates stylesheet count to use percentile rather than count
    
    * fix error in queries, simplify returned results
    
    * linting. my bad.
    
    * Update sql/2022/sustainability/unused_css_bytes_distribution.sql
    
    * Update sql/2022/sustainability/unused_js_bytes_distribution.sql
    
    * Set No CDN label
    
    * Review feedback
    
    * Fix caching query
    
    Co-authored-by: Fershad <itsfish@fershad.com>
    Co-authored-by: Barry Pollard <barrypollard@google.com>

---
+++ sql/2022/sustainability/script_count.sql
@@ -0,0 +1,31 @@
+#standardSQL
+# Breakdown of inline vs external scripts
+CREATE TEMPORARY FUNCTION getScripts(payload STRING)
+RETURNS STRUCT<total INT64, inline INT64, src INT64, async INT64, defer INT64, async_and_defer INT64, type_module INT64, nomodule INT64>
+LANGUAGE js AS '''
+try {
+  var $ = JSON.parse(payload);
+  var javascript = JSON.parse($._javascript);
+  return javascript.script_tags;
+} catch (e) {
+  return {};
+}
+''';
+
+SELECT
+  client,
+  SUM(script.total) AS total_scripts,
+  SUM(script.inline) AS inline_script,
+  SUM(script.src) AS external_script,
+  SUM(script.src) / SUM(script.total) AS pct_external_script,
+  SUM(script.inline) / SUM(script.total) AS pct_inline_script,
+  APPROX_QUANTILES(SAFE_DIVIDE(script.src, script.total), 1000)[OFFSET(500)] AS median_external,
+  APPROX_QUANTILES(SAFE_DIVIDE(script.inline, script.total), 1000)[OFFSET(500)] AS median_inline
+FROM (
+  SELECT
+    _TABLE_SUFFIX AS client,
+    getScripts(payload) AS script
+  FROM
+    `httparchive.pages.2022_06_01_*`)
+GROUP BY
+  client
