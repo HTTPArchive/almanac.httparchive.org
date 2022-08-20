3205e56a0011122f896b047e236c81d29e2d24cd
Eric Portis <e@ericportis.com>
2022-08-20 10:20:31 -0700

    Update sql/2022/media/color_spaces_and_depth.sql
    Co-authored-by: Rick Viscomi <rviscomi@users.noreply.github.com>

--- sql/2022/media/color_spaces_and_depth.sql
+++ sql/2022/media/color_spaces_and_depth.sql
@@ -2,25 +2,51 @@
 # info on color depth and spaces – will need to be further filtered/aggregated, but this is everything
 
 WITH color_info AS (
-SELECT
-  _TABLE_SUFFIX as client,
-  JSON_EXTRACT( payload, '$._image_details.magick.depth') as color_depth,
-  JSON_EXTRACT( payload, '$._image_details.magick.colorspace') as color_space,
-  JSON_EXTRACT( payload, '$._image_details.magick.properties.icc:description') as description
-FROM `requests.2022_06_01_*`
-WHERE
-  JSON_QUERY(payload, '$._image_details.magick.colorspace') IS NOT NULL
+  SELECT
+    _TABLE_SUFFIX as client,
+    JSON_EXTRACT( payload, '$._image_details.magick.depth') AS color_depth,
+    JSON_EXTRACT( payload, '$._image_details.magick.colorspace') AS color_space,
+    JSON_EXTRACT( payload, '$._image_details.magick.properties.icc:description') AS description
+  FROM
+    `requests.2022_06_01_*`
+  WHERE
+    JSON_QUERY(payload, '$._image_details.magick.colorspace') IS NOT NULL
 ),
+
 totals AS (
-  SELECT client, COUNT(0) as total FROM color_info
-  GROUP BY client
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    color_info
+  GROUP BY
+    client
 ),
+
 counts AS (
   SELECT
-    client, color_depth, color_space, description, count(0) as count
-  FROM color_info
-  GROUP BY client, color_depth, color_space, description
+    client,
+    color_depth,
+    color_space,
+    description,
+    COUNT(0) AS count
+  FROM
+    color_info
+  GROUP BY
+    client,
+    color_depth,
+    color_space,
+    description
 )
-SELECT *, count / total as pct
-FROM counts JOIN totals USING ( client )
-ORDER BY count DESC
+
+SELECT
+  *,
+  count / total AS pct
+FROM
+  counts
+JOIN
+  totals
+USING
+  (client)
+ORDER BY
+  count DESC
