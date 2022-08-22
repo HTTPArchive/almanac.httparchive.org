f7cbb0a478a306d69a7433b9e9e02aa253f780c4
Barry Pollard <barrypollard@google.com>
2022-08-22 20:10:36 +0100

    Add some more early hints queries to HTTP chapter (#3084)
    * Add some more early hints queries to HTTP chapter
    
    * Add shopify Early Hints stats

--- sql/2022/http/early_hints_per_page.sql
+++ sql/2022/http/early_hints_per_page.sql
@@ -2,30 +2,53 @@
 
 # Distribution of number of early hints resources
 
+CREATE TEMPORARY FUNCTION getNumEarlyHints(early_hints_header STRING)
+RETURNS STRUCT<num_hints INT, num_resources_hinted INT> LANGUAGE js AS """
+try {
+
+  var num_hints = 0;
+  var num_resources_hinted = 0;
+
+  theJSON = JSON.parse(early_hints_header);
+
+  for (var key of Object.keys(theJSON)) {
+      if (theJSON[key].startsWith('link:')) {
+        num_hints++;
+      } else {
+        continue;
+      };
+      num_resources_hinted = num_resources_hinted + theJSON[key].split(',').length;
+  }
+
+  return {
+    num_hints,
+    num_resources_hinted
+  };
+} catch {
+  return {
+    num_hints: 0,
+    num_resources_hinted: 0
+  };
+}
+""";
+
 SELECT
   client,
   percentile,
   COUNT(DISTINCT page) AS num_pages,
-  APPROX_QUANTILES(num_reqs, 1000)[OFFSET(percentile * 10)] AS num_reqs,
-  APPROX_QUANTILES(early_hints, 1000)[OFFSET(percentile * 10)] AS early_hints,
-  APPROX_QUANTILES(pct_early_hints, 1000)[OFFSET(percentile * 10)] AS pct_early_hints
+  APPROX_QUANTILES(early_hints.num_hints, 1000)[OFFSET(percentile * 10)] AS num_hints,
+  APPROX_QUANTILES(early_hints.num_resources_hinted, 1000)[OFFSET(percentile * 10)] AS num_resources_hinted
 FROM
   (
     SELECT
       client,
       page,
-      COUNT(0) AS num_reqs,
-      COUNTIF(status = 103) AS early_hints,
-      COUNTIF(status = 103) / COUNT(0) AS pct_early_hints
+      getNumEarlyHints(JSON_EXTRACT(payload, '$._early_hint_headers')) AS early_hints
     FROM
       `httparchive.almanac.requests`
     WHERE
-      date = '2022-06-01'
-    GROUP BY
-      client,
-      page
-    ORDER BY
-      client ASC
+      date = '2022-06-01' AND
+      firstHtml
   ),
   UNNEST(GENERATE_ARRAY(1, 100)) AS percentile
 GROUP BY
