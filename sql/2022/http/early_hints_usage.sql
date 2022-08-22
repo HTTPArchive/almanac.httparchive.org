8617d794d13e61760361b7500754a6625ef7f809
Barry Pollard <barrypollard@google.com>
2022-08-22 19:47:45 +0100

    Add shopify Early Hints stats

--- sql/2022/http/early_hints_usage.sql
+++ sql/2022/http/early_hints_usage.sql
@@ -6,7 +6,9 @@ SELECT
   client,
   COUNT(DISTINCT page) AS num_pages,
   COUNTIF(JSON_EXTRACT(payload, '$._early_hint_headers') IS NOT NULL) AS early_hints,
-  COUNTIF(JSON_EXTRACT(payload, '$._early_hint_headers') IS NOT NULL) / COUNT(DISTINCT page) AS early_hints_pct
+  COUNTIF(JSON_EXTRACT(payload, '$._early_hint_headers') IS NOT NULL) / COUNT(DISTINCT page) AS early_hints_pct,
+  COUNTIF(JSON_EXTRACT(payload, '$._early_hint_headers') LIKE '%shopify%') AS early_hints_shopify,
+  COUNTIF(JSON_EXTRACT(payload, '$._early_hint_headers') LIKE '%shopify%') / COUNT(DISTINCT page) AS early_hints_shopify_pct
 FROM
   `httparchive.almanac.requests`
 WHERE
