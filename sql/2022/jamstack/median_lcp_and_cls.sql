46d2f2ef33e3c9aceae57abc6f6cc72c26c7f794
Barry Pollard <barrypollard@google.com>
2022-08-28 17:38:05 +0100

    Further tweaks

--- sql/2022/jamstack/median_lcp_and_cls.sql
+++ sql/2022/jamstack/median_lcp_and_cls.sql
@@ -1,20 +1,15 @@
 # Find median lcp AND cls (include other percentiles for interest)
 SELECT
   device,
-  APPROX_QUANTILES(p75_lcp, 1000)[OFFSET(10 * 10)] AS p10_lcp,
-  APPROX_QUANTILES(p75_lcp, 1000)[OFFSET(25 * 10)] AS p25_lcp,
-  APPROX_QUANTILES(p75_lcp, 1000)[OFFSET(50 * 10)] AS median_lcp,
-  APPROX_QUANTILES(p75_lcp, 1000)[OFFSET(75 * 10)] AS p75_lcp,
-  APPROX_QUANTILES(p75_lcp, 1000)[OFFSET(90 * 10)] AS p90_lcp,
-  APPROX_QUANTILES(p75_cls, 1000)[OFFSET(10 * 10)] AS p10_cls,
-  APPROX_QUANTILES(p75_cls, 1000)[OFFSET(25 * 10)] AS p25_cls,
-  APPROX_QUANTILES(p75_cls, 1000)[OFFSET(50 * 10)] AS median_cls,
-  APPROX_QUANTILES(p75_cls, 1000)[OFFSET(75 * 10)] AS p75_cls,
-  APPROX_QUANTILES(p75_cls, 1000)[OFFSET(90 * 10)] AS p90_cls
+  percentile,
+  APPROX_QUANTILES(p75_lcp, 1000)[OFFSET(percentile * 10)] AS lcp,
+  APPROX_QUANTILES(p75_cls, 1000)[OFFSET(percentile * 10)] AS cls
 FROM
-  `chrome-ux-report.materialized.device_summary`
+  `chrome-ux-report.materialized.device_summary`,
+  UNNEST(GENERATE_ARRAY(1, 100)) AS percentile
 WHERE
   date = '2022-06-01' AND
   device IN ('desktop', 'phone')
 GROUP BY
-  device
+  device,
+  percentile
