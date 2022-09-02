2f925330fc36dcdbc8cade8396061a02c6de12b7
Kevin Farrugia <kevinfarrugia@users.noreply.github.com>
2022-09-02 17:18:32 +0200

    Updated FID and INP queries to not exclude 0ms (#3102)
    * Updated FID and INP queries to not exclude 0ms
    
    * Updated FID and INP queries to ignore NULLS

--- sql/2022/javascript/inp.sql
+++ sql/2022/javascript/inp.sql
@@ -1,10 +1,6 @@
 #standardSQL
 # INP by device
 
-CREATE TEMP FUNCTION IS_NON_ZERO (p75_inp FLOAT64, p75_inp_origin FLOAT64) RETURNS BOOL AS (
-  p75_inp + p75_inp_origin > 0
-);
-
 WITH
 base AS (
   SELECT
@@ -15,15 +11,14 @@ base AS (
     `chrome-ux-report.materialized.device_summary`
   WHERE
     device IN ('desktop', 'phone') AND
-    date IN ('2022-06-01') AND
-    IS_NON_ZERO(p75_inp, p75_inp_origin)
+    date IN ('2022-06-01')
 )
 
 SELECT
   device,
   percentile,
-  APPROX_QUANTILES(p75_inp, 1000)[OFFSET(percentile * 10)] AS p75_inp,
-  APPROX_QUANTILES(p75_inp_origin, 1000)[OFFSET(percentile * 10)] AS p75_inp_origin
+  APPROX_QUANTILES(p75_inp, 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS p75_inp,
+  APPROX_QUANTILES(p75_inp_origin, 1000 IGNORE NULLS)[OFFSET(percentile * 10)] AS p75_inp_origin
 FROM
   base,
   UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
