47811ca1e7852b20f9c7ff87b64d028725e494f1
Rick Viscomi <rviscomi@users.noreply.github.com>
2024-05-15 10:29:47 -0400

    2023 Performance (#3525)
    * cp 2022->2023
    
    * 2023ify
    
    * 2023/perf
    
    * lint
    
    * lint
    
    * fix initiator
    
    * null initiators

---
+++ sql/2023/performance/cls_unsized_image_height.sql
@@ -0,0 +1,27 @@
+WITH lh AS (
+  SELECT
+    client,
+    CAST(JSON_VALUE(unsized_image, '$.node.boundingRect.height') AS INT64) AS height
+  FROM
+    `httparchive.all.pages`,
+    UNNEST(JSON_QUERY_ARRAY(lighthouse, '$.audits.unsized-images.details.items')) AS unsized_image
+  WHERE
+    date = '2023-10-01' AND
+    is_root_page
+)
+
+
+SELECT
+  percentile,
+  client,
+  APPROX_QUANTILES(height, 1000)[OFFSET(percentile * 10)] AS height,
+  COUNT(0) AS unsized_images
+FROM
+  lh,
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+GROUP BY
+  percentile,
+  client
+ORDER BY
+  percentile,
+  client
