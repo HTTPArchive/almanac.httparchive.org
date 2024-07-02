8de2161478c67a721aee845dc612b97269c60656
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-02 16:49:08 +0200

    Update font_format_usage_without_services.sql

--- sql/2024/fonts/performance/font_format_usage_without_services.sql
+++ sql/2024/fonts/performance/font_format_usage_without_services.sql
@@ -1,37 +1,48 @@
-#standardSQL
-#font_formats
+CREATE TEMPORARY FUNCTION SERVICE(url STRING) AS (
+  CASE
+    WHEN REGEXP_CONTAINS(url, r'((use|fonts)\.typekit\.(net|com))|webfonts\.creativecloud\.com') THEN 'Adobe'
+    WHEN REGEXP_CONTAINS(url, r'(fonts\.(gstatic|googleapis)\.com)|themes.googleusercontent.com/static/fonts|ssl.gstatic.com/fonts') THEN 'Google'
+    WHEN REGEXP_CONTAINS(url, r'cloud\.typenetwork\.com') THEN 'typenetwork.com'
+    WHEN REGEXP_CONTAINS(url, r'cloud\.typography\.com') THEN 'typography.com'
+    WHEN REGEXP_CONTAINS(url, r'cloud\.webtype\.com') THEN 'webtype.com'
+    WHEN REGEXP_CONTAINS(url, r'f\.fontdeck\.com') THEN 'fontdeck.com'
+    WHEN REGEXP_CONTAINS(url, r'fast\.fonts\.(com|net)\/(jsapi|cssapi)') THEN 'fonts.com'
+    WHEN REGEXP_CONTAINS(url, r'fnt\.webink\.com') THEN 'webink.com'
+    WHEN REGEXP_CONTAINS(url, r'fontawesome\.com') THEN 'fontawesome.com'
+    WHEN REGEXP_CONTAINS(url, r'fonts\.typonine\.com') THEN 'typonine.com'
+    WHEN REGEXP_CONTAINS(url, r'fonts\.typotheque\.com') THEN 'typotheque.com'
+    WHEN REGEXP_CONTAINS(url, r'typesquare\.com') THEN 'typesquare.com'
+    WHEN REGEXP_CONTAINS(url, r'use\.edgefonts\.net') THEN 'edgefonts.net'
+    WHEN REGEXP_CONTAINS(url, r'webfont\.fontplus\.jp') THEN 'fontplus.jp'
+    WHEN REGEXP_CONTAINS(url, r'webfonts\.fontslive\.com') THEN 'fontslive.com'
+    WHEN REGEXP_CONTAINS(url, r'webfonts\.fontstand\.com') THEN 'fontstand.com'
+    WHEN REGEXP_CONTAINS(url, r'webfonts\.justanotherfoundry\.com') THEN 'justanotherfoundry.com'
+    ELSE 'self-hosted'
+  END
+);
+
 SELECT
   client,
-  LOWER(IFNULL(REGEXP_EXTRACT(mimeType, '/(?:x-)?(?:font-)?(.*)'), ext)) AS mime_type,
-  COUNT(0) AS freq,
+  LOWER(COALESCE(
+    REGEXP_EXTRACT(LOWER(header.value), r'(otf|sfnt|svg|ttf|woff2?|fontobject|opentype|truetype)'),
+    REGEXP_EXTRACT(url, r'\.(\w+)(?:$|\?|#)'),
+    header.value
+  )) AS format,
+  COUNT(0) AS count,
   SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS proportion
 FROM
-  `httparchive.almanac.requests`
+  `httparchive.all.requests`,
+  UNNEST(response_headers) AS header
 WHERE
-  date = '2022-06-01' AND
+  date = '2024-06-01' AND
   type = 'font' AND
-  mimeType != '' AND
-  NOT (REGEXP_CONTAINS(url, r'(fonts\.(gstatic|googleapis)\.com)|(themes.googleusercontent.com/static/fonts)|(ssl.gstatic.com/fonts/)') OR
-    REGEXP_CONTAINS(url, r'(use|fonts)\.typekit\.(net|com)') OR
-    REGEXP_CONTAINS(url, r'(use\.edgefonts\.net|webfonts\.creativecloud\.com)') OR
-    REGEXP_CONTAINS(url, r'fast\.fonts\.(com|net)\/(jsapi|cssapi)') OR
-    REGEXP_CONTAINS(url, r'f\.fontdeck\.com') OR
-    REGEXP_CONTAINS(url, r'cloud\.webtype\.com') OR
-    REGEXP_CONTAINS(url, r'cloud\.typenetwork\.com') OR
-    REGEXP_CONTAINS(url, r'cloud\.typography\.com') OR
-    REGEXP_CONTAINS(url, r'fnt\.webink\.com') OR
-    REGEXP_CONTAINS(url, r'fonts\.typotheque\.com') OR
-    REGEXP_CONTAINS(url, r'webfonts\.fontstand\.com') OR
-    REGEXP_CONTAINS(url, r'typesquare\.com') OR
-    REGEXP_CONTAINS(url, r'webfont\.fontplus\.jp') OR
-    REGEXP_CONTAINS(url, r'fontawesome\.com') OR
-    REGEXP_CONTAINS(url, r'webfonts\.fontslive\.com') OR
-    REGEXP_CONTAINS(url, r'webfonts\.justanotherfoundry\.com') OR
-    REGEXP_CONTAINS(url, r'fonts\.typonine\.com'))
+  LOWER(header.name) = 'content-type' AND
+  TRIM(header.value) != '' AND
+  SERVICE(url) = 'self-hosted'
 GROUP BY
   client,
-  mime_type
+  format
 ORDER BY
   client,
-  pct DESC
+  proportion DESC
