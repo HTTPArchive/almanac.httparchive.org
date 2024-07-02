1b75ddcd0951f547597e2408ad797d788110b7e8
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-02 16:59:46 +0200

    Update font_size_quantiles.sql

--- sql/2024/fonts/performance/font_size_quantiles.sql
+++ sql/2024/fonts/performance/font_size_quantiles.sql
@@ -1,20 +1,30 @@
-SELECT
-  percentile,
-  client,
-  COUNT(DISTINCT url) AS total,
-  APPROX_QUANTILES(KiB_size, 1000)[OFFSET(percentile * 10)] AS KiB_size
-FROM (
+WITH
+urls AS (
   SELECT
     url,
     client,
-    percentile,
-    respBodySize / 1024 AS KiB_size
+    AVG(PARSE_NUMERIC(header.value)) AS size
   FROM
-    `httparchive.almanac.requests`,
-    UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+    `httparchive.all.requests`,
+    UNNEST(response_headers) AS header
   WHERE
-    date = '2022-06-01' AND
-    type = 'font')
+    date = '2024-06-01' AND
+    type = 'font' AND
+    LOWER(header.name) = 'content-length' AND
+    TRIM(header.value) != ''
+  GROUP BY
+    url,
+    client
+)
+
+SELECT
+  client,
+  percentile,
+  COUNT(DISTINCT url) AS count,
+  APPROX_QUANTILES(size, 1000)[OFFSET(percentile * 10)] AS size
+FROM
+  urls,
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
 GROUP BY
   client,
   percentile
