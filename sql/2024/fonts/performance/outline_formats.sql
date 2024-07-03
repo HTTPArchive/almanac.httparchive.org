1f7f758d10502db5077b2ec8add48eccb176fee5
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-03 15:37:47 +0200

    Update outline_formats.sql

--- sql/2024/fonts/performance/outline_formats.sql
+++ sql/2024/fonts/performance/outline_formats.sql
@@ -5,21 +5,22 @@ fonts AS (
     url,
     JSON_EXTRACT(payload, '$._font_details.table_sizes') AS payload
   FROM
-    `httparchive.almanac.requests`
+    `httparchive.all.requests`
   WHERE
-    date = '2022-06-01' AND
+    date = '2024-06-01' AND
     type = 'font'
   GROUP BY
     client,
     url,
     payload
 )
+
 SELECT
   client,
   format,
-  COUNT(0) AS freq,
+  COUNT(0) AS count,
   SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct_freq
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS proportion
 FROM
   fonts,
   UNNEST(REGEXP_EXTRACT_ALL(payload, '(?i)(CFF |glyf|SVG|CFF2)')) AS format
@@ -27,4 +28,4 @@ GROUP BY
   client,
   format
 ORDER BY
-  pct_freq DESC
+  proportion DESC
