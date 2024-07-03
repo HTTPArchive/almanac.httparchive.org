0563ec462659b4ddc58d17c4ce74e4c82d31b632
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-03 16:38:33 +0200

    Update usage_by_country.sql

--- sql/2024/fonts/performance/usage_by_country.sql
+++ sql/2024/fonts/performance/usage_by_country.sql
@@ -1,24 +1,45 @@
-SELECT
-  _TABLE_SUFFIX AS client,
-  country,
-  COUNT(0) AS freq_url,
-  APPROX_QUANTILES(bytesFont, 1000)[OFFSET(500)] / 1024 AS median_font_kbytes
-FROM (
-  SELECT DISTINCT
-    origin,
-    device,
+WITH
+countries AS (
+  SELECT
+    IF(device = 'desktop', 'desktop', 'mobile') AS client,
+    NET.HOST(origin) AS domain,
     `chrome-ux-report`.experimental.GET_COUNTRY(country_code) AS country
   FROM
     `chrome-ux-report.materialized.country_summary`
   WHERE
-    yyyymm = 202206)
+    yyyymm = 202405
+  GROUP BY
+    client,
+    domain,
+    country
+),
+fonts AS (
+  SELECT
+    client,
+    NET.HOST(page) AS domain,
+    AVG(PARSE_NUMERIC(header.value)) AS size
+  FROM
+    `httparchive.all.requests`,
+    UNNEST(response_headers) AS header
+  WHERE
+    date = '2024-06-01' AND
+    type = 'font' AND
+    LOWER(header.name) = 'content-length' AND
+    TRIM(header.value) != ''
+  GROUP BY
+    client,
+    domain
+)
+
+SELECT
+  client,
+  country,
+  COUNT(0) AS count,
+  APPROX_QUANTILES(size, 1000)[OFFSET(500)] AS median_size
+FROM
+  fonts
 JOIN
-  `httparchive.summary_pages.2022_06_01_*`
-ON
-  CONCAT(origin, '/') = url AND
-  IF(device = 'desktop', 'desktop', 'mobile') = _TABLE_SUFFIX
-WHERE
-  bytesFont IS NOT NULL
+  countries USING (client, domain)
 GROUP BY
   client,
   country
