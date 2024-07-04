f5340ae4d9da60165ce9be67677d2a85a8bb46ec
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-04 10:38:11 +0200

    Change formats_file_self_hosted.sql to formats_file_by_service.sql

--- sql/2024/fonts/performance/formats_file_by_service.sql
+++ sql/2024/fonts/performance/formats_file_by_service.sql
@@ -21,13 +21,18 @@ CREATE TEMPORARY FUNCTION SERVICE(url STRING) AS (
   END
 );
 
+CREATE TEMPORARY FUNCTION FILE_FORMAT(url STRING, header STRING) AS (
+  LOWER(COALESCE(
+    REGEXP_EXTRACT(LOWER(header), r'(otf|sfnt|svg|ttf|woff2?|fontobject|opentype|truetype)'),
+    REGEXP_EXTRACT(url, r'\.(\w+)(?:$|\?|#)'),
+    header
+  ))
+);
+
 SELECT
   client,
-  LOWER(COALESCE(
-    REGEXP_EXTRACT(LOWER(header.value), r'(otf|sfnt|svg|ttf|woff2?|fontobject|opentype|truetype)'),
-    REGEXP_EXTRACT(url, r'\.(\w+)(?:$|\?|#)'),
-    header.value
-  )) AS format,
+  SERVICE(url) AS service,
+  FILE_FORMAT(url, header.value) AS format,
   COUNT(0) AS count,
   SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
   COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS proportion
@@ -38,11 +43,12 @@ WHERE
   date = '2024-06-01' AND
   type = 'font' AND
   LOWER(header.name) = 'content-type' AND
-  TRIM(header.value) != '' AND
-  SERVICE(url) = 'self-hosted'
+  TRIM(header.value) != ''
 GROUP BY
   client,
+  service,
   format
 ORDER BY
   client,
+  service,
   proportion DESC
