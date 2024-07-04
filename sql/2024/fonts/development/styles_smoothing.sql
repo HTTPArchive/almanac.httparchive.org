32afa8e8a2fd7c35852ca29b8898e7cb4f465b44
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-04 16:11:16 +0200

    Update font_smoothing_usage.sql

---
+++ sql/2024/fonts/development/styles_smoothing.sql
@@ -0,0 +1,62 @@
+-- Section: Development
+-- Question: How and how often is smoothing used?
+
+CREATE TEMPORARY FUNCTION PROPERTIES(json STRING)
+RETURNS ARRAY<STRING>
+LANGUAGE js
+OPTIONS (library = "gs://httparchive/lib/css-utils.js")
+AS '''
+try {
+  const ast = JSON.parse(json);
+  const result = [];
+  walkDeclarations(ast, decl => {
+    result.push(`${decl.property}: ${decl.value}`);
+  }, {
+    properties: ['-webkit-font-smoothing', '-moz-osx-font-smoothing', 'font-smooth']
+  });
+  return result;
+} catch (e) {
+  return [];
+}
+''';
+
+WITH
+pages AS (
+  SELECT
+    client,
+    COUNT(DISTINCT page) AS total
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-06-01'
+  GROUP BY
+    client
+),
+properties AS (
+  SELECT
+    client,
+    property,
+    COUNT(DISTINCT page) AS count
+  FROM
+    `httparchive.all.parsed_css`,
+    UNNEST(PROPERTIES(css)) AS property
+  WHERE
+    date = '2024-06-01'
+  GROUP BY
+    client,
+    property
+)
+
+SELECT
+  client,
+  property,
+  count,
+  total,
+  count / total AS proportion
+FROM
+  properties
+JOIN
+  pages USING (client)
+ORDER BY
+  client,
+  proportion DESC
