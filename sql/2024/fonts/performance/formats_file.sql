f9cfc51b48107a29265a1932980fa0a9682b83d1
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-04 10:38:11 +0200

    Change formats_file_self_hosted.sql to formats_file_by_service.sql

--- sql/2024/fonts/performance/formats_file.sql
+++ sql/2024/fonts/performance/formats_file.sql
@@ -1,10 +1,14 @@
+CREATE TEMPORARY FUNCTION FILE_FORMAT(url STRING, header STRING) AS (
+  LOWER(COALESCE(
+    REGEXP_EXTRACT(LOWER(header), r'(otf|sfnt|svg|ttf|woff2?|fontobject|opentype|truetype)'),
+    REGEXP_EXTRACT(url, r'\.(\w+)(?:$|\?|#)'),
+    header
+  ))
+);
+
 SELECT
   client,
-  LOWER(COALESCE(
-    REGEXP_EXTRACT(LOWER(header.value), r'(otf|sfnt|svg|ttf|woff2?|fontobject|opentype|truetype)'),
-    REGEXP_EXTRACT(url, r'\.(\w+)(?:$|\?|#)'),
-    header.value
-  )) AS format,
+  FILE_FORMAT(url, header.value) AS format,
   COUNT(0) AS count,
   SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
   COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS proportion
