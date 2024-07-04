174ce685fb6733669aeba0d9dc3f7347d2dc173d
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-04 12:56:15 +0200

    Update color_fonts.sql

--- sql/2024/fonts/development/color_fonts.sql
+++ sql/2024/fonts/development/color_fonts.sql
@@ -1,33 +1,28 @@
-#standardSQL
-#color_fonts
-WITH
-fonts AS (
-  SELECT
-    url,
-    client,
-    JSON_EXTRACT(payload, '$._font_details.color.formats') AS payload
-  FROM
-    `httparchive.almanac.requests`
-  WHERE
-    date = '2022-06-01' AND
-    type = 'font'
-  GROUP BY
-    url,
-    client,
-    payload
-)
+-- Section: Development
+-- Question: Which color-font formats are used?
+
+CREATE TEMPORARY FUNCTION COLOR_FORMATS(payload STRING) AS (
+  REGEXP_EXTRACT_ALL(
+    JSON_EXTRACT(payload, '$._font_details.color.formats'),
+    '(?i)(sbix|CBDT|SVG|COLRv0|COLRv1)'
+  )
+);
+
 SELECT
   client,
   format,
-  COUNT(0) AS freq,
+  COUNT(0) AS count,
   SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct_freq
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS proportion
 FROM
-  fonts,
-  # Color fonts have any of sbix, cbdt, svg, colrv0 or colrv1 tables.
-  UNNEST(REGEXP_EXTRACT_ALL(payload, '(?i)(sbix|CBDT|SVG|COLRv0|COLRv1)')) AS format
+  `httparchive.all.requests`,
+  UNNEST(COLOR_FORMATS(payload)) AS format
+WHERE
+  date = '2024-06-01' AND
+  type = 'font'
 GROUP BY
   client,
   format
 ORDER BY
-  pct_freq DESC
+  client,
+  proportion DESC
