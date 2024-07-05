b90aca6f010e9185b9b13dc9cb14ba30ff8e90ee
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-05 10:48:39 +0200

    Update variable_font_axes_css.sql

---
+++ sql/2024/fonts/development/styles_font_variable_settings_axis.sql
@@ -0,0 +1,71 @@
+-- Section: Development
+-- Question: Which axes are controlled via CSS?
+
+CREATE TEMPORARY FUNCTION PROPERTIES(js STRING)
+RETURNS ARRAY<STRING>
+LANGUAGE js
+AS '''
+try {
+  var reduceValues = (values, rule) => {
+    if ('rules' in rule) {
+      return rule.rules.reduce(reduceValues, values);
+    }
+    if (!('declarations' in rule)) {
+      return values;
+    }
+    return values.concat(
+      rule.declarations
+        .filter(d => d.property.toLowerCase() == 'font-variation-settings')
+        .map(d => d.value)
+    );
+  };
+  var $ = JSON.parse(js);
+  return $.stylesheet.rules.reduce(reduceValues, []);
+} catch (e) {
+  return [];
+}
+''';
+
+WITH
+pages AS (
+  SELECT
+    client,
+    COUNT(DISTINCT page) AS total
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-06-01'
+  GROUP BY
+    client
+),
+properties AS (
+  SELECT
+    client,
+    REGEXP_EXTRACT(chunk, r'''['"]([\w]{4})['"]''') AS axis,
+    COUNT(DISTINCT page) AS count
+  FROM
+    `httparchive.all.parsed_css`,
+    UNNEST(PROPERTIES(css)) AS property,
+    UNNEST(SPLIT(property, ',')) AS chunk
+  WHERE
+    date = '2024-06-01'
+  GROUP BY
+    client,
+    axis
+  HAVING
+    axis IS NOT NULL
+)
+
+SELECT
+  client,
+  axis,
+  count,
+  total,
+  count / total AS proportion
+FROM
+  properties
+JOIN
+  pages USING (client)
+ORDER BY
+  client,
+  proportion DESC
