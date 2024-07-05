50809cffb0bc726282218326015316afa6edd10d
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-05 11:53:53 +0200

    Revisit COUNT(0) in design

--- sql/2024/fonts/design/families_by_foundry.sql
+++ sql/2024/fonts/design/families_by_foundry.sql
@@ -5,9 +5,9 @@ SELECT
   client,
   JSON_EXTRACT_SCALAR(payload, '$._font_details.OS2.achVendID') AS foundry,
   JSON_EXTRACT_SCALAR(payload, '$._font_details.names[1]') AS family,
-  COUNT(0) AS count,
-  SUM(COUNT(0)) OVER(PARTITION BY client) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER(PARTITION BY client) AS proportion
+  COUNT(DISTINCT url) AS count,
+  SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS total,
+  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS proportion
 FROM
   `httparchive.all.requests`
 WHERE
