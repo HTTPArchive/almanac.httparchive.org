a42d4b5bcd79360e62b7e15d1ccb071f8557d700
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-11 16:45:13 +0200

    Add fonts/design/pages_script.sql

---
+++ sql/2024/fonts/design/pages_script.sql
@@ -0,0 +1,64 @@
+-- Section: Performance
+-- Question: Which scripts are used on the web?
+
+CREATE TEMPORARY FUNCTION SCRIPTS(body STRING)
+RETURNS ARRAY<STRING>
+LANGUAGE js
+OPTIONS (library = ["gs://httparchive/lib/text-utils.js"])
+AS r"""
+try {
+  let codepoints = body
+    .replace(/<\/[^>]+(>|$)/g, '')
+    .split('')
+    .map(c => c.codePointAt(0));
+  if (codepoints.length) {
+    return detectWritingScript(codepoints, 0.05);
+  } else {
+    return [];
+  }
+} catch (e) {
+  return [];
+}
+""";
+
+WITH
+scripts AS (
+  SELECT
+    client,
+    script,
+    COUNT(DISTINCT page) AS count
+  FROM
+    `httparchive.all.requests`,
+    UNNEST(SCRIPTS(response_body)) AS script
+  WHERE
+    date = '2024-06-01' AND
+    type = 'html'
+  GROUP BY
+    client,
+    script
+),
+pages AS (
+  SELECT
+    client,
+    COUNT(DISTINCT page) AS total
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-06-01'
+  GROUP BY
+    client
+)
+
+SELECT
+  client,
+  script,
+  count,
+  total,
+  count / total AS proportion
+FROM
+  scripts
+JOIN
+  pages USING (client)
+ORDER BY
+  client,
+  proportion DESC
