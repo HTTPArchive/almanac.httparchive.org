ce7a8e5c933c51caae7792459dab0d040bc61cdf
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-11 15:17:41 +0200

    Add design/fonts_designer.sql

---
+++ sql/2024/fonts/design/fonts_designer.sql
@@ -0,0 +1,43 @@
+-- Section: Design
+-- Question: Which designers are popular?
+
+WITH
+foundries AS (
+  SELECT
+    client,
+    NULLIF(TRIM(JSON_EXTRACT_SCALAR(payload, '$._font_details.names[9]')), '') AS designer,
+    COUNT(DISTINCT page) AS count
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-06-01' AND
+    type = 'font'
+  GROUP BY
+    client,
+    designer
+),
+pages AS (
+  SELECT
+    client,
+    COUNT(DISTINCT page) AS total
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-06-01'
+  GROUP BY
+    client
+)
+
+SELECT
+  client,
+  designer,
+  count,
+  total,
+  count / total AS proportion
+FROM
+  foundries
+JOIN
+  pages USING (client)
+ORDER BY
+  client,
+  proportion DESC
