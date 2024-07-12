6c8dde62b9971e15a9846f72eec1f7222386e190
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-07-12 10:08:38 +0200

    Add design/fonts_license.sql

---
+++ sql/2024/fonts/design/fonts_license.sql
@@ -0,0 +1,31 @@
+-- Section: Design
+-- Question: Which licenses are used?
+
+CREATE TEMPORARY FUNCTION LICENSE(value STRING) AS (
+  CASE
+    WHEN REGEXP_CONTAINS(value, 'adobe|typekit') THEN 'Adobe'
+    WHEN REGEXP_CONTAINS(value, 'apache') THEN 'Apache'
+    WHEN REGEXP_CONTAINS(value, 'fontawesome') THEN 'Font Awesome'
+    WHEN REGEXP_CONTAINS(value, 'linotype|monotype|myfonts') THEN 'Monotype'
+    WHEN REGEXP_CONTAINS(value, '(?i)(ofl|openfontlicense)') THEN 'OFL'
+    ELSE NULLIF(TRIM(value), '')
+  END
+);
+
+SELECT
+  client,
+  LICENSE(JSON_EXTRACT_SCALAR(payload, '$._font_details.names[14]')) AS license,
+  COUNT(DISTINCT page) AS count,
+  SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client) AS total,
+  COUNT(DISTINCT page) / SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client) AS proportion
+FROM
+  `httparchive.all.requests`
+WHERE
+  date = '2024-06-01' AND
+  type = 'font'
+GROUP BY
+  client,
+  license
+ORDER BY
+  client,
+  proportion DESC
