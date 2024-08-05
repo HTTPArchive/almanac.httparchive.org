b2264d31e2fe91891b4e6026d69a700d5908c61f
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-08-05 13:48:29 +0200

    Limit performance/fonts_family_by_service.sql

--- sql/2024/fonts/performance/fonts_family_by_service.sql
+++ sql/2024/fonts/performance/fonts_family_by_service.sql
@@ -3,22 +3,41 @@
 
 -- INCLUDE ../common.sql
 
+WITH
+fonts AS (
+  SELECT
+    client,
+    url,
+    SERVICE(url) AS service,
+    FAMILY(payload) AS family
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-07-01' AND
+    type = 'font'
+  GROUP BY
+    client,
+    url,
+    service,
+    family
+)
+
 SELECT
   client,
-  SERVICE(url) AS service,
-  FAMILY(payload) AS family,
+  service,
+  family,
   COUNT(DISTINCT url) AS count,
   SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS total,
-  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS proportion
+  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS proportion,
+  ROW_NUMBER() OVER (PARTITION BY client, service ORDER BY COUNT(0) DESC) AS rank
 FROM
-  `httparchive.all.requests`
-WHERE
-  date = '2024-07-01' AND
-  type = 'font'
+  fonts
 GROUP BY
   client,
   service,
   family
+QUALIFY
+  rank <= 10
 ORDER BY
   client,
   proportion DESC
