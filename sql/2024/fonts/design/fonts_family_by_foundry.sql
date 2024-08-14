d4ae93c587383fd2843a397ee294df2d092937fe
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-08-14 16:55:25 +0200

    Simplify design/fonts_family_by_foundry.sql

--- sql/2024/fonts/design/fonts_family_by_foundry.sql
+++ sql/2024/fonts/design/fonts_family_by_foundry.sql
@@ -3,42 +3,25 @@
 
 -- INCLUDE ../common.sql
 
-WITH
-fonts AS (
-  SELECT
-    client,
-    url,
-    ANY_VALUE(FOUNDRY(payload)) AS foundry,
-    ANY_VALUE(FAMILY(payload)) AS family
-  FROM
-    `httparchive.all.requests`
-  WHERE
-    date = '2024-07-01' AND
-    type = 'font'
-  GROUP BY
-    client,
-    url
-)
-
 SELECT
   client,
-  foundry,
-  family,
+  FOUNDRY(payload) AS foundry,
+  FAMILY(payload) AS family,
   COUNT(DISTINCT url) AS count,
-  SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS total,
-  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS proportion,
-  ROW_NUMBER() OVER (PARTITION BY client ORDER BY COUNT(DISTINCT url) DESC) AS foundry_rank,
-  ROW_NUMBER() OVER (PARTITION BY client, foundry ORDER BY COUNT(DISTINCT url) DESC) AS family_rank
+  SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS total,
+  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS proportion,
+  ROW_NUMBER() OVER (PARTITION BY client ORDER BY COUNT(DISTINCT url) DESC) AS rank
 FROM
-  fonts
+  `httparchive.all.requests`
+WHERE
+  date = '2024-07-01' AND
+  type = 'font'
 GROUP BY
   client,
   foundry,
   family
 QUALIFY
-  foundry_rank <= 100 AND
-  family_rank <= 10
+  rank <= 100
 ORDER BY
   client,
-  foundry,
   proportion DESC
