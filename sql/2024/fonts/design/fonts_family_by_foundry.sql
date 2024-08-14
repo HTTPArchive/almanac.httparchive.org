a1ece0a8c8ebed3b8bb7b6d360f8ab28702e1f43
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-08-14 14:18:22 +0200

    Limit to 100 and 10 in design/fonts_family_by_foundry.sql

--- sql/2024/fonts/design/fonts_family_by_foundry.sql
+++ sql/2024/fonts/design/fonts_family_by_foundry.sql
@@ -3,22 +3,42 @@
 
 -- INCLUDE ../common.sql
 
+WITH
+fonts AS (
+  SELECT
+    client,
+    url,
+    ANY_VALUE(FOUNDRY(payload)) AS foundry,
+    ANY_VALUE(FAMILY(payload)) AS family
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-07-01' AND
+    type = 'font'
+  GROUP BY
+    client,
+    url
+)
+
 SELECT
   client,
-  FOUNDRY(payload) AS foundry,
-  FAMILY(payload) AS family,
+  foundry,
+  family,
   COUNT(DISTINCT url) AS count,
   SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS total,
-  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS proportion
+  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS proportion,
+  ROW_NUMBER() OVER (PARTITION BY client, foundry ORDER BY COUNT(DISTINCT url) DESC) AS foundry_rank,
+  ROW_NUMBER() OVER (PARTITION BY client, foundry, family ORDER BY COUNT(DISTINCT url) DESC) AS family_rank
 FROM
-  `httparchive.all.requests`
-WHERE
-  date = '2024-07-01' AND
-  type = 'font'
+  fonts
 GROUP BY
   client,
   foundry,
   family
+QUALIFY
+  foundry_rank <= 100 AND
+  family_rank <= 10
 ORDER BY
   client,
+  foundry,
   proportion DESC
