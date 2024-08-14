85ded15ffffde6a60edbdfce8e18edc962e3b0f4
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-08-14 15:32:40 +0200

    Limit to 100 in development/fonts_compiler.sql

--- sql/2024/fonts/development/fonts_compiler.sql
+++ sql/2024/fonts/development/fonts_compiler.sql
@@ -13,20 +13,36 @@ CREATE TEMPORARY FUNCTION COMPILER(version STRING) AS (
   END
 );
 
+WITH
+fonts AS (
+  SELECT
+    client,
+    url,
+    ANY_VALUE(COMPILER(JSON_EXTRACT_SCALAR(payload, '$._font_details.names[5]'))) AS compiler
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-07-01' AND
+    type = 'font'
+  GROUP BY
+    client,
+    url
+)
+
 SELECT
   client,
-  COMPILER(JSON_EXTRACT_SCALAR(payload, '$._font_details.names[5]')) AS compiler,
+  compiler,
   COUNT(DISTINCT url) AS count,
   SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS total,
-  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS proportion
+  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER(PARTITION BY client) AS proportion,
+  ROW_NUMBER() OVER (PARTITION BY client ORDER BY COUNT(DISTINCT url) DESC) AS rank
 FROM
-  `httparchive.all.requests`
-WHERE
-  date = '2024-07-01' AND
-  type = 'font'
+  fonts
 GROUP BY
   client,
   compiler
+QUALIFY
+  rank <= 100
 ORDER BY
   client,
   proportion DESC
