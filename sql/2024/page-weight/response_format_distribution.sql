94df477dfbb20cfb9c45926ef65d9b068f6fa90d
burak.gueneli <burak.gueneli@uberall.com>
2024-08-15 10:45:24 +0200

    Queries of page-weight 2024

---
+++ sql/2024/page-weight/response_format_distribution.sql
@@ -0,0 +1,21 @@
+SELECT
+  client,
+  percentile,
+  CAST(JSON_VALUE(summary, '$.type') AS STRING) AS format,
+  is_root_page,
+  APPROX_QUANTILES(CAST(JSON_VALUE(summary, '$.respBodySize') AS INT64) / 1024, 1000)[OFFSET(percentile * 10)] AS resp_size
+FROM
+  `httparchive.all.requests`,
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+WHERE
+  date = '2024-06-01'  -- Adjust this date as needed
+GROUP BY
+  client,
+  percentile,
+  format,
+  is_root_page
+ORDER BY
+  format,
+  client,
+  is_root_page,
+  percentile
