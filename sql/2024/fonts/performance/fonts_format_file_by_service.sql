206f958be54e8705c9efe1d115c6adfa531b218d
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-08-15 10:31:01 +0200

    Limit to 10 in performance/fonts_format_file_by_service.sql

--- sql/2024/fonts/performance/fonts_format_file_by_service.sql
+++ sql/2024/fonts/performance/fonts_format_file_by_service.sql
@@ -3,25 +3,44 @@
 
 -- INCLUDE ../common.sql
 
+WITH
+fonts AS (
+  SELECT
+    client,
+    url,
+    SERVICE(url) AS service,
+    FILE_FORMAT(
+      JSON_EXTRACT_SCALAR(summary, '$.ext'),
+      JSON_EXTRACT_SCALAR(summary, '$.mimeType')
+    ) AS format
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-07-01' AND
+    type = 'font'
+  GROUP BY
+    client,
+    url,
+    service,
+    format
+)
+
 SELECT
   client,
-  SERVICE(url) AS service,
-  FILE_FORMAT(
-    JSON_EXTRACT_SCALAR(summary, '$.ext'),
-    JSON_EXTRACT_SCALAR(summary, '$.mimeType')
-  ) AS format,
+  service,
+  format,
   COUNT(DISTINCT url) AS count,
   SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS total,
-  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS proportion
+  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS proportion,
+  ROW_NUMBER() OVER (PARTITION BY client, service ORDER BY COUNT(DISTINCT url) DESC) AS rank
 FROM
-  `httparchive.all.requests`
-WHERE
-  date = '2024-07-01' AND
-  type = 'font'
+  fonts
 GROUP BY
   client,
   service,
   format
+QUALIFY
+  rank <= 10
 ORDER BY
   client,
   service,
