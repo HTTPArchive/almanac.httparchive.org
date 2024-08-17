5445b926e9d370773621514a133a6142ec2fc913
Ben Standaert <71239179+bstandaert-wustl@users.noreply.github.com>
2024-08-17 14:06:16 -0500

    fingerprinting_most_common_apis: improve resilience to malformed JSON (#3737)

--- sql/2024/privacy/fingerprinting_most_common_apis.sql
+++ sql/2024/privacy/fingerprinting_most_common_apis.sql
@@ -1,9 +1,19 @@
 CREATE TEMP FUNCTION getFingerprintingTypes(input STRING)
 RETURNS ARRAY<STRING>
-LANGUAGE js AS """return Object.keys(JSON.parse(input).privacy?.fingerprinting?.counts || {})""";
+LANGUAGE js AS """
+if (input) {
+  try {
+    return Object.keys(JSON.parse(input))
+  } catch (e) {
+    return []
+  }
+} else {
+  return []
+}
+""";
 
 WITH pages AS (
-  SELECT client, page, fingerprinting_type FROM `httparchive.all.pages`, -- TABLESAMPLE SYSTEM (0.001 PERCENT)
-    unnest(getFingerprintingTypes(custom_metrics)) AS fingerprinting_type WHERE date = '2024-06-01'
+  SELECT client, page, fingerprinting_type FROM `httparchive.all.pages`,
+    unnest(getFingerprintingTypes(JSON_EXTRACT(custom_metrics, '$.privacy.fingerprinting.counts'))) AS fingerprinting_type WHERE date = '2024-06-01'
 )
 SELECT client, fingerprinting_type, count(DISTINCT page) AS page_count FROM pages GROUP BY client, fingerprinting_type ORDER BY page_count DESC
