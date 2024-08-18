0ba12254fb24f778a671a262f3a69174ccb68495
Lucia Harcegová <lucia.harcegova@gmail.com>
2024-08-18 16:33:20 +0200

    Update cdn_adoption query

---
+++ sql/2024/sustainability/cdn_adoption.sql
@@ -0,0 +1,30 @@
+#standardSQL
+# The distribution of CDN adoption on websites by client.
+
+SELECT
+  client,
+  IF(cdn = '', 'No CDN', cdn) AS cdn,
+  COUNT(0) AS freq,
+  total,
+  COUNT(0) / total AS pct
+FROM (
+  SELECT
+    client,
+    COUNT(0) AS total,
+    ARRAY_CONCAT_AGG(SPLIT(JSON_EXTRACT_SCALAR(summary, '$.cdn'), ', ')) AS cdn_list
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01'
+  GROUP BY
+    client
+),
+UNNEST(cdn_list) AS cdn
+GROUP BY
+  client,
+  cdn,
+  total
+ORDER BY
+  pct DESC,
+  client,
+  cdn;
