e8a032a3ce6311820f966135c8a6df1960c4057a
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-08-19 01:15:03 +0200

    queries and notebook updates

--- sql/2024/privacy/number_of_websites_with_iab.sql
+++ sql/2024/privacy/number_of_websites_with_iab.sql
@@ -1,4 +1,4 @@
-# Counts of pages with IAB Frameworks (Transparency & Consent / US Privacy)
+# Counts of pages with IAB Frameworks
 
 WITH privacy_custom_metrics_data AS (
   SELECT
@@ -12,44 +12,49 @@ WITH privacy_custom_metrics_data AS (
 
 SELECT
   client,
-  number_of_pages_with_iab_tcf_v1 / number_of_pages AS pct_pages_with_iab_tcf_v1,
-  number_of_pages_with_iab_tcf_v1,
-  number_of_pages_with_iab_tcf_v2 / number_of_pages AS pct_pages_with_iab_tcf_v2,
-  number_of_pages_with_iab_tcf_v2,
-  number_of_pages_with_iab_usp / number_of_pages AS pct_pages_with_iab_usp,
-  number_of_pages_with_iab_usp,
-  number_of_pages_with_iab_tcf_any / number_of_pages AS pct_pages_with_iab_tcf_any,
-  number_of_pages_with_iab_tcf_any,
-  number_of_pages_with_iab_any / number_of_pages AS pct_pages_with_iab_any,
-  number_of_pages_with_iab_any,
-  number_of_pages_with_iab_tcf_v1_compliant / number_of_pages_with_iab_tcf_v1 AS pct_pages_with_iab_tcf_v1_compliant,
-  number_of_pages_with_iab_tcf_v1_compliant,
-  number_of_pages_with_iab_tcf_v2_compliant / number_of_pages_with_iab_tcf_v2 AS pct_pages_with_iab_tcf_v2_compliant,
-  number_of_pages_with_iab_tcf_v2_compliant
+  number_of_pages_with_tcfv1 / number_of_pages AS pct_pages_with_tcfv1,
+  number_of_pages_with_tcfv1,
+  number_of_pages_with_tcfv2 / number_of_pages AS pct_pages_with_tcfv2,
+  number_of_pages_with_tcfv2,
+  number_of_pages_with_usp / number_of_pages AS pct_pages_with_usp,
+  number_of_pages_with_usp,
+  number_of_pages_with_tcf / number_of_pages AS pct_pages_with_tcf,
+  number_of_pages_with_tcf,
+  number_of_pages_with_any / number_of_pages AS pct_pages_with_any,
+  number_of_pages_with_any,
+  number_of_pages_with_tcfv1_compliant / number_of_pages AS pct_pages_with_tcfv1_compliant,
+  number_of_pages_with_tcfv1_compliant,
+  number_of_pages_with_tcfv2_compliant / number_of_pages AS pct_pages_with_tcfv2_compliant,
+  number_of_pages_with_tcfv2_compliant,
+  number_of_pages_with_gpp / number_of_pages AS pct_pages_with_gpp,
+  number_of_pages_with_gpp,
+  number_of_pages_with_gpp_data / number_of_pages AS pct_pages_with_gpp_data,
+  number_of_pages_with_gpp_data
 FROM (
   SELECT
     client,
     COUNT(0) AS number_of_pages,
-    COUNTIF(JSON_VALUE(metrics, '$.iab_tcf_v1.present') = 'true') AS number_of_pages_with_iab_tcf_v1,
-    COUNTIF(JSON_VALUE(metrics, '$.iab_tcf_v2.present') = 'true') AS number_of_pages_with_iab_tcf_v2,
-    COUNTIF(JSON_VALUE(metrics, '$.iab_usp.present') = 'true') AS number_of_pages_with_iab_usp,
-    COUNTIF(
-      JSON_VALUE(metrics, '$.iab_tcf_v1.present') = 'true' OR
-      JSON_VALUE(metrics, '$.iab_tcf_v2.present') = 'true'
-    ) AS number_of_pages_with_iab_tcf_any,
-    COUNTIF(
-      JSON_VALUE(metrics, '$.iab_tcf_v1.present') = 'true' OR
-      JSON_VALUE(metrics, '$.iab_tcf_v2.present') = 'true' OR
-      JSON_VALUE(metrics, '$.iab_usp.present') = 'true'
-    ) AS number_of_pages_with_iab_any,
-    COUNTIF(
-      JSON_VALUE(metrics, '$.iab_tcf_v1.present') = 'true' AND
-      JSON_VALUE(metrics, '$.iab_tcf_v1.compliant_setup') = 'true'
-    ) AS number_of_pages_with_iab_tcf_v1_compliant,
-    COUNTIF(
-      JSON_VALUE(metrics, '$.iab_tcf_v2.present') = 'true' AND
-      JSON_VALUE(metrics, '$.iab_tcf_v2.compliant_setup') = 'true'
-    ) AS number_of_pages_with_iab_tcf_v2_compliant
-  FROM privacy_custom_metrics_data
+    COUNTIF(tcfv1) AS number_of_pages_with_tcfv1,
+    COUNTIF(tcfv2) AS number_of_pages_with_tcfv2,
+    COUNTIF(usp) AS number_of_pages_with_usp,
+    COUNTIF(tcfv1 OR tcfv2) AS number_of_pages_with_tcf,
+    COUNTIF(tcfv1 OR tcfv2 or usp or gpp) AS number_of_pages_with_any,
+    COUNTIF(tcfv1 AND tcfv1_compliant) AS number_of_pages_with_tcfv1_compliant,
+    COUNTIF(tcfv2 AND tcfv2_compliant) AS number_of_pages_with_tcfv2_compliant,
+    COUNTIF(gpp) AS number_of_pages_with_gpp,
+    COUNTIF(gpp_data) AS number_of_pages_with_gpp_data,
+  FROM (
+    SELECT
+      client,
+      JSON_VALUE(metrics, '$.iab_tcf_v1.present') = 'true' AS tcfv1,
+      JSON_VALUE(metrics, '$.iab_tcf_v2.present') = 'true' AS tcfv2,
+      JSON_VALUE(metrics, '$.iab_gpp.present') = 'true' AS gpp,
+      JSON_VALUE(metrics, '$.iab_usp.present') ='true' AS usp,
+      JSON_VALUE(metrics, '$.iab_tcf_v1.compliant_setup') = 'true' AS tcfv1_compliant,
+      JSON_VALUE(metrics, '$.iab_tcf_v2.compliant_setup') = 'true' AS tcfv2_compliant,
+      JSON_VALUE(metrics, '$.iab_gpp.data') IS NOT NULL AS gpp_data
+    FROM
+      privacy_custom_metrics_data
+  )
   GROUP BY client
 )
