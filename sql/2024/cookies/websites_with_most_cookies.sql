52b275c8ccce60bb4ed279de6292ae52a9d9800e
Yohan Beugin <yohan@beugin.org>
2024-08-19 18:13:45 -0500

    websites with most cookies

---
+++ sql/2024/cookies/websites_with_most_cookies.sql
@@ -0,0 +1,27 @@
+-- Extract top 100 websites that have the most first party cookies set on them
+
+WITH top_websites AS (
+    SELECT
+        client,
+        first_party_host,
+        COUNT(DISTINCT CONCAT(name, domain)) as distinct_cookie_count
+    FROM `httparchive.almanac.2024-06-01_top10k_cookies`
+    GROUP BY client, first_party_host
+),
+top_numbered_websites AS (
+    SELECT
+        client,
+        first_party_host,
+        distinct_cookie_count,
+        ROW_NUMBER() over (PARTITION BY client ORDER BY distinct_cookie_count DESC) AS row_num
+    FROM top_websites
+)
+
+SELECT 
+    client,
+    first_party_host,
+    distinct_cookie_count,
+    row_num
+FROM top_numbered_websites
+WHERE row_num <= 100
+ORDER BY client, row_num
