6f2b26b2395bf6d5bb6323ed6279ba5310c0b94e
Mike Gifford <mike.gifford@civicactions.com>
2024-08-22 17:41:12 +0200

    Create common_alt_text_length.sql
    Seems to work..

---
+++ sql/2024/accessibility/common_alt_text_length.sql
@@ -0,0 +1,36 @@
+#standardSQL
+# Most common lengths of alt text
+# Note: A value of -1 means there is no alt tag. 0 means it is empty
+# Note: Lengths of 2000+ characters are grouped together
+SELECT
+  client,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total_images,
+  SUM(COUNTIF(alt_length_clipped >= 0)) OVER (PARTITION BY client) AS total_alt_tags,
+
+  alt_length_clipped AS alt_length,
+  COUNT(0) AS occurrences,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct_all_occurrences
+FROM (
+  SELECT
+    client,
+    LEAST(alt_length, 2000) AS alt_length_clipped
+  FROM (
+    SELECT
+      client,
+      date,
+      SAFE_CAST(alt_length_string AS INT64) AS alt_length
+    FROM
+      `httparchive.all.pages`,
+      UNNEST(
+        JSON_EXTRACT_ARRAY(JSON_EXTRACT_SCALAR(payload, '$._almanac'), '$.images.alt_lengths')
+      ) AS alt_length_string
+  )
+  WHERE
+    date = '2024-06-01' AND
+    alt_length IS NOT NULL
+)
+GROUP BY
+  client,
+  alt_length
+ORDER BY
+  alt_length ASC
