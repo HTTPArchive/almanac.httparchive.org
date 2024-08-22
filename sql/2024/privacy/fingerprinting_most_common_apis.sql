fda33dd3ce2686bf7eb435bffc8ed4f63affa782
bstandaert-wustl <b.g.standaert@wustl.edu>
2024-08-22 14:21:34 -0500

    cookies, ccpa, fingerprinting: calculate percent of total pages

--- sql/2024/privacy/fingerprinting_most_common_apis.sql
+++ sql/2024/privacy/fingerprinting_most_common_apis.sql
@@ -13,7 +13,7 @@ if (input) {
 """;
 
 WITH pages AS (
-  SELECT client, page, fingerprinting_type FROM `httparchive.all.pages`,
+  SELECT client, page, fingerprinting_type, count(DISTINCT page) OVER (PARTITION BY client) AS total_pages FROM `httparchive.all.pages`, --TABLESAMPLE SYSTEM (0.01 PERCENT)
     unnest(getFingerprintingTypes(JSON_EXTRACT(custom_metrics, '$.privacy.fingerprinting.counts'))) AS fingerprinting_type WHERE date = '2024-06-01'
 )
-SELECT client, fingerprinting_type, count(DISTINCT page) AS page_count FROM pages GROUP BY client, fingerprinting_type ORDER BY page_count DESC
+SELECT client, fingerprinting_type, count(DISTINCT page) AS page_count, count(DISTINCT page) / any_value(total_pages) AS pct_pages FROM pages GROUP BY client, fingerprinting_type ORDER BY page_count DESC
--- sql/2024/privacy/fingerprinting_script_count.sql
+++ sql/2024/privacy/fingerprinting_script_count.sql
@@ -1,1 +1,5 @@
-SELECT client, array_length(JSON_QUERY_ARRAY(custom_metrics, '$.privacy.fingerprinting.likelyFingerprintingScripts')) AS script_count, count(DISTINCT page) AS page_count FROM `httparchive.all.pages` WHERE date = '2024-06-01' GROUP BY script_count, client ORDER BY script_count ASC;
+WITH pages AS (
+  SELECT page, client, array_length(JSON_QUERY_ARRAY(custom_metrics, '$.privacy.fingerprinting.likelyFingerprintingScripts')) AS script_count, count(DISTINCT page) OVER (PARTITION BY client) AS total_pages FROM `httparchive.all.pages` --TABLESAMPLE SYSTEM (0.01 PERCENT)
+  WHERE date = '2024-06-01'
+)
+SELECT script_count, client, count(DISTINCT page) AS page_count, count(DISTINCT page) / any_value(total_pages) AS pct_pages FROM pages GROUP BY script_count, client ORDER BY script_count ASC;
