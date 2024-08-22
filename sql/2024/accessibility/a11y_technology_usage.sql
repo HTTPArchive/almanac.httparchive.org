246d7a0d37003597c20902d10e83716ae3dd7cdf
Mike Gifford <mike.gifford@civicactions.com>
2024-08-22 16:33:04 +0200

    Create a11y_technology_usage.sql
    This seems to work.

---
+++ sql/2024/accessibility/a11y_technology_usage.sql
@@ -0,0 +1,18 @@
+#standardSQL
+# Accessibility (A11y) technology, ie. Overlays, usage by client
+  
+SELECT
+  client,  # Client domain
+  COUNT(DISTINCT page) AS total_sites,  # Total number of unique sites for the client
+  COUNT(DISTINCT IF(category = 'Accessibility', page, NULL)) AS sites_with_a11y_tech,  # Number of unique sites that use accessibility technology
+  COUNT(DISTINCT IF(category = 'Accessibility', page, NULL)) / COUNT(DISTINCT page) AS pct_sites_with_a11y_tech  # Percentage of sites using accessibility technology
+FROM
+  `httparchive.all.pages`,
+  UNNEST(technologies) AS tech,
+  UNNEST(categories) AS category
+WHERE
+  date = '2024-06-01' # Specific date for data extraction
+GROUP BY
+  client  # Group by client domain
+ORDER BY
+  client;  # Order results by client domain
