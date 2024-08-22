cbcb2a7ea833de7fad881ca7afa689112bf6ce27
JannisBush <33023300+JannisBush@users.noreply.github.com>
2024-08-22 12:43:15 +0200

    Fix typo

--- sql/2024/security/fp_header_prevalence.sql
+++ sql/2024/security/fp_header_prevalence.sql
@@ -4,15 +4,15 @@
 # Note: Considers headers of main document responses
 SELECT
   client,
-  coop_header,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_coop_headers,
+  fp_header,
+  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_fp_headers,
   COUNT(DISTINCT host) AS freq,
   COUNT(DISTINCT host) / SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS pct
 FROM (
   SELECT
     client,
     NET.HOST(url) AS host,
-    response_headers.value AS coop_header
+    response_headers.value AS fp_header
   FROM
     `httparchive.all.requests`,
     UNNEST(response_headers) AS response_headers
@@ -23,7 +23,7 @@ FROM (
     LOWER(response_headers.name) = 'feature-policy')
 GROUP BY
   client,
-  coop_header
+  fp_header
 ORDER BY
   pct DESC
 LIMIT
--- sql/2024/security/pp_header_prevalence.sql
+++ sql/2024/security/pp_header_prevalence.sql
@@ -4,15 +4,15 @@
 # Note: Considers headers of main document responses
 SELECT
   client,
-  coop_header,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_coop_headers,
+  pp_header,
+  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_pp_headers,
   COUNT(DISTINCT host) AS freq,
   COUNT(DISTINCT host) / SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS pct
 FROM (
   SELECT
     client,
     NET.HOST(url) AS host,
-    response_headers.value AS coop_header
+    response_headers.value AS pp_header
   FROM
     `httparchive.all.requests`,
     UNNEST(response_headers) AS response_headers
@@ -23,7 +23,7 @@ FROM (
     LOWER(response_headers.name) = 'permissions-policy')
 GROUP BY
   client,
-  coop_header
+  pp_header
 ORDER BY
   pct DESC
 LIMIT
