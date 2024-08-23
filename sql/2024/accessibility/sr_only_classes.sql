e2ec46b239642cbd545692fd78923c8204aa8e0b
Mike Gifford <mike.gifford@civicactions.com>
2024-08-23 21:49:00 +0200

    Update sr_only_classes.sql
    is_root_page

--- sql/2024/accessibility/sr_only_classes.sql
+++ sql/2024/accessibility/sr_only_classes.sql
@@ -1,13 +1,16 @@
 #standardSQL
 # Sites using sr-only or visually-hidden classes
+
 SELECT
   client,
+  is_root_page,
   COUNT(0) AS total_sites,
   COUNTIF(uses_sr_only) AS sites_with_sr_only,
   COUNTIF(uses_sr_only) / COUNT(0) AS pct_sites_with_sr_only
 FROM (
   SELECT
     client,
+    is_root_page,
     CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._a11y'), '$.screen_reader_classes') AS BOOL) AS uses_sr_only
   FROM
     `httparchive.all.pages`
@@ -15,4 +18,5 @@ FROM (
     date = '2024-06-01'
 )
 GROUP BY
-  client
+  client,
+  is_root_page;
