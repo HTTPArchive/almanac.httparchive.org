3940084d3c1e9d7da1486825f15b9f767f8b4bfd
Mike Gifford <mike.gifford@civicactions.com>
2024-08-23 21:12:57 +0200

    Update anchors_with_role_button.sql

--- sql/2024/accessibility/anchors_with_role_button.sql
+++ sql/2024/accessibility/anchors_with_role_button.sql
@@ -2,6 +2,7 @@
 # Anchors with role='button'
 SELECT
   client,
+  is_root_page,
   COUNTIF(total_anchors > 0) AS sites_with_anchors,
   COUNTIF(total_anchors_with_role_button > 0) AS sites_with_anchor_role_button,
 
@@ -10,6 +11,7 @@ SELECT
 FROM (
   SELECT
     client,
+    is_root_page,
     date,
     CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._a11y'), '$.total_anchors_with_role_button') AS INT64) AS total_anchors_with_role_button,
     IFNULL(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._element_count'), '$.a') AS INT64), 0) AS total_anchors
@@ -19,4 +21,5 @@ FROM (
 WHERE
   date = '2024-06-01'
 GROUP BY
-  client
+  client,
+  is_root_page;
