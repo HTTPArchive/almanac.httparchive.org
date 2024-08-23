b065255551a76a5bbcbbdad33e2a288f52a93ffe
Mike Gifford <mike.gifford@civicactions.com>
2024-08-23 21:50:28 +0200

    Update tabindex_usage_and_values.sql
    is_root_page

--- sql/2024/accessibility/tabindex_usage_and_values.sql
+++ sql/2024/accessibility/tabindex_usage_and_values.sql
@@ -1,5 +1,6 @@
 #standardSQL
 # Positive tabindex value occurrences
+
 CREATE TEMPORARY FUNCTION getTotalPositiveTabIndexes(payload STRING)
 RETURNS STRUCT<total INT64, total_positive INT64, total_negative INT64, total_zero INT64> LANGUAGE js AS '''
 try {
@@ -29,19 +30,20 @@ try {
 
 SELECT
   client,
+  is_root_page,
   COUNT(0) AS total_sites,
   COUNTIF(tab_index_stats.total > 0) AS total_with_tab_indexes,
   COUNTIF(tab_index_stats.total_positive > 0) AS total_with_positive_tab_indexes,
   COUNTIF(tab_index_stats.total_negative > 0) AS total_with_negative_tab_indexes,
   COUNTIF(tab_index_stats.total_zero > 0) AS total_with_zero_tab_indexes,
   COUNTIF(tab_index_stats.total_negative > 0 OR tab_index_stats.total_zero > 0) AS total_with_negative_or_zero,
-
   COUNTIF(tab_index_stats.total > 0) / COUNT(0) AS pct_with_tab_indexes,
   COUNTIF(tab_index_stats.total_positive > 0) / COUNT(0) AS pct_with_positive_tab_indexes,
   COUNTIF(tab_index_stats.total_positive > 0) / COUNTIF(tab_index_stats.total > 0) AS pct_positive_in_sites_with_tab_indexes
 FROM (
   SELECT
     client,
+    is_root_page,
     getTotalPositiveTabIndexes(JSON_EXTRACT_SCALAR(payload, '$._almanac')) AS tab_index_stats
   FROM
     `httparchive.all.pages`
@@ -49,4 +51,5 @@ FROM (
     date = '2024-06-01'
 )
 GROUP BY
-  client
+  client,
+  is_root_page;
