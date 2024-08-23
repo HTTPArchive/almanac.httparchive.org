cd375a42b467fc3634e0b6ca782ccb095143f4a4
Mike Gifford <mike.gifford@civicactions.com>
2024-08-23 06:54:50 +0200

    Create sr_only_classes.sql
    I think this probably could be extended to include other screen reader classes which are common.

---
+++ sql/2024/accessibility/sr_only_classes.sql
@@ -0,0 +1,18 @@
+#standardSQL
+# Sites using sr-only or visually-hidden classes
+SELECT
+  client,
+  COUNT(0) AS total_sites,
+  COUNTIF(uses_sr_only) AS sites_with_sr_only,
+  COUNTIF(uses_sr_only) / COUNT(0) AS pct_sites_with_sr_only
+FROM (
+  SELECT
+    client,
+    CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._a11y'), '$.screen_reader_classes') AS BOOL) AS uses_sr_only
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01'
+)
+GROUP BY
+  client
