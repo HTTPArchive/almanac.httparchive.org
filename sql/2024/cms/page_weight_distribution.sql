4c096b5919b9bf8b4dd8ba651f514d160a1be16f
Nurullah Demir <13475745+nrllh@users.noreply.github.com>
2024-08-24 14:14:32 +0200

    Update page_weight_distribution.sql

--- sql/2024/cms/page_weight_distribution.sql
+++ sql/2024/cms/page_weight_distribution.sql
@@ -1,29 +1,38 @@
-#standardSQL
-# Total page weight distribution by CMS
+#standardSQL 
+# Total page weight distribution BY CMS
+# page_weight_distribution.sql
 SELECT
   percentile,
   client,
   cms,
   COUNT(0) AS pages,
-  APPROX_QUANTILES(total_kb, 1000)[OFFSET(percentile * 10)] AS total_kb
+  APPROX_QUANTILES(total_kb, 1000)[
+OFFSET
+  (percentile * 10)] AS total_kb
 FROM (
-  SELECT DISTINCT
-    _TABLE_SUFFIX AS client,
-    url,
-    app AS cms
+  SELECT
+    DISTINCT client,
+    page AS url,
+    technologies.technology AS cms
   FROM
-    `httparchive.technologies.2024_06_01_*`
+    `httparchive.all.pages`,
+    UNNEST (technologies) AS technologies,
+    UNNEST(technologies.categories) AS cats
   WHERE
-    category = 'CMS')
+    cats = 'CMS'
+    AND date="2024-06-01")
 JOIN (
   SELECT
-    _TABLE_SUFFIX AS client,
-    url,
-    bytesTotal / 1024 AS total_kb
+    client,
+    page AS url,
+    cast(json_value(summary, "$.bytesTotal") as int64) / 1024 AS total_kb
   FROM
-    `httparchive.summary_pages.2024_06_01_*`)
+    `httparchive.all.pages`
+  WHERE
+    date="2024-06-01")
 USING
-  (client, url),
+  (client,
+    url),
   UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
 GROUP BY
   percentile,
