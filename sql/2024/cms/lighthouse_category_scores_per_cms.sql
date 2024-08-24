712a594aa81715bd6dfb1b1f89ac7a8504f6319e
Nurullah Demir <13475745+nrllh@users.noreply.github.com>
2024-08-24 14:07:19 +0200

    Update lighthouse_category_scores_per_cms.sql

--- sql/2024/cms/lighthouse_category_scores_per_cms.sql
+++ sql/2024/cms/lighthouse_category_scores_per_cms.sql
@@ -1,31 +1,46 @@
-#standardSQL
+#standardSQL 
 # Lighthouse category scores per CMS
+# lighthouse_category_scores_per_cms.sql
 SELECT
   client,
   cms,
   COUNT(DISTINCT url) AS freq,
-  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.performance.score') AS NUMERIC), 1000)[OFFSET(500)] * 100 AS median_performance,
-  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.accessibility.score') AS NUMERIC), 1000)[OFFSET(500)] * 100 AS median_accessibility,
-  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.seo.score') AS NUMERIC), 1000)[OFFSET(500)] * 100 AS median_seo,
-  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$."best-practices".score') AS NUMERIC), 1000)[OFFSET(500)] * 100 AS median_best_practices
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.performance.score') AS NUMERIC), 1000)[
+OFFSET
+  (500)] * 100 AS median_performance,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.accessibility.score') AS NUMERIC), 1000)[
+OFFSET
+  (500)] * 100 AS median_accessibility,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.seo.score') AS NUMERIC), 1000)[
+OFFSET
+  (500)] * 100 AS median_seo,
+  APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$."best-practices".score') AS NUMERIC), 1000)[
+OFFSET
+  (500)] * 100 AS median_best_practices
 FROM (
   SELECT
-    _TABLE_SUFFIX AS client,
-    url,
-    JSON_EXTRACT(report, '$.categories') AS categories
+    client,
+    page AS url,
+    JSON_EXTRACT(lighthouse, '$.categories') AS categories
   FROM
-    `httparchive.lighthouse.2024_06_01_*`)
-JOIN (
-  SELECT DISTINCT
-    _TABLE_SUFFIX AS client,
-    app AS cms,
-    url
-  FROM
-    `httparchive.technologies.2024_06_01_*`
+    `httparchive.all.pages`
   WHERE
-    category = 'CMS')
+    date="2024-06-01")
+JOIN (
+  SELECT
+    DISTINCT client,
+    technologies.technology AS cms,
+    page AS url
+  FROM
+    `httparchive.all.pages`,
+    UNNEST (technologies) AS technologies,
+    UNNEST(technologies.categories) AS cats
+  WHERE
+    cats = 'CMS'
+    AND date ="2024-06-01")
 USING
-  (url, client)
+  (url,
+    client)
 GROUP BY
   client,
   cms
