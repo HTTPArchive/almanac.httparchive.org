264e0c40148a4fa60608139fed020a9f64f874f3
Nurullah Demir <13475745+nrllh@users.noreply.github.com>
2024-08-24 12:35:19 +0200

    Update core_web_vitals_yoy.sql

--- sql/2024/cms/core_web_vitals_yoy.sql
+++ sql/2024/cms/core_web_vitals_yoy.sql
@@ -1,4 +1,5 @@
 # cms passing core web vitals
+# core_web_vitals_yoy.sql
 CREATE TEMP FUNCTION IS_GOOD (good FLOAT64, needs_improvement FLOAT64, poor FLOAT64) RETURNS BOOL AS (
   good / (good + needs_improvement + poor) >= 0.75
 );
@@ -17,21 +18,22 @@ SELECT
     COUNT(DISTINCT IF(IS_GOOD(fast_lcp, avg_lcp, slow_lcp), origin, NULL)),
     COUNT(DISTINCT IF(IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp), origin, NULL))) AS pct_good_lcp,
 
-  # Origins with good FID divided by origins with any FID.
+  # Origins with good INP divided by origins with any INP.
   SAFE_DIVIDE(
-    COUNT(DISTINCT IF(IS_GOOD(fast_fid, avg_fid, slow_fid), origin, NULL)),
-    COUNT(DISTINCT IF(IS_NON_ZERO(fast_fid, avg_fid, slow_fid), origin, NULL))) AS pct_good_fid,
+    COUNT(DISTINCT IF(IS_GOOD(fast_inp, avg_inp, slow_inp), origin, NULL)),
+    COUNT(DISTINCT IF(IS_NON_ZERO(fast_inp, avg_inp, slow_inp), origin, NULL))) AS pct_good_fid,
+    
 
   # Origins with good CLS divided by origins with any CLS.
   SAFE_DIVIDE(
     COUNT(DISTINCT IF(IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL)),
     COUNT(DISTINCT IF(IS_NON_ZERO(small_cls, medium_cls, large_cls), origin, NULL))) AS pct_good_cls,
 
-  # Origins with good LCP, FID (optional), and CLS divided by origins with any LCP and CLS.
+  # Origins with good LCP, INP (optional), and CLS divided by origins with any LCP and CLS.
   SAFE_DIVIDE(
     COUNT(DISTINCT IF(
       IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AND
-      IS_GOOD(fast_fid, avg_fid, slow_fid) IS NOT FALSE AND
+      IS_GOOD(fast_inp, avg_inp, slow_inp) IS NOT FALSE AND
       IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL)),
     COUNT(DISTINCT IF(
       IS_NON_ZERO(fast_lcp, avg_lcp, slow_lcp) AND
@@ -40,13 +42,16 @@ FROM
   `chrome-ux-report.materialized.device_summary`
 JOIN (
   SELECT
-    _TABLE_SUFFIX AS client,
-    url,
-    app AS cms
+    client,
+    page url,
+    technologies.technology AS cms
   FROM
-    `httparchive.technologies.2024_06_01_*`
+    `httparchive.all.pages`,
+    UNNEST (technologies) AS technologies,
+    UNNEST(technologies.categories) AS cats
   WHERE
-    category = 'CMS')
+    cats = 'CMS'
+    and date ="2024-06-01")
 ON
   CONCAT(origin, '/') = url AND
   IF(device = 'desktop', 'desktop', 'mobile') = client
@@ -92,13 +97,16 @@ FROM
   `chrome-ux-report.materialized.device_summary`
 JOIN (
   SELECT
-    _TABLE_SUFFIX AS client,
-    url,
-    app AS cms
+    client,
+    page as url,
+    technologies.technology AS cms
   FROM
-    `httparchive.technologies.2023_06_01_*`
+    `httparchive.all.pages`,
+    UNNEST (technologies) AS technologies,
+    UNNEST(technologies.categories) AS cats
   WHERE
-    category = 'CMS')
+    cats = 'CMS'
+    and date ="2024-06-01")
 ON
   CONCAT(origin, '/') = url AND
   IF(device = 'desktop', 'desktop', 'mobile') = client
@@ -144,13 +152,16 @@ FROM
   `chrome-ux-report.materialized.device_summary`
 JOIN (
   SELECT
-    _TABLE_SUFFIX AS client,
-    url,
-    app AS cms
+    client,
+    page as url,
+    technologies.technology AS cms
   FROM
-    `httparchive.technologies.2022_06_01_*`
+  `httparchive.all.pages`,
+    UNNEST (technologies) AS technologies,
+    UNNEST(technologies.categories) AS cats
   WHERE
-    category = 'CMS')
+    cats = 'CMS'
+    and date ="2024-06-01")
 ON
   CONCAT(origin, '/') = url AND
   IF(device = 'desktop', 'desktop', 'mobile') = client
