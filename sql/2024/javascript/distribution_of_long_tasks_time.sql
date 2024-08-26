90db4ad1d076d374cecbe38ee514ec62cffb2eab
Mike Gifford <mike.gifford@civicactions.com>
2024-08-26 17:44:17 +0200

    Update distribution_of_long_tasks_time.sql
    adjusting for linter

--- sql/2024/javascript/distribution_of_long_tasks_time.sql
+++ sql/2024/javascript/distribution_of_long_tasks_time.sql
@@ -22,12 +22,13 @@ try {
 WITH long_tasks_pages AS (
   SELECT
     client,
-    page as url,
+    page AS url,
     _longTasks AS long_tasks
   FROM
     `httparchive.all.pages`,
     UNNEST(getLongTasks(payload)) AS _longTasks
-    where date = "2024-06-01"
+    WHERE
+      date = '2024-06-01'
 ),
 
 long_tasks_by_page AS (
