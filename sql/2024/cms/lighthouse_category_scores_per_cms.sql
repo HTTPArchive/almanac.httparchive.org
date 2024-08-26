bd734bd3288523efb0907cf29c60203fd6e71652
Mike Gifford <mike.gifford@civicactions.com>
2024-08-26 21:07:49 +0200

    Update lighthouse_category_scores_per_cms.sql
    Linting...

--- sql/2024/cms/lighthouse_category_scores_per_cms.sql
+++ sql/2024/cms/lighthouse_category_scores_per_cms.sql
@@ -6,13 +6,13 @@ SELECT
   cms,
   COUNT(DISTINCT url) AS freq,
   APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.performance.score') AS NUMERIC), 1000)[
-  OFFSET(500)] * 100 AS median_performance,
+    OFFSET(500)] * 100 AS median_performance,
   APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.accessibility.score') AS NUMERIC), 1000)[
-  OFFSET(500)] * 100 AS median_accessibility,
+    OFFSET(500)] * 100 AS median_accessibility,
   APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$.seo.score') AS NUMERIC), 1000)[
-  OFFSET(500)] * 100 AS median_seo,
+    OFFSET(500)] * 100 AS median_seo,
   APPROX_QUANTILES(CAST(JSON_VALUE(categories, '$."best-practices".score') AS NUMERIC), 1000)[
-  OFFSET(500)] * 100 AS median_best_practices
+    OFFSET(500)] * 100 AS median_best_practices
 FROM (
   SELECT
     client,
@@ -24,13 +24,13 @@ FROM (
     date = '2024-06-01'
 )
 JOIN (
-  SELECT
-    DISTINCT client,
+  SELECT DISTINCT 
+    client,
     technologies.technology AS cms,
     page AS url
   FROM
     `httparchive.all.pages`,
-    UNNEST (technologies) AS technologies,
+    UNNEST(technologies) AS technologies,
     UNNEST(technologies.categories) AS cats
   WHERE
     cats = 'CMS' AND
