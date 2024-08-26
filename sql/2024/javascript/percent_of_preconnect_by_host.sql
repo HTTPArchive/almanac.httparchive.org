12c9dd391c7e097c21b28e8a37c2ad7aa85e3bac
Mike Gifford <mike.gifford@civicactions.com>
2024-08-26 17:17:45 +0200

    Update percent_of_preconnect_by_host.sql
    Fixing linter errors

--- sql/2024/javascript/percent_of_preconnect_by_host.sql
+++ sql/2024/javascript/percent_of_preconnect_by_host.sql
@@ -20,13 +20,14 @@ WITH resource_hints AS (
     host
   FROM (
     SELECT
-       client,
-       page,
+      client,
+      page,
       NET.HOST(href) AS host
     FROM
       `httparchive.all.pages`,
       UNNEST(getResourceHintsHrefs(payload, 'preconnect')) AS href
-      where date = "2024-06-01"
+    WHERE
+      date = "2024-06-01"
   )
   GROUP BY
     client,
