42e4e44f8df0f32dca9bce9416be7b2f660c510c
Mike Gifford <mike.gifford@civicactions.com>
2024-08-26 18:04:03 +0200

    Update web_components_specs.sql
    Linter

--- sql/2024/javascript/web_components_specs.sql
+++ sql/2024/javascript/web_components_specs.sql
@@ -12,8 +12,10 @@ FROM (
     ARRAY_LENGTH(JSON_EXTRACT_ARRAY(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.web_component_specs.shadow_roots')) AS shadow_roots,
     ARRAY_LENGTH(JSON_EXTRACT_ARRAY(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.web_component_specs.template')) AS template
   FROM
-    `httparchive.all.pages` where
-    date='2024-06-01'),
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01'
+  ),
   UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
 GROUP BY
   percentile,
