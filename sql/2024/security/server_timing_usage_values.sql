4ec01b6b8ec968e9ff8c7e5101c6101a555bcfb6
JannisBush <33023300+JannisBush@users.noreply.github.com>
2024-08-27 15:06:10 +0200

    FIx st query + add sampling data

--- sql/2024/security/server_timing_usage_values.sql
+++ sql/2024/security/server_timing_usage_values.sql
@@ -49,22 +49,28 @@ WITH parsed_server_timing AS (
   SELECT
     client,
     NET.HOST(url) AS host,
+    COUNT(0) OVER (PARTITION BY CLIENT) AS total_st, # Total number of ST header, if a response has more than one ST header each of them is counted
+    COUNT(DISTINCT NET.HOST(url)) OVER (PARTITION BY CLIENT) as total_st_hosts,
     response_headers.value AS server_timing_header,
     JSON_EXTRACT(parseServerTiming(response_headers.value), "$.metric_names") AS metric_names,
     JSON_EXTRACT(parseServerTiming(response_headers.value), "$.dur") AS dur,
     JSON_EXTRACT(parseServerTiming(response_headers.value), "$.desc") AS desc1
   FROM
-    `httparchive.all.requests`,
+    #`httparchive.all.requests`,
+    `httparchive.sample_data.requests_1k`,
     UNNEST(response_headers) AS response_headers
   WHERE
-    date = '2024-06-01'
-    AND is_root_page
-    AND LOWER(response_headers.name) = 'server-timing'
+    # date = '2024-06-01' AND
+    date = '2024-08-01' AND
+    is_root_page AND
+    LOWER(response_headers.name) = 'server-timing'
 ), st_details AS (
 SELECT
   client,
   host,
   server_timing_header,
+  total_st,
+  total_st_hosts,
   metric_name,
   dur,
   desc1
@@ -79,34 +85,47 @@ WHERE
 ), totals AS (
   SELECT
     client,
-    COUNT(0) AS total
+    COUNT(0) AS total,
+    COUNT(DISTINCT NET.HOST(url)) AS total_hosts
   FROM
-    `httparchive.all.requests`
+    #`httparchive.all.requests`
+    `httparchive.sample_data.requests_1k`
   WHERE
-    date = '2024-06-01' AND
+    #date = '2024-06-01' AND
+    date = '2024-08-01' AND
     is_root_page
   GROUP BY
     client
 )
 
 # Most common headers
+# (Data already has one row for each metric (header split at comma) thus some headers occur multiple times)
+
 SELECT
   client,
   server_timing_header,
-  total AS total_responses,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_server_timing_headers,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) / total AS pct_server_timing,
-  COUNT(DISTINCT host) AS freq,
-  COUNT(host) AS freq_non_unique,
-  COUNT(DISTINCT host) / SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS pct_value,
   metric_name,
   dur,
   desc1,
+  total AS total_responses,
+  total_hosts,
+  total_st,
+  total_st_hosts,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total_st_metrics, # Counts the total number of metrics
+  total_st / total AS pct_server_timing,
+  total_st_hosts / total_hosts AS pct_server_timing_hosts,
+  COUNT(DISTINCT host) AS freq_host,
+  COUNT(0) AS freq_req,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct_value, # How common this exact "row" (i.e., server_timing_header or metric_name or dur or desc) is
+  COUNT(DISTINCT host) / total_st_hosts AS pct_hosts, # What is the percentage of hosts of all hosts that have a ST header that belong to this row's group
 FROM st_details
   JOIN totals USING (client)
 GROUP BY
   client,
   total,
+  total_st,
+  total_st_hosts,
+  total_hosts,
   server_timing_header,
   metric_name,
   dur,
@@ -116,23 +135,32 @@ ORDER BY
 LIMIT
   100
 
+
 # Most common metric names
 /*
 SELECT
   client,
-  metric_name
+  metric_name,
   total AS total_responses,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_server_timing_headers,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) / total AS pct_server_timing,
-  COUNT(DISTINCT host) AS freq,
-  COUNT(host) AS freq_non_unique,
-  COUNT(DISTINCT host) / SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS pct_value,
+  total_hosts,
+  total_st,
+  total_st_hosts,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total_st_metrics, # Counts the total number of metrics
+  total_st / total AS pct_server_timing,
+  total_st_hosts / total_hosts AS pct_server_timing_hosts,
+  COUNT(DISTINCT host) AS freq_host,
+  COUNT(0) AS freq_req,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct_value, # How common this exact "row" (i.e., server_timing_header or metric_name or dur or desc) is
+  COUNT(DISTINCT host) / total_st_hosts AS pct_hosts, # What is the percentage of hosts of all hosts that have a ST header that belong to this row's group
 FROM st_details
   JOIN totals USING (client)
 GROUP BY
   client,
   total,
-  metric_name,
+  total_st,
+  total_st_hosts,
+  total_hosts,
+  metric_name
 ORDER BY
   pct_value DESC
 LIMIT
@@ -145,16 +173,24 @@ SELECT
   client,
   dur,
   total AS total_responses,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_server_timing_headers,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) / total AS pct_server_timing,
-  COUNT(DISTINCT host) AS freq,
-  COUNT(host) AS freq_non_unique,
-  COUNT(DISTINCT host) / SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS pct_value,
+  total_hosts,
+  total_st,
+  total_st_hosts,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total_st_metrics, # Counts the total number of metrics
+  total_st / total AS pct_server_timing,
+  total_st_hosts / total_hosts AS pct_server_timing_hosts,
+  COUNT(DISTINCT host) AS freq_host,
+  COUNT(0) AS freq_req,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct_value, # How common this exact "row" (i.e., server_timing_header or metric_name or dur or desc) is
+  COUNT(DISTINCT host) / total_st_hosts AS pct_hosts, # What is the percentage of hosts of all hosts that have a ST header that belong to this row's group
 FROM st_details
   JOIN totals USING (client)
 GROUP BY
   client,
   total,
+  total_st,
+  total_st_hosts,
+  total_hosts,
   dur
 ORDER BY
   pct_value DESC
@@ -168,17 +204,24 @@ SELECT
   client,
   desc1,
   total AS total_responses,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_server_timing_headers,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) / total AS pct_server_timing,
-  COUNT(DISTINCT host) AS freq,
-  COUNT(host) AS freq_non_unique,
-  COUNT(DISTINCT host) / SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS pct_value,
-
+  total_hosts,
+  total_st,
+  total_st_hosts,
+  SUM(COUNT(0)) OVER (PARTITION BY client) AS total_st_metrics, # Counts the total number of metrics
+  total_st / total AS pct_server_timing,
+  total_st_hosts / total_hosts AS pct_server_timing_hosts,
+  COUNT(DISTINCT host) AS freq_host,
+  COUNT(0) AS freq_req,
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS pct_value, # How common this exact "row" (i.e., server_timing_header or metric_name or dur or desc) is
+  COUNT(DISTINCT host) / total_st_hosts AS pct_hosts, # What is the percentage of hosts of all hosts that have a ST header that belong to this row's group
 FROM st_details
   JOIN totals USING (client)
 GROUP BY
   client,
   total,
+  total_st,
+  total_st_hosts,
+  total_hosts,
   desc1
 ORDER BY
   pct_value DESC
