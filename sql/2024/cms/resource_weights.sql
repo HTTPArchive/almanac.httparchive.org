6d2a461924c2aa95a4baeadd665347217281481b
Nurullah Demir <13475745+nrllh@users.noreply.github.com>
2024-08-27 12:29:20 +0200

    Update resource_weights.sql

--- sql/2024/cms/resource_weights.sql
+++ sql/2024/cms/resource_weights.sql
@@ -1,5 +1,7 @@
 #standardSQL
 # Median resource weights by CMS
+#resource_weights.sql
+
 SELECT
   client,
   cms,
@@ -12,25 +14,30 @@ SELECT
   APPROX_QUANTILES(font_kb, 1000)[OFFSET(500)] AS median_font_kb
 FROM (
   SELECT DISTINCT
-    _TABLE_SUFFIX AS client,
-    url,
-    app AS cms
+    client,
+    page as url,
+    technologies.technology AS cms
   FROM
-    `httparchive.technologies.2024_06_01_*`
+    `httparchive.all.pages`,
+    UNNEST (technologies) AS technologies,
+    UNNEST(technologies.categories) AS cats
   WHERE
-    category = 'CMS')
+    cats = 'CMS'
+    AND date ='2024-06-01')
 JOIN (
   SELECT
-    _TABLE_SUFFIX AS client,
-    url,
-    bytesTotal / 1024 AS total_kb,
-    bytesHtml / 1024 AS html_kb,
-    bytesJS / 1024 AS js_kb,
-    bytesCSS / 1024 AS css_kb,
-    bytesImg / 1024 AS img_kb,
-    bytesFont / 1024 AS font_kb
+    client,
+    page as url,
+    cast(json_value(summary, "$.bytesTotal") as int64) / 1024 AS total_kb,
+    cast(json_value(summary, "$.bytesHtml") as int64)  / 1024 AS html_kb,
+    cast(json_value(summary, "$.bytesJS") as int64) / 1024 AS js_kb,
+    cast(json_value(summary, "$.bytesCSS") as int64) / 1024 AS css_kb,
+    cast(json_value(summary, "$.bytesImg") as int64) / 1024 AS img_kb,
+    cast(json_value(summary, "$.bytesFont") as int64) / 1024 AS font_kb
   FROM
-    `httparchive.summary_pages.2024_06_01_*`)
+    `httparchive.all.pages`
+  WHERE 
+    date = '2024-06-01')
 USING
   (client, url)
 GROUP BY
