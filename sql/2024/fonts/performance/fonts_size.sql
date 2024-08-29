545eb96e40e0f41069e6762cf28d3e74debfc6b6
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-08-29 14:04:33 +0200

    Break down performance/fonts_size.sql by format

--- sql/2024/fonts/performance/fonts_size.sql
+++ sql/2024/fonts/performance/fonts_size.sql
@@ -1,12 +1,18 @@
 -- Section: Performance
 -- Question: What is the distribution of the file size?
 
+-- INCLUDE ../common.sql
+
 WITH
 fonts AS (
   SELECT
     client,
     url,
-    AVG(PARSE_NUMERIC(JSON_EXTRACT_SCALAR(summary, '$.respBodySize'))) AS size
+    FILE_FORMAT(
+      JSON_EXTRACT_SCALAR(ANY_VALUE(summary), '$.ext'),
+      JSON_EXTRACT_SCALAR(ANY_VALUE(summary), '$.mimeType')
+    ) AS format,
+    PARSE_NUMERIC(JSON_EXTRACT_SCALAR(ANY_VALUE(summary), '$.respBodySize')) AS size
   FROM
     `httparchive.all.requests`
   WHERE
@@ -15,19 +21,38 @@ fonts AS (
   GROUP BY
     client,
     url
+),
+formats AS (
+  SELECT
+    client,
+    format,
+    ROW_NUMBER() OVER (PARTITION BY client ORDER BY COUNT(DISTINCT url) DESC) AS rank
+  FROM
+    fonts
+  GROUP BY
+    client,
+    format
 )
 
 SELECT
   client,
+  format,
   percentile,
   COUNT(DISTINCT url) AS count,
   ROUND(APPROX_QUANTILES(size, 1000)[OFFSET(percentile * 10)]) AS size
 FROM
   fonts,
   UNNEST([10, 25, 50, 75, 90, 99]) AS percentile
+INNER JOIN
+  formats USING (client, format)
+WHERE
+  rank <= 10
 GROUP BY
   client,
+  format,
+  rank,
   percentile
 ORDER BY
   client,
+  rank,
   percentile
