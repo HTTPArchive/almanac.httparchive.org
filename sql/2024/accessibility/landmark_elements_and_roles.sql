13f193ebeda22aa229c346e346a4189a684ecaa1
Mike Gifford <mike.gifford@civicactions.com>
2024-09-06 22:30:42 +0200

    Update landmark_elements_and_roles.sql
    Fixed query - Thanks to @tunetheweb

--- sql/2024/accessibility/landmark_elements_and_roles.sql
+++ sql/2024/accessibility/landmark_elements_and_roles.sql
@@ -14,10 +14,13 @@ try {
 CREATE TEMPORARY FUNCTION get_element_types(element_count_string STRING)
 RETURNS ARRAY<STRING> LANGUAGE js AS '''
 try {
-    if (!element_count_string) return [];
-    var element_count = JSON.parse(element_count_string);
+    if (!element_count_string) return []; // 2019 had a few cases
+
+    var element_count = JSON.parse(element_count_string); // should be an object with element type properties with values of how often they are present
+
     if (Array.isArray(element_count)) return [];
     if (typeof element_count != 'object') return [];
+
     return Object.keys(element_count);
 } catch (e) {
     return [];
@@ -38,118 +41,83 @@ elements AS (
   SELECT
     client,
     page,
-    is_root_page,
     element_type
   FROM
     `httparchive.all.pages`,
-    UNNEST(get_element_types(JSON_EXTRACT_SCALAR(payload, '$._element_count'))) AS element_type
+    UNNEST(get_element_types(JSON_EXTRACT(custom_metrics, '$.element_count'))) AS element_type
   JOIN
     mappings
   USING (element_type)
   WHERE
-    date = '2024-06-01'
+    date = '2024-06-01' AND
+    is_root_page
 ),
 
 roles AS (
   SELECT
     client,
     page,
-    is_root_page,
     role_type
   FROM
     `httparchive.all.pages`,
-    UNNEST(getUsedRoles(JSON_EXTRACT_SCALAR(payload, '$._almanac'))) AS role_type
+    UNNEST(getUsedRoles(JSON_EXTRACT(custom_metrics, '$.almanac'))) AS role_type
   JOIN
     mappings
   USING (role_type)
   WHERE
-    date = '2024-06-01'
+    date = '2024-06-01' AND
+    is_root_page
 ),
 
 base AS (
   SELECT
-    e.client,
-    e.page,
-    e.is_root_page,
-    m.mapping_id,
-    m.element_type,
-    m.role_type,
+    client,
+    page,
+    mapping_id,
+    element_type,
+    role_type,
     COUNTIF(e.element_type IS NOT NULL) AS element_usage,
     COUNTIF(r.role_type IS NOT NULL) AS role_usage
   FROM
+    `httparchive.all.pages`
+  INNER JOIN mappings ON (TRUE)
+  LEFT OUTER JOIN
     elements e
-  INNER JOIN
-    mappings m
-  ON
-    e.element_type = m.element_type
-  LEFT JOIN
+  USING (client, page, element_type)
+  LEFT OUTER JOIN
     roles r
-  ON
-    e.client = r.client AND e.page = r.page AND e.is_root_page = r.is_root_page AND e.element_type = r.role_type
-  GROUP BY
-    e.client,
-    e.page,
-    e.is_root_page,
-    m.mapping_id,
-    m.element_type,
-    m.role_type
-),
-
-total_pages_per_client_root AS (
-  SELECT
-    client,
-    is_root_page,
-    COUNT(DISTINCT page) AS total_pages
-  FROM
-    base
-  GROUP BY
-    client,
+  USING (client, page, role_type)
+  WHERE
+    date = '2024-06-01' AND
     is_root_page
-),
-
-aggregated_usage AS (
-  SELECT
-    b.client,
-    b.is_root_page,
-    b.mapping_id,
-    b.element_type,
-    b.role_type,
-    t.total_pages,
-    COUNTIF(b.element_usage > 0) AS element_usage,
-    COUNTIF(b.role_usage > 0) AS role_usage,
-    COUNTIF(b.element_usage > 0 OR b.role_usage > 0) AS both_usage
-  FROM
-    base b
-  JOIN
-    total_pages_per_client_root t
-  ON
-    b.client = t.client AND b.is_root_page = t.is_root_page
   GROUP BY
-    b.client,
-    b.is_root_page,
-    b.mapping_id,
-    b.element_type,
-    b.role_type,
-    t.total_pages
+    client,
+    page,
+    mapping_id,
+    element_type,
+    role_type
 )
 
 SELECT
   client,
-  is_root_page,
   mapping_id,
   element_type,
   role_type,
-  total_pages,
-  element_usage,
-  role_usage,
-  both_usage,
-  SAFE_DIVIDE(element_usage, total_pages) AS element_pct,
-  SAFE_DIVIDE(role_usage, total_pages) AS role_pct,
-  SAFE_DIVIDE(both_usage, total_pages) AS both_pct
+  COUNT(DISTINCT page) AS total_pages,
+  COUNTIF(element_usage > 0) AS element_usage,
+  COUNTIF(role_usage > 0) AS role_usage,
+  COUNTIF(element_usage > 0 OR role_usage > 0) AS both_usage,
+  COUNTIF(element_usage > 0) / COUNT(DISTINCT page) AS element_pct,
+  COUNTIF(role_usage > 0) / COUNT(DISTINCT page) AS role_pct,
+  COUNTIF(element_usage > 0 OR role_usage > 0) / COUNT(DISTINCT page) AS both_pct
 FROM
-  aggregated_usage
+  base
+GROUP BY
+  client,
+  mapping_id,
+  element_type,
+  role_type
 ORDER BY
   client,
-  is_root_page,
   mapping_id,
-  element_type;
+  element_type
