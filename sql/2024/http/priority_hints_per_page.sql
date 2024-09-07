df8f071f7cd4ecafa1b21caaa8b823d9b680d724
Barry Pollard <barrypollard@google.com>
2024-09-07 10:58:33 +0100

    More conversions

--- sql/2024/http/priority_hints_per_page.sql
+++ sql/2024/http/priority_hints_per_page.sql
@@ -2,35 +2,26 @@
 
 # Number of resources with priority hints.
 
-#### NOT CONVERTED to 2024 YET!!!
-
-CREATE TEMPORARY FUNCTION getNumPriorityHints(payload STRING)
-RETURNS INT LANGUAGE js AS """
-try {
-  const $ = JSON.parse(payload)
-  const almanac = JSON.parse($._almanac);
-  return almanac['priority-hints']['total'];
-} catch (e) {
-  return -1;
-}
-""";
-
 SELECT
   client,
+  is_root_page,
   percentile,
   APPROX_QUANTILES(num_priority_hints, 1000)[OFFSET(percentile * 10)] AS num_percentiles
 FROM (
   SELECT
-    _TABLE_SUFFIX AS client,
-    url AS page,
-    getNumPriorityHints(payload) AS num_priority_hints
+    client,
+    is_root_page,
+    CAST(JSON_EXTRACT_SCALAR(custom_metrics, '$.almanac.priority-hints.total') AS INT64) AS num_priority_hints
   FROM
-    `httparchive.pages.2022_06_01_*`
-),
-UNNEST([10, 25, 50, 75, 90, 95, 100]) AS percentile
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01'),
+  UNNEST([10, 25, 50, 75, 90, 95, 100]) AS percentile
 GROUP BY
   client,
+  is_root_page,
   percentile
 ORDER BY
   client,
+  is_root_page,
   percentile
--- sql/2024/http/priority_hints_usage.sql
+++ sql/2024/http/priority_hints_usage.sql
@@ -2,33 +2,25 @@
 
 # Number of resources with priority hints.
 
-#### NOT CONVERTED to 2024 YET!!!
-
-CREATE TEMPORARY FUNCTION getNumPriorityHints(payload STRING)
-RETURNS INT LANGUAGE js AS """
-try {
-  const $ = JSON.parse(payload)
-  const almanac = JSON.parse($._almanac);
-  return almanac['priority-hints']['total'];
-} catch (e) {
-  return -1;
-}
-""";
-
 SELECT
   client,
+  is_root_page,
   COUNT(0) AS total_sites,
   COUNTIF(num_priority_hints > 0) AS sites_using_priority_hints,
   COUNTIF(num_priority_hints > 0) / COUNT(0) AS sites_using_priority_hints_pct
 FROM (
   SELECT
-    _TABLE_SUFFIX AS client,
-    url AS page,
-    getNumPriorityHints(payload) AS num_priority_hints
+    client,
+    is_root_page,
+    CAST(JSON_EXTRACT_SCALAR(custom_metrics, '$.almanac.priority-hints.total') AS INT64) AS num_priority_hints
   FROM
-    `httparchive.pages.2022_06_01_*`
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01'
 )
 GROUP BY
-  client
+  client,
+is_root_page
 ORDER BY
-  client
+  client,
+  is_root_page
