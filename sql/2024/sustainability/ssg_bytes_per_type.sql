a1cb58f096f99d73bcafec9a4c5de7ddcbdfe01a
Lucia Harcegová <lucia.harcegova@gmail.com>
2024-09-08 20:26:36 +0200

    Update ecommerce with new co2 calc

--- sql/2024/sustainability/ssg_bytes_per_type.sql
+++ sql/2024/sustainability/ssg_bytes_per_type.sql
@@ -1,7 +1,9 @@
 #standardSQL
-# Median resource weights by static site generator with detailed CO2e breakdown
 
+# Median resource weights by static site generator with detailed CO2e breakdown
+# Source: https://sustainablewebdesign.org/calculating-digital-emissions/
 # Declare variables to calculate the carbon emissions per gigabyte (kWh/GB)
+
 DECLARE grid_intensity NUMERIC DEFAULT 494;
 DECLARE embodied_emissions_data_centers NUMERIC DEFAULT 0.012;
 DECLARE embodied_emissions_network NUMERIC DEFAULT 0.013;
