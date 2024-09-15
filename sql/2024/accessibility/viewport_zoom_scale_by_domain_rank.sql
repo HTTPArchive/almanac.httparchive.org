5a6b15495d08bfc56639a10bdd2fa60a5e0aa2a9
Mike Gifford <mike.gifford@civicactions.com>
2024-09-15 18:32:27 -0400

    Update viewport_zoom_scale_by_domain_rank.sql
    Linting problem

--- sql/2024/accessibility/viewport_zoom_scale_by_domain_rank.sql
+++ sql/2024/accessibility/viewport_zoom_scale_by_domain_rank.sql
@@ -38,7 +38,7 @@ AggregatedData AS (
     COUNTIF(REGEXP_EXTRACT(meta_viewport, r'(?i)user-scalable\s*=\s*(no|0)') IS NOT NULL) AS total_no_scale,
     COUNTIF(SAFE_CAST(REGEXP_EXTRACT(meta_viewport, r'(?i)maximum-scale\s*=\s*([0-9]*\.[0-9]+|[0-9]+)') AS FLOAT64) <= 1) AS total_locked_max_scale,
     COUNTIF(REGEXP_EXTRACT(meta_viewport, r'(?i)user-scalable\s*=\s*(no|0)') IS NOT NULL
-        OR SAFE_CAST(REGEXP_EXTRACT(meta_viewport, r'(?i)maximum-scale\s*=\s*([0-9]*\.[0-9]+|[0-9]+)') AS FLOAT64) <= 1) AS total_either
+    OR SAFE_CAST(REGEXP_EXTRACT(meta_viewport, r'(?i)maximum-scale\s*=\s*([0-9]*\.[0-9]+|[0-9]+)') AS FLOAT64) <= 1) AS total_either
   FROM
     RankGroups
   LEFT JOIN
