6d1f5d53631b4614fcd9212f9244b3d5da8048d9
Mike Gifford <mike.gifford@civicactions.com>
2024-09-15 18:19:22 -0400

    Update lighthouse_score_by_government.sql
    Update to where.

--- sql/2024/accessibility/lighthouse_score_by_government.sql
+++ sql/2024/accessibility/lighthouse_score_by_government.sql
@@ -162,25 +162,21 @@ FROM
   score_data
 WHERE
   REGEXP_CONTAINS(page, r'(?i)('
-    || '\.un\.org/|\.worldbank.org/|\.undp.org/|\.reliefweb.int/|\.who.int/|\.unfccc.int/|\.unccd.int/|\.unesco.org/'  -- United Nations
-    || '\.europa\.eu/'              -- European Union
-    || '\.gov'                      -- Catch-all for any .gov domains (e.g., .gov, .gov.uk, .gov.au, .gov.tw, etc.)
-    || '\.mil'                      -- U.S. Military domains
-    || '\.mil\.'                    -- Non-U.S. Military domains
-    || '\.gouv\.'                   -- France and other French-speaking countries using gouv
-    || '\.gob\.'                    -- Mexico and other Spanish-speaking countries using gob
-    || '\.gv\.'                     -- Austria and Ukraine
-    || '\.go\.'                     -- Governments of Kenya, South Africa, South Korea, Indonesia, Japan and Argentina
-    || '\.gc\.ca/|\.canada\.ca/'    -- Canada and the Provinces
-    || '\.gub\.uy/'                 -- Uruguay
-    || '\.belgium\.be/|\.fgov\.be/|\.vlaanderen\.be/|\.wallonie\.be/|\.mil\.be|' -- Belgium
-    || '\.gov\.se|1177\.se|funktionstjanster\.se|hemnet\.se|smhi\.se|sverigesradio\.se|klart\.se|bankid\.com|synonymer\.se|arbetsformedlingen\.se|skatteverket\.se|schoolsoft\.se|postnord\.se|grandid\.com|viaplay\.se|skola24\.se|forsakringskassan\.se|vklass\.se|sl\.se|familjeliv\.se|' -- Sweden
-    || '\.gov\.fi|\.valtioneuvosto\.fi|\.minedu\.fi|\.formin\.fi|\.intermin\.fi|' -- Finland
-    || '\.govt\.'                   -- New Zealand, Sri Lanka, India and Pakistan
-    || '\.riik\.ee/'                 -- Estonia
-    || '\.admin\.ch/'               -- Switzerland
-    || '\.public\.lu/'              -- Luxembourg
-    || '\.gov\.nl/|\.overheid\.nl/|\.mijnoverheid\.nl/'  -- Netherlands
+    || '\\.un\\.org/|\\.worldbank\\.org/|\\.undp\\.org/|\\.reliefweb\\.int/|\\.who\\.int/|\\.unfccc\\.int/|\\.unccd\\.int/|\\.unesco\\.org/'  -- United Nations
+    || '\\.europa\\.eu/'               -- European Union
+    || '\.gov/'                        -- Catch-all for any USA .gov domains
+    || '\.mil/'                        -- Catch-all for any USA .mil domains
+    || '\\.gov(\\.[a-z]{2,3})?/'       -- Global .gov domains, including .gov.uk, .gov.au, .gov.uk, .gov.au, .gov.tw etc.
+    || '\\.mil(\\.[a-z]{2,3})?/'       -- Global military domains
+    || '(?i)(\w+\.){0,3}(gouv|gob|go|gv|gub|govt)\.[a-z]{2,3}/' -- Other generic government formats
+    || '\\.gc\\.ca/|\\.canada\\.ca/'   -- Canada and provinces
+    || '\\.belgium\\.be/|\\.fgov\\.be/|\\.vlaanderen\\.be/|\\.wallonie\\.be/|\\.brussels\\.be/|\\.economie\\.fgov\\.be/|\\.health\\.belgium\\.be/|\\.justice\\.belgium\\.be/|\\.mobilit\\.belgium\\.be/|\\.mil\\.be/' -- Belgium
+    || '\\.gov\\.se\\b|1177\\.se|funktionstjanster\\.se|hemnet\\.se|smhi\\.se|sverigesradio\\.se|klart\\.se|bankid\\.com|synonymer\\.se|arbetsformedlingen\\.se|skatteverket\\.se|schoolsoft\\.se|postnord\\.se|grandid\\.com|viaplay\\.se|skola24\\.se|forsakringskassan\\.se|vklass\\.se|sl\\.se|familjeliv\\.se/' -- Sweden
+    || '\\.gov\\.fi|\\.valtioneuvosto\\.fi|\\.minedu\\.fi|\\.formin\\.fi|\\.intermin\\.fi/' -- Finland
+    || '\\.riik\\.ee/'                 -- Estonia
+    || '\\.admin\\.ch/'                -- Switzerland
+    || '\\.public\\.lu/'               -- Luxembourg
+    || '\\.gov\\.nl/|\\.overheid\\.nl/|\\.mijnoverheid\\.nl/'  -- Netherlands
     || ')')
 )
 
