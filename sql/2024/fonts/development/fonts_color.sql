1dd4de7c8921c19db320e4cde94407a5b6fbfbcb
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-26 13:55:22 +0200

    Add dates to development/fonts_color.sql

--- sql/2024/fonts/development/fonts_color.sql
+++ sql/2024/fonts/development/fonts_color.sql
@@ -7,32 +7,37 @@
 WITH
 fonts AS (
   SELECT
+    date,
     client,
     COUNT(DISTINCT page) AS count
   FROM
     `httparchive.all.requests`
   WHERE
-    date = '2024-07-01' AND
+    date IN ('2022-07-01', '2023-07-01', '2024-07-01') AND
     is_root_page AND
     type = 'font' AND
     IS_COLOR(payload)
   GROUP BY
+    date,
     client
 ),
 sites AS (
   SELECT
+    date,
     client,
     COUNT(DISTINCT page) AS total
   FROM
     `httparchive.all.requests`
   WHERE
-    date = '2024-07-01' AND
+    date IN ('2022-07-01', '2023-07-01', '2024-07-01') AND
     is_root_page
   GROUP BY
+    date,
     client
 )
 
 SELECT
+  date,
   client,
   count,
   total,
@@ -40,6 +45,7 @@ SELECT
 FROM
   fonts
 JOIN
-  sites USING (client)
+  sites USING (date, client)
 ORDER BY
+  date,
   proportion DESC
