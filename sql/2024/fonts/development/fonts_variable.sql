ac0e9d0bd483cc4b9dcba8225dc22e08cc703f5d
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-26 14:19:08 +0200

    Add dates to development/fonts_variable.sql

--- sql/2024/fonts/development/fonts_variable.sql
+++ sql/2024/fonts/development/fonts_variable.sql
@@ -7,32 +7,37 @@
 WITH
 fonts AS (
   SELECT
+    date,
     client,
     COUNT(DISTINCT page) AS count
   FROM
     `httparchive.all.requests`
   WHERE
-    date = '2024-07-01' AND
+    date IN ('2022-07-01', '2023-07-01', '2024-07-01') AND
     is_root_page AND
     type = 'font' AND
     IS_VARIABLE(payload)
   GROUP BY
+    date,
     client
 ),
 sites AS (
   SELECT
+    date,
     client,
     COUNT(DISTINCT page) AS total
   FROM
     `httparchive.all.requests`
   WHERE
-    date = '2024-07-01' AND
+    date IN ('2022-07-01', '2023-07-01', '2024-07-01') AND
     is_root_page
   GROUP BY
+    date,
     client
 )
 
 SELECT
+  date,
   client,
   count,
   total,
@@ -40,7 +45,8 @@ SELECT
 FROM
   fonts
 JOIN
-  sites USING (client)
+  sites USING (date, client)
 ORDER BY
+  date,
   client,
   proportion DESC
