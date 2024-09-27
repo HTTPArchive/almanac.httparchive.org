7dbdc582bcf91db2b9a636a310ab191f4475cc9b
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-27 09:23:12 +0200

    Remove the format from performance/fonts_size_by_country.sql

--- sql/2024/fonts/performance/fonts_size_by_country.sql
+++ sql/2024/fonts/performance/fonts_size_by_country.sql
@@ -23,10 +23,6 @@ requests AS (
   SELECT
     client,
     NET.HOST(page) AS domain,
-    FILE_FORMAT(
-      JSON_EXTRACT_SCALAR(summary, '$.ext'),
-      JSON_EXTRACT_SCALAR(summary, '$.mimeType')
-    ) AS format,
     PARSE_NUMERIC(JSON_EXTRACT_SCALAR(summary, '$.respBodySize')) AS size
   FROM
     `httparchive.all.requests`
@@ -34,43 +30,20 @@ requests AS (
     date = '2024-07-01' AND
     is_root_page AND
     type = 'font'
-),
-formats AS (
-  SELECT
-    client,
-    country,
-    format,
-    ROW_NUMBER() OVER (PARTITION BY client, country ORDER BY COUNT(0) DESC) AS rank
-  FROM
-    requests
-  INNER JOIN
-    countries USING (client, domain)
-  GROUP BY
-    client,
-    country,
-    format
 )
 
 SELECT
   client,
   country,
-  format,
   COUNT(0) AS count,
   ROUND(APPROX_QUANTILES(size, 1000)[OFFSET(500)]) AS size
 FROM
   requests
 INNER JOIN
   countries USING (client, domain)
-INNER JOIN
-  formats USING (client, country, format)
-WHERE
-  rank <= 10
 GROUP BY
   client,
-  country,
-  format,
-  rank
+  country
 ORDER BY
   client,
-  country,
-  rank
+  country
