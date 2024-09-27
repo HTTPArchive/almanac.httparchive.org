58b90594f160dd1ebbd3668d5087be39d17377de
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-27 11:12:02 +0200

    Add more dates to development/fonts_feature_kerning.sql

--- sql/2024/fonts/development/fonts_feature_kerning.sql
+++ sql/2024/fonts/development/fonts_feature_kerning.sql
@@ -25,6 +25,7 @@ try {
 WITH
 fonts AS (
   SELECT
+    date,
     client,
     url,
     (
@@ -34,27 +35,31 @@ fonts AS (
   FROM
     `httparchive.all.requests`
   WHERE
-    date = '2024-07-01' AND
+    date IN ('2022-07-01', '2023-07-01', '2024-07-01') AND
     is_root_page AND
     type = 'font'
   GROUP BY
+    date,
     client,
     url,
     support
 )
 
 SELECT
+  date,
   client,
   support,
   COUNT(DISTINCT url) AS count,
-  SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS total,
-  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS proportion
+  SUM(COUNT(DISTINCT url)) OVER (PARTITION BY date, client) AS total,
+  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY date, client) AS proportion
 FROM
   fonts
 GROUP BY
+  date,
   client,
   support
 ORDER BY
+  date,
   client,
   support,
   proportion DESC
