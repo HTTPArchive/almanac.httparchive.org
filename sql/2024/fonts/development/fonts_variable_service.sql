7854376e3dcc5907a59c31f0b03735b2dc1aebdd
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-27 10:35:06 +0200

    Add more dates to development/fonts_variable_service.sql

--- sql/2024/fonts/development/fonts_variable_service.sql
+++ sql/2024/fonts/development/fonts_variable_service.sql
@@ -5,20 +5,23 @@
 -- INCLUDE ../common.sql
 
 SELECT
+  date,
   client,
   SERVICE(url) AS service,
   COUNT(0) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS proportion
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY date, client) AS proportion
 FROM
   `httparchive.all.requests`
 WHERE
-  date = '2024-07-01' AND
+  date IN ('2022-07-01', '2023-07-01', '2024-07-01') AND
   is_root_page AND
   type = 'font' AND
   IS_VARIABLE(payload)
 GROUP BY
+  date,
   client,
   service
 ORDER BY
+  date,
   client,
   proportion DESC
