3f8352df57343e4a59b58795d93857979e7dd2ff
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-30 10:09:05 +0200

    Move the totals in design/fonts_family_by_script.sql

--- sql/2024/fonts/design/fonts_family_by_script.sql
+++ sql/2024/fonts/design/fonts_family_by_script.sql
@@ -4,27 +4,40 @@
 
 -- INCLUDE ../common.sql
 
+WITH
+links AS (
+  SELECT
+    client,
+    SCRIPTS(payload) AS scripts,
+    FAMILY(payload) AS family,
+    COUNT(0) OVER (PARTITION BY client) AS total
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-07-01' AND
+    type = 'font' AND
+    is_root_page AND
+    RAND() > 0.5
+)
+
 SELECT
   client,
   script,
-  FAMILY(payload) AS family,
+  family,
   2 * COUNT(0) AS count,
-  2 * SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS proportion,
+  2 * total AS total,
+  COUNT(0) / total AS proportion,
   ROW_NUMBER() OVER (PARTITION BY client, script ORDER BY COUNT(0) DESC) AS rank
 FROM
-  `httparchive.all.requests`,
-  UNNEST(SCRIPTS(payload)) AS script
+  links,
+  UNNEST(scripts) AS script
 WHERE
-  date = '2024-07-01' AND
-  type = 'font' AND
-  is_root_page AND
-  FAMILY(payload) NOT IN ('Adobe Blank') AND
-  RAND() > 0.5
+  family != 'Adobe Blank'
 GROUP BY
   client,
   script,
-  family
+  family,
+  links.total
 QUALIFY
   rank <= 10
 ORDER BY
