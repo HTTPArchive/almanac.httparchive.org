7e9990a8c5bfa1250bfd7fe5a599c28d45507fe0
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-30 09:15:37 +0200

    Move the totals in performance/fonts_format_file_by_service.sql

--- sql/2024/fonts/performance/fonts_format_file_by_service.sql
+++ sql/2024/fonts/performance/fonts_format_file_by_service.sql
@@ -13,7 +13,9 @@ links AS (
     FILE_FORMAT(
       JSON_EXTRACT_SCALAR(summary, '$.ext'),
       JSON_EXTRACT_SCALAR(summary, '$.mimeType')
-    ) AS format
+    ) AS format,
+    COUNT(0) OVER (PARTITION BY client) AS total,
+    COUNT(DISTINCT url) OVER (PARTITION BY client) AS total_secondary
   FROM
     `httparchive.all.requests`
   WHERE
@@ -28,17 +30,19 @@ SELECT
   format,
   COUNT(0) AS count,
   COUNT(DISTINCT url) AS count_secondary,
-  SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
-  SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS total_secondary,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) AS proportion,
-  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS proportion_secondary,
+  total,
+  total_secondary,
+  COUNT(0) / total AS proportion,
+  COUNT(DISTINCT url) / total_secondary AS proportion_secondary,
   ROW_NUMBER() OVER (PARTITION BY client, service ORDER BY COUNT(0) DESC) AS rank
 FROM
   links
 GROUP BY
   client,
   service,
-  format
+  format,
+  total,
+  total_secondary
 QUALIFY
   rank <= 10
 ORDER BY
