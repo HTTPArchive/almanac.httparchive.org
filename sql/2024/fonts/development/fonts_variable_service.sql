9e3fac497411800210016507d533b64950732b5a
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-30 10:19:57 +0200

    Change the normalization to Fonts in development/fonts_variable_service.sql

--- sql/2024/fonts/development/fonts_variable_service.sql
+++ sql/2024/fonts/development/fonts_variable_service.sql
@@ -1,27 +1,44 @@
 -- Section: Development
 -- Question: Who is serving variable fonts?
--- Normalization: Links
+-- Normalization: Fonts (only variable)
 
 -- INCLUDE ../common.sql
+
+WITH
+fonts AS (
+  SELECT
+    date,
+    client,
+    url,
+    SERVICE(url) AS service,
+    COUNT(0) OVER (PARTITION BY date, client) AS total
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date IN ('2022-07-01', '2023-07-01', '2024-07-01') AND
+    type = 'font' AND
+    is_root_page AND
+    IS_VARIABLE(payload)
+  GROUP BY
+    date,
+    client,
+    url
+)
 
 SELECT
   date,
   client,
-  SERVICE(url) AS service,
+  service,
   COUNT(0) AS count,
-  SUM(COUNT(0)) OVER (PARTITION BY date, client) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY date, client) AS proportion
+  total,
+  COUNT(0) / total AS proportion
 FROM
-  `httparchive.all.requests`
-WHERE
-  date IN ('2022-07-01', '2023-07-01', '2024-07-01') AND
-  type = 'font' AND
-  is_root_page AND
-  IS_VARIABLE(payload)
+  fonts
 GROUP BY
   date,
   client,
-  service
+  service,
+  total
 ORDER BY
   date,
   client,
