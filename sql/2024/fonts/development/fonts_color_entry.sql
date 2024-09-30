e8378fae8692024ea1543eeb11e2f2961c9e0027
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-09-30 12:57:59 +0200

    Move the totals in development/fonts_color_entry.sql

--- sql/2024/fonts/development/fonts_color_entry.sql
+++ sql/2024/fonts/development/fonts_color_entry.sql
@@ -1,25 +1,45 @@
 -- Section: Development
 -- Question: How many entries are there in color palettes?
--- Normalization: Fonts
+-- Normalization: Fonts (color only)
 
 -- INCLUDE ../common.sql
 
+WITH
+fonts AS (
+  SELECT
+    client,
+    url,
+    SAFE_CAST(
+      JSON_EXTRACT_SCALAR(
+        ANY_VALUE(payload),
+        '$._font_details.color.numPaletteEntries'
+      ) AS INT64
+    ) AS entries,
+    COUNT(*) OVER (PARTITION BY client) AS total
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-07-01' AND
+    type = 'font' AND
+    is_root_page AND
+    IS_COLOR(payload)
+  GROUP BY
+    client,
+    url
+)
+
 SELECT
   client,
-  SAFE_CAST(JSON_EXTRACT_SCALAR(payload, '$._font_details.color.numPaletteEntries') AS INT64) AS entries,
-  COUNT(DISTINCT url) AS count,
-  SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS total,
-  COUNT(DISTINCT url) / SUM(COUNT(DISTINCT url)) OVER (PARTITION BY client) AS proportion
+  entries,
+  COUNT(0) AS count,
+  total,
+  COUNT(0) / total AS proportion
 FROM
-  `httparchive.all.requests`
-WHERE
-  date = '2024-07-01' AND
-  type = 'font' AND
-  is_root_page AND
-  IS_COLOR(payload)
+  fonts
 GROUP BY
   client,
-  entries
+  entries,
+  total
 ORDER BY
   client,
   proportion DESC
