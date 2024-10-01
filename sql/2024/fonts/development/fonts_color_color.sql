31a45b5c7a8899b7f6ba13d4d8364c7cf9e3e0d3
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-10-01 11:02:56 +0200

    Make a few cosmetic adjustments

--- sql/2024/fonts/development/fonts_color_color.sql
+++ sql/2024/fonts/development/fonts_color_color.sql
@@ -31,12 +31,7 @@ fonts AS (
   SELECT
     client,
     url,
-    COLORS(
-      JSON_EXTRACT(
-        ANY_VALUE(payload),
-        '$._font_details.color.palettes'
-      )
-    ) AS colors,
+    COLORS(JSON_EXTRACT(ANY_VALUE(payload), '$._font_details.color.palettes')) AS colors,
     COUNT(0) OVER (PARTITION BY client) AS total
   FROM
     `httparchive.all.requests`
