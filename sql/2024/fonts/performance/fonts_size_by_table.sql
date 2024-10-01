a70905e9df7643b10ce54a9d18143aa9a55fcdfa
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-10-01 12:54:16 +0200

    Make a cosmetic adjustment

--- sql/2024/fonts/performance/fonts_size_by_table.sql
+++ sql/2024/fonts/performance/fonts_size_by_table.sql
@@ -18,7 +18,7 @@ fonts AS (
   SELECT
     client,
     url,
-    JSON_EXTRACT(ANY_VALUE(payload), '$._font_details.table_sizes') AS payload
+    TABLES(JSON_EXTRACT(ANY_VALUE(payload), '$._font_details.table_sizes')) AS tables
   FROM
     `httparchive.all.requests`
   WHERE
@@ -28,25 +28,17 @@ fonts AS (
   GROUP BY
     client,
     url
-),
-tables AS (
-  SELECT
-    client,
-    table.name AS table,
-    table.size
-  FROM
-    fonts,
-    UNNEST(TABLES(payload)) AS table
 )
 
 SELECT
   client,
-  table,
+  table.name AS table,
   percentile,
   COUNT(0) AS count,
   ROUND(APPROX_QUANTILES(size, 1000)[OFFSET(percentile * 10)]) AS size
 FROM
-  tables,
+  fonts,
+  UNNEST(tables) AS table,
   UNNEST([10, 25, 50, 75, 90, 99]) AS percentile
 GROUP BY
   client,
