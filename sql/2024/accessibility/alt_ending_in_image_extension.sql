336441d51e5ef4a1d2135e13cd98b38f61e6e41d
Mike Gifford <mike.gifford@civicactions.com>
2024-09-30 20:56:56 -0400

    Update alt_ending_in_image_extension.sql
    Thanks to @tunetheweb for this.

--- sql/2024/accessibility/alt_ending_in_image_extension.sql
+++ sql/2024/accessibility/alt_ending_in_image_extension.sql
@@ -1,7 +1,5 @@
 #standardSQL
 # Alt text ending in an image extension
-
-# Temporary function to extract file extension statistics from the JSON payload
 CREATE TEMPORARY FUNCTION getUsedExtensions(payload STRING)
 RETURNS ARRAY<STRUCT<extension STRING, total INT64>> LANGUAGE js AS '''
 try {
@@ -14,49 +12,65 @@ try {
   return [];
 }
 ''';
-
-# Main query to analyze alt text usage across sites
 SELECT
   client,
   is_root_page,
-  COUNTIF(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(custom_metrics, '$.markup'), '$.images.img.alt.present') AS INT64) > 0) AS sites_with_non_empty_alt,  # Count sites with non-empty alt attributes
-  COUNTIF(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._a11y'), '$.file_extension_alts.total_with_file_extension') AS INT64) > 0) AS sites_with_file_extension_alt,  # Count sites with alt attributes ending in a file extension
-  SUM(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._markup'), '$.images.img.alt.present') AS INT64)) AS total_non_empty_alts,  # Sum of all non-empty alt attributes across sites
-  SUM(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._a11y'), '$.file_extension_alts.total_with_file_extension') AS INT64)) AS total_alts_with_file_extensions,  # Sum of all alt attributes ending in a file extension
+  sites_with_non_empty_alt,
+  sites_with_file_extension_alt,
+  total_alts_with_file_extensions,
 
-  # Percentage of sites with non-empty alt attributes that also have an alt ending in a file extension
-  SAFE_DIVIDE(
-    COUNTIF(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._a11y'), '$.file_extension_alts.total_with_file_extension') AS INT64) > 0),
-    COUNTIF(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._markup'), '$.images.img.alt.present') AS INT64) > 0)
-  ) AS pct_sites_with_file_extension_alt,
+  # Of sites with a non-empty alt, what % have an alt with a file extension
+  sites_with_file_extension_alt / sites_with_non_empty_alt AS pct_sites_with_file_extension_alt,
+  # Given a random alt, how often will it end in a file extension
+  total_alts_with_file_extensions / total_non_empty_alts AS pct_alts_with_file_extension,
 
-  # Percentage of all non-empty alt attributes that end in a file extension
-  SAFE_DIVIDE(
-    SUM(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._a11y'), '$.file_extension_alts.total_with_file_extension') AS INT64)),
-    SUM(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._markup'), '$.images.img.alt.present') AS INT64))
-  ) AS pct_alts_with_file_extension,
+  extension_stat.extension AS extension,
+  COUNT(0) AS total_sites_using,
+  # Of sites with a non-empty alt, what % have an alt with this file extension
+  COUNT(0) / sites_with_non_empty_alt AS pct_applicable_sites_using,
 
-  extension_stat.extension AS extension,  # File extension used in the alt attribute
-  COUNT(DISTINCT client) AS total_sites_using,  # Number of sites using this specific file extension in their alt attributes
-
-  # Percentage of sites with non-empty alt attributes that use this specific file extension
-  SAFE_DIVIDE(COUNT(DISTINCT client), COUNTIF(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._markup'), '$.images.img.alt.present') AS INT64) > 0)) AS pct_applicable_sites_using,
-
-  # Total occurrences of this specific file extension across all alt attributes
+  # Of sites with a non-empty alt, what % have an alt with this file extension
   SUM(extension_stat.total) AS total_occurances,
-
-  # Percentage of alt attributes ending in any file extension that end in this specific file extension
-  SAFE_DIVIDE(SUM(extension_stat.total), SUM(CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT_SCALAR(payload, '$._a11y'), '$.file_extension_alts.total_with_file_extension') AS INT64))) AS pct_total_occurances
+  # Given a random alt ending in a file extension, how often will it end in this file extension
+  SUM(extension_stat.total) / total_alts_with_file_extensions AS pct_total_occurances
 FROM
-  `httparchive.all.pages`,  # Single table containing all the necessary data
-  UNNEST(getUsedExtensions(JSON_EXTRACT_SCALAR(payload, '$._a11y'))) AS extension_stat  # Unnest the file extension statistics from the payload JSON
+  `httparchive.all.pages`,
+  UNNEST(getUsedExtensions(JSON_EXTRACT(custom_metrics, '$.a11y'))) AS extension_stat
+LEFT JOIN (
+  SELECT
+    client,
+    is_root_page,
+    COUNTIF(total_non_empty_alt > 0) AS sites_with_non_empty_alt,
+    COUNTIF(total_with_file_extension > 0) AS sites_with_file_extension_alt,
+
+    SUM(total_non_empty_alt) AS total_non_empty_alts,
+    SUM(total_with_file_extension) AS total_alts_with_file_extensions
+  FROM (
+    SELECT
+      client,
+      is_root_page,
+      CAST(JSON_EXTRACT_SCALAR(custom_metrics, '$.markup.images.img.alt.present') AS INT64) AS total_non_empty_alt,
+      CAST(JSON_EXTRACT_SCALAR(custom_metrics, '$.a11y.file_extension_alts.total_with_file_extension') AS INT64) AS total_with_file_extension
+    FROM
+      `httparchive.all.pages`
+    WHERE
+      date = '2024-06-01'
+  )
+  GROUP BY
+    client,
+    is_root_page
+) USING (client, is_root_page)
 WHERE
   date = '2024-06-01'
 GROUP BY
   client,
   is_root_page,
-  extension_stat.extension
+  sites_with_non_empty_alt,
+  sites_with_file_extension_alt,
+  total_non_empty_alts,
+  total_alts_with_file_extensions,
+  extension
 ORDER BY
   client,
   is_root_page,
-  total_occurances DESC;  # Order by the number of occurrences of each file extension in descending order
+  total_occurances DESC
