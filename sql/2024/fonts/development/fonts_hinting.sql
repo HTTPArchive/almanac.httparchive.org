ddfbd93eb57d119a7395ddf2116990d5b5ab3486
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-10-04 15:31:04 +0200

    Add development/fonts_hinting.sql

---
+++ sql/2024/fonts/development/fonts_hinting.sql
@@ -0,0 +1,58 @@
+-- Section: Development
+-- Question: How prevalent is autohinting?
+-- Normalization: Fonts (parsed only)
+
+-- INCLUDE ../common.sql
+
+CREATE TEMPORARY FUNCTION IS_HINTED(payload STRING) AS (
+  REGEXP_CONTAINS(
+    JSON_EXTRACT(payload, '$._font_details.table_sizes'),
+    '(?i)fpgm|prep'
+  )
+);
+
+
+CREATE TEMPORARY FUNCTION IS_AUTOHINTED(payload STRING) AS (
+  REGEXP_CONTAINS(
+    JSON_EXTRACT_SCALAR(payload, '$._font_details.names[5]'),
+    'autohint'
+  )
+);
+
+WITH
+sites AS (
+  SELECT
+    client,
+    COUNT(DISTINCT page) AS total
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-07-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+
+SELECT
+  client,
+  IS_AUTOHINTED(payload) AS autohinted,
+  COUNT(DISTINCT page) AS count,
+  total,
+  COUNT(DISTINCT page) / total AS proportion
+FROM
+  `httparchive.all.requests`
+INNER JOIN
+  sites USING (client)
+WHERE
+  date = '2024-07-01' AND
+  type = 'font' AND
+  is_root_page AND
+  IS_PARSED(payload) AND
+  IS_HINTED(payload)
+GROUP BY
+  client,
+  autohinted,
+  total
+ORDER BY
+  client,
+  autohinted
