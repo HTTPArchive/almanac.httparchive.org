0273f76a0b054e81b2d2bf47d0351cea6ec4a33c
Jonathan Pagel <63317370+jcmpagel@users.noreply.github.com>
2024-10-07 14:13:36 +0200

    Update top_cmp_bydevice.sql

--- sql/2024/ecommerce/top_cmp_bydevice.sql
+++ sql/2024/ecommerce/top_cmp_bydevice.sql
@@ -1,51 +1,49 @@
 #standardSQL
 # Top A/B Testing used in Ecommerce sites
+WITH category AS (
+  SELECT
+    client,
+    page,
+
+    technology
+  FROM `httparchive.sample_data.pages_10k`,
+    UNNEST(technologies) AS tech,
+    UNNEST(categories) AS category
+  WHERE
+    date = '2024-09-01'
+    AND is_root_page = TRUE
+    AND category = 'Cookie compliance'
+),
+ecommerce_data AS (
+  SELECT
+    client,
+    page,
+    rank,
+    COUNT(DISTINCT page) OVER (PARTITION BY client) AS total_websites
+  FROM `httparchive.sample_data.pages_10k`,
+    UNNEST(technologies) AS tech,
+    UNNEST(categories) AS category
+  WHERE
+    date = '2024-09-01'
+    AND is_root_page = TRUE
+    AND category = 'Ecommerce'
+    AND 
+      (
+    tech.technology != 'Cart Functionality' AND
+    tech.technology != 'Google Analytics Enhanced eCommerce'
+  )
+  GROUP BY client, page, rank
+)
+-- join the first query with the second query (ecommerce_data)
 SELECT
-  _TABLE_SUFFIX AS client,
-  app,
-  COUNT(DISTINCT url) AS freq,
-  total,
-  COUNT(DISTINCT url) / total AS pct
-FROM
-  `httparchive.technologies.2024_08_01_*`
-JOIN (
-  SELECT
-    _TABLE_SUFFIX,
-    url
-  FROM
-    `httparchive.technologies.2024_08_01_*`
-  WHERE
-    category = 'Ecommerce' AND
-    (
-      app != 'Cart Functionality' AND
-      app != 'Google Analytics Enhanced eCommerce'
-    )
-)
-USING
-  (_TABLE_SUFFIX, url)
-JOIN (
-  SELECT
-    _TABLE_SUFFIX,
-    COUNT(DISTINCT url) AS total
-  FROM
-    `httparchive.technologies.2024_08_01_*`
-  WHERE
-    category = 'Ecommerce' AND
-    (
-      app != 'Cart Functionality' AND
-      app != 'Google Analytics Enhanced eCommerce'
-    )
-  GROUP BY
-    _TABLE_SUFFIX
-)
-USING
-  (_TABLE_SUFFIX)
-WHERE
-  category = 'Cookie compliance'
-GROUP BY
-  client,
-  app,
-  total
-ORDER BY
-  client DESC,
-  pct DESC
+  c.client,
+  c.technology,
+  COUNT(c.page) AS page_count,
+  MIN(e.total_websites) AS total_ecom_websites,
+  COUNT(c.page) / MIN(e.total_websites) AS pct_of_ecom_websites,
+
+FROM category c
+JOIN ecommerce_data e
+  ON c.client = e.client
+  AND c.page = e.page
+GROUP BY c.client, c.technology
