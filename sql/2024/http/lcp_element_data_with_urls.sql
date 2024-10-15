7cb81213380460befb9ee9a944e53a00a58ba1cb
Barry Pollard <barrypollard@google.com>
2024-10-15 03:33:10 +0100

    More queries

---
+++ sql/2024/http/lcp_element_data_with_urls.sql
@@ -0,0 +1,142 @@
+CREATE TEMP FUNCTION getLoadingAttr(attributes STRING) RETURNS STRING LANGUAGE js AS '''
+  try {
+    const data = JSON.parse(attributes);
+    const loadingAttr = data.find(attr => attr["name"] === "loading")
+    return loadingAttr.value
+  } catch (e) {
+    return "";
+  }
+''';
+
+CREATE TEMP FUNCTION getDecodingAttr(attributes STRING) RETURNS STRING LANGUAGE js AS '''
+  try {
+    const data = JSON.parse(attributes);
+    const decodingAttr = data.find(attr => attr["name"] === "decoding")
+    return decodingAttr.value
+  } catch (e) {
+    return "";
+  }
+''';
+
+CREATE TEMP FUNCTION getFetchPriorityAttr(attributes STRING) RETURNS STRING LANGUAGE js AS '''
+  try {
+    const data = JSON.parse(attributes);
+    const fetchPriorityAttr = data.find(attr => attr["name"] === "fetchpriority")
+    return fetchPriorityAttr.value
+  } catch (e) {
+    return "";
+  }
+''';
+
+CREATE TEMP FUNCTION getLoadingClasses(attributes STRING) RETURNS STRING LANGUAGE js AS '''
+  try {
+    const data = JSON.parse(attributes);
+    const classes = data.find(attr => attr["name"] === "class").value
+    if (classes.indexOf('lazyload') !== -1) {
+        return classes
+    } else {
+        return ""
+    }
+  } catch (e) {
+    return "";
+  }
+''';
+
+CREATE TEMPORARY FUNCTION getResourceHints(linkNodes STRING)
+RETURNS STRUCT<preload BOOLEAN, prefetch BOOLEAN, preconnect BOOLEAN, prerender BOOLEAN, `dns-prefetch` BOOLEAN, `modulepreload` BOOLEAN>
+LANGUAGE js AS '''
+var hints = ['preload', 'prefetch', 'preconnect', 'prerender', 'dns-prefetch', 'modulepreload'];
+try {
+  var linkNodes = JSON.parse(linkNodes);
+  return hints.reduce((results, hint) => {
+    results[hint] = !!linkNodes.nodes.find(link => link.rel.toLowerCase() == hint);
+    return results;
+  }, {});
+} catch (e) {
+  return hints.reduce((results, hint) => {
+    results[hint] = false;
+    return results;
+  }, {});
+}
+''';
+
+WITH lcp_stats AS (
+  SELECT
+    client,
+    page,
+    JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_elem_stats.nodeName') AS nodeName,
+    JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_elem_stats.url') AS elementUrl,
+    CAST(JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_elem_stats.size') AS INT64) AS size,
+    CAST(JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_elem_stats.loadTime') AS FLOAT64) AS loadTime,
+    CAST(JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_elem_stats.startTime') AS FLOAT64) AS startTime,
+    CAST(JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_elem_stats.renderTime') AS FLOAT64) AS renderTime,
+    JSON_EXTRACT(custom_metrics, '$.performance.lcp_elem_stats.attributes') AS attributes,
+    getLoadingAttr(JSON_EXTRACT(custom_metrics, '$.performance.lcp_elem_stats.attributes')) AS loading,
+    getDecodingAttr(JSON_EXTRACT(custom_metrics, '$.performance.lcp_elem_stats.attributes')) AS decoding,
+    getLoadingClasses(JSON_EXTRACT(custom_metrics, '$.performance.lcp_elem_stats.attributes')) AS classWithLazyload,
+    getFetchPriorityAttr(JSON_EXTRACT(custom_metrics, '$.performance.lcp_elem_stats.attributes')) AS fetchPriority,
+    LOWER(JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_resource.initialPriority')) AS initalPriority,
+    LOWER(JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_resource.priority')) AS priority,
+    getResourceHints(JSON_EXTRACT(custom_metrics, '$.almanac.link-nodes')) AS hints
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01' AND
+    is_root_page AND
+    JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_elem_stats.url') IS NOT NULL AND
+    JSON_EXTRACT_SCALAR(custom_metrics, '$.performance.lcp_elem_stats.url') != ""
+)
+
+SELECT
+  client,
+  COUNT(DISTINCT page) AS pages,
+  ANY_VALUE(total) AS total,
+  COUNT(DISTINCT page) / ANY_VALUE(total) AS pct,
+  COUNTIF(elementUrl != '') AS haveImages,
+  COUNTIF(elementUrl != '') / COUNT(DISTINCT page) AS pct_haveImages,
+  COUNTIF(loading = 'eager') AS native_eagerload,
+  COUNTIF(loading = 'lazy') AS native_lazyload,
+  COUNTIF(classWithLazyload != '') AS lazyload_class,
+  COUNTIF(classWithLazyload != '' OR loading = 'lazy') AS probably_lazyLoaded,
+  COUNTIF(classWithLazyload != '' OR loading = 'lazy') / COUNT(DISTINCT page) AS pct_prob_lazyloaded,
+  COUNTIF(decoding = 'async') AS async_decoding,
+  COUNTIF(decoding = 'sync') AS sync_decoding,
+  COUNTIF(decoding = 'auto') AS auto_decoding,
+  COUNTIF(fetchPriority = 'low') AS fetchpriority_low,
+  COUNTIF(fetchPriority = 'high') AS fetchpriority_high,
+  COUNTIF(fetchPriority = 'low') / COUNT(DISTINCT page) AS pct_fetchpriority_low,
+  COUNTIF(fetchPriority = 'high') / COUNT(DISTINCT page) AS pct_fetchpriority_high,
+  COUNTIF(loading = 'lazy' AND fetchPriority = 'high') AS native_lazyload_and_fetch_priority,
+  COUNTIF(loading = 'lazy' AND fetchPriority = 'high') / COUNT(DISTINCT page) AS pct_native_lazyload_and_fetch_priority,
+  COUNTIF(initalPriority = 'low') AS inital_priority_low,
+  COUNTIF(initalPriority = 'medium') AS inital_priority_medium,
+  COUNTIF(initalPriority = 'high') AS inital_priority_high_and_fetchpriority,
+  COUNTIF(initalPriority = 'high' AND fetchPriority = 'high') AS inital_priority_high,
+  COUNTIF(initalPriority = 'low') / COUNT(DISTINCT page) AS pct_inital_priority_low,
+  COUNTIF(initalPriority = 'medium') / COUNT(DISTINCT page) AS pct_inital_priority_medium,
+  COUNTIF(initalPriority = 'high') / COUNT(DISTINCT page) AS pct_inital_priority_high,
+  COUNTIF(initalPriority = 'high' AND fetchPriority = 'high') / COUNT(DISTINCT page) AS pct_inital_priority_high_and_fetchpriority,
+  COUNTIF(loading = 'lazy' AND fetchPriority = 'high') / COUNT(DISTINCT page) AS pct_native_lazyload_and_fetch_priority,
+  COUNTIF(hints.preload) AS preload,
+  COUNTIF(hints.preload) / COUNT(DISTINCT page) AS pct_preload
+FROM
+  lcp_stats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01' AND
+    is_root_page
+  GROUP BY
+    client)
+USING
+  (client)
+GROUP BY
+  client
+HAVING
+  pages > 1000
+ORDER BY
+  pct DESC
--- sql/2024/http/unused_preloads.sql
+++ sql/2024/http/unused_preloads.sql
@@ -5,17 +5,20 @@ SELECT
   unused_preloads,
   COUNT(0) AS freq,
   SUM(COUNT(0)) OVER (PARTITION BY client, is_root_page) AS total,
-  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client, is_root_page) AS pct
+  COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client, is_root_page) AS pct,
+  ARRAY_TO_STRING(ARRAY_AGG(DISTINCT page LIMIT 5), ' ') AS sample_urls
 FROM (
   SELECT
     client,
     is_root_page,
-    COALESCE(ARRAY_LENGTH(REGEXP_EXTRACT_ALL(consoleLog, r'was preloaded using link preload but not used within a few seconds')), 0) AS unused_preloads
+    COALESCE(ARRAY_LENGTH(REGEXP_EXTRACT_ALL(consoleLog, r'was preloaded using link preload but not used within a few seconds')), 0) AS unused_preloads,
+    page
   FROM (
     SELECT
       client,
       is_root_page,
-      JSON_QUERY(payload, '$._consoleLog') AS consoleLog
+      JSON_QUERY(payload, '$._consoleLog') AS consoleLog,
+      page
     FROM
       `httparchive.all.pages`
     WHERE
@@ -26,6 +29,8 @@ GROUP BY
   client,
   is_root_page,
   unused_preloads
+HAVING
+  SUM(COUNT(0)) OVER (PARTITION BY client, is_root_page) >
 ORDER BY
   client,
   is_root_page,
