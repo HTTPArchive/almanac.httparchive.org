499b26a715642d61c1c716e71e7b72aa4e50c148
Burak Güneli <burak.guneli@gmail.com>
2024-10-23 16:12:06 +0200

    Create global_emissions_per_page.sql, page_byte_pre_type.sql, responsive_images.sql, text_compression.sql

---
+++ sql/2024/sustainability/responsive_images.sql
@@ -0,0 +1,47 @@
+#standardSQL
+# percent of sites using images with srcset w/wo sizes, or picture element
+CREATE TEMPORARY FUNCTION get_media_info(media_string STRING)
+RETURNS STRUCT<
+  num_srcset_all INT64,
+  num_srcset_sizes INT64,
+  num_picture_img INT64
+> LANGUAGE js AS '''
+var result = {
+  num_srcset_all: 0,
+  num_srcset_sizes: 0,
+  num_picture_img: 0
+};
+try {
+    var media = JSON.parse(media_string);
+    if (Array.isArray(media) || typeof media != 'object') return result;
+    result.num_srcset_all = media.num_srcset_all || 0;
+    result.num_srcset_sizes = media.num_srcset_sizes || 0;
+    result.num_picture_img = media.num_picture_img || 0;
+} catch (e) {}
+return result;
+''';
+
+WITH page_data AS (
+  SELECT
+    client,
+    get_media_info(JSON_EXTRACT_SCALAR(payload, '$._media')) AS media_info
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01'
+    AND is_root_page
+)
+
+SELECT
+  client,
+  ROUND(SAFE_DIVIDE(COUNTIF(media_info.num_srcset_all > 0), COUNT(0)) * 100, 2) AS pages_with_srcset_pct,
+  ROUND(SAFE_DIVIDE(COUNTIF(media_info.num_srcset_sizes > 0), COUNT(0)) * 100, 2) AS pages_with_srcset_sizes_pct,
+  ROUND(SAFE_DIVIDE((COUNTIF(media_info.num_srcset_all > 0) - COUNTIF(media_info.num_srcset_sizes > 0)), COUNT(0)) * 100, 2) AS pages_with_srcset_wo_sizes_pct,
+  ROUND(SAFE_DIVIDE(SUM(media_info.num_srcset_sizes), SUM(media_info.num_srcset_all)) * 100, 2) AS instances_of_srcset_sizes_pct,
+  ROUND(SAFE_DIVIDE((SUM(media_info.num_srcset_all) - SUM(media_info.num_srcset_sizes)), SUM(media_info.num_srcset_all)) * 100, 2) AS instances_of_srcset_wo_sizes_pct,
+  ROUND(SAFE_DIVIDE(COUNTIF(media_info.num_picture_img > 0), COUNT(0)) * 100, 2) AS pages_with_picture_pct
+FROM page_data
+GROUP BY
+  client
+ORDER BY
+  client
---
+++ sql/2024/sustainability/text_compression.sql
@@ -0,0 +1,53 @@
+CREATE TEMP FUNCTION getContentEncoding(headers STRING)
+RETURNS STRING
+LANGUAGE js AS """
+  try {
+    const parsedHeaders = JSON.parse(headers);
+    for (let i = 0; i < parsedHeaders.length; i++) {
+      if (parsedHeaders[i].name.toLowerCase() === 'content-encoding') {
+        return parsedHeaders[i].value.toLowerCase();
+      }
+    }
+  } catch (e) {}
+  return null;
+""";
+
+WITH request_data AS (
+  SELECT
+    client,
+    getContentEncoding(JSON_EXTRACT(payload, '$.response.headers')) AS resp_content_encoding
+  FROM
+    `httparchive.all.requests`
+  WHERE
+    date = '2024-06-01'
+),
+
+compression_data AS (
+  SELECT
+    client,
+    CASE 
+      WHEN resp_content_encoding = 'gzip' THEN 'Gzip'
+      WHEN resp_content_encoding = 'br' THEN 'Brotli'
+      WHEN resp_content_encoding IS NULL THEN 'no text compression'
+      ELSE 'other'
+    END AS compression_type,
+    COUNT(0) AS num_requests,
+    SUM(COUNT(0)) OVER (PARTITION BY client) AS total,
+    ROUND(COUNT(0) / SUM(COUNT(0)) OVER (PARTITION BY client) * 100, 2) AS pct
+  FROM
+    request_data
+  GROUP BY
+    client,
+    compression_type
+)
+
+SELECT
+  client,
+  compression_type,
+  num_requests,
+  total,
+  pct
+FROM compression_data
+ORDER BY
+  client,
+  num_requests DESC
