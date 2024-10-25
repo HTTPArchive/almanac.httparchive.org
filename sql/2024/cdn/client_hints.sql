36b0c11ef531feb90b386d31b0dae0f1bc7907c5
Joe Viggiano <17817811+joeviggiano@users.noreply.github.com>
2024-10-25 02:22:05 -0400

    Added counts for client hints, added early hints counts by CDN

--- sql/2024/cdn/client_hints.sql
+++ sql/2024/cdn/client_hints.sql
@@ -1,6 +1,12 @@
-#standardSQL
-# client_hints.sql : Distribution of status code by client
-
-select client,sc,count(sc) from (
-select client,JSON_EXTRACT_SCALAR(summary, '$.status') as sc from `httparchive.all.requests` WHERE date = "2024-06-01")
-group by client,sc
+SELECT 
+DISTINCT(COUNT(*)) AS Total
+FROM `httparchive.all.requests`
+WHERE date = "2024-06-01"
+UNION ALL
+SELECT 
+DISTINCT(COUNT(req)) AS ClientHints
+FROM `httparchive.all.requests` AS req,
+UNNEST(response_headers) as header
+WHERE date = "2024-06-01"
+AND header.name = 'accept-ch'
+AND header.value IS NOT NULL
