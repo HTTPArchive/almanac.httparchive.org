a646f8e005d0688aeeddcc60495e4aa1730a77bf
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-10-31 23:34:33 +0100

    lint

--- sql/2024/privacy/most_common_cname_domains.sql
+++ sql/2024/privacy/most_common_cname_domains.sql
@@ -26,8 +26,8 @@ WITH whotracksme AS (
     NET.REG_DOMAIN(cnames.cname) AS cname_domain,
     COUNT(DISTINCT NET.REG_DOMAIN(cnames.origin)) AS number_of_request_domains,
     COUNT(DISTINCT page) AS number_of_pages
-    --ARRAY_AGG(DISTINCT cnames.origin LIMIT 2) AS request_domain_examples,
-    --ARRAY_AGG(DISTINCT page LIMIT 2) AS page_examples,
+  --ARRAY_AGG(DISTINCT cnames.origin LIMIT 2) AS request_domain_examples,
+  --ARRAY_AGG(DISTINCT page LIMIT 2) AS page_examples,
   FROM `httparchive.all.pages`,
     UNNEST(convert_cname_json(JSON_QUERY(custom_metrics, '$.privacy.request_hostnames_with_cname'))) AS cnames
   WHERE date = '2024-06-01' AND
@@ -52,9 +52,9 @@ SELECT
   cnames.cname_domain AS cname,
   number_of_request_domains,
   number_of_pages,
-  (number_of_pages / total_pages) AS pct_pages,
-  --request_domain_examples,
-  --page_examples
+  (number_of_pages / total_pages) AS pct_pages
+--request_domain_examples,
+--page_examples
 FROM cnames
 LEFT JOIN pages_total
 USING (client)
