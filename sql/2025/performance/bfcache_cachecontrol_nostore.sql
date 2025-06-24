cfffbe0bc56ae614c62a01ad0c165ba8d400c772
Tanner Hodges <me@tannerhodges.com>
2025-06-24 00:32:28 -0400

    Begin migrating 2024 queries to 2025

---
+++ sql/2025/performance/bfcache_cachecontrol_nostore.sql
@@ -0,0 +1,29 @@
+CREATE TEMP FUNCTION HAS_NO_STORE_DIRECTIVE(cache_control STRING) RETURNS BOOL AS (
+  REGEXP_CONTAINS(cache_control, r'(?i)\bno-store\b')
+);
+
+WITH requests AS (
+  SELECT
+    client,
+    LOGICAL_OR(HAS_NO_STORE_DIRECTIVE(JSON_VALUE(payload._cacheControl))) AS includes_ccns
+  FROM
+    `httparchive.crawl.requests`
+  WHERE
+    date = '2025-06-01' AND
+    is_main_document
+  GROUP BY
+    client,
+    page
+)
+
+SELECT
+  client,
+  COUNTIF(includes_ccns) AS pages,
+  COUNT(0) AS total,
+  COUNTIF(includes_ccns) / COUNT(0) AS pct
+FROM
+  requests
+GROUP BY
+  client
+ORDER BY
+  client
---
+++ sql/2025/performance/font_resource_hints_usage.sql
@@ -0,0 +1,65 @@
+CREATE TEMPORARY FUNCTION getResourceHints(custom_metrics STRING)
+RETURNS ARRAY<STRUCT<name STRING, href STRING>>
+LANGUAGE js AS '''
+var hints = new Set(['preload', 'prefetch', 'preconnect', 'prerender', 'dns-prefetch']);
+try {
+    var $ = JSON.parse(custom_metrics);
+    var almanac = $.other.almanac;
+    return almanac['link-nodes'].nodes.reduce((results, link) => {
+        var hint = link.rel.toLowerCase();
+        if (!hints.has(hint)) {
+            return results;
+        }
+        results.push({
+            name: hint,
+            href: link.href
+        });
+        return results;
+    }, []);
+} catch (e) {
+    return [];
+}
+''';
+
+WITH resource_hints AS (
+  SELECT DISTINCT
+    client,
+    page,
+    hint.name
+  FROM
+    `httparchive.crawl.pages`
+  LEFT JOIN
+    UNNEST(getResourceHints(TO_JSON_STRING(custom_metrics))) AS hint
+  WHERE
+    date = '2025-06-01' AND
+    is_root_page
+),
+
+font_requests AS (
+  SELECT
+    client,
+    page,
+    type
+  FROM
+    `httparchive.crawl.requests`
+  WHERE
+    date = '2025-06-01' AND
+    type = 'font' AND
+    is_root_page
+)
+
+SELECT
+  client,
+  name,
+  COUNT(DISTINCT page) AS pages,
+  SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client) AS total,
+  COUNT(DISTINCT page) / SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client) AS pct_hints
+FROM
+  resource_hints
+LEFT JOIN
+  font_requests
+USING (client, page)
+GROUP BY
+  client, name, type
+ORDER BY
+  pct_hints DESC;
