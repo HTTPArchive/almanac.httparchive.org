af39f30205669d15e31a80361508416741b28092
Tanner Hodges <me@tannerhodges.com>
2025-06-28 20:25:10 -0400

    Migrate 2024 queries to 2025: Render-Blocking Resources

---
+++ sql/2025/performance/render_blocking_savings_lcp.sql
@@ -0,0 +1,41 @@
+WITH data AS (
+  SELECT
+    client,
+    page,
+    rank,
+    CAST(JSON_VALUE(lighthouse.audits.`render-blocking-resources`.metricSavings.LCP) AS INT64) AS metricSavings_lcp,
+    REGEXP_EXTRACT(JSON_VALUE(custom_metrics.performance.lcp_elem_stats.url), r'([^#]*)') AS lcp_url
+  FROM
+    `httparchive.crawl.pages`
+  WHERE
+    date = '2025-06-01' AND
+    is_root_page
+)
+
+SELECT
+  percentile,
+  client,
+  APPROX_QUANTILES(metricSavings_lcp, 1000)[OFFSET(percentile * 10)] AS metricSavings_lcp,
+  lcp_type
+FROM (
+  SELECT
+    client,
+    metricSavings_lcp,
+    CASE
+      WHEN lcp_url = '' THEN 'text'
+      ELSE 'image'
+    END AS lcp_type
+  FROM
+    data
+),
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+WHERE
+  lcp_type = 'text'
+GROUP BY
+  percentile,
+  lcp_type,
+  client
+ORDER BY
+  percentile,
+  lcp_type,
+  client
