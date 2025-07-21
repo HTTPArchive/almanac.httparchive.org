9d4d1a41bf896ff36475e7a904f58b6f2b7c1ac2
Burak Güneli <burak.guneli@gmail.com>
2025-07-21 09:48:46 +0200

    Migrate 2024 sustainability queries to 2025 crawl dataset

---
+++ sql/2025/sustainability/unused_js_bytes.sql
@@ -0,0 +1,18 @@
+#standardSQL
+# Distribution of unused JS request bytes per page
+
+SELECT
+  client,
+  percentile,
+  APPROX_QUANTILES(CAST(JSON_VALUE(lighthouse, '$.audits.unused-javascript.details.overallSavingsBytes') AS INT64) / 1024, 1000)[OFFSET(percentile * 10)] AS js_kilobytes
+FROM
+  `httparchive.crawl.pages`,
+  UNNEST([10, 25, 50, 75, 90, 100]) AS percentile
+WHERE
+  date = '2025-06-01'
+GROUP BY
+  client,
+  percentile
+ORDER BY
+  client,
+  percentile 
