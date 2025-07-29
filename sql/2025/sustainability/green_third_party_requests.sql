badf4154cd484407bd985f967269e546618b142e
Burak Güneli <burak.guneli@gmail.com>
2025-07-29 13:50:55 +0100

    fix linter errors for green_third_party

--- sql/2025/sustainability/green_third_party_requests.sql
+++ sql/2025/sustainability/green_third_party_requests.sql
@@ -1,4 +1,5 @@
 #standardSQL
+# Median third-parties & green third-party requests per websites by rank
 
 WITH requests AS (
     SELECT
@@ -34,80 +35,80 @@ pages AS (
 
 third_party AS (
     SELECT
-        domain,
-        COUNT(DISTINCT page) AS page_usage
+        tp.domain,
+        COUNT(DISTINCT r.page) AS page_usage
     FROM
         `httparchive.almanac.third_parties` AS tp
     INNER JOIN
         requests AS r
     ON NET.HOST(r.url) = NET.HOST(tp.domain)
     WHERE
-        date = '2025-06-01' AND
-        category NOT IN ('hosting')
+        tp.date = '2025-06-01' AND
+        tp.category NOT IN ('hosting')
     GROUP BY
-        domain
+        tp.domain
     HAVING
         page_usage >= 50
 ),
 
 green_tp AS (
-    SELECT domain
+    SELECT tp.domain
     FROM
         `httparchive.almanac.third_parties` AS tp
     INNER JOIN
         green AS g
     ON NET.HOST(g.host) = NET.HOST(tp.domain)
     WHERE
-        date = '2025-06-01' AND
-        category NOT IN ('hosting')
+        tp.date = '2025-06-01' AND
+        tp.category NOT IN ('hosting')
     GROUP BY
-        domain
+        tp.domain
 ),
 
 base AS (
     SELECT
-        client,
-        page,
-        rank,
-        COUNT(domain) AS third_parties_per_page
+        r.client,
+        r.page,
+        p.rank,
+        COUNT(tp.domain) AS third_parties_per_page
     FROM
-        requests
+        requests AS r
     LEFT JOIN
-        third_party
+        third_party AS tp
     ON
-        NET.HOST(requests.url) = NET.HOST(third_party.domain)
+        NET.HOST(r.url) = NET.HOST(tp.domain)
     INNER JOIN
-        pages
-    USING (client, page)
+        pages AS p
+    ON r.client = p.client AND r.page = p.page
     GROUP BY
-        client,
-        page,
-        rank
+        r.client,
+        r.page,
+        p.rank
 ),
 
 base_green AS (
     SELECT
-        client,
-        page,
-        rank,
-        COUNT(domain) AS green_third_parties_per_page
+        r.client,
+        r.page,
+        p.rank,
+        COUNT(gtp.domain) AS green_third_parties_per_page
     FROM
-        requests
+        requests AS r
     LEFT JOIN
-        green_tp
+        green_tp AS gtp
     ON
-        NET.HOST(requests.url) = NET.HOST(green_tp.domain)
+        NET.HOST(r.url) = NET.HOST(gtp.domain)
     INNER JOIN
-        pages
-    USING (client, page)
+        pages AS p
+    ON r.client = p.client AND r.page = p.page
     GROUP BY
-        client,
-        page,
-        rank
+        r.client,
+        r.page,
+        p.rank
 )
 
 SELECT
-    client,
+    b.client,
     rank_grouping,
     CASE
         WHEN rank_grouping = 0 THEN ''
@@ -115,28 +116,31 @@ SELECT
         ELSE FORMAT("%'d", rank_grouping)
     END AS ranking,
     APPROX_QUANTILES(
-        third_parties_per_page, 1000
+        b.third_parties_per_page, 1000
     ) [OFFSET(500)] AS p50_third_parties_per_page,
     APPROX_QUANTILES(
-        green_third_parties_per_page, 1000
+        bg.green_third_parties_per_page, 1000
     ) [OFFSET(500)] AS p50_green_third_parties_per_page,
     APPROX_QUANTILES(
-        SAFE_DIVIDE(green_third_parties_per_page, third_parties_per_page), 1000
+        SAFE_DIVIDE(
+            bg.green_third_parties_per_page,
+            b.third_parties_per_page
+        ), 1000
     ) [OFFSET(500)] AS pct_green
 FROM
-    base,
+    base AS b,
     UNNEST([1000, 10000, 100000, 1000000, 10000000, 100000000]) AS rank_grouping
 INNER JOIN
-    base_green
+    base_green AS bg
 ON
-    base.client = base_green.client AND
-    base.page = base_green.page AND
-    base.rank = base_green.rank
+    b.client = bg.client AND
+    b.page = bg.page AND
+    b.rank = bg.rank
 WHERE
-    rank <= rank_grouping
+    b.rank <= rank_grouping
 GROUP BY
-    client,
+    b.client,
     rank_grouping
 ORDER BY
-    client,
+    b.client,
     rank_grouping
