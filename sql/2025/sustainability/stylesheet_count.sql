df2d968eac161aa8820a216f267613544e9a113c
Burak Güneli <burak.guneli@gmail.com>
2025-07-29 16:18:34 +0100

    fix linter issues

--- sql/2025/sustainability/stylesheet_count.sql
+++ sql/2025/sustainability/stylesheet_count.sql
@@ -4,92 +4,25 @@ WITH stylesheet_data AS (
   SELECT
     client,
     page,
-    CAST(
-      JSON_EXTRACT_SCALAR(
-        JSON_EXTRACT(
-          JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'
-        ),
-        '$.stylesheets'
-      ) AS INT64
-    ) AS external_stylesheets,
-    CAST(
-      JSON_EXTRACT_SCALAR(
-        JSON_EXTRACT(
-          JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'
-        ),
-        '$.inlineStyles'
-      ) AS INT64
-    ) AS inline_stylesheets,
+    CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.stylesheets') AS INT64) AS external_stylesheets,
+    CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.inlineStyles') AS INT64) AS inline_stylesheets,
     SAFE_DIVIDE(
-      CAST(
-        JSON_EXTRACT_SCALAR(
-          JSON_EXTRACT(
-            JSON_EXTRACT_SCALAR(payload, '$._javascript'),
-            '$.document'
-          ),
-          '$.inlineStyles'
-        ) AS INT64
-      ),
-      CAST(
-        JSON_EXTRACT_SCALAR(
-          JSON_EXTRACT(
-            JSON_EXTRACT_SCALAR(payload, '$._javascript'),
-            '$.document'
-          ),
-          '$.stylesheets'
-        ) AS INT64
-      ) +
-      CAST(
-        JSON_EXTRACT_SCALAR(
-          JSON_EXTRACT(
-            JSON_EXTRACT_SCALAR(payload, '$._javascript'),
-            '$.document'
-          ),
-          '$.inlineStyles'
-        ) AS INT64
-      )
+      CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.inlineStyles') AS INT64),
+      CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.stylesheets') AS INT64) +
+      CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.inlineStyles') AS INT64)
     ) AS pct_inline_stylesheets,
     SAFE_DIVIDE(
-      CAST(
-        JSON_EXTRACT_SCALAR(
-          JSON_EXTRACT(
-            JSON_EXTRACT_SCALAR(payload, '$._javascript'),
-            '$.document'
-          ),
-          '$.stylesheets'
-        ) AS INT64
-      ),
-      CAST(
-        JSON_EXTRACT_SCALAR(
-          JSON_EXTRACT(
-            JSON_EXTRACT_SCALAR(payload, '$._javascript'),
-            '$.document'
-          ),
-          '$.stylesheets'
-        ) AS INT64
-      ) +
-      CAST(
-        JSON_EXTRACT_SCALAR(
-          JSON_EXTRACT(
-            JSON_EXTRACT_SCALAR(payload, '$._javascript'),
-            '$.document'
-          ),
-          '$.inlineStyles'
-        ) AS INT64
-      )
+      CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.stylesheets') AS INT64),
+      CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.stylesheets') AS INT64) +
+      CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.inlineStyles') AS INT64)
     ) AS pct_external_stylesheets
   FROM
     `httparchive.crawl.pages`
   WHERE
-    date = '2025-06-01'
+    date = '2024-06-01'
     AND
     is_root_page = TRUE AND
-    JSON_EXTRACT_SCALAR(
-      JSON_EXTRACT(
-        JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'
-      ),
-      '$.stylesheets'
-    ) IS NOT NULL
+    JSON_EXTRACT_SCALAR(JSON_EXTRACT(JSON_EXTRACT_SCALAR(payload, '$._javascript'), '$.document'), '$.stylesheets') IS NOT NULL
 )
 
 SELECT
@@ -97,25 +30,10 @@ SELECT
   COUNT(DISTINCT page) AS pages_analyzed,
   SUM(external_stylesheets) AS external_stylesheets,
   SUM(inline_stylesheets) AS inline_stylesheets,
-  SAFE_DIVIDE(
-    SUM(inline_stylesheets), SUM(inline_stylesheets + external_stylesheets)
-  ) AS pct_inline_stylesheets,
-  SAFE_DIVIDE(
-    SUM(external_stylesheets),
-    SUM(inline_stylesheets + external_stylesheets)
-  ) AS pct_external_stylesheets,
-  APPROX_QUANTILES(
-    SAFE_DIVIDE(
-      inline_stylesheets, inline_stylesheets + external_stylesheets
-    ),
-    1000
-  ) [OFFSET(500)] AS median_inline_stylesheets,
-  APPROX_QUANTILES(
-    SAFE_DIVIDE(
-      external_stylesheets, inline_stylesheets + external_stylesheets
-    ),
-    1000
-  ) [OFFSET(500)] AS median_external_stylesheets
+  SAFE_DIVIDE(SUM(inline_stylesheets), SUM(inline_stylesheets + external_stylesheets)) AS pct_inline_stylesheets,
+  SAFE_DIVIDE(SUM(external_stylesheets), SUM(inline_stylesheets + external_stylesheets)) AS pct_external_stylesheets,
+  APPROX_QUANTILES(SAFE_DIVIDE(inline_stylesheets, inline_stylesheets + external_stylesheets), 1000)[OFFSET(500)] AS median_inline_stylesheets,
+  APPROX_QUANTILES(SAFE_DIVIDE(external_stylesheets, inline_stylesheets + external_stylesheets), 1000)[OFFSET(500)] AS median_external_stylesheets
 FROM
   stylesheet_data
 GROUP BY
