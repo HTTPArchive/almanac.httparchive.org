76aefbf99c6a62daf478cc7621d010bea41295ab
Dave Smart <dave@tamethebots.com>
2025-07-30 12:59:14 +0100

    correct facades query

--- sql/2025/page-weight/facades-usage.sql
+++ sql/2025/page-weight/facades-usage.sql
@@ -1,11 +1,11 @@
 SELECT
   client,
   is_root_page,
-  COUNTIF(JSON_VALUE(lighthouse, '$.audits.third-party-facades.scoreDisplayMode') != 'notApplicable') AS pages_with_facades_available,
+  COUNTIF(JSON_VALUE(lighthouse, '$.audits.third-party-facades.score') IS NOT NULL) AS pages_with_facades_available,
   COUNTIF(JSON_VALUE(lighthouse, '$.audits.third-party-facades.scoreDisplayMode') = 'notApplicable') AS not_applicable_count,
   SUM(CAST(JSON_VALUE(lighthouse, '$.audits.third-party-facades.metricSavings.TBT') AS FLOAT64)) AS total_blocking_time_savings_ms,
   COUNT(0) AS total_pages,
-  COUNTIF(JSON_VALUE(lighthouse, '$.audits.third-party-facades.scoreDisplayMode') != 'notApplicable') / (COUNT(0) - COUNTIF(JSON_VALUE(lighthouse, '$.audits.third-party-facades.scoreDisplayMode') = 'notApplicable')) AS pct_pages_with_opportunities
+  COUNTIF(JSON_VALUE(lighthouse, '$.audits.third-party-facades.score') IS NOT NULL) / (COUNT(0) - COUNTIF(JSON_VALUE(lighthouse, '$.audits.third-party-facades.scoreDisplayMode') = 'notApplicable')) AS pct_pages_with_opportunities
 FROM
   `httparchive.crawl.pages`
 WHERE
