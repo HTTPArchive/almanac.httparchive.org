764500983278e2a45028a05b8170856e7b7424ce
Daan Vansteenhuyse <daan.vansteenhuyse@kuleuven.be>
2025-07-30 17:46:37 +0200

    Rewrote feature adaption queries to use response headers field

--- sql/2025/security/feature_adoption_by_category.sql
+++ sql/2025/security/feature_adoption_by_category.sql
@@ -2,18 +2,19 @@
 # Section: Drivers of security mechanism adoption - Website Category
 # Question: How is security feature adoption and category of a website related?
 # Note: Not all headers have their individual percentages
-# Note: Currenly uses regex search on respOtherHeaders that can have false positives if a header name is used as a value of a header; could use the new response_header struct instead
 # Note: Only on the main document (is_main_document)
-CREATE TEMP FUNCTION getNumSecurityHeaders(headers STRING) AS (
+CREATE TEMP FUNCTION getNumSecurityHeaders(headers ARRAY<STRING>) AS (
   (
     SELECT
-      COUNTIF(REGEXP_CONTAINS(headers, CONCAT('(?i)', headername, ' ')))
+      COUNT(*)
     FROM
       UNNEST([
-        'Content-Security-Policy', 'Content-Security-Policy-Report-Only', 'Cross-Origin-Embedder-Policy', 'Cross-Origin-Opener-Policy',
-        'Cross-Origin-Resource-Policy', 'Expect-CT', 'Feature-Policy', 'Permissions-Policy', 'Referrer-Policy', 'Report-To',
-        'Strict-Transport-Security', 'X-Content-Type-Options', 'X-Frame-Options', 'X-XSS-Protection'
+        'content-security-policy', 'content-security-policy-report-only', 'cross-origin-embedder-policy', 'cross-origin-opener-policy',
+        'cross-origin-resource-policy', 'expect-ct', 'feature-policy', 'permissions-policy', 'referrer-policy', 'report-to',
+        'strict-transport-security', 'x-content-type-options', 'x-frame-options', 'x-xss-protection'
       ]) AS headername
+      WHERE
+        headername IN UNNEST(headers)
   )
 );
 
@@ -23,23 +24,23 @@ SELECT
   COUNT(0) AS total_pages_for_category,
   COUNTIF(STARTS_WITH(url, 'https')) AS freq_https,
   SAFE_DIVIDE(COUNTIF(STARTS_WITH(url, 'https')), COUNT(0)) AS pct_https,
-  SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)X-Frame-Options ')), COUNT(0)) AS pct_xfo,
-  SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)Strict-Transport-Security ')), COUNT(0)) AS pct_hsts,
-  SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)X-Content-Type-Options ')), COUNT(0)) AS pct_xcto,
-  SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)Expect-CT ')), COUNT(0)) AS pct_expectct,
-  SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)Content-Security-Policy ')), COUNT(0)) AS pct_csp,
-  SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)Content-Security-Policy-Report-Only ')), COUNT(0)) AS pct_cspro,
-  SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(respOtherHeaders, '(?i)X-XSS-Protection ')), COUNT(0)) AS pct_xss,
-  AVG(getNumSecurityHeaders(respOtherHeaders)) AS avg_security_headers,
-  APPROX_QUANTILES(getNumSecurityHeaders(respOtherHeaders), 1000)[OFFSET(500)] AS median_security_headers
+  SAFE_DIVIDE(COUNTIF('x-frame-options' IN UNNEST(respHeaders)), COUNT(0)) AS pct_xfo,
+  SAFE_DIVIDE(COUNTIF('strict-transport-security' IN UNNEST(respHeaders)), COUNT(0)) AS pct_hsts,
+  SAFE_DIVIDE(COUNTIF('x-content-type-options' IN UNNEST(respHeaders)), COUNT(0)) AS pct_xcto,
+  SAFE_DIVIDE(COUNTIF('expect-ct' IN UNNEST(respHeaders)), COUNT(0)) AS pct_expectct,
+  SAFE_DIVIDE(COUNTIF('content-security-policy' IN UNNEST(respHeaders)), COUNT(0)) AS pct_csp,
+  SAFE_DIVIDE(COUNTIF('content-security-policy-report-only' IN UNNEST(respHeaders)), COUNT(0)) AS pct_cspro,
+  SAFE_DIVIDE(COUNTIF('x-xss-protection' IN UNNEST(respHeaders)), COUNT(0)) AS pct_xss,
+  AVG(getNumSecurityHeaders(respHeaders)) AS avg_security_headers,
+  APPROX_QUANTILES(getNumSecurityHeaders(respHeaders), 1000)[OFFSET(500)] AS median_security_headers
 FROM (
   SELECT
     client,
     SPLIT(parent_category, '/')[1] AS category,
-    JSON_VALUE(r.summary, '$.respOtherHeaders') AS respOtherHeaders,
+    ARRAY(SELECT h.name from UNNEST(r.response_headers) as h) AS respHeaders,
     url
   FROM
-    `httparchive.all.requests` AS r
+    `httparchive.crawl.requests` AS r
   INNER JOIN
     UNNEST(`httparchive.fn.GET_HOST_CATEGORIES`(url))
   WHERE
