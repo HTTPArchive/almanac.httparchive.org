e1c773958f6f3787a8d85f4522683474edee118e
Daan Vansteenhuyse <daan.vansteenhuyse@kuleuven.be>
2025-07-30 12:29:37 +0200

    Rewrote documentdomain usage to use crawl.pages

--- sql/2025/security/documentdomain_usage.sql
+++ sql/2025/security/documentdomain_usage.sql
@@ -6,14 +6,34 @@
 # Note: DocumentDomainSettingWithoutOriginAgentClusterHeader seems broken as the OAC header is very rare and yet the difference between DocumentSetDomain and DocumentDomainSettingWithoutOriginAgentClusterHeader is large (Explanation: from '20230201' they count no header as header exist.)
 SELECT
   client,
-  feature,
-  num_urls,
-  total_urls,
-  pct_urls
-FROM
-  `httparchive.blink_features.usage`
-WHERE
-  yyyymmdd = '20250701' AND
-  feature IN UNNEST(['DocumentSetDomain', 'DocumentDomainSettingWithoutOriginAgentClusterHeader', 'DocumentDomainSetWithDefaultPort', 'DocumentDomainSetWithNonDefaultPort', 'CrossOriginAccessBasedOnDocumentDomain', 'DocumentDomainEnabledCrossOriginAccess', 'DocumentDomainBlockedCrossOriginAccess', 'DocumentOpenAliasedOriginDocumentDomain'])
+  featurename,
+  COUNT(DISTINCT page) AS total_pages,
+  COUNT(DISTINCT IF(LOWER(feature) = LOWER(featurename), page, NULL)) AS count_with_feature,
+  COUNT(DISTINCT IF(LOWER(feature) = LOWER(featurename), page, NULL)) / COUNT(DISTINCT page) AS pct_with_feature
+FROM (
+  SELECT
+    client,
+    page,
+    features.feature AS feature
+  FROM
+    `httparchive.crawl.pages`,
+    UNNEST(features) AS features
+  WHERE
+    date = '2025-07-01'
+),
+  UNNEST([
+    'DocumentSetDomain',
+    'DocumentDomainSettingWithoutOriginAgentClusterHeader',
+    'DocumentDomainSetWithDefaultPort',
+    'DocumentDomainSetWithNonDefaultPort',
+    'CrossOriginAccessBasedOnDocumentDomain',
+    'DocumentDomainEnabledCrossOriginAccess',
+    'DocumentDomainBlockedCrossOriginAccess',
+    'DocumentOpenAliasedOriginDocumentDomain'
+  ]) AS featurename
+GROUP BY
+  client,
+  featurename
 ORDER BY
-  pct_urls DESC
+  client,
+  featurename
