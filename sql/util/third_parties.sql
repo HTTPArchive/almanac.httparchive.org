02e2665c7701ba974395d767601a854d31dbc823
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-08-02 01:54:21 +0200

    cleanup

--- sql/util/third_parties.sql
+++
@@ -1,6 +0,0 @@
---In general we want to avoid "SELECT *" but we'll make an exception here so disable rile L044
-SELECT -- noqa: AM04
-  DATE('2020-08-01') AS date,
-  *
-FROM
-  `lighthouse-infrastructure.third_party_web.2020_08_01`
