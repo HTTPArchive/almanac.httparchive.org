463c11b621723f02f3024ac4961cb260b6cabc19
Daan Vansteenhuyse <daan.vansteenhuyse@kuleuven.be>
2025-09-01 15:00:37 +0200

    Updated json selectors

--- sql/2025/security/audit_issues.sql
+++ sql/2025/security/audit_issues.sql
@@ -1,29 +1,22 @@
 #standardSQL
 # Question: What Audit issues are present in the pages? https://chromedevtools.github.io/devtools-protocol/tot/Audits/#type-InspectorIssueCode
-#standardSQL
 SELECT
   client,
   issuename,
   COUNT(DISTINCT page) AS total_pages,
-  COUNT(DISTINCT IF(LOWER(JSON_VALUE(issue, '$.code')) = LOWER(issuename), page, NULL)) AS count_with_issue,
-  COUNT(DISTINCT IF(LOWER(JSON_VALUE(issue, '$.code')) = LOWER(issuename), page, NULL)) / COUNT(DISTINCT page) AS pct_with_issue
-FROM (
-  SELECT
-    client,
-    page,
-    issue
-  FROM
-    `httparchive.crawl.pages`,
-    UNNEST(JSON_QUERY_ARRAY(payload, '$._audit_issues')) AS issue
-  WHERE
-    date = '2025-07-01'
-),
+  COUNT(DISTINCT IF(LOWER(JSON_VALUE(issue.code)) = LOWER(issuename), page, NULL)) AS count_with_issue,
+  COUNT(DISTINCT IF(LOWER(JSON_VALUE(issue.code)) = LOWER(issuename), page, NULL)) / COUNT(DISTINCT page) AS pct_with_issue
+FROM
+  `httparchive.crawl.pages`,
+  UNNEST(JSON_QUERY_ARRAY(payload._audit_issues)) AS issue,
   UNNEST([
     'CookieIssue',
     'MixedContentIssue',
     'ContentSecurityPolicyIssue',
     'CorsIssue'
   ]) AS issuename
+WHERE
+  date = '2025-07-01' AND is_root_page
 GROUP BY
   client,
   issuename
