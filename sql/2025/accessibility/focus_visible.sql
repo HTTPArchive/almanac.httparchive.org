bcc48944f43578bb0648036ee1b6cd960d7aede3
Mike Gifford <mike.gifford@civicactions.com>
2025-09-01 21:41:11 -0400

    bringing over basic SQL with new date and with crawl.pages

---
+++ sql/2025/accessibility/focus_visible.sql
@@ -0,0 +1,85 @@
+#standardSQL
+
+CREATE TEMPORARY FUNCTION getSelectorParts(css STRING)
+RETURNS STRUCT<
+  class ARRAY<STRING>,
+  id ARRAY<STRING>,
+  attribute ARRAY<STRING>,
+  pseudo_class ARRAY<STRING>,
+  pseudo_element ARRAY<STRING>
+>
+LANGUAGE js
+OPTIONS (library = "gs://httparchive/lib/css-utils.js")
+AS '''
+try {
+  function compute(ast) {
+    let ret = {
+      class: {},
+      id: {},
+      attribute: {},
+      "pseudo-class": {},
+      "pseudo-element": {}
+    };
+
+    walkSelectors(ast, selector => {
+      let sast = parsel.parse(selector, {list: false});
+
+      parsel.walk(sast, node => {
+        if (node.type in ret) {
+          incrementByKey(ret[node.type], node.name);
+        }
+      }, {subtree: true});
+    });
+
+    for (let type in ret) {
+      ret[type] = sortObject(ret[type]);
+    }
+
+    return ret;
+  }
+
+  function unzip(obj) {
+    return Object.entries(obj).filter(([name, value]) => {
+      return !isNaN(value);
+    }).map(([name, value]) => name);
+  }
+
+  const ast = JSON.parse(css);
+  let parts = compute(ast);
+  return {
+    class: unzip(parts.class),
+    id: unzip(parts.id),
+    attribute: unzip(parts.attribute),
+    pseudo_class: unzip(parts['pseudo-class']),
+    pseudo_element: unzip(parts['pseudo-element'])
+  }
+} catch (e) {
+  return null;
+}
+''';
+
+SELECT
+  client,
+  COUNTIF(num_focus_visible > 0) AS pages_with_focus_visible,
+  COUNT(0) AS total_pages,
+  SAFE_DIVIDE(COUNTIF(num_focus_visible > 0), COUNT(0)) AS pct_pages_focus_visible
+FROM (
+  SELECT
+    client,
+    page,
+    COUNTIF(pseudo_class = 'focus-visible') AS num_focus_visible
+  FROM
+    `httparchive.all.parsed_css`
+  LEFT JOIN
+    UNNEST(getSelectorParts(css).pseudo_class) AS pseudo_class
+  WHERE
+    date = '2025-07-01' AND
+    LENGTH(css) < 0.1 * 1024 * 1024  -- Limit the size of the CSS to avoid OOM crashes
+  GROUP BY
+    client,
+    page
+)
+GROUP BY
+  client
+ORDER BY
+  pct_pages_focus_visible DESC;
---
+++ sql/2025/accessibility/skip_links.sql
@@ -0,0 +1,26 @@
+#standardSQL
+# % of pages having skip links
+
+CREATE TEMPORARY FUNCTION getEarlyHash(payload STRING)
+RETURNS INT64 LANGUAGE js AS '''
+try {
+  const almanac = JSON.parse(payload);
+  return almanac['seo-anchor-elements'].earlyHash;
+} catch (e) {
+  return 0;
+}
+''';
+
+SELECT
+  client,
+  is_root_page,
+  COUNTIF(getEarlyHash(JSON_EXTRACT_SCALAR(payload, '$._almanac')) > 0) AS pages,
+  COUNT(0) AS total,
+  COUNTIF(getEarlyHash(JSON_EXTRACT_SCALAR(payload, '$._almanac')) > 0) / COUNT(0) AS pct
+FROM
+  `httparchive.crawl.pages`
+WHERE
+  date = '2025-07-01'
+GROUP BY
+  client,
+  is_root_page;
