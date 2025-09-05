4d02f5cfd4ce52d27084c99bf40f9cf5faedb6ec
Chris Green <chr156r33n@gmail.com>
2025-09-05 19:27:06 +0100

    fix lighthouse query

--- sql/2025/seo/lighthouse-seo-stats-2025.sql
+++ sql/2025/seo/lighthouse-seo-stats-2025.sql
@@ -1,25 +1,24 @@
 #standardSQL
-# Lighthouse SEO Stats
+-- Lighthouse SEO Stats
 
 CREATE TEMPORARY FUNCTION getAudits(audits JSON)
 RETURNS ARRAY<STRUCT<
   id STRING,
-  weight INT64,
   title STRING,
   description STRING,
   score FLOAT64
->> LANGUAGE js AS '''
+>>
+LANGUAGE js AS r'''
 var auditsObj = audits || {};
 var results = [];
 for (var auditId in auditsObj) {
   if (Object.prototype.hasOwnProperty.call(auditsObj, auditId)) {
-    var audit = auditsObj[auditId] || {};
+    var a = auditsObj[auditId] || {};
     results.push({
       id: auditId,
-      weight: audit.weight || 0,
-      title: audit.title || '',
-      description: audit.description || '',
-      score: (audit.score ?? null)
+      title: a.title || '',
+      description: a.description || '',
+      score: (a.score ?? null)
     });
   }
 }
@@ -30,19 +29,24 @@ WITH lighthouse_extraction AS (
   SELECT
     client,
     page,
-    lighthouse AS report
+    lighthouse AS report,
+    CASE
+      WHEN is_root_page = FALSE THEN 'Secondarypage'
+      WHEN is_root_page = TRUE  THEN 'Homepage'
+      ELSE 'No Assigned Page'
+    END AS is_root_page
   FROM `httparchive.crawl.pages`
   WHERE date = '2025-07-01'
 )
 
 SELECT
   client,
+  is_root_page,
   audits.id AS id,
   COUNTIF(audits.score > 0) AS num_pages,
   COUNT(DISTINCT page) AS sites,
   COUNTIF(audits.score IS NOT NULL) AS total_applicable,
   SAFE_DIVIDE(COUNTIF(audits.score > 0), COUNTIF(audits.score IS NOT NULL)) AS pct,
-  APPROX_QUANTILES(audits.weight, 100)[OFFSET(50)] AS median_weight,
   MAX(audits.title) AS title,
   MAX(audits.description) AS description,
   SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client) AS total
@@ -50,8 +54,8 @@ FROM lighthouse_extraction,
 UNNEST(getAudits(JSON_EXTRACT(report, '$.audits'))) AS audits
 GROUP BY
   client,
+  is_root_page,
   audits.id
 ORDER BY
   client,
-  median_weight DESC,
   id;
