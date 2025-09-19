dc126be63c2eec4fba01d5232338d18b09a9f53a
Daan Vansteenhuyse <daan.vansteenhuyse@kuleuven.be>
2025-09-19 15:28:39 +0200

    Subquery removal

--- sql/2025/security/clear-site-data_value_prevalence.sql
+++ sql/2025/security/clear-site-data_value_prevalence.sql
@@ -4,24 +4,18 @@
 # Notes: Many used values are still invalid (without quotes). We only count each host-value pair once.
 SELECT
   client,
-  csd_header,
-  SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS total_csd_headers,
-  COUNT(DISTINCT host) AS freq,
-  COUNT(DISTINCT host) / SUM(COUNT(DISTINCT host)) OVER (PARTITION BY client) AS pct
-FROM (
-  SELECT
-    client,
-    NET.HOST(url) AS host,
-    response_headers.value AS csd_header
-  FROM
-    `httparchive.crawl.requests`,
-    UNNEST(response_headers) AS response_headers
-  WHERE
-    date = '2025-07-01' AND
-    is_root_page AND
-    # AND is_main_document # (Uncomment to only run on the main document response; majority of CSD headers are set on them)
-    LOWER(response_headers.name) = 'clear-site-data'
-)
+  response_headers.value AS csd_header,
+  SUM(COUNT(DISTINCT NET.HOST(url))) OVER (PARTITION BY client) AS total_csd_headers,
+  COUNT(DISTINCT NET.HOST(url)) AS freq,
+  COUNT(DISTINCT NET.HOST(url)) / SUM(COUNT(DISTINCT NET.HOST(url))) OVER (PARTITION BY client) AS pct
+FROM
+  `httparchive.crawl.requests`,
+  UNNEST(response_headers) AS response_headers
+WHERE
+  date = '2025-07-01'
+  AND is_root_page
+  # AND is_main_document # (Uncomment to only run on the main document response; majority of CSD headers are set on them)
+  AND LOWER(response_headers.name) = 'clear-site-data'
 GROUP BY
   client,
   csd_header
