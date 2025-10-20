7bfb9e88a55c560c96483485401181c45fb1b9de
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-10-20 22:49:55 +0200

    Include sql/** in lint workflow and lint only changed SQL files on PRs

--- sql/util/green_web_foundation.sql
+++ sql/util/green_web_foundation.sql
@@ -2,7 +2,7 @@ SELECT -- noqa: AM04
   DATE('2022-06-01') AS date,
   NET.HOST(LOWER(url)) AS host,
   NET.REG_DOMAIN(LOWER(url)) AS domain,
-  *
+  *,
 FROM
   # This is the raw database dump from GWF.
   `httparchive.almanac.green_web_foundation_raw`
