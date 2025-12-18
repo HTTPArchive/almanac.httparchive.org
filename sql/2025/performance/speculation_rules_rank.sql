0f99c99b18e47ee0a135f9a7cce3df2e8330f075
Tanner Hodges <me@tannerhodges.com>
2025-12-17 21:36:42 -0500

    Add total columns to speculation_rules_rank.sql

--- sql/2025/performance/speculation_rules_rank.sql
+++ sql/2025/performance/speculation_rules_rank.sql
@@ -1,12 +1,19 @@
 SELECT
   IF(ranking < 100000000, CAST(ranking AS STRING), 'all') AS ranking,
   client,
+  COUNT(DISTINCT page) AS total_sites,
   COUNTIF(
     custom_metrics.performance.speculation_rules IS NOT NULL AND (
       ARRAY_LENGTH(JSON_QUERY_ARRAY(custom_metrics.performance.speculation_rules.htmlRules)) > 0 OR
       ARRAY_LENGTH(JSON_QUERY_ARRAY(custom_metrics.performance.speculation_rules.httpHeaderRules)) > 0
     )
-  ) / COUNT(0) AS has_speculation_rules
+  ) AS sites_with_speculation_rules,
+  COUNTIF(
+    custom_metrics.performance.speculation_rules IS NOT NULL AND (
+      ARRAY_LENGTH(JSON_QUERY_ARRAY(custom_metrics.performance.speculation_rules.htmlRules)) > 0 OR
+      ARRAY_LENGTH(JSON_QUERY_ARRAY(custom_metrics.performance.speculation_rules.httpHeaderRules)) > 0
+    )
+  ) / COUNT(DISTINCT page) AS pct_speculation_rules
 FROM
   `httparchive.crawl.pages`,
   UNNEST([1000, 10000, 100000, 1000000, 10000000, 100000000]) AS ranking
