0fd9f2c5ca7a5bc46206857e3037e044be7d3dc0
Joe Viggiano <17817811+joeviggiano@users.noreply.github.com>
2025-12-27 16:28:39 -0500

    Removed old early hints queries.

--- sql/2025/cdn/early_hints_by_cdn.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  (COUNT(0)) AS hints,
-  client,
-  REGEXP_EXTRACT(JSON_EXTRACT_SCALAR(summary, '$._cdn_provider'), r'^([^,]*).*') AS cdn
-FROM `httparchive.crawl.requests`
-WHERE date = '2025-07-01' AND
-  JSON_EXTRACT_ARRAY(payload, '$._early_hint_headers') IS NOT NULL AND
-  REGEXP_EXTRACT(JSON_EXTRACT_SCALAR(summary, '$._cdn_provider'), r'^([^,]*).*') IS NOT NULL
-GROUP BY cdn, client
--- sql/2025/cdn/early_hints_cdn_vs_origin.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  (COUNT(0)) AS Total,
-  client,
-  IF(IFNULL(NULLIF(REGEXP_EXTRACT(JSON_EXTRACT_SCALAR(summary, '$._cdn_provider'), r'^([^,]*).*'), ''), '') = '', 'ORIGIN', 'CDN') AS cdn
-FROM `httparchive.crawl.requests`
-WHERE date = '2025-07-01' AND
-  JSON_EXTRACT_ARRAY(payload, '$._early_hint_headers') IS NOT NULL
-GROUP BY cdn, client
