aa79bdff6eef9f0ec015993777b542375a955acd
Chris Green <75560394+chr156r33n@users.noreply.github.com>
2025-12-30 11:07:26 +0000

    Update sql/2025/seo/invalid-head-sites-2025.sql
    Co-authored-by: Barry Pollard <barrypollard@google.com>

--- sql/2025/seo/invalid-head-sites-2025.sql
+++ sql/2025/seo/invalid-head-sites-2025.sql
@@ -1,6 +1,9 @@
 #standardSQL
 # Counted metrics of invalid head elements in HTML
 
+#standardSQL
+# Counted metrics of invalid head elements in HTML
+
 WITH totals AS (
   SELECT
     client,
@@ -10,21 +13,12 @@ WITH totals AS (
       ELSE 'No Assigned Page'
     END AS is_root_page,
     page,
-    COALESCE(
-      JSON_EXTRACT_SCALAR(TO_JSON_STRING(custom_metrics.other),
-                          '$[\'valid-head\'][\'invalidHead\']'),
-      JSON_EXTRACT_SCALAR(TO_JSON_STRING(custom_metrics.other),
-                          '$[\'valid_head\'][\'invalidHead\']')
-    ) AS invalidHead,
-    ARRAY_LENGTH(
-      IFNULL(
-        JSON_EXTRACT_ARRAY(TO_JSON_STRING(custom_metrics.other),
-                           '$[\'valid-head\'][\'invalidElements\']'),
-        []
-      )
-    ) AS invalidCount
-  FROM `httparchive.crawl.pages`
-  WHERE date = '2025-07-01'
+    JSON_VALUE(custom_metrics.other.`valid-head`.invalidHead) AS invalidHead,
+    ARRAY_LENGTH(IFNULL(JSON_QUERY_ARRAY(custom_metrics.other.`valid-head`.invalidElements), [])) AS invalidCount
+  FROM
+    `httparchive.crawl.pages`
+  WHERE
+    date = '2025-07-01'
 )
 
 SELECT
@@ -35,6 +29,11 @@ SELECT
   SAFE_DIVIDE(COUNTIF(invalidHead = 'true'), COUNT(DISTINCT page)) AS pct_invalidHeads,
   COUNT(DISTINCT page) AS sites,
   SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client, is_root_page) AS total,
-FROM totals
-GROUP BY client, is_root_page
-ORDER BY client;
+FROM
+  totals
+GROUP BY
+  client,
+  is_root_page
+ORDER BY
+  client,
+  is_root_page
