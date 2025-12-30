fb27c6c911843c1832c9c02b81e6821e3a3a89c3
Chris Green <75560394+chr156r33n@users.noreply.github.com>
2025-12-30 10:54:54 +0000

    Update sql/2025/seo/anchor-same-site-occurance-stats-2025.sql
    Co-authored-by: Barry Pollard <barrypollard@google.com>

--- sql/2025/seo/anchor-same-site-occurance-stats-2025.sql
+++ sql/2025/seo/anchor-same-site-occurance-stats-2025.sql
@@ -53,15 +53,15 @@ WITH same_links_info AS (
 
 SELECT
   client,
-  wpt_bodies_info.links_same_site AS links_same_site,
+  anchors_info.links_same_site AS links_same_site,
   is_root_page,
   COUNT(DISTINCT page) AS sites, -- Counting all occurrences of links_same_site
   SAFE_DIVIDE(COUNT(0), COUNT(DISTINCT page)) AS pct_links_same_site,  -- Percentage of same-site links
-  AVG(wpt_bodies_info.links_window_location) AS avg_links_window_location,
-  AVG(wpt_bodies_info.links_window_open) AS avg_links_window_open,
-  AVG(wpt_bodies_info.links_href_javascript) AS avg_links_href_javascript,
-  AVG(wpt_bodies_info.links_window_location + wpt_bodies_info.links_window_open + wpt_bodies_info.links_href_javascript) AS avg_links_any,
-  MAX(wpt_bodies_info.links_window_location + wpt_bodies_info.links_window_open + wpt_bodies_info.links_href_javascript) AS max_links_any,
+  AVG(anchors_info.links_window_location) AS avg_links_window_location,
+  AVG(anchors_info.links_window_open) AS avg_links_window_open,
+  AVG(anchors_info.links_href_javascript) AS avg_links_href_javascript,
+  AVG(anchors_info.links_window_location + anchors_info.links_window_open + anchors_info.links_href_javascript) AS avg_links_any,
+  MAX(anchors_info.links_window_location + anchors_info.links_window_open + anchors_info.links_href_javascript) AS max_links_any,
   SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client, is_root_page) AS total,
   COUNT(0) / SUM(COUNT(DISTINCT page)) OVER (PARTITION BY client, is_root_page) AS pct -- Secondary page percentage within group
 FROM
@@ -69,6 +69,6 @@ FROM
 GROUP BY
   client,
   is_root_page,
-  wpt_bodies_info.links_same_site
+  anchors_info.links_same_site
 ORDER BY
   links_same_site ASC;
