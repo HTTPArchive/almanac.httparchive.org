5c9fa153d80b5ca29c1f155ceea54c3db3180647
Chris Green <75560394+chr156r33n@users.noreply.github.com>
2025-12-30 11:06:48 +0000

    Update sql/2025/seo/invalid-head-elements-2025.sql
    Co-authored-by: Barry Pollard <barrypollard@google.com>

--- sql/2025/seo/invalid-head-elements-2025.sql
+++ sql/2025/seo/invalid-head-elements-2025.sql
@@ -1,37 +1,17 @@
 #standardSQL
 # Invalid Head Elements
 
-WITH pages AS (
-  SELECT
-    client,
-    CASE
-      WHEN is_root_page = FALSE THEN 'Secondarypage'
-      WHEN is_root_page = TRUE  THEN 'Homepage'
-      ELSE 'No Assigned Page'
-    END AS is_root_page,
-    page,
-    custom_metrics.other AS other
-  FROM `httparchive.crawl.pages`
-  WHERE date = '2025-07-01'
-),
-
-invalid_elements AS (
+WITH invalid_elements AS (
   SELECT
     client,
     is_root_page,
     page,
-    -- each el is a JSON-formatted string; unwrap to plain string
-    JSON_EXTRACT_SCALAR(el, '$') AS element
-  FROM pages,
-  UNNEST(
-    IFNULL(
-      JSON_EXTRACT_ARRAY(
-        TO_JSON_STRING(other),
-        '$[\'valid-head\'][\'invalidElements\']'
-      ),
-      []
-    )
-  ) AS el
+    JSON_VALUE(el) AS element
+  FROM
+    `httparchive.crawl.pages`,
+    UNNEST(IFNULL(JSON_EXTRACT_ARRAY(custom_metrics.other.`valid-head`.invalidElements), [])) AS el
+  WHERE
+    date = '2025-07-01'
 ),
 
 total_sites AS (
@@ -39,19 +19,37 @@ total_sites AS (
     client,
     is_root_page,
     COUNT(DISTINCT page) AS total_sites
-  FROM pages
-  GROUP BY client, is_root_page
+  FROM
+    `httparchive.crawl.pages`
+  WHERE
+    date = '2025-07-01'
+  GROUP BY
+    client,
+    is_root_page
 )
 
 SELECT
   ie.client,
-  ie.is_root_page,
+  CASE
+    WHEN ie.is_root_page = FALSE THEN 'Secondarypage'
+    WHEN ie.is_root_page = TRUE  THEN 'Homepage'
+    ELSE 'No Assigned Page'
+  END AS is_root_page,
   ie.element,
   COUNT(DISTINCT ie.page) AS invalid_sites,
   ts.total_sites,
   SAFE_DIVIDE(COUNT(DISTINCT ie.page), ts.total_sites) AS pct_invalid
-FROM invalid_elements ie
-JOIN total_sites ts
-  ON ie.client = ts.client AND ie.is_root_page = ts.is_root_page
-GROUP BY ie.client, ie.is_root_page, ts.total_sites, ie.element
-ORDER BY ie.client;
+FROM
+  invalid_elements ie
+JOIN
+  total_sites ts ON ie.client = ts.client AND ie.is_root_page = ts.is_root_page
+GROUP BY
+  ie.client,
+  is_root_page,
+  ts.total_sites,
+  ie.element
+ORDER BY
+  ie.client,
+  is_root_page,
+  ts.total_sites,
+  ie.element
