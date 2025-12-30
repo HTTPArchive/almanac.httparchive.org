53b61cf7cb2ae9960971d8e0647ff91b52d50ba1
Chris Green <75560394+chr156r33n@users.noreply.github.com>
2025-12-30 10:50:31 +0000

    Update sql/2025/seo/unused-css-js-2025.sql
    Co-authored-by: Barry Pollard <barrypollard@google.com>

--- sql/2025/seo/unused-css-js-2025.sql
+++ sql/2025/seo/unused-css-js-2025.sql
@@ -11,33 +11,35 @@ SELECT
   COUNT(DISTINCT page) AS pages,
   SUM(unused_javascript) / COUNT(DISTINCT page) AS unused_javascript_kib_avg,
   SUM(unused_css_rules) / COUNT(DISTINCT page) AS unused_css_rules_kib_avg
-FROM (
-  SELECT
-    client,
-    page,
-    rank
-  FROM `httparchive.crawl.pages`
-  WHERE date = '2025-07-01'
-)
-LEFT JOIN (
-  SELECT
-    client,
-    page,
-    SAFE_DIVIDE(
-      CAST(JSON_EXTRACT_SCALAR(TO_JSON_STRING(lighthouse),
-           '$.audits["unused-javascript"].details.overallSavingsBytes') AS INT64),
-      1024
-    ) AS unused_javascript,
-    SAFE_DIVIDE(
-      CAST(JSON_EXTRACT_SCALAR(TO_JSON_STRING(lighthouse),
-           '$.audits["unused-css-rules"].details.overallSavingsBytes') AS INT64),
-      1024
-    ) AS unused_css_rules
-  FROM `httparchive.crawl.pages`
-  WHERE date = '2025-07-01'
-)
-USING (client, page),
-UNNEST([1000, 10000, 100000, 1000000, 10000000, 100000000]) AS rank_grouping
-WHERE rank <= rank_grouping
-GROUP BY client, rank_grouping
+FROM
+  (
+    SELECT
+      client,
+      page,
+      rank
+    FROM
+      `httparchive.crawl.pages`
+    WHERE
+      date = '2025-07-01'
+  )
+LEFT JOIN
+  (
+    SELECT
+      client,
+      page,
+      SAFE_DIVIDE(SAFE.INT64(lighthouse.audits.`unused-javascript`.details.overallSavingsBytes), 1024) AS unused_javascript,
+      SAFE_DIVIDE(SAFE.INT64(lighthouse.audits.`unused-css-rules`.details.overallSavingsBytes), 1024) AS unused_css_rules
+    FROM
+      `httparchive.crawl.pages`
+    WHERE
+      date = '2025-07-01'
+  ) USING (client, page),
+  UNNEST([1000, 10000, 100000, 1000000, 10000000, 100000000]) AS rank_grouping
+WHERE
+  rank <= rank_grouping
+GROUP BY
+  client,
+  rank_grouping
+ORDER BY
+  rank_grouping;
 ORDER BY rank_grouping;
