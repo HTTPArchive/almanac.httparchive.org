78b5212aa785500ab0177499566720e5a3aa1351
Barry Pollard <barrypollard@google.com>
2026-01-03 00:04:53 +0000

    Technical edits

--- sql/2025/seo/core-web-vitals-2025.sql
+++ sql/2025/seo/core-web-vitals-2025.sql
@@ -16,16 +16,24 @@ SELECT
     COUNT(DISTINCT IF(IS_GOOD(fast_fid, avg_fid, slow_fid), origin, NULL)),
     COUNT(DISTINCT IF(IS_NON_ZERO(fast_fid, avg_fid, slow_fid), origin, NULL))
   ) AS pct_good_fid,
+  # Origins with good INP divided by origins with any INP.
+  SAFE_DIVIDE(
+    COUNT(DISTINCT IF(IS_GOOD(fast_inp, avg_inp, slow_inp), origin, NULL)),
+    COUNT(DISTINCT IF(IS_NON_ZERO(fast_inp, avg_inp, slow_inp), origin, NULL))
+  ) AS pct_good_inp,
   # Origins with good CLS divided by origins with any CLS.
   SAFE_DIVIDE(
     COUNT(DISTINCT IF(IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL)),
     COUNT(DISTINCT IF(IS_NON_ZERO(small_cls, medium_cls, large_cls), origin, NULL))
   ) AS pct_good_cls,
-  # Origins with good LCP, FID, and CLS dividied by origins with any LCP, FID, and CLS.
+  # Origins with good LCP, FID/INP, and CLS dividied by origins with any LCP, and CLS.
   SAFE_DIVIDE(
     COUNT(DISTINCT IF(
       IS_GOOD(fast_lcp, avg_lcp, slow_lcp) AND
-      IS_GOOD(fast_fid, avg_fid, slow_fid) IS NOT FALSE AND
+      (
+        (date >= DATE'2024-03-01' AND IS_GOOD(fast_inp, avg_inp, slow_inp) IS NOT FALSE) OR
+        (date < DATE'2024-03-01' AND IS_GOOD(fast_fid, avg_fid, slow_fid) IS NOT FALSE)
+      ) AND
       IS_GOOD(small_cls, medium_cls, large_cls), origin, NULL
     )),
     COUNT(DISTINCT IF(
