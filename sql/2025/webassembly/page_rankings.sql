5142f9b62d9b2ba63933a0339189880243686400
Barry Pollard <barrypollard@google.com>
2026-01-11 15:40:00 +0000

    Lint

--- sql/2025/webassembly/page_rankings.sql
+++ sql/2025/webassembly/page_rankings.sql
@@ -1,21 +1,21 @@
 SELECT
-client,
-rank_grouping,
-CASE
-WHEN rank_grouping = 100000000 THEN 'all'
-ELSE CAST(rank_grouping AS STRING)
-END AS ranking,
-COUNT(DISTINCT page) AS pages
+  client,
+  rank_grouping,
+  CASE
+    WHEN rank_grouping = 100000000 THEN 'all'
+    ELSE CAST(rank_grouping AS STRING)
+  END AS ranking,
+  COUNT(DISTINCT page) AS pages
 FROM
-`httparchive.crawl.requests`,
-UNNEST([1000, 10000, 100000, 1000000, 10000000, 100000000]) AS rank_grouping
+  `httparchive.crawl.requests`,
+  UNNEST([1000, 10000, 100000, 1000000, 10000000, 100000000]) AS rank_grouping
 WHERE
-date = '2025-07-01' AND
-type = 'wasm' AND
-rank <= rank_grouping
+  date = '2025-07-01' AND
+  type = 'wasm' AND
+  rank <= rank_grouping
 GROUP BY
-client,
-rank_grouping
+  client,
+  rank_grouping
 ORDER BY
-client,
-rank_grouping
+  client,
+  rank_grouping
