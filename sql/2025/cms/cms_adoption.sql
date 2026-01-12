65bac445b8dec4ab9654081f82a53a80e64f66ed
Onur Guler <onur18guler@gmail.com>
2026-01-12 16:59:15 +0100

    updated 2025 cms sql queries

---
+++ sql/2025/cms/cms_adoption.sql
@@ -0,0 +1,161 @@
+#standardSQL
+# CMS adoption OVER time
+# cms_adoption.sql
+
+SELECT
+  client,
+  2025 AS year,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.crawl.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.crawl.pages`
+  WHERE
+    date = '2025-06-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'CMS' AND
+  date = '2025-06-01' AND
+  is_root_page
+GROUP BY
+  client,
+  total
+UNION ALL
+SELECT
+  client,
+  2024 AS year,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.crawl.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.crawl.pages`
+  WHERE
+    date = '2024-06-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'CMS' AND
+  date = '2024-06-01' AND
+  is_root_page
+GROUP BY
+  client,
+  total
+UNION ALL
+SELECT
+  client,
+  2023 AS year,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.crawl.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.crawl.pages`
+  WHERE
+    date = '2023-06-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'CMS' AND
+  date = '2023-06-01' AND
+  is_root_page
+GROUP BY
+  client,
+  total
+UNION ALL
+SELECT
+  client,
+  2022 AS year,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.crawl.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.crawl.pages`
+  WHERE
+    date = '2022-06-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'CMS' AND
+  date = '2022-06-01' AND
+  is_root_page
+GROUP BY
+  client,
+  total
+UNION ALL
+SELECT
+  client,
+  2021 AS year,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.crawl.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.crawl.pages`
+  WHERE
+    date = '2021-06-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'CMS' AND
+  date = '2021-06-01' AND
+  is_root_page
+GROUP BY
+  client,
+  total
+ORDER BY
+  year DESC,
+  pct DESC
---
+++ sql/2025/cms/cms_adoption_by_geo.sql
@@ -0,0 +1,57 @@
+#standardSQL
+# All CMS popularity per geo
+# cms_adoption_by_geo.sql
+
+WITH geo_summary AS (
+  SELECT
+    `chrome-ux-report`.experimental.GET_COUNTRY(country_code) AS geo,
+    IF(device = 'desktop', 'desktop', 'mobile') AS client,
+    origin,
+    COUNT(DISTINCT origin) OVER (PARTITION BY country_code, IF(device = 'desktop', 'desktop', 'mobile')) AS total
+  FROM
+    `chrome-ux-report.materialized.country_summary`
+  WHERE
+    yyyymm = 202506
+)
+
+SELECT
+  *
+FROM (
+  SELECT
+    client,
+    geo,
+    COUNT(0) AS pages,
+    ANY_VALUE(total) AS total,
+    COUNT(0) / ANY_VALUE(total) AS pct
+  FROM (
+    SELECT DISTINCT
+      geo,
+      client,
+      total,
+      CONCAT(origin, '/') AS page
+    FROM
+      geo_summary
+  )
+  JOIN (
+    SELECT
+      client,
+      page
+    FROM
+      `httparchive.crawl.pages`,
+      UNNEST(technologies) AS technologies,
+      UNNEST(technologies.categories) AS cats
+    WHERE
+      date = '2025-06-01' AND
+      cats = 'CMS' AND
+      is_root_page
+  )
+  USING (client, page)
+  GROUP BY
+    client,
+    geo
+)
+WHERE
+  pages > 1000
+ORDER BY
+  pages DESC
+
