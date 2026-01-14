92dca50febebba10199f8359231638172429a68d
Barry Pollard <barrypollard@google.com>
2026-01-14 20:30:51 +0000

    Ecommerce 2025 SQL queries (#4354)
    * Add 2025 ecommerce SQL queries
    
    * Update to July
    
    ---------
    
    Co-authored-by: Amandeep Singh <amandeepsinghvirdi@gmail.com>

---
+++ sql/2025/ecommerce/top_ecommerce.sql
@@ -0,0 +1,175 @@
+#standardSQL
+# Top Ecommerce platforms, compared to 2021
+# top_ecommerce.sql
+SELECT
+  client,
+  2025 AS year,
+  technologies.technology AS ecommerce,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.all.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2025-07-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'Ecommerce' AND
+  date = '2025-07-01' AND
+  is_root_page AND
+  technologies.technology NOT IN ('Cart Functionality', 'Google Analytics Enhanced eCommerce')
+GROUP BY
+  client,
+  total,
+  ecommerce
+UNION ALL
+SELECT
+  client,
+  2024 AS year,
+  technologies.technology AS ecommerce,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.all.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2024-06-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'Ecommerce' AND
+  date = '2024-06-01' AND
+  is_root_page AND
+  technologies.technology NOT IN ('Cart Functionality', 'Google Analytics Enhanced eCommerce')
+GROUP BY
+  client,
+  total,
+  ecommerce
+UNION ALL
+SELECT
+  client,
+  2023 AS year,
+  technologies.technology AS ecommerce,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.all.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2023-06-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'Ecommerce' AND
+  date = '2023-06-01' AND
+  is_root_page AND
+  technologies.technology NOT IN ('Cart Functionality', 'Google Analytics Enhanced eCommerce')
+GROUP BY
+  client,
+  total,
+  ecommerce
+UNION ALL
+SELECT
+  client,
+  2022 AS year,
+  technologies.technology AS ecommerce,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.all.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2022-08-01' AND -- noqa: CV09
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'Ecommerce' AND
+  date = '2022-08-01' AND -- noqa: CV09
+  is_root_page AND
+  technologies.technology NOT IN ('Cart Functionality', 'Google Analytics Enhanced eCommerce')
+GROUP BY
+  client,
+  total,
+  ecommerce
+UNION ALL
+SELECT
+  client,
+  2021 AS year,
+  technologies.technology AS ecommerce,
+  COUNT(DISTINCT page) AS freq,
+  total,
+  COUNT(DISTINCT page) / total AS pct
+FROM
+  `httparchive.all.pages`,
+  UNNEST(technologies) AS technologies,
+  UNNEST(technologies.categories) AS cats
+JOIN (
+  SELECT
+    client,
+    COUNT(0) AS total
+  FROM
+    `httparchive.all.pages`
+  WHERE
+    date = '2021-07-01' AND
+    is_root_page
+  GROUP BY
+    client
+)
+USING (client)
+WHERE
+  cats = 'Ecommerce' AND
+  date = '2021-07-01' AND
+  is_root_page AND
+  technologies.technology NOT IN ('Cart Functionality', 'Google Analytics Enhanced eCommerce')
+GROUP BY
+  client,
+  total,
+  ecommerce
+ORDER BY
+  year DESC,
+  pct DESC
---
+++ sql/2025/ecommerce/top_payment_provider_by_geo.sql
@@ -0,0 +1,69 @@
+#standardSQL
+# payment popularity per geo
+# top_payment_provider_by_geo.sql
+WITH geo_summary AS (
+  SELECT
+    `chrome-ux-report`.experimental.GET_COUNTRY(country_code) AS geo,
+    IF(device = 'desktop', 'desktop', 'mobile') AS client,
+    origin,
+    COUNT(DISTINCT origin) OVER (PARTITION BY country_code, IF(device = 'desktop', 'desktop', 'mobile')) AS total
+  FROM
+    `chrome-ux-report.materialized.country_summary`
+  WHERE
+    yyyymm = 202506
+  UNION ALL
+  SELECT
+    'ALL' AS geo,
+    IF(device = 'desktop', 'desktop', 'mobile') AS client,
+    origin,
+    COUNT(DISTINCT origin) OVER (PARTITION BY IF(device = 'desktop', 'desktop', 'mobile')) AS total
+  FROM
+    `chrome-ux-report.materialized.device_summary`
+  WHERE
+    yyyymm = 202506
+)
+
+SELECT
+  *
+FROM (
+  SELECT
+    client,
+    geo,
+    payment,
+    COUNT(0) AS pages,
+    ANY_VALUE(total) AS total,
+    COUNT(DISTINCT url) / ANY_VALUE(total) AS pct
+  FROM (
+    SELECT DISTINCT
+      geo,
+      client,
+      CONCAT(origin, '/') AS url,
+      total
+    FROM
+      geo_summary
+  ) JOIN (
+    SELECT DISTINCT
+      client,
+      cats,
+      technologies.technology AS payment,
+      page AS url
+    FROM
+      `httparchive.all.pages`,
+      UNNEST(technologies) AS technologies,
+      UNNEST(technologies.categories) AS cats
+    WHERE
+      technologies.technology IS NOT NULL AND
+      cats = 'Payment processors' AND
+      technologies.technology != '' AND
+      date = '2025-07-01' AND
+      is_root_page
+  ) USING (client, url)
+  GROUP BY
+    client,
+    geo,
+    payment
+)
+WHERE
+  pages > 1000
+ORDER BY
+  pages DESC
