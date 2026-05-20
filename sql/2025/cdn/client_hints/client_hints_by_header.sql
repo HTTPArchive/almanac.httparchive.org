68f9a2d9868e33fa6449313327140b2f493932e7
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2026-05-20 13:21:27 +0200

    Fix layout.indent (LT02) violations in client hints queries

--- sql/2025/cdn/client_hints/client_hints_by_header.sql
+++ sql/2025/cdn/client_hints/client_hints_by_header.sql
@@ -34,26 +34,26 @@ hint_rollup AS (
 dict AS (
   SELECT *
   FROM UNNEST([
-    STRUCT('sec-ch-ua' AS client_hint, 'User-Agent' AS category, 'Low' AS entropy, 1 AS ord),
-    ('sec-ch-ua-mobile', 'User-Agent', 'Low', 2),
-    ('sec-ch-ua-platform', 'User-Agent', 'Low', 3),
-    ('sec-ch-ua-platform-version', 'User-Agent', 'High', 4),
-    ('sec-ch-ua-model', 'Device', 'High', 5),
-    ('sec-ch-ua-full-version-list', 'User-Agent', 'High', 6),
-    ('sec-ch-ua-arch', 'Device', 'High', 7),
-    ('sec-ch-ua-bitness', 'Device', 'High', 8),
-    ('sec-ch-ua-wow64', 'Device', 'High', 9),
-    ('sec-ch-ua-full-version', 'User-Agent', 'High', 10),
-    ('sec-ch-prefers-color-scheme', 'User Preference', 'Low', 11),
-    ('sec-ch-viewport-width', 'Viewport', 'High', 12),
-    ('sec-ch-dpr', 'Device', 'High', 13),
-    ('sec-ch-device-memory', 'Device', 'High', 14),
-    ('sec-ch-ua-form-factors', 'Device', 'High', 15),
-    ('sec-ch-viewport-height', 'Viewport', 'High', 16),
-    ('sec-ch-prefers-reduced-motion', 'User Preference', 'Low', 17),
-    ('sec-ch-prefers-reduced-transparency', 'User Preference', 'Low', 18),
-    ('sec-ch-width', 'Viewport', 'High', 19)
-  ])
+      STRUCT('sec-ch-ua' AS client_hint, 'User-Agent' AS category, 'Low' AS entropy, 1 AS ord),
+      ('sec-ch-ua-mobile', 'User-Agent', 'Low', 2),
+      ('sec-ch-ua-platform', 'User-Agent', 'Low', 3),
+      ('sec-ch-ua-platform-version', 'User-Agent', 'High', 4),
+      ('sec-ch-ua-model', 'Device', 'High', 5),
+      ('sec-ch-ua-full-version-list', 'User-Agent', 'High', 6),
+      ('sec-ch-ua-arch', 'Device', 'High', 7),
+      ('sec-ch-ua-bitness', 'Device', 'High', 8),
+      ('sec-ch-ua-wow64', 'Device', 'High', 9),
+      ('sec-ch-ua-full-version', 'User-Agent', 'High', 10),
+      ('sec-ch-prefers-color-scheme', 'User Preference', 'Low', 11),
+      ('sec-ch-viewport-width', 'Viewport', 'High', 12),
+      ('sec-ch-dpr', 'Device', 'High', 13),
+      ('sec-ch-device-memory', 'Device', 'High', 14),
+      ('sec-ch-ua-form-factors', 'Device', 'High', 15),
+      ('sec-ch-viewport-height', 'Viewport', 'High', 16),
+      ('sec-ch-prefers-reduced-motion', 'User Preference', 'Low', 17),
+      ('sec-ch-prefers-reduced-transparency', 'User Preference', 'Low', 18),
+      ('sec-ch-width', 'Viewport', 'High', 19)
+    ])
 )
 
 SELECT
